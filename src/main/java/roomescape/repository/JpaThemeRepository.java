977073ea273bc306b7f0d505936ba4bd66dd75b8
Gyeongho Yang <chchch1213@naver.com>
2024-05-14 17:02:35 +0900

    refactor: Theme Repository Native Query 추가

--- src/main/java/roomescape/repository/JpaThemeRepository.java
+++ src/main/java/roomescape/repository/JpaThemeRepository.java
@@ -1,14 +1,23 @@
 package roomescape.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.reservation.Theme;
 
+import java.util.List;
+
 @Repository
 public interface JpaThemeRepository extends JpaRepository<Theme, Long> {
 
-    @Query("SELECT t FROM Theme t")
+    @Query(value = """
+            SELECT th.id, th.name, th.description, th.thumbnail
+            FROM theme AS th
+            INNER JOIN reservation AS r ON th.id = r.theme_id
+            WHERE r.date BETWEEN :startDate AND :endDate
+            GROUP BY th.id
+            ORDER BY COUNT(th.id) DESC
+            LIMIT :themeCount;
+            """, nativeQuery = true)
     List<Theme> findTopThemesDescendingByDescription(String startDate, String endDate, int count);
 }
