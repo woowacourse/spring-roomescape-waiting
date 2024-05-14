20e8418c95fdd715ba032b4ff314bd773eb158ee
Minjoo <m0522inju@gmail.com>
2024-05-14 16:56:34 +0900

    refactor: 토큰 만료기한 설정
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/roomescape/service/auth/TokenProvider.java
+++ src/main/java/roomescape/service/auth/TokenProvider.java
@@ -3,6 +3,7 @@ package roomescape.service.auth;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.Cookie;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.exception.AuthenticationException;
@@ -16,15 +17,22 @@ public class TokenProvider {
     private static final String TOKEN = "token";
 
     private final Key key;
+    private final long validityInMilliseconds;
 
-    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+    public TokenProvider(@Value("${security.jwt.token.secret-key}") String secretKey,
+                         @Value("${security.jwt.token.expire-length}") long validityInMilliseconds) {
         this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.validityInMilliseconds = validityInMilliseconds;
     }
 
     public String generateAccessToken(long memberId) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
         return Jwts.builder()
                 .setSubject(String.valueOf(memberId))
                 .signWith(key)
+                .expiration(validity)
                 .compact();
     }
 
