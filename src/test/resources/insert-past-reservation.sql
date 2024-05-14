345a00889e3a2414d9d0601ebd77135c23e5de9d
HyungUk Ryu <hw0603@naver.com>
2024-05-14 22:46:25 +0900

    remove: JDBC 테스트 관련 파일 제거
    Co-authored-By <2001yerin@naver.com>

--- src/test/resources/insert-past-reservation.sql
+++ src/test/resources/insert-past-reservation.sql
@@ -8,7 +8,11 @@ INSERT INTO member(name, email, password, role) VALUES('리니', 'lini@email.com
 INSERT INTO member(name, email, password, role) VALUES('릴리', 'lily@email.com', 'lily123', 'GUEST');
 INSERT INTO member(name, email, password, role) VALUES('토미', 'tomi@email.com', 'tomi123', 'GUEST');
 
+INSERT INTO schedule(date, time_id) VALUES(DATEADD('DAY', -1, CURRENT_DATE), 1);
+INSERT INTO schedule(date, time_id) VALUES(DATEADD('DAY', -7, CURRENT_DATE), 1);
+INSERT INTO schedule(date, time_id) VALUES(DATEADD('DAY', -8, CURRENT_DATE), 1);
 
-INSERT INTO reservation(date, member_id, time_id, theme_id) VALUES (DATEADD('DAY', -1, CURRENT_DATE), 1, 1, 1);
-INSERT INTO reservation(date, member_id, time_id, theme_id) VALUES (DATEADD('DAY', -7, CURRENT_DATE), 2, 1, 2);
-INSERT INTO reservation(date, member_id, time_id, theme_id) VALUES (DATEADD('DAY', -8, CURRENT_DATE), 3, 1, 3);
+
+INSERT INTO reservation(member_id, schedule_id, theme_id) VALUES (1, 1, 1);
+INSERT INTO reservation(member_id, schedule_id, theme_id) VALUES (2, 2, 2);
+INSERT INTO reservation(member_id, schedule_id, theme_id) VALUES (3, 3, 3);
