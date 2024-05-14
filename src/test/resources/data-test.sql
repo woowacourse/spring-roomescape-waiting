745fae11f7bd599ce18437f0c7b251609d13342d
Eugene Jang <kyunellroll@gmail.com>
2024-05-14 21:53:39 +0900

    build(data) : Mock 데이터 재 추가 및 수정

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,18 +1,18 @@
 SET REFERENTIAL_INTEGRITY FALSE;
 TRUNCATE TABLE reservation RESTART IDENTITY;
-TRUNCATE TABLE reservation_time RESTART IDENTITY;
+TRUNCATE TABLE time RESTART IDENTITY;
 TRUNCATE TABLE theme RESTART IDENTITY;
 TRUNCATE TABLE member RESTART IDENTITY;
 SET REFERENTIAL_INTEGRITY TRUE;
 
-INSERT INTO reservation_time (start_at)
+INSERT INTO time (start_at)
 VALUES ('15:40'),
        ('13:40'),
        ('17:40');
 
 INSERT INTO member (name, email, password, role)
-VALUES ('어드민', 'polla@gmail.com', 'pollari99', 'admin'),
-       ('일반', 'polla@naver.com', 'pollari999', 'member');
+VALUES ('어드민', 'polla@gmail.com', 'pollari99', 'ADMIN'),
+       ('일반', 'polla@naver.com', 'pollari999', 'MEMBER');
 
 
 INSERT INTO theme (name, description, thumbnail)
@@ -23,4 +23,3 @@ VALUES ('polla', '폴라 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461
 INSERT INTO reservation (date, time_id, theme_id, member_id)
 VALUES ('2024-04-30', 1, 1, 1),
        ('2025-05-01', 2, 1, 2);
-
