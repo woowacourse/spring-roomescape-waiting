aac94f547369f6a772fda0848a57bbbb38f0fcc8
takoyakimchi <c4nyou@gmail.com>
2024-05-14 14:21:14 +0900

    config: 토큰 secret-key 변경 및 토큰 fixture 변경
    Co-authored-by: ksk0605 <sungkyum1@naver.com>

--- src/test/java/roomescape/TokenTestFixture.java
+++ src/test/java/roomescape/TokenTestFixture.java
@@ -2,6 +2,6 @@ package roomescape;
 
 public class TokenTestFixture {
 
-    public static final String ADMIN_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmFtZSI6Iuq0gOumrOyekCIsImVtYWlsIjoiYWRtaW5AYS5jb20iLCJyb2xlIjoiQURNSU4iLCJleHAiOjYyODAwOTMwODAwfQ.G6-29hdF_8f7ZhbU7-QXa8Ha5n-q-hmFkp2BDEBXt7w";
-    public static final String USER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwibmFtZSI6IuyCrOyaqeyekCIsImVtYWlsIjoidXNlckBhLmNvbSIsInJvbGUiOiJVU0VSIiwiZXhwIjo2MjgwMDkzMDgwMH0.YMWo0Pn21DiG9zPWHRCr9Wzp3I8fqWEPyJnqINr0JeY";
+    public static final String ADMIN_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmFtZSI6Iuq0gOumrOyekCIsImVtYWlsIjoiYWRtaW5AYS5jb20iLCJyb2xlIjoiQURNSU4iLCJleHAiOjYyODAwOTMwODAwfQ.tWEZEBRoF6lAHDhApao8d86lfz9O-B6aiJn-LxnTlK4";
+    public static final String USER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwibmFtZSI6IuyCrOyaqeyekCIsImVtYWlsIjoidXNlckBhLmNvbSIsInJvbGUiOiJVU0VSIiwiZXhwIjo2MjgwMDkzMDgwMH0.22wCa-AYzR5zFrZtm7QISJ_-vo-qMQK4rmRn9N02wFE";
 }
