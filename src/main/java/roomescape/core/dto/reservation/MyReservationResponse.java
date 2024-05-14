ec6fb2dd3ae0d0dcc7ebd8bd6bf8795a907a7695
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-14 18:09:43 +0900

    feat: MyReservationResponse 구현

---
+++ src/main/java/roomescape/core/dto/reservation/MyReservationResponse.java
@@ -0,0 +1,44 @@
+package roomescape.core.dto.reservation;
+
+import roomescape.core.domain.Reservation;
+
+public class MyReservationResponse {
+    private Long reservationId;
+    private String theme;
+    private String date;
+    private String time;
+    private String status;
+
+    public MyReservationResponse(Reservation reservation) {
+        this(reservation.getId(), reservation.getTheme().getName(), reservation.getDateString(),
+                reservation.getReservationTime().getStartAtString(), "예약");
+    }
+
+    public MyReservationResponse(Long reservationId, String theme, String date, String time, String status) {
+        this.reservationId = reservationId;
+        this.theme = theme;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
