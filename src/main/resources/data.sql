d6fdbb3035eacac6f3ff642eb72d23811e04fcb9
robinjoon <robin980108@naver.com>
2024-05-14 22:09:34 +0900

    chore: 불필요한 DDL 삭제
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO member(name, email, password,role) VALUES ( 'member', 'email@email.com','f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3','MEMBER');
+INSERT INTO member(name, email, password,role) VALUES ( 'admin','email2@email.com','f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3','ADMIN');
