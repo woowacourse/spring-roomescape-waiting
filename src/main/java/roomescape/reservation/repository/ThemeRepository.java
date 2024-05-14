a1872a6675b5ebb7447b829c376974c3ee1cf2aa
MingyeomKim <mingyum119@naver.com>
2024-05-14 17:44:44 +0900

    fix: JPQL을 사용하여 삭제 구현

--- src/main/java/roomescape/reservation/repository/ThemeRepository.java
+++ src/main/java/roomescape/reservation/repository/ThemeRepository.java
@@ -1,9 +1,14 @@
 package roomescape.reservation.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import roomescape.reservation.domain.Theme;
 
 public interface ThemeRepository extends CrudRepository<Theme, Long> {
 
-    int deleteById(long id);
+    @Modifying
+    @Query("delete from Theme where id = :id")
+    int deleteById(@Param("id") long id);
 }
