8c64cc2a256b38546ed7db2a5ccd05d538e18c06
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-05-16 03:29:18 +0900

    style: 개행 정리

--- src/main/java/roomescape/infrastructure/MemberRepository.java
+++ src/main/java/roomescape/infrastructure/MemberRepository.java
@@ -1,15 +1,15 @@
 package roomescape.infrastructure;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Member;
 import roomescape.domain.MemberEmail;
 import roomescape.domain.MemberPassword;
 
-import java.util.Optional;
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     boolean existsByEmail(MemberEmail email);
 
     Optional<Member> findByEmail(MemberEmail email);
