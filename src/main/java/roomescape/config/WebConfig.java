20a72857b327a799b0ac84f6ec15b064188fdd8d
robinjoon <robin980108@naver.com>
2024-05-15 17:29:15 +0900

    fix: 로그인이 필요한 기능은 로그인을 확인하도록 수정
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -7,21 +7,28 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.infra.AdminCheckInterceptor;
 import roomescape.infra.AuthArgumentResolver;
+import roomescape.infra.LoginCheckInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final AuthArgumentResolver authArgumentResolver;
-    private final AdminCheckInterceptor interceptor;
+    private final AdminCheckInterceptor adminCheckInterceptor;
+    private final LoginCheckInterceptor loginCheckInterceptor;
 
-    public WebConfig(AuthArgumentResolver authArgumentResolver, AdminCheckInterceptor interceptor) {
+    public WebConfig(AuthArgumentResolver authArgumentResolver, AdminCheckInterceptor adminCheckInterceptor,
+                     LoginCheckInterceptor loginCheckInterceptor) {
         this.authArgumentResolver = authArgumentResolver;
-        this.interceptor = interceptor;
+        this.adminCheckInterceptor = adminCheckInterceptor;
+        this.loginCheckInterceptor = loginCheckInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor)
+        registry.addInterceptor(adminCheckInterceptor)
                 .addPathPatterns("/admin/**");
+        registry.addInterceptor(loginCheckInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login/**", "/", "/themes/ranking");
     }
 
     @Override
