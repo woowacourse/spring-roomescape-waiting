ac72c2364785c486d015408275f0725431422bf5
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-15 22:47:48 +0900

    refactor(ReservationTimeFixture): 사용하지 않는 Fixture 메서드 제거
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/test/java/roomescape/fixture/ReservationTimeFixture.java
+++ src/test/java/roomescape/fixture/ReservationTimeFixture.java
@@ -10,17 +10,4 @@ public class ReservationTimeFixture {
     public static ReservationTime getOne() {
         return new ReservationTime(LocalTime.parse("10:00"));
     }
-
-    public static List<ReservationTime> get(int count) {
-        final List<ReservationTime> reservationTimes = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            reservationTimes.add(new ReservationTime(
-                    null,
-                    LocalTime.of(10, count))
-            );
-        }
-
-        return reservationTimes;
-    }
 }
