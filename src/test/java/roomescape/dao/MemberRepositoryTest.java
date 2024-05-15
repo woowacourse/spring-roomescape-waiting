3eb91acadaabb00bd1926d709189df3903217487
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-15 22:15:11 +0900

    test : DB 초기화 로직 변경

--- src/test/java/roomescape/dao/MemberRepositoryTest.java
+++ src/test/java/roomescape/dao/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import roomescape.domain.user.Email;
 import roomescape.fixture.MemberFixture;
+import roomescape.util.DatabaseCleaner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,9 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberRepositoryTest {
     @Autowired
     MemberRepository sut;
+    @Autowired
+    DatabaseCleaner databaseCleaner;
     @BeforeEach
     void setup(){
-        sut.deleteAll();
+        databaseCleaner.initialize();
     }
 
     @Test
