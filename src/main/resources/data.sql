b34816d7f2b077341926b4f1aebb91288493ce43
ikjo39 <chi4321@naver.com>
2024-05-15 15:02:49 +0900

    chore: 예약 필드 status 추가로 인한 data.sql 수정
    Co-authored-by: ashsty <qkralsdk991@naver.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,15 @@
-insert into member(name, email, password, role) values ('ash', 'test@email.com', '1234', 'ADMIN');
-INSERT INTO member(name,email,password,role) VALUES('dominic','test2@email.com','1234', 'USER');
+INSERT INTO member(name, email, password, role)
+VALUES ('dominic', 'test2@email.com', '1234', 'USER'),
+       ('ash', 'test@email.com', '1234', 'ADMIN');
 
-INSERT INTO time_slot(start_at) VALUES('10:00');
-INSERT INTO time_slot(start_at) VALUES('11:00');
-INSERT INTO time_slot(start_at) VALUES('12:00');
-INSERT INTO time_slot(start_at) VALUES('13:00');
+INSERT INTO time_slot(start_at)
+VALUES ('10:00'),
+       ('11:00'),
+       ('12:00'),
+       ('13:00');
 
-INSERT INTO theme(name,description,thumbnail) VALUES('레벨1 탈출','우테코 레벨2를 탈출하는 내용입니다.','https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name,description,thumbnail) VALUES('레벨2 탈출','우테코 레벨3를 탈출하는 내용입니다.','https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name,description,thumbnail) VALUES('레벨3 탈출','우테코 레벨4를 탈출하는 내용입니다.','https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('레벨1 탈출', '우테코 레벨2를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('레벨2 탈출', '우테코 레벨3를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('레벨3 탈출', '우테코 레벨4를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
