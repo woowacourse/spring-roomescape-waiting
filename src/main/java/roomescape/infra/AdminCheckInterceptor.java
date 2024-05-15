6eaf94c285dabe07330842dc9a0865d783fe6b84
robinjoon <robin980108@naver.com>
2024-05-15 17:37:42 +0900

    fix: 관리자 권한이 필요한데 없는 경우 메인 페이지로 이동하도록 수정
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/roomescape/infra/AdminCheckInterceptor.java
+++ src/main/java/roomescape/infra/AdminCheckInterceptor.java
@@ -1,9 +1,8 @@
 package roomescape.infra;
 
-import static roomescape.exception.ExceptionType.PERMISSION_DENIED;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.domain.Role;
@@ -23,13 +22,23 @@ public class AdminCheckInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String token = TokenExtractor.extractFrom(request.getCookies());
-        long userIdFromToken = tokenService.findUserIdFromToken(token);
-        UserInfo userInfo = memberService.findByUserId(userIdFromToken);
-        if (!userInfo.role().equals(Role.ADMIN.name())) {
-            throw new RoomescapeException(PERMISSION_DENIED);
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws IOException {
+        try {
+            String token = TokenExtractor.extractFrom(request.getCookies());
+            long userIdFromToken = tokenService.findUserIdFromToken(token);
+            UserInfo userInfo = memberService.findByUserId(userIdFromToken);
+            sendRedirectIfNotAdmin(response, userInfo);
+            return userInfo.role().equals(Role.ADMIN.name());
+        } catch (RoomescapeException e) {
+            response.sendRedirect("/");
+            return false;
         }
-        return true;
+    }
+
+    private void sendRedirectIfNotAdmin(HttpServletResponse response, UserInfo userInfo) throws IOException {
+        if (!userInfo.role().equals(Role.ADMIN.name())) {
+            response.sendRedirect("/");
+        }
     }
 }
