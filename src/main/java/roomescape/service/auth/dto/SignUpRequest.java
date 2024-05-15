a4347dfe7268960c142b4dfea7f030d09135842a
HyungUk Ryu <hw0603@naver.com>
2024-05-15 12:37:27 +0900

    refactor(/auth/dto): 요청DTO에서 래핑된 객체를 받도록 변경
    Co-authored-By <2001yerin@naver.com>
    정

--- src/main/java/roomescape/service/auth/dto/SignUpRequest.java
+++ src/main/java/roomescape/service/auth/dto/SignUpRequest.java
@@ -1,10 +1,13 @@
 package roomescape.service.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import roomescape.domain.member.Email;
+import roomescape.domain.member.MemberName;
+import roomescape.domain.member.Password;
 
 public record SignUpRequest(
-        @NotBlank(message = "이름을 입력해주세요.") String name,
-        @NotBlank(message = "이메일을 입력해주세요.") String email,
-        @NotBlank(message = "비밀번호를 입력해주세요.") String password
+        @NotBlank(message = "이름을 입력해주세요.") MemberName name,
+        @NotBlank(message = "이메일을 입력해주세요.") Email email,
+        @NotBlank(message = "비밀번호를 입력해주세요.") Password password
 ) {
 }
