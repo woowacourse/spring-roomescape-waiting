09c0665caf92f8191a25f135288e56793c23689d
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-15 21:22:03 +0900

    feat(MemberPageController): 내 예약 페이지 응답 기능 구현

--- src/main/java/roomescape/controller/page/MemberPageController.java
+++ src/main/java/roomescape/controller/page/MemberPageController.java
@@ -27,4 +27,8 @@ public class MemberPageController {
     public String userSignupPage() {
         return "signup";
     }
+    @GetMapping("/reservation-mine")
+    public String userMineReservationPage() {
+        return "/reservation-mine";
+    }
 }
