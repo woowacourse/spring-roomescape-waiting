c61f66d594d71741ab2ccdb151ddb466a35b0a56
J-I-H-O <jeongjiho0731@gmail.com>
2024-05-16 17:46:01 +0900

    fix: 웹 페이지가 동작하지 않던 현상 수정

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -7,32 +7,21 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.infra.AdminCheckInterceptor;
 import roomescape.infra.AuthArgumentResolver;
-import roomescape.infra.LoginCheckInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final AuthArgumentResolver authArgumentResolver;
     private final AdminCheckInterceptor adminCheckInterceptor;
-    private final LoginCheckInterceptor loginCheckInterceptor;
 
-    public WebConfig(
-            AuthArgumentResolver authArgumentResolver,
-            AdminCheckInterceptor adminCheckInterceptor,
-            LoginCheckInterceptor loginCheckInterceptor
-    ) {
+    public WebConfig(AuthArgumentResolver authArgumentResolver, AdminCheckInterceptor adminCheckInterceptor) {
         this.authArgumentResolver = authArgumentResolver;
         this.adminCheckInterceptor = adminCheckInterceptor;
-        this.loginCheckInterceptor = loginCheckInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminCheckInterceptor)
                 .addPathPatterns("/admin/**");
-
-        registry.addInterceptor(loginCheckInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login/**", "/", "/themes/ranking");
     }
 
     @Override
