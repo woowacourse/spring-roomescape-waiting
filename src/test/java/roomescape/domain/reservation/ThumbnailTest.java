68c7e5e0b02e846a4f672f50c1ac870a41b06d87
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-16 11:20:05 +0900

    feat(Thumbnail) : 확장자 검사 기능 제거

--- src/test/java/roomescape/domain/reservation/ThumbnailTest.java
+++ src/test/java/roomescape/domain/reservation/ThumbnailTest.java
@@ -1,10 +1,9 @@
 package roomescape.domain.reservation;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class ThumbnailTest {
 
@@ -13,12 +12,5 @@ class ThumbnailTest {
     void create_domain_with_string() {
         assertThatCode(() -> new Thumbnail("https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"))
                 .doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("지정된 형식자로 끝나지 않으면 예외를 발생한다.")
-    void throw_exception_when_create_with_not_allowed_extension() {
-        assertThatThrownBy(() -> new Thumbnail("https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4"))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }
