a1c2ad17b52d7d4eeb96fe155aeb8bd3bac9e8a5
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-05-16 13:43:39 +0900

    refactor: 반환값 null로 변경

--- src/main/java/roomescape/auth/handler/cookie/CookieRequestHandler.java
+++ src/main/java/roomescape/auth/handler/cookie/CookieRequestHandler.java
@@ -17,6 +17,6 @@ public class CookieRequestHandler implements RequestHandler {
                 return cookie.getValue();
             }
         }
-        return "";
+        return null;
     }
 }
