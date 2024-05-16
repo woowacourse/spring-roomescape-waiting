4fa3d837aee23f6f20b3fd8e77c604181867cfaf
kunsanglee <gosmdochee2@gmail.com>
2024-05-16 22:50:46 +0900

    refactor: 쿼리문 multiline string으로 변경

--- src/main/java/roomescape/domain/ReservationQueryRepository.java
+++ src/main/java/roomescape/domain/ReservationQueryRepository.java
@@ -13,23 +13,38 @@ public interface ReservationQueryRepository extends JpaRepository<Reservation, L
 
     List<Reservation> findAllByMemberIdOrderByDateDesc(Long memberId);
 
-    @Query("select new roomescape.domain.dto.AvailableTimeDto(rt.id, rt.startAt, "
-            + "(select count(r) > 0 from Reservation r where r.time.id = rt.id and r.date = :date and r.theme.id = :themeId)) "
-            + "from ReservationTime rt")
+    @Query("""
+            select new roomescape.domain.dto.AvailableTimeDto(
+            rt.id, rt.startAt, (
+                select count(r) > 0
+                from Reservation r
+                where r.time.id = rt.id
+                and r.date = :date
+                and r.theme.id = :themeId)
+            )
+            from ReservationTime rt
+            """)
     List<AvailableTimeDto> findAvailableReservationTimes(LocalDate date, long themeId);
 
-    @Query("select t from Theme t "
-            + "join Reservation r on r.theme.id = t.id "
-            + "and r.date between :startDate and :endDate "
-            + "group by t.id, t.name, t.description, t.thumbnail "
-            + "order by count(r.id) desc")
+    @Query("""
+            select t
+            from Theme t
+            join Reservation r
+            on r.theme.id = t.id
+            where r.date between :startDate and :endDate
+            group by t.id, t.name, t.description, t.thumbnail
+            order by count(r.id) desc
+            """)
     List<Theme> findPopularThemesDateBetween(LocalDate startDate, LocalDate endDate);
 
 
-    @Query(value = "select r from Reservation r "
-            + "where r.theme.id = :themeId and "
-            + "r.member.id = :memberId and "
-            + ":dateFrom <= r.date and "
-            + "r.date <= :dateTo")
+    @Query("""
+            select r
+            from Reservation r
+            where r.theme.id = :themeId
+            and r.member.id = :memberId
+            and :dateFrom <= r.date
+            and r.date <= :dateTo
+            """)
     List<Reservation> findByCriteria(Long themeId, Long memberId, LocalDate dateFrom, LocalDate dateTo);
 }
