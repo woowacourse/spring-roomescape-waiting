70735bf2f893c8145ca49ffe030920218d51148e
MingyeomKim <mingyum119@naver.com>
2024-05-16 17:34:02 +0900

    test: 서비스 레이어 테스트 시 재시작하지 않도록 수정

--- src/test/java/roomescape/reservation/service/AdminReservationServiceTest.java
+++ src/test/java/roomescape/reservation/service/AdminReservationServiceTest.java
@@ -9,11 +9,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.reservation.controller.dto.response.ReservationResponse;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest
+@Transactional
 class AdminReservationServiceTest {
 
     @Autowired
