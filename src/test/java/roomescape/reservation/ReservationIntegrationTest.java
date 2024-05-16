04b53a552fa216d4aa10d6e62624638762ffdb3e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-16 11:29:19 +0900

    test(ReservationIntegrationTest): 테스트 개선
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/test/java/roomescape/reservation/ReservationIntegrationTest.java
+++ src/test/java/roomescape/reservation/ReservationIntegrationTest.java
@@ -1,13 +1,16 @@
 package roomescape.reservation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +23,7 @@ import roomescape.auth.dto.request.LoginRequest;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.Role;
 import roomescape.member.repository.MemberRepository;
+import roomescape.reservation.dto.response.FindAvailableTimesResponse;
 import roomescape.reservation.model.Reservation;
 import roomescape.reservation.repository.ReservationRepository;
 import roomescape.reservationtime.model.ReservationTime;
@@ -63,14 +67,14 @@ class ReservationIntegrationTest {
     private void saveTimeThemeMemberForReservation() {
         memberRepository.save(new Member("몰리", Role.USER, "login@naver.com", "hihi"));
         reservationTimeRepository.save(new ReservationTime(LocalTime.parse("20:00")));
-        themeRepository.save(new Theme( "테마이름", "설명", "썸네일"));
+        themeRepository.save(new Theme("테마이름", "설명", "썸네일"));
     }
 
     @Test
     @DisplayName("방탈출 예약 생성 성공 시, 생성된 시간대의 정보를 반환한다.")
     void createReservationTime() {
         reservationTimeRepository.save(new ReservationTime(LocalTime.parse("20:00")));
-        themeRepository.save(new Theme( "테마이름", "설명", "썸네일"));
+        themeRepository.save(new Theme("테마이름", "설명", "썸네일"));
 
         Map<String, Object> params = new HashMap<>();
         params.put("date", "2024-11-30");
@@ -253,7 +257,7 @@ class ReservationIntegrationTest {
     @DisplayName("예약 생성 시 해당하는 시간이 없는 경우 예외를 반환한다.")
     void createReservation_WhenTimeNotExist() {
         // reservationTimeRepository.save(new ReservationTime(LocalTime.parse("20:00")));
-        themeRepository.save(new Theme( "테마이름", "설명", "썸네일"));
+        themeRepository.save(new Theme("테마이름", "설명", "썸네일"));
 
         Map<String, Object> params = new HashMap<>();
         params.put("date", "2024-11-30");
@@ -277,7 +281,7 @@ class ReservationIntegrationTest {
         Member member = memberRepository.save(new Member("롸키", Role.USER, "loki@naver.com", "loki"));
         ReservationTime reservationTime = reservationTimeRepository.save(
                 new ReservationTime(LocalTime.parse("20:00")));
-        Theme theme = themeRepository.save(new Theme( "테마이름", "설명", "썸네일"));
+        Theme theme = themeRepository.save(new Theme("테마이름", "설명", "썸네일"));
         Reservation reservation = reservationRepository.save(
                 new Reservation(member, LocalDate.parse("2025-12-23"), reservationTime, theme));
 
@@ -301,8 +305,10 @@ class ReservationIntegrationTest {
     @DisplayName("방탈출 예약 목록을 조회한다.")
     void getReservationTimes() {
         saveTimeThemeMemberForReservation();
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -318,8 +324,10 @@ class ReservationIntegrationTest {
     @DisplayName("방탈출 예약 하나를 조회한다.")
     void getReservationTime() {
         saveTimeThemeMemberForReservation();
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -350,21 +358,25 @@ class ReservationIntegrationTest {
     void getAvailableTimes() {
         saveTimeThemeMemberForReservation();
         reservationTimeRepository.save(new ReservationTime(LocalTime.parse("10:00")));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
 
-        RestAssured.given().log().all()
+        List<FindAvailableTimesResponse> findAvailableTimesResponses = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .when().get("/reservations/times?date=2024-11-23&themeId=1")
                 .then().log().all()
+                .extract().jsonPath()
+                .getList(".", FindAvailableTimesResponse.class);;
 
-                .statusCode(200)
-                .body("[0].id", equalTo(1))
-                .body("[0].alreadyBooked", equalTo(true))
-
-                .body("[1].id", equalTo(2))
-                .body("[1].alreadyBooked", equalTo(false))
-                .body("size()", equalTo(2));
+        assertAll(
+                () -> assertThat(findAvailableTimesResponses).hasSize(2),
+                () -> assertThat(findAvailableTimesResponses).containsExactlyInAnyOrder(
+                        new FindAvailableTimesResponse(1L, LocalTime.parse("20:00"), true),
+                        new FindAvailableTimesResponse(2L, LocalTime.parse("10:00"), false)
+                )
+        );
     }
 
     @Test
@@ -372,9 +384,12 @@ class ReservationIntegrationTest {
     void searchBy() {
         saveTimeThemeMemberForReservation();
         reservationTimeRepository.save(new ReservationTime(LocalTime.parse("10:00")));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2025-01-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-12-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2025-01-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -393,7 +408,8 @@ class ReservationIntegrationTest {
     @DisplayName("방탈출 예약 하나를 삭제한다.")
     void deleteReservationTime() {
         saveTimeThemeMemberForReservation();
-        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"), reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
+        reservationRepository.save(new Reservation(memberRepository.getById(1L), LocalDate.parse("2024-11-23"),
+                reservationTimeRepository.getById(1L), themeRepository.getById(1L)));
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
