a52b2cc5325750de42a9b227d46c360ab3bb1d25
takoyakimchi <c4nyou@gmail.com>
2024-05-16 16:36:13 +0900

    fix: 이메일 및 비밀번호 정규식 패턴 수정

--- src/main/java/roomescape/controller/dto/LoginRequest.java
+++ src/main/java/roomescape/controller/dto/LoginRequest.java
@@ -1,17 +1,16 @@
 package roomescape.controller.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public class LoginRequest {
 
     @NotBlank(message = "null이거나 비어있을 수 없습니다.")
-    @Email(message = "올바르지 않은 이메일 형식입니다.")
+    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "올바르지 않은 이메일 형식입니다.")
     private final String email;
 
     @NotBlank(message = "null이거나 비어있을 수 없습니다.")
-    @Pattern(regexp = "^[A-Za-z0-9]{1,20}$", message = "패스워드는 1자 이상 20자 이하의 영문, 숫자 조합이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{1,20}$", message = "패스워드는 1자 이상 20자 이하의 영문, 숫자, 기호 조합이어야 합니다.")
     private final String password;
 
     public LoginRequest(String email, String password) {
