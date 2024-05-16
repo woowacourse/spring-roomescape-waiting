0a098f812ddf9ca99368febd5c3e38b2dff96efe
ikjo39 <chi4321@naver.com>
2024-05-16 15:43:13 +0900

    refactor: 예외 처리 하나로 통합

--- src/main/java/roomescape/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/handler/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package roomescape.controller.handler;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({IllegalArgumentException.class, NumberFormatException.class, BadRequestException.class})
     public ResponseEntity<String> badRequestException(Exception ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        if (e.getCause() instanceof JsonMappingException jsonMappingException) {
+            String errorMessage = jsonMappingException.getPath().get(0).getFieldName() + "필드 내용이 잘못되었습니다.";
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+        String errorMessage = "[ERROR] 적절하지 않은 입력값 입니다";
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> unPredictableException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
