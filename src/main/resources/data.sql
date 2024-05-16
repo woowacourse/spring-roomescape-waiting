667fd8fecfa70b459404671dc3e0b12f8b869a80
lemone <lejinhy@gmail.com>
2024-05-16 17:12:58 +0900

    chore(data.sql): 데이터 내용 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,9 +9,9 @@ VALUES ('어드민', 'polla@gmail.com', 'pollari99', 'ADMIN'),
 
 
 INSERT INTO theme (name, description, thumbnail)
-VALUES ('polla', '폴라 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('dobby', '도비 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('pobi', '포비 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+VALUES ('테마 - polla', '폴라 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('테마 - dobby', '도비 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('테마 - pobi', '포비 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 INSERT INTO reservation (date, time_id, theme_id, member_id)
 VALUES ('2024-04-30', 1, 1, 1),
