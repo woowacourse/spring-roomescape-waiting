c5a14050d1e51c2e4986acfa7dff1ac4f17d8722
DobbyMac <kimdobby@g.skku.edu>
2024-05-16 16:40:25 +0900

    refactor(ReservationController): 사용자-관리자 예약 컨트롤러 분리

---
+++ src/main/java/roomescape/reservation/controller/AdminReservationController.java
@@ -0,0 +1,49 @@
+package roomescape.reservation.controller;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.reservation.dto.AdminReservationRequest;
+import roomescape.reservation.dto.ReservationConditionSearchRequest;
+import roomescape.reservation.dto.ReservationResponse;
+import roomescape.reservation.service.ReservationService;
+
+@RestController
+@RequestMapping("/admin/reservations")
+public class AdminReservationController {
+
+    private final ReservationService reservationService;
+
+    public AdminReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> createReservation(
+            @RequestBody AdminReservationRequest adminReservationRequest) {
+        ReservationResponse reservationCreateResponse = reservationService.addReservation(adminReservationRequest);
+        URI uri = URI.create("/reservations/" + reservationCreateResponse.id());
+        return ResponseEntity.created(uri)
+                .body(reservationCreateResponse);
+    }
+
+    @GetMapping("/search")
+    public List<ReservationResponse> reservationListInCondition(
+            @RequestParam("themeId") long themeId,
+            @RequestParam("memberId") long memberId,
+            @RequestParam("dateFrom") LocalDate dateFrom,
+            @RequestParam("dateTo") LocalDate dateTo
+    ) {
+        ReservationConditionSearchRequest request = new ReservationConditionSearchRequest(memberId, themeId, dateFrom,
+                dateTo);
+        return reservationService.findReservationsByConditions(request);
+    }
+
+}
--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.member.dto.MemberProfileInfo;
-import roomescape.reservation.dto.AdminReservationRequest;
-import roomescape.reservation.dto.ReservationConditionSearchRequest;
+import roomescape.reservation.dto.MyReservationResponse;
 import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
 import roomescape.reservation.dto.ReservationTimeAvailabilityResponse;
 import roomescape.reservation.service.ReservationService;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -29,43 +29,25 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> createReservation(@Valid ReservationRequest reservationRequest,
-            MemberProfileInfo memberProfileInfo) {
-        ReservationResponse reservationCreateResponse = reservationService.addReservation(reservationRequest,
-                memberProfileInfo);
+    @PostMapping
+    public ResponseEntity<ReservationResponse> createReservation(@Valid ReservationRequest reservationRequest) {
+        ReservationResponse reservationCreateResponse = reservationService.addReservation(reservationRequest);
         URI uri = URI.create("/reservations/" + reservationCreateResponse.id());
         return ResponseEntity.created(uri)
                 .body(reservationCreateResponse);
     }
 
-    @PostMapping("/admin/reservations")
-    public ResponseEntity<ReservationResponse> createReservation(
-            @RequestBody AdminReservationRequest adminReservationRequest) {
-        ReservationResponse reservationCreateResponse = reservationService.addReservation(adminReservationRequest);
-        URI uri = URI.create("/reservations/" + reservationCreateResponse.id());
-        return ResponseEntity.created(uri)
-                .body(reservationCreateResponse);
-    }
-
-    @GetMapping("/reservations")
+    @GetMapping
     public List<ReservationResponse> reservaionList() {
         return reservationService.findReservations();
     }
 
-    @GetMapping("/admin/reservations/search")
-    public List<ReservationResponse> reservationListInCondition(
-            @RequestParam("themeId") long themeId,
-            @RequestParam("memberId") long memberId,
-            @RequestParam("dateFrom") LocalDate dateFrom,
-            @RequestParam("dateTo") LocalDate dateTo
-    ) {
-        ReservationConditionSearchRequest request = new ReservationConditionSearchRequest(memberId, themeId, dateFrom,
-                dateTo);
-        return reservationService.findReservationsByConditions(request);
+    @GetMapping("/mine")
+    public List<MyReservationResponse> myReservationList(MemberProfileInfo memberProfileInfo) {
+        return reservationService.findReservationByMemberId(memberProfileInfo.id());
     }
 
-    @GetMapping("/reservations/times/{themeId}")
+    @GetMapping("/times/{themeId}")
     public ResponseEntity<List<ReservationTimeAvailabilityResponse>> reservationTimeList(@PathVariable long themeId,
             @RequestParam LocalDate date) {
         List<ReservationTimeAvailabilityResponse> timeAvailabilityReadResponse = reservationService.findTimeAvailability(
@@ -73,7 +55,7 @@ public class ReservationController {
         return ResponseEntity.ok(timeAvailabilityReadResponse);
     }
 
-    @DeleteMapping("/reservations/{reservationId}")
+    @DeleteMapping("/{reservationId}")
     public ResponseEntity<Void> deleteReservation(@PathVariable long reservationId) {
         reservationService.removeReservations(reservationId);
         return ResponseEntity.noContent()
