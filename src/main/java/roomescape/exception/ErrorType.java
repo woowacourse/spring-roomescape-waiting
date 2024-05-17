639d7922ba4611567411ce9452e8e889cd97c167
hyun <osh123chm@gmail.com>
2024-05-18 00:25:25 +0900

    feat: 예외 로깅 기능 추가

--- src/main/java/roomescape/exception/ErrorType.java
+++ src/main/java/roomescape/exception/ErrorType.java
@@ -3,7 +3,7 @@ package roomescape.exception;
 public enum ErrorType {
     INVALID_DATA_TYPE("요청 데이터 형태가 일치하지 않습니다."),
     EMPTY_VALUE_NOT_ALLOWED("필수로 입력해야하는 값이 누락되어 있습니다."),
-    INVALID_ID("올바르지 않는 Id 입니다.");
+    INVALID_ID("올바르지 않은 Id 입니다.");
 
     private final String message;
 
--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package roomescape.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -13,25 +15,30 @@ public class GlobalExceptionHandler {
     private static final String MESSAGE_FORMAT = "%s 필드명 : [%s]";
     private static final String MESSAGE_FORMAT_WITH_VALUE = "%s 필드명 : [%s], 값 : [%s]";
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     @ExceptionHandler(value = InvalidClientFieldException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidClientField(InvalidClientFieldException ex,
+    public ResponseEntity<ErrorResponse> handleInvalidClientField(InvalidClientFieldException e,
                                                                   HttpServletRequest req) {
-        final String message = MESSAGE_FORMAT.formatted(ex.getErrorType().getMessage(), ex.getFieldName());
+        logWarn(e);
+        final String message = MESSAGE_FORMAT.formatted(e.getErrorType().getMessage(), e.getFieldName());
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(), message);
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(value = InvalidClientFieldWithValueException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidClientFieldWithValue(InvalidClientFieldWithValueException ex,
+    public ResponseEntity<ErrorResponse> handleInvalidClientFieldWithValue(InvalidClientFieldWithValueException e,
                                                                            HttpServletRequest req) {
-        final String message = MESSAGE_FORMAT_WITH_VALUE.formatted(ex.getErrorType().getMessage(), ex.getFieldName(),
-                ex.getValue());
+        logWarn(e);
+        final String message = MESSAGE_FORMAT_WITH_VALUE.formatted(e.getErrorType().getMessage(), e.getFieldName(),
+                e.getValue());
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(), message);
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(ReservationFailException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(ReservationFailException e, HttpServletRequest req) {
+        logWarn(e);
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(),
                 e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
@@ -39,6 +46,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DeleteNotAllowException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(DeleteNotAllowException e, HttpServletRequest req) {
+        logWarn(e);
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(),
                 e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
@@ -46,6 +54,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(SignupFailException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(SignupFailException e, HttpServletRequest req) {
+        logWarn(e);
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(),
                 e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
@@ -53,6 +62,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateNotAllowException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(DuplicateNotAllowException e, HttpServletRequest req) {
+        logWarn(e);
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(),
                 e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
@@ -60,6 +70,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessNotAllowException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(AccessNotAllowException e, HttpServletRequest req) {
+        logWarn(e);
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED, req.getRequestURI(),
                 e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
@@ -67,9 +78,10 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public Object handleMissingServletRequestParameterException(
-            MissingServletRequestParameterException ex, HttpServletRequest req) {
+            MissingServletRequestParameterException e, HttpServletRequest req) {
+        logWarn(e);
         final String message = MESSAGE_FORMAT
-                .formatted(ErrorType.EMPTY_VALUE_NOT_ALLOWED.getMessage(), ex.getParameterName());
+                .formatted(ErrorType.EMPTY_VALUE_NOT_ALLOWED.getMessage(), e.getParameterName());
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(), message);
         return ResponseEntity.badRequest().body(errorResponse);
     }
@@ -77,9 +89,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException e,
                                                             HttpServletRequest req) {
+        logWarn(e);
         final String message = MESSAGE_FORMAT
                 .formatted(ErrorType.INVALID_DATA_TYPE.getMessage(), e.getName(), e.getValue().toString());
         final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, req.getRequestURI(), message);
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    private void logWarn(Exception exception) {
+        logger.warn(exception.toString());
     }
 }
