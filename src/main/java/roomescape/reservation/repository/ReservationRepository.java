a623150d3e56ced46c9f971c2995053fd0f59ed5
Hyeon0208 <dmsgktn0208@gmail.com>
2024-05-17 15:37:10 +0900

    refactor: 메서드 네이밍 수정 및 fetch join 적용

--- src/main/java/roomescape/reservation/repository/ReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -3,23 +3,34 @@ package roomescape.reservation.repository;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import roomescape.reservation.domain.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    @Query("select r.reservationTime.id from Reservation r where r.date = :date and r.theme.id = :themeId")
+    @Override
+    @EntityGraph(attributePaths = {"member", "theme", "reservationTime"})
+    List<Reservation> findAll();
+
+    @Query("""
+             select r.reservationTime.id from Reservation r 
+             join fetch ReservationTime rt on r.reservationTime.id = rt.id  
+             where r.date = :date and r.theme.id = :themeId
+             """)
     List<Long> findTimeIdsByDateAndThemeId(LocalDate date, Long themeId);
 
-    List<Reservation> findAllByMember_Id(Long memberId);
+    @EntityGraph(attributePaths = {"member", "theme", "reservationTime"})
+    List<Reservation> findAllByMemberId(Long memberId);
 
-    List<Reservation> findAllByTheme_IdAndMember_IdAndDateBetween(
+    @EntityGraph(attributePaths = {"member", "theme", "reservationTime"})
+    List<Reservation> findAllByThemeIdAndMemberIdAndDateBetween(
             Long themeId,
             Long memberId,
             LocalDate dateFrom,
             LocalDate dateTo
     );
 
-    boolean existsByDateAndReservationTime_StartAt(LocalDate date, LocalTime startAt);
+    boolean existsByDateAndReservationTimeStartAt(LocalDate date, LocalTime startAt);
 }
