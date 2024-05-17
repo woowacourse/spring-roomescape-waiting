a0f1aae379964d7829ca75d02cc376e9cd6e05a3
robinjoon <robin980108@naver.com>
2024-05-17 14:01:55 +0900

    fix: 쿠키에 잘못된 토큰이 포함된 경우 예외 처리 추가
    - https://github.com/woowacourse/spring-roomescape-waiting/pull/37#discussion_r1603042686 반영

--- src/main/java/roomescape/infra/TokenExtractor.java
+++ src/main/java/roomescape/infra/TokenExtractor.java
@@ -7,15 +7,32 @@ import java.util.Arrays;
 import roomescape.exception.RoomescapeException;
 
 public class TokenExtractor {
+
+    public static final int VALID_TOKEN_COOKIE_COUNT = 1;
+
     public static String extractFrom(Cookie[] cookies) {
         if (cookies == null) {
             throw new RoomescapeException(INVALID_TOKEN);
         }
+        validateTokenCookieCount(cookies);
         return Arrays.stream(cookies)
-                .filter(cookie -> cookie.getName().equals("token"))
-                .limit(1)
+                .filter(TokenExtractor::isTokenCookie)
+                .limit(VALID_TOKEN_COOKIE_COUNT)
                 .findAny()
                 .map(Cookie::getValue)
                 .orElseThrow(() -> new RoomescapeException(INVALID_TOKEN));
+    }
+
+    private static void validateTokenCookieCount(Cookie[] cookies) {
+        long tokenCookieCount = Arrays.stream(cookies)
+                .filter(TokenExtractor::isTokenCookie)
+                .count();
+        if (tokenCookieCount != VALID_TOKEN_COOKIE_COUNT) {
+            throw new RoomescapeException(INVALID_TOKEN);
+        }
+    }
+
+    private static boolean isTokenCookie(Cookie cookie) {
+        return cookie.getName().equals("token");
     }
 }
