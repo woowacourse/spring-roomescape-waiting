31a2d3dfd2b1a7ac9629b9a991b27bd0b85f1ac7
MingyeomKim <mingyum119@naver.com>
2024-05-17 14:06:42 +0900

    refactor: 예약 데이터의 fetchType을 LAZY로 수정

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -3,6 +3,7 @@ package roomescape.reservation.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,15 +19,15 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
     private LocalDate date;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private ReservationTime time;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Theme theme;
 
     @Enumerated(EnumType.STRING)
