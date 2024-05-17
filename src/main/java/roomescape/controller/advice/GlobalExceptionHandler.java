9f489932d6c42d18d36026a284387202073d796a
Choo <choo000407@naver.com>
2024-05-17 21:42:35 +0900

    refactor: api 응답 시 예외도 문자열 대신 object 형식으로 반환하게 수정

--- src/main/java/roomescape/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/advice/GlobalExceptionHandler.java
@@ -7,28 +7,29 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+import roomescape.exception.ErrorResponse;
 import roomescape.exception.RoomescapeException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = RoomescapeException.class)
-    public ResponseEntity<String> handleCustomException(RoomescapeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
+    public ResponseEntity<ErrorResponse> handleCustomException(RoomescapeException exception) {
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), exception.getStatus());
     }
 
     @ExceptionHandler(value = HttpMessageConversionException.class)
-    public ResponseEntity<String> handleJsonParsingException() {
-        return new ResponseEntity<>("요청 body에 유효하지 않은 필드가 존재합니다.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleJsonParsingException() {
+        return new ResponseEntity<>(new ErrorResponse("요청 body에 유효하지 않은 필드가 존재합니다."), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {NoResourceFoundException.class, MissingServletRequestParameterException.class})
-    public ResponseEntity<String> handleRequestException() {
-        return new ResponseEntity<>("요청 경로에 필요한 변수가 제공되지 않았습니다.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleRequestException() {
+        return new ResponseEntity<>(new ErrorResponse("요청 경로에 필요한 변수가 제공되지 않았습니다."), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
         e.printStackTrace();
-        return new ResponseEntity<>("서버 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ErrorResponse("서버 에러입니다."), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
