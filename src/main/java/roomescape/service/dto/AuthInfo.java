a3faf0ba858d57f13cf616631d3605723eee2b26
pricelees <priceelees@gmail.com>
2024-05-19 01:11:58 +0900

    feat: AuthInfo 생성자에 null validation 추가

--- src/main/java/roomescape/service/dto/AuthInfo.java
+++ src/main/java/roomescape/service/dto/AuthInfo.java
@@ -3,8 +3,21 @@ package roomescape.service.dto;
 import static roomescape.domain.Role.ADMIN;
 
 import roomescape.domain.Role;
+import roomescape.exception.AuthenticationException;
 
 public record AuthInfo(Long id, String name, Role role) {
+
+    public AuthInfo {
+        if (id == null) {
+            throw new AuthenticationException("토큰으로부터 불러온 회원 ID가 존재하지 않습니다.");
+        }
+        if (name == null || name.isBlank()) {
+            throw new AuthenticationException("토큰으로부터 불러온 회원 이름이 존재하지 않습니다.");
+        }
+        if (role == null) {
+            throw new AuthenticationException("토큰으로부터 불러온 회원 권한이 존재하지 않습니다.");
+        }
+    }
 
     public boolean isAdmin() {
         return role == ADMIN;
