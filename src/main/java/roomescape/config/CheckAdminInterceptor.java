63883ff47a70bac92a87a4d585320dbbcfdce8ac
reddevilmidzy <midzy0228@gmail.com>
2024-05-18 21:33:22 +0900

    refactor: loginMember 상수 사용

--- src/main/java/roomescape/config/CheckAdminInterceptor.java
+++ src/main/java/roomescape/config/CheckAdminInterceptor.java
@@ -11,7 +11,7 @@ public class CheckAdminInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request,
                              final HttpServletResponse response, final Object handler) {
-        final LoginMember loginMember = (LoginMember) request.getAttribute("loginMember");
+        final LoginMember loginMember = (LoginMember) request.getAttribute(CheckLoginInterceptor.LOGIN_MEMBER);
         if (!loginMember.isAdmin()) {
             throw new AuthorizationException("어드민만 접근할 수 있습니다.");
         }
