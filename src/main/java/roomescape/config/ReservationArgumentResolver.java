cb2b5dead919febd5e336a3e436c8bb410f9e10b
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-19 00:26:28 +0900

    refactor(ReservationArgumentResolver): throws 지양을 위해 제거

--- src/main/java/roomescape/config/ReservationArgumentResolver.java
+++ src/main/java/roomescape/config/ReservationArgumentResolver.java
@@ -1,20 +1,24 @@
 package roomescape.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Objects;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import roomescape.exception.AuthorizationException;
 import roomescape.member.domain.Member;
 import roomescape.member.dto.MemberProfileInfo;
@@ -23,7 +27,6 @@ import roomescape.member.service.MemberService;
 import roomescape.reservation.dto.ReservationRequest;
 
 public class ReservationArgumentResolver implements HandlerMethodArgumentResolver {
-
     private final MemberAuthService memberAuthService;
     private final MemberService memberService;
 
@@ -40,11 +43,10 @@ public class ReservationArgumentResolver implements HandlerMethodArgumentResolve
 
     @Override
     public ReservationRequest resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+                                              NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        Cookie[] cookies = Objects.requireNonNull(request).getCookies();
 
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        Cookie[] cookies = Objects.requireNonNull(request)
-                .getCookies();
         if (memberAuthService.isLoginMember(cookies)) {
             MemberProfileInfo payload = memberAuthService.extractPayload(cookies);
             Member member = memberService.findMemberById(payload.id());
@@ -59,13 +61,14 @@ public class ReservationArgumentResolver implements HandlerMethodArgumentResolve
         throw new AuthorizationException("로그인이 만료되었습니다. 다시 로그인 해주세요.");
     }
 
-    private ReservationRequest convertToRequestBody(HttpServletRequest request) throws IOException {
+    private ReservationRequest convertToRequestBody(HttpServletRequest request) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(request.getInputStream(), StandardCharsets.UTF_8))
-        ) {
+                new InputStreamReader(request.getInputStream(), StandardCharsets.UTF_8))) {
             return objectMapper.readValue(reader, ReservationRequest.class);
+        } catch (IOException e) {
+            throw new AuthorizationException("로그인이 만료되었습니다. 다시 로그인 해주세요.");
         }
     }
 
