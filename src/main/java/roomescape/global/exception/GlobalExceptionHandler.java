890bfdd1b17705a5124c33cd8cf5eb9160061da0
BurningFalls <sherlocky31415@gmail.com>
2024-05-18 17:54:57 +0900

    refactor: nullable과 unique 에러 처리하는 핸들러 생성

--- src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package roomescape.global.exception;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -34,5 +36,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleInternalServerError(Exception e) {
         log.error(e.getMessage());
         return new ResponseEntity<>("서버 관리자에게 문의하세요", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.info(e.getMessage());
+        return new ResponseEntity<>("필수 정보가 부족합니다.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        log.info(e.getMessage());
+        return new ResponseEntity<>("이미 해당 데이터가 존재합니다.", HttpStatus.CONFLICT);
     }
 }
