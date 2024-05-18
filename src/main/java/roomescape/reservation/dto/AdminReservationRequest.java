0b9be0e9187dfb565b0f92751bb333410af8a17f
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-19 03:04:14 +0900

    refactor(RequestDto): Fail Fast를 위해 Request Dto에서 @NotNull을 통해 null 입력을 확인한다

--- src/main/java/roomescape/reservation/dto/AdminReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/AdminReservationRequest.java
@@ -5,10 +5,14 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.NotNull;
 
 public record AdminReservationRequest(
-        @NotNull(message = "날짜가 존재하지 않습니다.") LocalDate date,
-        long memberId,
-        long timeId,
-        long themeId
+        @NotNull(message = "날짜가 선택되지 않았습니다.")
+        LocalDate date,
+        @NotNull(message = "예약자 정보가 없습니다.")
+        Long memberId,
+        @NotNull(message = "시간이 선택되지 않았습니다.")
+        Long timeId,
+        @NotNull(message = "테마가 선택되지 않았습니다.")
+        Long themeId
 ) {
 
 }
