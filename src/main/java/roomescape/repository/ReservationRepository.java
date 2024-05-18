8db27369a902231858e404d155e0c783acaf5fba
hyeonjilee <skylar12200@gmail.com>
2024-05-18 21:08:41 +0900

    refactor: 예약 조회에 fetch join 적용

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package roomescape.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.domain.Member;
 import roomescape.domain.Reservation;
@@ -10,11 +11,15 @@ import roomescape.domain.Theme;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @EntityGraph(attributePaths = {"member", "time", "theme"})
+    List<Reservation> findAll();
+
     List<Reservation> findAllByDateAndTheme(LocalDate date, Theme theme);
 
     boolean existsByTimeAndDate(ReservationTime time, LocalDate date);
 
     List<Reservation> findAllByThemeAndMemberAndDateBetween(Theme theme, Member member, LocalDate from, LocalDate to);
 
+    @EntityGraph(attributePaths = {"time", "theme"})
     List<Reservation> findAllByMember(Member member);
 }
