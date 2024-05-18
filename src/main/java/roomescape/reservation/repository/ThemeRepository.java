ee317eff6029af590f136c05d4cc77a552ffa091
eun-byeol <joeunbyeol98@gmail.com>
2024-05-19 01:39:50 +0900

    refactor: nativeQuery JPQL로 변경

--- src/main/java/roomescape/reservation/repository/ThemeRepository.java
+++ src/main/java/roomescape/reservation/repository/ThemeRepository.java
@@ -14,15 +14,15 @@ public interface ThemeRepository extends JpaRepository<Theme, Long> {
     @Query("select t from Theme t join Reservation r on t.id = r.theme.id where t.id = :id")
     List<Theme> findThemesThatReservationReferById(Long id);
 
-    @Query(value = """
-            select t.id, t.name, t.description, t.thumbnail, count(*) as cnt
-            from theme t
-            join reservation r
-            on r.theme_id = t.id
-            where r.date >= ?
+    @Query("""
+            select t
+            from Theme t
+            join Reservation r
+            on t.id = r.theme.id
+            where r.date >= :dateFrom
             group by t.id
-            order by cnt desc
-            limit ?;
-               """, nativeQuery = true)
+            order by count(*) desc
+            limit :limitCount
+               """)
     List<Theme> findPopularThemesDescOfLastWeekForLimit(LocalDate dateFrom, int limitCount);
 }
