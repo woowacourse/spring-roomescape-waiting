b191433044e6bb85d4d973cad183f20e83f6ae3a
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-18 21:40:55 +0900

    refactor: 예외 메시지 설정

--- src/main/java/roomescape/service/dto/request/ThemeSaveRequest.java
+++ src/main/java/roomescape/service/dto/request/ThemeSaveRequest.java
@@ -6,15 +6,15 @@ import roomescape.domain.Theme;
 
 public record ThemeSaveRequest(
         @NotBlank
-        @Size(max = 30) // TODO 예외 메시지 설정하기
+        @Size(max = 30, message= "테마 이름은 30자까지 가능합니다.")
         String name,
 
         @NotBlank
-        @Size(max = 255)
+        @Size(max = 255, message = "테마 설명은 255자까지 가능합니다.")
         String description,
 
         @NotBlank
-        @Size(max = 255)
+        @Size(max = 255, message = "썸네일 링크는 255자까지 가능합니다.")
         String thumbnail
 ) {
 
