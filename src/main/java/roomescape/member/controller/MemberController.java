960df3c15f3ed69deaceb45cf7cb967a0d25892d
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-19 04:22:11 +0900

    feat(MemberController): 회원가입 기능 구현

--- src/main/java/roomescape/member/controller/MemberController.java
+++ src/main/java/roomescape/member/controller/MemberController.java
@@ -1,11 +1,16 @@
 package roomescape.member.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import roomescape.member.dto.MemberCreateRequest;
 import roomescape.member.dto.MemberProfileInfo;
 import roomescape.member.service.MemberService;
 
@@ -21,5 +26,14 @@ public class MemberController {
     @GetMapping
     public List<MemberProfileInfo> findMembers() {
         return memberService.findAllMembers();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createMember(@RequestBody MemberCreateRequest request) {
+        MemberProfileInfo response = memberService.createMember(request);
+
+        URI location = URI.create("/members/" + response.id());
+        return ResponseEntity.created(location)
+                .build();
     }
 }
