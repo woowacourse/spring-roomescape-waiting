60f04808ad672de11f53ddfd02366d838845d6ed
pricelees <priceelees@gmail.com>
2024-05-18 18:23:21 +0900

    fix: List를 포장한 객체를 응답했을 때 데이터가 렌더링되지 않는 문제 해결

--- src/main/java/roomescape/service/dto/response/ListResponse.java
+++ src/main/java/roomescape/service/dto/response/ListResponse.java
@@ -2,7 +2,7 @@ package roomescape.service.dto.response;
 
 import java.util.List;
 
-public record ListResponse<T>(List<T> items, int count) {
+public record ListResponse<T>(List<T> responses, int count) {
 
     public ListResponse(List<T> items) {
         this(items, items.size());
