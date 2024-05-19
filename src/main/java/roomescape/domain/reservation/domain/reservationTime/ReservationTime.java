89654294547cba300cdb67c11a4545fea4204604
ehtjsv2 <ehtjsv2@naver.com>
2024-05-19 10:32:06 +0900

    refactor: reservationTime 컬럼명 추가

--- src/main/java/roomescape/domain/reservation/domain/reservationTime/ReservationTime.java
+++ src/main/java/roomescape/domain/reservation/domain/reservationTime/ReservationTime.java
@@ -1,5 +1,6 @@
 package roomescape.domain.reservation.domain.reservationTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ public class ReservationTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "start_at", nullable = false)
     private LocalTime startAt;
 
     public ReservationTime() {
