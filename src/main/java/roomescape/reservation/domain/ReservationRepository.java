0c718611b46e51686467a38e1adfe95ae924736b
jongmee <ra123221@gmail.com>
2024-05-20 01:30:45 +0900

    fix: 레포지토리에 transactional 애너테이션 추가

--- src/main/java/roomescape/reservation/domain/ReservationRepository.java
+++ src/main/java/roomescape/reservation/domain/ReservationRepository.java
@@ -3,11 +3,13 @@ package roomescape.reservation.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.member.domain.Member;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     @Query("SELECT r FROM Reservation r JOIN FETCH r.time JOIN FETCH r.theme JOIN FETCH r.member")
