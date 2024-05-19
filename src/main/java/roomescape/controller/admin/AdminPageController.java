69a11575a10496ed6f3d52776dae132354e4d926
Arachneee <jhg2819@naver.com>
2024-05-19 21:28:42 +0900

    feat: 예약 대기 페이지 렌더링 기능 구현

--- src/main/java/roomescape/controller/admin/AdminPageController.java
+++ src/main/java/roomescape/controller/admin/AdminPageController.java
@@ -27,4 +27,9 @@ public class AdminPageController {
     public String theme() {
         return "/admin/theme";
     }
+
+    @GetMapping("/waiting")
+    public String waiting() {
+        return "/admin/waiting";
+    }
 }
