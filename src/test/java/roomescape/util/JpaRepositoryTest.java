e9c8ec06c5e317d8b9488d09cc8b63a528cd7b40
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-19 23:32:13 +0900

    refactor(JpaRepositoryTest): JpaRepositoryTest에 불필요한 DirtiesContext 제거

--- src/test/java/roomescape/util/JpaRepositoryTest.java
+++ src/test/java/roomescape/util/JpaRepositoryTest.java
@@ -5,13 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles("test")
 @DataJpaTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public @interface JpaRepositoryTest {
 }
