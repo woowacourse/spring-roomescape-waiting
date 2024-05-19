a2c6767c00ae653495d483559ff9729f712df773
masonkimseoul <masonkimseoul@gmail.com>
2024-05-19 21:50:58 +0900

    refactor: Hibernate에 의존적인 구현 수정
    @Length -> @Size

--- src/main/java/roomescape/theme/dto/ThemeAddRequest.java
+++ src/main/java/roomescape/theme/dto/ThemeAddRequest.java
@@ -1,16 +1,16 @@
 package roomescape.theme.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 import roomescape.theme.domain.Theme;
 
 public record ThemeAddRequest(
         @NotBlank(message = "테마 이름은 필수입니다.")
-        @Length(max = 40, message = "테마 이름의 길이를 확인해주세요")
+        @Size(max = 40, message = "테마 이름의 길이를 확인해주세요")
         String name,
 
         @NotBlank(message = "테마 설명은 필수입니다.")
-        @Length(max = 80, message = "테마 설명의 길이를 확인해주세요")
+        @Size(max = 80, message = "테마 설명의 길이를 확인해주세요")
         String description,
 
         @NotBlank(message = "테마 썸네일 경로는 필수입니다.")
