ced1b9ddaa0065a41f34dc7d0e95e21670bfe1d6
ehtjsv2 <ehtjsv2@naver.com>
2024-05-19 10:44:48 +0900

    refactor: 멤버 이름 길이 제한 추가

--- src/main/java/roomescape/domain/member/domain/MemberName.java
+++ src/main/java/roomescape/domain/member/domain/MemberName.java
@@ -3,12 +3,14 @@ package roomescape.domain.member.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.Objects;
+import roomescape.global.exception.ValueLengthException;
 import roomescape.global.exception.ValueNullOrEmptyException;
 
 @Embeddable
 public class MemberName {
 
-    @Column(name = "name", nullable = false)
+    private static final int NAME_MAX_LENGTH = 10;
+    @Column(name = "name", nullable = false, length = NAME_MAX_LENGTH)
     private String value;
 
     public MemberName() {
@@ -16,13 +18,24 @@ public class MemberName {
     }
 
     public MemberName(String value) {
-        validateNullAndBlank(value);
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(String value) {
+        validateNullAndBlank(value);
+        validateLength(value);
     }
 
     private void validateNullAndBlank(String value) {
         if (value == null || value.isBlank()) {
             throw new ValueNullOrEmptyException("멤버이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > 10) {
+            throw new ValueLengthException("멤버의 이름은 " + NAME_MAX_LENGTH + "을 초과할 수 없습니다.");
         }
     }
 
---
+++ src/main/java/roomescape/global/exception/ValueLengthException.java
@@ -0,0 +1,8 @@
+package roomescape.global.exception;
+
+public class ValueLengthException extends EscapeApplicationException {
+
+    public ValueLengthException(String message) {
+        super(message);
+    }
+}
