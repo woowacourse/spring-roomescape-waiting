128ca78676b124b7614f8fc1e1bf2d6d796e9538
hjk0761 <hjk0761@naver.com>
2024-05-20 15:50:23 +0900

    feat: Waiting 도메인 구현

---
+++ src/main/java/roomescape/model/Waiting.java
@@ -0,0 +1,86 @@
+package roomescape.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class Waiting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ReservationTime time;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Theme theme;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    protected Waiting() {
+    }
+
+    public Waiting(Long id, LocalDate date, ReservationTime time, Theme theme, Member member) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.theme = theme;
+        this.member = member;
+    }
+
+    public Waiting(LocalDate date, ReservationTime time, Theme theme, Member member) {
+        this(null, date, time, theme, member);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public ReservationTime getTime() {
+        return time;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Waiting that = (Waiting) object;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getDate(), that.getDate())
+                && Objects.equals(getTime(), that.getTime()) && Objects.equals(getTheme(),
+                that.getTheme()) && Objects.equals(getMember(), that.getMember());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getDate(), getTime(), getTheme(), getMember());
+    }
+
+    @Override
+    public String toString() {
+        return "Waiting{" +
+                "id=" + id +
+                ", date=" + date +
+                ", time=" + time +
+                ", theme=" + theme +
+                ", member=" + member +
+                '}';
+    }
+}
