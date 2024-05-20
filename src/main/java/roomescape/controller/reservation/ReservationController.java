072098ba1a7cbe5bdd68c26c1c7bf5a746ee9203
reddevilmidzy <midzy0228@gmail.com>
2024-05-21 00:45:56 +0900

    refactor: 예약 삭제 api 변경

--- src/main/java/roomescape/controller/reservation/ReservationController.java
+++ src/main/java/roomescape/controller/reservation/ReservationController.java
@@ -2,9 +2,7 @@ package roomescape.controller.reservation;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -72,12 +70,5 @@ public class ReservationController {
         return filter.stream()
                 .map(ReservationResponse::from)
                 .toList();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable("id") final Long id) {
-        reservationService.deleteReservation(id);
-        return ResponseEntity.noContent()
-                .build();
     }
 }
