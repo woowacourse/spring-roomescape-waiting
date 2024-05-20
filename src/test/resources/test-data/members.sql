9bbfa345b1adf10b852242409896d96eceeda69f
3juhwan <13selfesteem91@naver.com>
2024-05-20 11:15:19 +0900

    chore: 테스트에 필요한 초기 데이터 파일 추가

---
+++ src/test/resources/test-data/members.sql
@@ -0,0 +1,5 @@
+insert into members (id, name, email, password, role)
+values (1, '미르', 'mrmrmrmr@woowa.net', 'passoword', 'ADMIN'),
+       (2, '망쵸', 'mangcho@woowa.net', 'passoword', 'NORMAL'),
+       (3, '피카츄', 'picachu@woowa.net', 'passoword', 'NORMAL'),
+       (4, '라이츄', 'richu@woowa.net', 'passoword', 'NORMAL');
