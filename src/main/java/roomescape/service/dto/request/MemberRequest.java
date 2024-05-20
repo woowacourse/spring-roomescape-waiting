e52d99f40c48d9747e18edbe34ddd3d5c17d0614
leetaehoon <xogns1514@gmail.com>
2024-05-20 17:34:46 +0900

    refactor: 예외처리문구 수정

--- src/main/java/roomescape/service/dto/request/MemberRequest.java
+++ src/main/java/roomescape/service/dto/request/MemberRequest.java
@@ -10,7 +10,7 @@ public record MemberRequest(
         @NotBlank(message = "이름을 입력해주세요.")
         String name,
         @NotBlank(message = "이메일을 입력해주세요.")
-        @Email(message = "email형식에 맞지 않습니다.")
+        @Email(message = "이메일 형식에 맞지 않습니다.")
         String email,
         @NotBlank(message = "패스워드를 입력해주세요.")
         String password,
