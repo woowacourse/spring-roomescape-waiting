77385b2817c924b94086b5635322cdbdc53186aa
3juhwan <13selfesteem91@naver.com>
2024-05-21 00:46:53 +0900

    fix: 테스트 데이터 수정

--- src/test/resources/test-data/members.sql
+++ src/test/resources/test-data/members.sql
@@ -1,5 +1,5 @@
 insert into members (id, name, email, password, role)
-values (1, '미르', 'mrmrmrmr@woowa.net', 'password', 'ADMIN'),
-       (2, '망쵸', 'mangcho@woowa.net', 'password', 'NORMAL'),
-       (3, '피카츄', 'picachu@woowa.net', 'password', 'NORMAL'),
-       (4, '라이츄', 'richu@woowa.net', 'password', 'NORMAL');
+values (1, '미르', 'mrmrmrmr@woowa.net', 'password', 'admin'),
+       (2, '망쵸', 'mangcho@woowa.net', 'password', 'normal'),
+       (3, '피카츄', 'picachu@woowa.net', 'password', 'normal'),
+       (4, '라이츄', 'richu@woowa.net', 'password', 'normal');
