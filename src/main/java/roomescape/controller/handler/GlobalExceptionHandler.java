b1d50d3b402d413fd1525f963a6f9f1bb5e04acf
ikjo39 <chi4321@naver.com>
2024-05-20 11:13:32 +0900

    refactor: IndexOutOfBound 예외 방지

--- src/main/java/roomescape/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/handler/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package roomescape.controller.handler;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.coyote.BadRequestException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,27 +21,30 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({IllegalArgumentException.class, NumberFormatException.class, BadRequestException.class})
     public ResponseEntity<String> badRequestException(Exception e) {
-        return ResponseEntity.badRequest().body(MESSAGE_HEADER + e.getMessage());
+        return ResponseEntity.badRequest()
+                .body(MESSAGE_HEADER + e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> methodArgumentValidException(MethodArgumentNotValidException e) {
-        String defaultMessage = e.getBindingResult()
-                .getAllErrors()
-                .get(0)
-                .getDefaultMessage();
-
+        List<ObjectError> allErrors = e.getBindingResult()
+                .getAllErrors();
+        String errorMessages = allErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(System.lineSeparator()));
         return ResponseEntity.badRequest()
-                .body(MESSAGE_HEADER + defaultMessage);
+                .body(MESSAGE_HEADER + errorMessages);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> httpMessageNotReadableException(HttpMessageNotReadableException e) {
         if (e.getCause() instanceof JsonMappingException jsonMappingException) {
-            List<Reference> path = jsonMappingException.getPath();
-            Reference reference = path.get(0);
-            String errorMessage = reference.getFieldName() + "필드 내용이 잘못되었습니다.";
-            return ResponseEntity.badRequest().body(MESSAGE_HEADER + errorMessage);
+            List<Reference> paths = jsonMappingException.getPath();
+            String errorMessages = paths.stream()
+                    .map(reference -> reference.getFieldName() + " 필드 내용이 잘못되었습니다.")
+                    .collect(Collectors.joining(System.lineSeparator()));
+            return ResponseEntity.badRequest()
+                    .body(MESSAGE_HEADER + errorMessages);
         }
         String errorMessage = MESSAGE_HEADER + "적절하지 않은 입력값 입니다";
         return ResponseEntity.badRequest()
