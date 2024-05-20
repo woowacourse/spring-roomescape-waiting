da96a53b357bc91bdc955c1dcac9c8da43d998b5
3juhwan <13selfesteem91@naver.com>
2024-05-21 00:43:20 +0900

    chore: 테스트 데이터 수정

--- src/test/resources/test-data/members.sql
+++ src/test/resources/test-data/members.sql
@@ -1,5 +1,5 @@
 insert into members (id, name, email, password, role)
-values (1, '미르', 'mrmrmrmr@woowa.net', 'passoword', 'ADMIN'),
-       (2, '망쵸', 'mangcho@woowa.net', 'passoword', 'NORMAL'),
-       (3, '피카츄', 'picachu@woowa.net', 'passoword', 'NORMAL'),
-       (4, '라이츄', 'richu@woowa.net', 'passoword', 'NORMAL');
+values (1, '미르', 'mrmrmrmr@woowa.net', 'password', 'ADMIN'),
+       (2, '망쵸', 'mangcho@woowa.net', 'password', 'NORMAL'),
+       (3, '피카츄', 'picachu@woowa.net', 'password', 'NORMAL'),
+       (4, '라이츄', 'richu@woowa.net', 'password', 'NORMAL');
