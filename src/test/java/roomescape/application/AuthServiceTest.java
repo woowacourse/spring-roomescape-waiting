08ec63f47c272d8a88f0e6523e58a9e53d7c87b4
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-20 09:32:28 +0900

    test: auth service test

--- src/test/java/roomescape/application/AuthServiceTest.java
+++ src/test/java/roomescape/application/AuthServiceTest.java
@@ -1,28 +1,21 @@
 package roomescape.application;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
 
 import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import roomescape.domain.member.Role;
 import roomescape.application.dto.request.LoginRequest;
 import roomescape.application.dto.request.SignupRequest;
 import roomescape.application.dto.response.MemberResponse;
+import roomescape.domain.member.Role;
 import roomescape.exception.BadRequestException;
 import roomescape.support.BaseServiceTest;
 
 class AuthServiceTest extends BaseServiceTest {
-
-    private static final String EMAIL = "auth@gmail.com";
-    private static final String PASSWORD = "password";
-    private static final String NICKNAME = "nickname";
 
     @Autowired
     private AuthService authService;
@@ -30,58 +23,54 @@ class AuthServiceTest extends BaseServiceTest {
     @Autowired
     private MemberService memberService;
 
-    @SpyBean
+    @Autowired
     private TokenProvider tokenProvider;
-
-    @BeforeEach
-    void setUp() {
-        SignupRequest request = new SignupRequest(EMAIL, PASSWORD, NICKNAME);
-        memberService.createMember(request);
-    }
 
     @Test
     @DisplayName("토큰을 생성한다.")
     void createToken() {
-        doReturn("created_token").when(tokenProvider).createToken(any());
-
         String token = authService.createToken(1L);
 
-        SoftAssertions.assertSoftly(softly -> {
-            softly.assertThat(token).isEqualTo("created_token");
-        });
+        assertThatCode(() -> tokenProvider.getMemberId(token))
+                .doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("비밀번호가 일치하는지 검증한다.")
     void validatePassword() {
-        LoginRequest request = new LoginRequest(EMAIL, PASSWORD);
+        memberService.createMember(new SignupRequest("ex@gmail.com", "password", "구름"));
+
+        LoginRequest request = new LoginRequest("ex@gmail.com", "password");
 
         MemberResponse response = authService.validatePassword(request);
 
         SoftAssertions.assertSoftly(softly -> {
-            softly.assertThat(response.email()).isEqualTo(EMAIL);
-            softly.assertThat(response.name()).isEqualTo(NICKNAME);
+            softly.assertThat(response.email()).isEqualTo("ex@gmail.com");
+            softly.assertThat(response.name()).isEqualTo("구름");
             softly.assertThat(response.role()).isEqualTo(Role.USER);
         });
     }
 
     @Test
+    @DisplayName("토큰으로 회원 아이디를 가져올 수 있다.")
+    void getMemberId() {
+        Long memberId = 1L;
+
+        String token = tokenProvider.createToken(memberId.toString());
+        Long memberIdByToken = authService.getMemberIdByToken(token);
+
+        assertThat(memberIdByToken).isEqualTo(memberId);
+    }
+
+    @Test
     @DisplayName("비밀번호가 일치하지 않을 경우 예외를 발생시킨다.")
-    void validatePassword_fail_when_password_not_matched() {
-        LoginRequest request = new LoginRequest(EMAIL, "wrong_password");
+    void validatePasswordWhenPasswordIsNotMatch() {
+        memberService.createMember(new SignupRequest("ex@gmail.com", "password", "구름"));
+
+        LoginRequest request = new LoginRequest("ex@gmail.com", "wrong_password");
 
         assertThatThrownBy(() -> authService.validatePassword(request))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessage("비밀번호가 일치하지 않습니다.");
-    }
-
-    @Test
-    @DisplayName("토큰으로 회원 아이디를 가져올 수 있다.")
-    void getMemberId() {
-        doReturn(1L).when(tokenProvider).getMemberId(any());
-
-        Long memberId = authService.getMemberIdByToken("token");
-
-        assertThat(memberId).isEqualTo(1L);
     }
 }
