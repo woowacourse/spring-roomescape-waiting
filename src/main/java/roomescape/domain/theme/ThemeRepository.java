9b97a6014000c27aebd54b26ca2696b567639cad
linirini <2001yerin@naver.com>
2024-05-20 19:44:47 +0900

    fix: 엔티티 변경에 따른 네이티브 쿼리 수정

--- src/main/java/roomescape/domain/theme/ThemeRepository.java
+++ src/main/java/roomescape/domain/theme/ThemeRepository.java
@@ -10,14 +10,18 @@ import java.util.List;
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
     boolean existsByName(ThemeName name);
 
+    //TODO: 테스트 추가
     @Query(value = """
-            SELECT t.id, name, description, thumbnail, COUNT(r.theme_id) AS reservation_count
-            FROM theme t INNER JOIN reservation r
-            ON r.theme_id = t.id
-            WHERE r.date BETWEEN :startDate AND :endDate
-            GROUP BY r.theme_id
-            ORDER BY reservation_count DESC
-            LIMIT :limit""", nativeQuery = true)
+        SELECT t.id, t.name, t.description, t.thumbnail, COUNT(r.id) AS reservation_count
+        FROM theme t
+        INNER JOIN reservation_detail rd ON rd.theme_id = t.id
+        INNER JOIN reservation r ON r.reservation_detail_id = rd.id
+        WHERE rd.date BETWEEN :startDate AND :endDate
+        AND r.status = 'RESERVED'
+        GROUP BY t.id
+        ORDER BY reservation_count DESC
+        LIMIT :limit
+        """, nativeQuery = true)
     List<Theme> findByReservationTermAndLimit(@Param("startDate") LocalDate startDate,
                                               @Param("endDate") LocalDate endDate, @Param("limit") long limit);
 }
