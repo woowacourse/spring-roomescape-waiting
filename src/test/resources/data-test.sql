aaebb12886160b2d2bdde086756bd6a3253991ca
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-21 15:21:53 +0900

    refactor(Member): @Column 을 통해 필드 DB 세부 설정 추가

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -7,6 +7,6 @@ SET REFERENTIAL_INTEGRITY TRUE;
 
 INSERT INTO theme (id, name, description, thumbnail) VALUES (1, '테마1', '설명1' ,'https://image.jpg');
 INSERT INTO time (id, start_at) VALUES (1, '10:00');
-INSERT INTO member(id, name, email, password, role) VALUES (1, '켬미', 'aaa@naver.com', '1111', 1);
+INSERT INTO member(id, name, email, password, role) VALUES (1, '켬미', 'aaa@naver.com', '1111', 'ADMIN');
 INSERT INTO reservation (id, date, member_id, time_id, theme_id) VALUES (999, '2023-08-05', 1, 1, 1);
 
