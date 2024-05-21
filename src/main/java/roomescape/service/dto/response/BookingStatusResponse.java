105b664306c82ad26e719499a9caac799d1c327c
KimSangHyeok <tkdgur0906@naver.com>
2024-05-21 16:42:00 +0900

    feat: 예약페이지 시간 출력 시 시,분만 출력되도록 기능 추가

--- src/main/java/roomescape/service/dto/response/BookingStatusResponse.java
+++ src/main/java/roomescape/service/dto/response/BookingStatusResponse.java
@@ -1,10 +1,11 @@
 package roomescape.service.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import roomescape.domain.ReservationTime;
 
 import java.time.LocalTime;
 
-public record BookingStatusResponse(LocalTime startAt, Long timeId, boolean alreadyBooked) {
+public record BookingStatusResponse(@JsonFormat(pattern = "HH:mm") LocalTime startAt, Long timeId, boolean alreadyBooked) {
 
     public BookingStatusResponse(ReservationTime reservationTime, boolean alreadyBooked) {
         this(reservationTime.getStartAt(), reservationTime.getId(), alreadyBooked);
