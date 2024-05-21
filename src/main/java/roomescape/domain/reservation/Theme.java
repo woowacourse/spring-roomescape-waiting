fb58f1ca3692b1791b829577d023e925ed2879f0
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-21 22:45:04 +0900

    refactor: 테마 필드 값 검증 및 테스트 추가

--- src/main/java/roomescape/domain/reservation/Theme.java
+++ src/main/java/roomescape/domain/reservation/Theme.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import roomescape.exception.BadRequestException;
 
 @Entity
 public class Theme {
@@ -16,12 +17,33 @@ public class Theme {
     private String thumbnail;
 
     public Theme(String name, String description, String thumbnail) {
+        validateName(name);
+        validateDescription(description);
+        validateThumbnail(thumbnail);
         this.name = name;
         this.description = description;
         this.thumbnail = thumbnail;
     }
 
     protected Theme() {
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new BadRequestException("테마 이름은 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateDescription(String description) {
+        if (description == null || description.isBlank()) {
+            throw new BadRequestException("테마 설명은 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateThumbnail(String thumbnail) {
+        if (thumbnail == null || thumbnail.isBlank()) {
+            throw new BadRequestException("테마 썸네일은 반드시 입력되어야 합니다.");
+        }
     }
 
     public Long getId() {
