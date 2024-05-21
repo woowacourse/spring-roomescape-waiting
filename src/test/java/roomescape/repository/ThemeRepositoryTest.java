13858914ac66697601476af89b257c66f20a4724
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-21 16:00:09 +0900

    refactor(ReservationInserter) : 불필요한 파일 삭제
    - 반영된 코드 수정

--- src/test/java/roomescape/repository/ThemeRepositoryTest.java
+++ src/test/java/roomescape/repository/ThemeRepositoryTest.java
@@ -4,16 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import roomescape.domain.reservation.ReservationTime;
-import roomescape.domain.reservation.Theme;
-import roomescape.domain.user.Member;
-import roomescape.fixture.MemberFixture;
-import roomescape.fixture.ReservationTimeFixture;
 import roomescape.fixture.ThemeFixture;
 import roomescape.util.DatabaseCleaner;
-import roomescape.util.ReservationInserter;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,15 +14,7 @@ class ThemeRepositoryTest {
     @Autowired
     ThemeRepository sut;
     @Autowired
-    ReservationInserter reservationInserter;
-    @Autowired
     DatabaseCleaner databaseCleaner;
-    @Autowired
-    private ThemeRepository themeRepository;
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private ReservationTimeRepository reservationTimeRepository;
 
     @BeforeEach
     void setup() {
@@ -66,27 +50,5 @@ class ThemeRepositoryTest {
 
         final var result = sut.findAll();
         assertThat(result).hasSize(2);
-    }
-
-    @Test
-    void getPopularTheme() {
-        final Theme theme1 = Theme.of("공포", "진짜 공포임", "a.jpg");
-        final Theme theme2 = Theme.of("감동", "실화", "b.jpg");
-        final Theme theme3 = Theme.of("충격", "충충실화", "c.jpg");
-
-        final Member member = memberRepository.save(MemberFixture.getDomain());
-        final ReservationTime reservationTime = reservationTimeRepository.save(ReservationTimeFixture.getDomain());
-
-
-        reservationInserter.addNewReservation("2024-10-03", theme1, member, reservationTime);
-        reservationInserter.addNewReservation("2024-10-02", theme1, member, reservationTime);
-        reservationInserter.addNewReservation("2024-10-02", theme2, member, reservationTime);
-        reservationInserter.addNewReservation("2024-10-05", theme2, member, reservationTime);
-        reservationInserter.addNewReservation("2024-10-03", theme3, member, reservationTime);
-        reservationInserter.addNewReservation("2024-10-02", theme3, member, reservationTime);
-
-        final List<Theme> themes = themeRepository.getPopularTheme("2024-10-02", "2024-10-04", 3);
-        assertThat(themes).containsExactly(theme1, theme3, theme2);
-
     }
 }
