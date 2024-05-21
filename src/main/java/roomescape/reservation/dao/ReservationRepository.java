10621fe060501cce26fb88432c9df7922eba1b4a
DobbyMac <kimdobby@g.skku.edu>
2024-05-21 12:53:43 +0900

    refactor(ReservationRepository): 미사용 SQL 쿼리문 제거

--- src/main/java/roomescape/reservation/dao/ReservationRepository.java
+++ src/main/java/roomescape/reservation/dao/ReservationRepository.java
@@ -3,16 +3,13 @@ package roomescape.reservation.dao;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import roomescape.reservation.domain.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findAllByOrderByDateAsc();
 
-    @Query("SELECT r FROM Reservation r WHERE r.theme.id = :themeId AND r.date = :date")
-    List<Reservation> findAllByTheme_IdAndDate(@Param("themeId") Long themeId, @Param("date") LocalDate date);
+    List<Reservation> findAllByTheme_IdAndDate(Long themeId, LocalDate date);
 
     List<Reservation> findAllByMember_Id(Long memberId);
 
