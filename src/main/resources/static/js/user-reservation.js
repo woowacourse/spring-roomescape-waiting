bac27fcf4b23c38055a28f0e2dad9617708bea19
ehtjsv2 <ehtjsv2@naver.com>
2024-05-21 16:07:26 +0900

    feat: js 예약 대기 버튼 api 추가

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -182,14 +182,10 @@ function onWaitButtonClick() {
     if (selectedDate && selectedThemeId && selectedTimeId) {
         const reservationData = {
             date: selectedDate,
-            theme: selectedThemeId,
-            time: selectedTimeId
+            themeId: selectedThemeId,
+            timeId: selectedTimeId
         };
-
-        /*
-        TODO: [3단계] 예약 대기 생성 요청 API 호출
-         */
-        fetch('', {
+        fetch('/reservation?waiting=true', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
