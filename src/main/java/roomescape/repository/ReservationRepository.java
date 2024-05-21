5be2ec423ec5085491f90cc96194e6b3e5e8f25a
jinwoo22 <jenny010925@naver.com>
2024-05-21 20:51:37 +0900

    feat: 엔티티 그래프 적용

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -3,6 +3,7 @@ package roomescape.repository;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import roomescape.domain.Reservation;
@@ -13,6 +14,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long>,
 
     Optional<List<Reservation>> findByThemeId(Long themeId);
 
+    @EntityGraph(attributePaths = {"member", "time", "theme"})
     List<Reservation> findByMemberId(Long memberId);
 
     Optional<Reservation> findByDateAndTimeAndTheme(LocalDate date, ReservationTime time, RoomTheme theme);
