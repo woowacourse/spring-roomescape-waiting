494c6323d504df4adcfa9ad43032c3bce9f37f14
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-21 23:51:48 +0900

    feat: rank 로 status 출력 formatting

--- src/main/java/roomescape/core/domain/Status.java
+++ src/main/java/roomescape/core/domain/Status.java
@@ -2,12 +2,16 @@ package roomescape.core.domain;
 
 public enum Status {
     BOOKED("예약"),
-    STANDBY("대기");
+    STANDBY("예약대기");
 
     private final String value;
 
     Status(String value) {
         this.value = value;
+    }
+
+    public String waitingRankStatus(Integer rank) {
+        return rank + "번째 " + this.getValue();
     }
 
     public String getValue() {
