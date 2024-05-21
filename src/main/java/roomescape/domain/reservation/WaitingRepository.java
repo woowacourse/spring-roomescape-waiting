f671f2bd9a0aeaf151024c5d794988c4d9c8c761
Arachneee <jhg2819@naver.com>
2024-05-21 14:55:29 +0900

    style: 불필요 클레스 제거

--- src/main/java/roomescape/domain/reservation/WaitingRepository.java
+++ src/main/java/roomescape/domain/reservation/WaitingRepository.java
@@ -5,27 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import roomescape.domain.member.Member;
 import roomescape.domain.reservation.dto.WaitingReadOnly;
-import roomescape.domain.reservation.dto.WaitingWithRank;
 
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
-
-    @Query("""
-            select new roomescape.domain.reservation.dto.WaitingWithRank(
-                w,
-                (select (count(w2))
-                from Waiting w2
-                where w2.reservation = w.reservation and w2.id <= w.id)
-            )
-            from Waiting w
-            join fetch w.reservation r
-            join fetch r.slot s
-            join fetch s.time
-            join fetch s.theme
-            where w.member = :member and w.reservation.slot.date >= :date
-            """)
-    List<WaitingWithRank> findWaitingRankByMemberAndDateAfter(Member member, LocalDate date);
 
     @Query("select w.member.id from Waiting w where w.id = :id")
     Long findMemberIdById(Long id);
--- src/main/java/roomescape/domain/reservation/dto/WaitingWithRank.java
+++
@@ -1,9 +0,0 @@
-package roomescape.domain.reservation.dto;
-
-import roomescape.domain.reservation.Waiting;
-
-public record WaitingWithRank(
-        Waiting waiting,
-        Long rank
-) {
-}
