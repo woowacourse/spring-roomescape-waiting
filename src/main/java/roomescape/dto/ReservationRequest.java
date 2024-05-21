b26339a38ae9ad4e522d224ea6b9842421bffd2d
J-I-H-O <jeongjiho0731@gmail.com>
2024-05-21 14:03:49 +0900

    refactor: 코드 재정렬

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -2,5 +2,10 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 
-public record ReservationRequest(LocalDate date, Long memberId, long timeId, long themeId) {
+public record ReservationRequest(
+        LocalDate date,
+        Long memberId,
+        long timeId,
+        long themeId
+) {
 }
