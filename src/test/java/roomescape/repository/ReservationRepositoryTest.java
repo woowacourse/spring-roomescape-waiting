240090f50d8b5bdcf844163032a20c9cb4f9d300
ikjo39 <chi4321@naver.com>
2024-05-21 16:38:03 +0900

    test: JPQL 쿼리 테스트 추가

---
+++ src/test/java/roomescape/repository/ReservationRepositoryTest.java
@@ -0,0 +1,39 @@
+package roomescape.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import roomescape.domain.Member;
+import roomescape.domain.ReservationRank;
+
+@DataJpaTest
+class ReservationRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @DisplayName("예약내역과 순위를 반환한다.")
+    @Test
+    void findReservationRanksWithMember() {
+        Member member = memberRepository.findById(2L).orElseThrow();
+
+        List<ReservationRank> reservationRanksWithMember =
+                reservationRepository.findReservationRanksWithMember(member);
+        List<Long> pendingRanks = reservationRanksWithMember.stream()
+                .filter(reservationRank -> reservationRank.getReservation().getStatus().isPending())
+                .map(ReservationRank::getRank)
+                .toList();
+
+        System.out.println(pendingRanks);
+
+        assertThat(reservationRanksWithMember).hasSize(10);
+        assertThat(pendingRanks).hasSize(4);
+    }
+}
