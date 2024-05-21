988684dc55e561d235c0a6fad716891aad71551a
takoyakimchi <c4nyou@gmail.com>
2024-05-21 11:46:45 +0900

    test: 실패하는 테스트 수정

--- src/test/java/roomescape/controller/api/UserReservationControllerTest.java
+++ src/test/java/roomescape/controller/api/UserReservationControllerTest.java
@@ -116,17 +116,17 @@ class UserReservationControllerTest {
     @Test
     void deleteStandby_ReservedByOther() {
         jdbcTemplate.update("""
-            INSERT INTO member(name, email, password, role) VALUES ('트레', 'tre@a.com', '123a!', 'ADMIN');
+            INSERT INTO member(name, email, password, role) VALUES ('트레', 'tre@a.com', '123a!', 'USER');
             INSERT INTO reservation_time(start_at) VALUES ('10:00');
             INSERT INTO theme(name, description, thumbnail) VALUES ('t1', 'd1', 'https://test.com/test.jpg');
             INSERT INTO reservation (member_id, reserved_date, created_at, time_id, theme_id, status)
-            VALUES (2, '2060-01-01', '2024-01-01', 1, 1, 'RESERVED'),
-                   (1, '2060-01-01', '2024-01-02', 1, 1, 'STANDBY');
+            VALUES (1, '2060-01-01', '2024-01-01', 1, 1, 'RESERVED'),
+                   (2, '2060-01-01', '2024-01-02', 1, 1, 'STANDBY');
             """);
 
         RestAssured.given().log().all()
             .cookie("token", userToken)
-            .when().delete("/reservations/standby/1")
+            .when().delete("/reservations/standby/2")
             .then().log().all()
             .statusCode(400)
             .body("message", is("자신의 예약만 삭제할 수 있습니다."));
