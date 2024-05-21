f0bb1345c8d4d453353fc9695c53a8a24d213c7b
hyun <osh123chm@gmail.com>
2024-05-21 13:33:18 +0900

    refactor: 예약 정보 조회 fetch join 적용

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -3,18 +3,42 @@ package roomescape.repository;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import roomescape.domain.Member;
 import roomescape.domain.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     boolean existsByDateAndTimeIdAndThemeId(LocalDate date, Long timeId, Long themeId);
 
-    List<Reservation> findAllByThemeIdAndMemberIdAndDateBetween(Long themeId, Long memberId, LocalDate dateFrom,
-                                                                LocalDate dateTo);
-
     boolean existsByTimeId(Long timeId);
 
     boolean existsByThemeId(Long themeId);
 
+    @Query("""
+            SELECT r FROM Reservation r
+            JOIN FETCH r.member
+            JOIN FETCH r.time
+            JOIN FETCH r.theme
+            """)
+    List<Reservation> findAll();
+
+    @Query("""
+            SELECT r FROM Reservation r
+            JOIN FETCH r.member
+            JOIN FETCH r.time
+            JOIN FETCH r.theme
+            WHERE r.member = :member
+            """)
     List<Reservation> findByMember(Member member);
+
+    @Query("""
+            SELECT r FROM Reservation r
+            JOIN FETCH r.member
+            JOIN FETCH r.time
+            JOIN FETCH r.theme
+            WHERE r.member.id = :memberId AND r.theme.id = :themeId
+                  AND r.date >= :dateFrom AND r.date <= :dateTo
+            """)
+    List<Reservation> findAllByThemeIdAndMemberIdAndDateBetween(Long themeId, Long memberId, LocalDate dateFrom,
+                                                                LocalDate dateTo);
 }
