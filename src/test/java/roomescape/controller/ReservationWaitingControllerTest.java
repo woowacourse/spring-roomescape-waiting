2825b2e42f314f3d2b978ee0e1056804bb2eae8e
JUHA <84626225+khabh@users.noreply.github.com>
2024-05-21 22:42:48 +0900

    test: 예약 대기 테스트 구체적으로 수정

--- src/test/java/roomescape/controller/ReservationWaitingControllerTest.java
+++ src/test/java/roomescape/controller/ReservationWaitingControllerTest.java
@@ -13,6 +13,7 @@ import roomescape.web.controller.request.MemberWaitingWebRequest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static roomescape.Fixture.VALID_RESERVATION_TIME;
 import static roomescape.Fixture.VALID_THEME;
@@ -36,6 +37,25 @@ public class ReservationWaitingControllerTest extends ControllerTest {
 
     @Autowired
     private ReservationWaitingRepository reservationWaitingRepository;
+
+    @DisplayName("로그인 되지 않은 사용자가 예약 대기를 저장한다. -> 400")
+    @Test
+    void unAuthorizedWaiting() {
+        ReservationTime time = reservationTimeRepository.save(VALID_RESERVATION_TIME);
+        Theme theme = themeRepository.save(VALID_THEME);
+        String date = LocalDate.now().plusMonths(1).toString();
+        Member reservedMember = memberRepository.save(new Member(new MemberName("감자"), new MemberEmail("111@aaa.com"), new MemberPassword("asd"), MemberRole.USER));
+        reservationRepository.save(new Reservation(reservedMember, new ReservationDate(date), time, theme));
+
+        MemberWaitingWebRequest request = new MemberWaitingWebRequest(date, time.getId(), theme.getId());
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(400);
+    }
 
     @DisplayName("사용자 예약 대기를 저장한다. -> 201")
     @Test
@@ -81,18 +101,21 @@ public class ReservationWaitingControllerTest extends ControllerTest {
                 .statusCode(204);
     }
 
-    @DisplayName("사용자 예약 대기를 조회한다. -> 200")
+    @DisplayName("내 예약 대기 목록을 조회한다. -> 200")
     @Test
     void findAllByMember() {
         ReservationTime time = reservationTimeRepository.save(VALID_RESERVATION_TIME);
         Theme theme = themeRepository.save(VALID_THEME);
-        String date = LocalDate.now().plusMonths(1).toString();
+        LocalDate date = LocalDate.now().plusMonths(1);
         LocalDateTime createdDateTime = LocalDateTime.now().minusMonths(2);
         Member waitingMember = memberRepository.save(new Member(new MemberName("감자"), new MemberEmail("111@aaa.com"), new MemberPassword("asd"), MemberRole.USER));
+        Member otherMember = memberRepository.save(new Member(new MemberName("고구마"), new MemberEmail("222@aaa.com"), new MemberPassword("asd"), MemberRole.USER));
         reservationWaitingRepository.save(new ReservationWaiting(createdDateTime, waitingMember, new ReservationDate(date), time, theme));
+        reservationWaitingRepository.save(new ReservationWaiting(createdDateTime, otherMember, new ReservationDate(date.plusMonths(1)), time, theme));
+        reservationWaitingRepository.save(new ReservationWaiting(createdDateTime, otherMember, new ReservationDate(date.plusMonths(2)), time, theme));
         String token = jwtProvider.createToken(waitingMember.getEmail().getEmail());
 
-        MemberWaitingWebRequest request = new MemberWaitingWebRequest(date, time.getId(), theme.getId());
+        MemberWaitingWebRequest request = new MemberWaitingWebRequest(date.toString(), time.getId(), theme.getId());
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -101,6 +124,9 @@ public class ReservationWaitingControllerTest extends ControllerTest {
                 .when().get("/waitings/mine")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(1));
+                .body("size()", is(1))
+                .body("[0].name", equalTo("감자"))
+                .body("[0].id", equalTo(1))
+                .body("[0].date", equalTo("2024-06-21"));
     }
 }
