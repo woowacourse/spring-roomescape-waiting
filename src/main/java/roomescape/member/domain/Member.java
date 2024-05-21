6b91bb1c178225b27be85bff9585fde00d187af9
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-21 15:21:53 +0900

    refactor(Member): @Column 을 통해 필드 DB 세부 설정 추가

--- src/main/java/roomescape/member/domain/Member.java
+++ src/main/java/roomescape/member/domain/Member.java
@@ -2,7 +2,10 @@ package roomescape.member.domain;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,9 +15,15 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private MemberRole role;
 
     public Member() {
