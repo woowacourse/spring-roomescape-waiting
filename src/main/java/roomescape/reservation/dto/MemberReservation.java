d5629473e5ab1d5931ac954f305d191b2750b882
libienz <leekh10195@gmail.com>
2024-05-22 02:29:01 +0900

    feat: 클라이언트 코드 예약 대기 삭제 api 연동 구현

--- src/main/java/roomescape/reservation/dto/MemberReservation.java
+++ src/main/java/roomescape/reservation/dto/MemberReservation.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 import roomescape.reservation.domain.Reservation;
 
 public record MemberReservation(
-        Long reservationId,
+        Long id,
         String themeName,
         LocalDate date,
         LocalTime time,
