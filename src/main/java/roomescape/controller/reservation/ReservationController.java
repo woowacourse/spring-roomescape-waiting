3bcc5cdc1721f585d5cf97605a9b9d01ccef54d3
reddevilmidzy <midzy0228@gmail.com>
2024-05-21 11:10:21 +0900

    feat: 예약 대기 취소 기능 구현

--- src/main/java/roomescape/controller/reservation/ReservationController.java
+++ src/main/java/roomescape/controller/reservation/ReservationController.java
@@ -2,7 +2,9 @@ package roomescape.controller.reservation;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,5 +72,12 @@ public class ReservationController {
         return filter.stream()
                 .map(ReservationResponse::from)
                 .toList();
+    }
+
+    @DeleteMapping("/wait/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") final long id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent()
+                .build();
     }
 }
