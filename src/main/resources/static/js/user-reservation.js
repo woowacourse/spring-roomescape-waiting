41b8998b1014b4f818eb29ffbc1725b07ff5e0ac
DobbyMac <kimdobby@g.skku.edu>
2024-05-22 17:55:16 +0900

    feat(view): 예약 대기 사용자 버튼 추가

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   document.getElementById('reserve-button').addEventListener('click', onReservationButtonClick);
+  document.getElementById('wait-button').addEventListener('click', onWaitButtonClick);
 });
 
 function renderTheme(themes) {
@@ -45,11 +46,6 @@ function renderTheme(themes) {
   themes.forEach(theme => {
     const name = theme.name;
     const themeId = theme.id;
-    /*
-    TODO: [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-          createSlot('theme', theme name, theme id) 형태로 호출
-    */
     themeSlots.appendChild(createSlot('theme', name, themeId));
   });
 }
@@ -61,9 +57,6 @@ function createSlot(type, text, id, booked) {
   div.setAttribute('data-' + type + '-id', id);
   if (type === 'time') {
     div.setAttribute('data-time-booked', booked);
-    if (booked) {
-      div.classList.add('disabled');
-    }
   }
   return div;
 }
@@ -94,10 +87,6 @@ function checkDateAndTheme() {
 }
 
 function fetchAvailableTimes(date, themeId) {
-  /*
-  TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
-        요청 포맷에 맞게 설정
-  */
   fetch(`${RESERVATION_API_ENDPOINT}/times/${themeId}?date=${date}`, { // 예약 가능 시간 조회 API endpoint
     method: 'GET',
     headers: {
@@ -126,10 +115,6 @@ function renderAvailableTimes(times) {
     return;
   }
   times.forEach(time => {
-    /*
-    TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-    */
     const startAt = time.startAt;
     const timeId = time.timeId;
     const alreadyBooked = time.alreadyBooked;
@@ -144,18 +129,22 @@ function checkDateAndThemeAndTime() {
   const selectedThemeElement = document.querySelector('.theme-slot.active');
   const selectedTimeElement = document.querySelector('.time-slot.active');
   const reserveButton = document.getElementById("reserve-button");
+  const waitButton = document.getElementById("wait-button");
 
   if (selectedDate && selectedThemeElement && selectedTimeElement) {
     if (selectedTimeElement.getAttribute('data-time-booked') === 'true') {
       // 선택된 시간이 이미 예약된 경우
       reserveButton.classList.add("disabled");
+      waitButton.classList.remove("disabled"); // 예약 대기 버튼 활성화
     } else {
       // 선택된 시간이 예약 가능한 경우
       reserveButton.classList.remove("disabled");
+      waitButton.classList.add("disabled"); // 예약 대기 버튼 비활성화
     }
   } else {
     // 날짜, 테마, 시간 중 하나라도 선택되지 않은 경우
     reserveButton.classList.add("disabled");
+    waitButton.classList.add("disabled");
   }
 }
 
@@ -165,11 +154,6 @@ function onReservationButtonClick() {
   const selectedTimeId = document.querySelector('.time-slot.active')?.getAttribute('data-time-id');
 
   if (selectedDate && selectedThemeId && selectedTimeId) {
-
-    /*
-          [5단계] 예약 생성 기능 변경 - 사용자
-          request 명세에 맞게 설정
-    */
     const reservationData = {
       date: selectedDate,
       themeId: selectedThemeId,
@@ -202,6 +186,45 @@ function onReservationButtonClick() {
         });
   } else {
     alert("Please select a date, theme, and time before making a reservation.");
+  }
+}
+
+function onWaitButtonClick() {
+  const selectedDate = document.getElementById("datepicker").value;
+  const selectedThemeId = document.querySelector('.theme-slot.active')?.getAttribute('data-theme-id');
+  const selectedTimeId = document.querySelector('.time-slot.active')?.getAttribute('data-time-id');
+
+  if (selectedDate && selectedThemeId && selectedTimeId) {
+    const reservationData = {
+      date: selectedDate,
+      theme: selectedThemeId,
+      time: selectedTimeId
+    };
+
+    /*
+    TODO: [3단계] 예약 대기 생성 요청 API 호출
+     */
+    fetch('', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reservationData)
+    })
+        .then(response => {
+          if (!response.ok) throw new Error('Reservation waiting failed');
+          return response.json();
+        })
+        .then(data => {
+          alert('Reservation waiting successful!');
+          window.location.href = "/";
+        })
+        .catch(error => {
+          alert("An error occurred while making the reservation waiting.");
+          console.error(error);
+        });
+  } else {
+    alert("Please select a date, theme, and time before making a reservation waiting.");
   }
 }
 
