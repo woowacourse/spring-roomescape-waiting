b98de6638642cd1f9e7570fdcdfccfc28db2c5e0
MingyeomKim <mingyum119@naver.com>
2024-05-22 16:28:21 +0900

    style: 사용하지 않은 자바스크립트 코드 삭제

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -29,30 +29,8 @@ function render(data) {
 
         const actionCell = row.insertCell(row.cells.length);
 
-        /*
-        TODO: [4단계] 예약 대기 관리 기능
-              예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
-         */
-        // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
         actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
-}
-
-function approve(event) {
-    const row = event.target.closest('tr');
-    const id = row.cells[0].textContent;
-
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 승인 API 호출
-     */
-    const endpoint = '' + id;
-    return fetch(endpoint, {
-        method: ''
-    }).then(response => {
-        if (response.status === 200) return;
-        throw new Error('Delete failed');
-    }).then(() => location.reload());
 }
 
 function deny(event) {
