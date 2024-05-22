b78b55136aebe00deb5467f63d3218410a3c12d6
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-22 19:10:54 +0900

    build : 샘플용 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member (name, email, password, role) VALUES
+                                                     ('조이썬', 'joyson5582@example.com', 'password1234', 'USER'),
+                                                     ('어드민', 'admin@email.com', 'password1234', 'ADMIN');
