e10a79bb10db0078af2fe3d95d61ed9b9ceffaae
robinjoon <robin980108@naver.com>
2024-05-22 14:21:37 +0900

    docs: 필요없는 Todo 삭제

--- src/main/java/roomescape/dto/ReservationWaitingResponse.java
+++ src/main/java/roomescape/dto/ReservationWaitingResponse.java
@@ -2,7 +2,6 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 
-//Todo DTO 분리?
 public record ReservationWaitingResponse(long id, String name, LocalDate date, ReservationTimeResponse time,
                                          ThemeResponse theme, Integer priority) {
 }
