a5d34ec879cfb0887af961ec10faa20f1234cf46
MingyeomKim <mingyum119@naver.com>
2024-05-22 15:56:30 +0900

    test: 예약 대기 등록 서비스 기능

--- src/test/java/roomescape/reservation/service/ReservationServiceTest.java
+++ src/test/java/roomescape/reservation/service/ReservationServiceTest.java
@@ -10,11 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.member.domain.Member;
 import roomescape.member.repository.MemberRepository;
 import roomescape.reservation.controller.dto.request.ReservationSaveRequest;
@@ -23,10 +26,11 @@ import roomescape.reservation.controller.dto.response.ReservationDeleteResponse;
 import roomescape.reservation.controller.dto.response.ReservationResponse;
 import roomescape.reservation.controller.dto.response.SelectableTimeResponse;
 import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.domain.Status;
 import roomescape.reservation.repository.ReservationRepository;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@Transactional
 class ReservationServiceTest {
     @Autowired
     private ReservationService reservationService;
@@ -72,10 +76,12 @@ class ReservationServiceTest {
 
         // then
         assertNotNull(responses);
-        assertEquals(3, responses.size());
+        assertEquals(5, responses.size());
         assertThat(responses.get(0).date()).isEqualTo(LocalDate.parse("2024-12-12"));
         assertThat(responses.get(1).date()).isEqualTo(LocalDate.parse("2024-12-23"));
         assertThat(responses.get(2).date()).isEqualTo(LocalDate.parse("2024-12-25"));
+        assertThat(responses.get(3).date()).isEqualTo(LocalDate.parse("2024-12-23"));
+        assertThat(responses.get(4).date()).isEqualTo(LocalDate.parse("2024-12-23"));
     }
 
     @DisplayName("선택 가능한 시간을 조회한다.")
@@ -107,9 +113,11 @@ class ReservationServiceTest {
 
         // then
         assertNotNull(reservations);
-        assertEquals(2, reservations.size());
+        assertEquals(4, reservations.size());
         assertThat(reservations.get(0).getDate()).isEqualTo(LocalDate.parse("2024-12-12"));
         assertThat(reservations.get(1).getDate()).isEqualTo(LocalDate.parse("2024-12-23"));
+        assertThat(reservations.get(2).getDate()).isEqualTo(LocalDate.parse("2024-12-23"));
+        assertThat(reservations.get(3).getDate()).isEqualTo(LocalDate.parse("2024-12-23"));
     }
 
     @DisplayName("회원 ID로 예약을 조회한다.")
@@ -144,5 +152,45 @@ class ReservationServiceTest {
 
         // when & then
         assertThrows(NoSuchElementException.class, () -> reservationService.delete(reservationId));
+    }
+
+    @DisplayName("예약 대기 등록에 성공한다.")
+    @Test
+    void registerWaitingSuccess() {
+        // given
+        Member member = memberRepository.save(Member.of("anna", "brown@gmail.com", "password", "MEMBER"));
+        ReservationSaveRequest saveRequest = new ReservationSaveRequest(1L, LocalDate.parse("2024-12-24"), 2L);
+
+        // when
+        ReservationResponse reservationResponse = reservationService.registerWaiting(saveRequest, member);
+
+        // then
+        Optional<Reservation> reservation = reservationRepository.findById(reservationResponse.id());
+        assertTrue(reservation.isPresent());
+        Assertions.assertThat(reservation.get().getStatus()).isEqualTo(Status.PENDING);
+    }
+
+    @DisplayName("이미 예약 대기 등록을 한 사용자이기 때문에 예약 대기 등록에 실패한다.")
+    @Test
+    void registerWaitingFailureWhenAlreadyRegistered() {
+        // given
+        Member member = memberRepository.findById(2L).get();
+        ReservationSaveRequest saveRequest = new ReservationSaveRequest(3L, LocalDate.parse("2024-12-23"), 2L);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> reservationService.registerWaiting(saveRequest, member))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이미 예약을 한 사용자이기 때문에 예약 대기 등록에 실패한다.")
+    @Test
+    void registerWaitingFailureWhenAlreadyReserved() {
+        // given
+        Member member = memberRepository.findById(1L).get();
+        ReservationSaveRequest saveRequest = new ReservationSaveRequest(1L, LocalDate.parse("2024-12-12"), 1L);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> reservationService.registerWaiting(saveRequest, member))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
