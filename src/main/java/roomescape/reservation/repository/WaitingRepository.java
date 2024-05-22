5e12e2515702b6bf72f7046469763ff584cfd77d
haiseong <wjdgotjd9908@gmail.com>
2024-05-22 20:02:28 +0900

    refactor: 쿼리 개선

--- src/main/java/roomescape/reservation/repository/WaitingRepository.java
+++ src/main/java/roomescape/reservation/repository/WaitingRepository.java
@@ -10,22 +10,21 @@ import roomescape.reservation.model.Waiting;
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
     boolean existsByDateAndReservationTimeIdAndThemeId(LocalDate date, Long timeId, Long themeId);
 
-    boolean existsByDateAndReservationTimeIdAndThemeIdAndMemberId(LocalDate date, Long timeId, Long themeId, Long memberId);
-
-    List<Waiting> findAllByMemberId(Long memberId);
+    boolean existsByDateAndReservationTimeIdAndThemeIdAndMemberId(LocalDate date, Long timeId, Long themeId,
+                                                                  Long memberId);
 
     @Query("""
                 SELECT new roomescape.member.dto.response.FindWaitingResponse(
                     myWaiting.id AS waitingId,
-                    w.theme.name AS theme,
-                    w.date AS date,
-                    w.reservationTime.startAt AS time,
-                    COUNT(1) AS waitingNumber
+                    myWaiting.theme.name AS theme,
+                    myWaiting.date AS date,
+                    myWaiting.reservationTime.startAt AS time,
+                    COUNT(otherWaiting.id) AS waitingNumber
                 )
-                FROM Waiting w
-                JOIN Waiting myWaiting ON w.date = myWaiting.date AND w.reservationTime = myWaiting.reservationTime AND w.theme = myWaiting.theme
-                WHERE myWaiting.member.id = :memberId AND w.id <= myWaiting.id
-                GROUP BY w.date, w.reservationTime, w.theme
+                FROM Waiting otherWaiting
+                JOIN Waiting myWaiting ON otherWaiting.date = myWaiting.date AND otherWaiting.reservationTime = myWaiting.reservationTime AND otherWaiting.theme = myWaiting.theme
+                WHERE myWaiting.member.id = :memberId AND otherWaiting.id <= myWaiting.id
+                GROUP BY otherWaiting.date, otherWaiting.reservationTime, otherWaiting.theme
             """)
     List<FindWaitingResponse> findAllWaitingResponses(Long memberId);
 }
