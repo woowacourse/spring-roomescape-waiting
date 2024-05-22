a2e087a24717ad269d653e44afe0b76991038173
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-22 17:35:31 +0900

    feat(AdminPageController) : 예약 대기 관리 페이지 응답 기능 구현
    js,html 반영

---
+++ src/main/resources/static/js/waiting.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /*
+    TODO: [4단계] 예약 대기 관리 기능
+          예약 대기 목록 조회 endpoint 설정
+     */
+    fetch('/admin/waitings') // 내 예약 목록 조회 API 호출
+        .then(response => {
+            if (response.status === 200) return response.json();
+            throw new Error('Read failed');
+        })
+        .then(data => data.data)
+        .then(render)
+        .catch(error => console.error('Error fetching reservations:', error));
+});
+
+function render(data) {
+    const tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    data.forEach(item => {
+        const row = tableBody.insertRow();
+
+        /*
+        TODO: [4단계] 예약 대기 관리 기능
+              예약 대기 목록 조회 response 명세에 맞춰 값 설정
+         */
+
+        row.insertCell(0).textContent = item.id;
+        row.insertCell(1).textContent = item.member.name;
+        row.insertCell(2).textContent = item.theme.name;
+        row.insertCell(3).textContent = item.date;
+        row.insertCell(4).textContent = item.time.startAt;
+
+        const actionCell = row.insertCell(row.cells.length);
+
+ //       actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
+        actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
+    });
+}
+
+function deny(event) {
+    const row = event.target.closest('tr');
+    const id = row.cells[0].textContent;
+
+    return fetch('/admin/waiting/'+id, {
+        method: 'DELETE'
+    }).then(response => {
+        if (response.status === 204) return;
+        throw new Error('Delete failed');
+    }).then(() => location.reload());
+}
+
+function createActionButton(label, className, eventListener) {
+    const button = document.createElement('button');
+    button.textContent = label;
+    button.classList.add('btn', className, 'mr-2');
+    button.addEventListener('click', eventListener);
+    return button;
+}
