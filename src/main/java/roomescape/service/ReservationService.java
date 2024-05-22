07f66e519fdf3a51578eb4890d4c6178f996723e
3juhwan <13selfesteem91@naver.com>
2024-05-22 17:37:11 +0900

    feat: 멤버의 예약 삭제 기능 추가

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -135,15 +135,25 @@ public class ReservationService {
     }
 
     @Transactional
-    public void cancelReservation(Long reservationId) {
+    public void cancelReservation(Long reservationId, MemberInfo memberInfo) {
         Reservation reservation = reservationRepository.getById(reservationId);
-        rejectAlreadyCanceled(reservation);
+        rejectUnauthorizedCancel(memberInfo, reservation);
         reservation.cancel();
     }
 
-    private void rejectAlreadyCanceled(Reservation reservation) {
-        if (reservation.isCanceled()) {
-            throw new CancelReservationException("이미 취소된 예약입니다.");
+    private void rejectUnauthorizedCancel(MemberInfo memberInfo, Reservation reservation) {
+        if (reservation.isNotOwner(memberInfo.id())) {
+            throw new CancelReservationException("다른 회원의 예약을 취소할 수 없습니다.");
         }
+        if (reservation.isReserved()) {
+            throw new CancelReservationException("예약 취소는 어드민만 할 수 있습니다.");
+        }
+    }
+
+    @Transactional
+    public void cancelReservation(Long reservationId) {
+        Reservation reservation = reservationRepository.getById(reservationId);
+        rejectIfAlreadyCanceled(reservation);
+        reservation.cancel();
     }
 }
