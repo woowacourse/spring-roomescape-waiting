9bfb182209526c11282d2eb7a22c26bdfbca7875
Eugene Jang <kyunellroll@gmail.com>
2024-05-23 11:33:53 +0900

    docs(js) : 완료 TODO 삭제

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -1,8 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  /*
-  TODO: [2단계] 내 예약 목록 조회 기능
-        endpoint 설정
-   */
+
   fetch('/members/reservations') // 내 예약 목록 조회 API 호출
       .then(response => {
         if (response.status === 200) return response.json();
@@ -18,11 +15,6 @@ function render(data) {
 
   data.forEach(item => {
     const row = tableBody.insertRow();
-
-    /*
-    TODO: [2단계] 내 예약 목록 조회 기능
-          response 명세에 맞춰 값 설정
-     */
     const theme = item.themeName;
     const date = item.date;
     const time = item.time;
@@ -33,9 +25,6 @@ function render(data) {
     row.insertCell(2).textContent = time;
     row.insertCell(3).textContent = status;
 
-    /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
-     */
     if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
       const cancelCell = row.insertCell(4);
       const cancelButton = document.createElement('button');
@@ -52,9 +41,6 @@ function render(data) {
 }
 
 function requestDeleteWaiting(id) {
-  /*
-  TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
-   */
   const endpoint = `/reservations/waiting/${id}`;
   return fetch(endpoint, {
     method: 'DELETE'
--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -37,11 +37,7 @@ function renderTheme(themes) {
   themes.forEach(theme => {
     const name = theme.name;
     const themeId = theme.id;
-    /*
-    TODO: [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-          createSlot('theme', theme name, theme id) 형태로 호출
-    */
+
     themeSlots.appendChild(createSlot('theme', name, themeId));
   });
 }
@@ -82,10 +78,6 @@ function checkDateAndTheme() {
 }
 
 function fetchAvailableTimes(date, themeId) {
-  /*
-  TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
-        요청 포맷에 맞게 설정
-  */
   fetch(`${RESERVATION_API_ENDPOINT}/${themeId}?date=${date}`, { // 예약 가능 시간 조회 API endpoint
     method: 'GET',
     headers: {
@@ -113,10 +105,6 @@ function renderAvailableTimes(times) {
     return;
   }
   times.forEach(time => {
-    /*
-    TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-    */
     const startAt = time.startAt;
     const timeId = time.timeId;
     const alreadyBooked = time.alreadyBooked;
@@ -155,7 +143,7 @@ function onReservationButtonClick() {
   const selectedTimeId = document.querySelector('.time-slot.active')?.getAttribute('data-time-id');
   if (selectedDate && selectedThemeId && selectedTimeId) {
     /*
-    TODO: [3단계] 사용자 예약 - 예약 요청 API 호출
+    TODO:
           [5단계] 예약 생성 기능 변경 - 사용자
           request 명세에 맞게 설정
     */
@@ -202,9 +190,6 @@ function onWaitButtonClick() {
       timeId: selectedTimeId
     };
 
-    /*
-    TODO: [3단계] 예약 대기 생성 요청 API 호출
-     */
     fetch(`${RESERVATION_API_ENDPOINT}/waiting`, {
       method: 'POST',
       headers: {
