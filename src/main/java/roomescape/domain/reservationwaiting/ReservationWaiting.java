79d9ba602660463a4576dc4b22a956bbd6b090f6
Nam Gi Beom <rlqja0523@naver.com>
2024-05-23 13:37:13 +0900

    feat: Lazy, Unique 제약 조건, 멤버 비교 기능 추가

--- src/main/java/roomescape/domain/reservationwaiting/ReservationWaiting.java
+++ src/main/java/roomescape/domain/reservationwaiting/ReservationWaiting.java
@@ -1,27 +1,30 @@
 package roomescape.domain.reservationwaiting;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import roomescape.domain.member.Member;
 import roomescape.domain.reservation.Reservation;
 
 @Entity
-// todo unique constraint 추가
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"reservation_id", "member_id"}))
 public class ReservationWaiting {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne()
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Reservation reservation;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Member member;
 
@@ -36,6 +39,10 @@ public class ReservationWaiting {
         this.id = id;
         this.reservation = reservation;
         this.member = member;
+    }
+
+    public boolean isSameMember(Member member) {
+        return this.member.equals(member);
     }
 
     public Long getId() {
