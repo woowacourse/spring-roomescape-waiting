7151e77f93646e091ea8cae3654b57650608ab36
hangillee <skfcb10@naver.com>
2024-05-23 18:10:05 +0900

    test: 예약 대기 주인 판별 기능 테스트 추가

--- src/test/java/roomescape/core/domain/WaitingTest.java
+++ src/test/java/roomescape/core/domain/WaitingTest.java
@@ -1,5 +1,6 @@
 package roomescape.core.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,5 +20,15 @@ class WaitingTest {
         assertThatThrownBy(() -> new Waiting(member, date, time, theme))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Waiting.DATE_FORMAT_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("예약 대기의 주인과 멤버가 일치하는지 검증한다.")
+    void validateMember() {
+        final Member waitingOwner = TestFixture.getAdmin();
+        final Member anotherMember = TestFixture.getMember();
+        final Waiting waiting = new Waiting(waitingOwner, TestFixture.getTodayDate(), time, theme);
+
+        assertThat(waiting.isNotOwner(anotherMember)).isTrue();
     }
 }
