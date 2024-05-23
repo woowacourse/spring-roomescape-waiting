6bd938732748a085427dae2295097f2c11d9fb38
tsulocalize <tsulocalize@gmail.com>
2024-05-23 20:10:52 +0900

    test: 불필요한 테스트 설정 제거

--- src/test/java/roomescape/config/P6spyEventListener.java
+++ src/test/java/roomescape/config/P6spyEventListener.java
@@ -3,14 +3,11 @@ package roomescape.config;
 import com.p6spy.engine.common.ConnectionInformation;
 import com.p6spy.engine.event.JdbcEventListener;
 import com.p6spy.engine.spy.P6SpyOptions;
-import com.p6spy.engine.spy.appender.Slf4JLogger;
-import com.p6spy.engine.spy.appender.StdoutLogger;
 import java.sql.SQLException;
 
 public class P6spyEventListener extends JdbcEventListener {
     @Override
     public void onAfterGetConnection(ConnectionInformation connectionInformation, SQLException e) {
         P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spyFormatter.class.getName());
-        P6SpyOptions.getActiveInstance().setAppender(Slf4JLogger.class.getName());
     }
 }
