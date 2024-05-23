bed25c55eefd11fe07fff1961297de245ab42da1
lemone <lejinhy@gmail.com>
2024-05-23 21:58:32 +0900

    feat(AdminController, AdminViewController): 예약 대기 조회 기능 추가

--- src/main/java/roomescape/admin/controller/AdminController.java
+++ src/main/java/roomescape/admin/controller/AdminController.java
@@ -14,15 +14,19 @@ import roomescape.auth.annotation.Auth;
 import roomescape.member.domain.MemberRole;
 import roomescape.registration.reservation.dto.ReservationResponse;
 import roomescape.registration.reservation.service.ReservationService;
+import roomescape.registration.waiting.dto.WaitingResponse;
+import roomescape.registration.waiting.service.WaitingService;
 
 @RestController
 @Auth(roles = MemberRole.ADMIN)
 public class AdminController {
 
     private final ReservationService reservationService;
+    private final WaitingService waitingService;
 
-    public AdminController(ReservationService reservationService) {
+    public AdminController(ReservationService reservationService, WaitingService waitingService) {
         this.reservationService = reservationService;
+        this.waitingService = waitingService;
     }
 
     @PostMapping("/admin/reservations")
@@ -37,5 +41,10 @@ public class AdminController {
     public List<ReservationResponse> reservationFilteredList(
             @ModelAttribute ReservationFilterRequest reservationFilterRequest) {
         return reservationService.findFilteredReservations(reservationFilterRequest);
+    }
+
+    @GetMapping("/admin/waitings")
+    public List<WaitingResponse> waitingList() {
+        return waitingService.findWaitings();
     }
 }
