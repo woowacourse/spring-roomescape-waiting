b54e6f7eb4a9802f488740e886a0473c4f1eae06
pricelees <priceelees@gmail.com>
2024-05-23 15:59:47 +0900

    fix: 예약 취소가 되지 않는 문제 해결
    - API 엔드포인트 수정 및 컨트롤러에 메서드 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,7 +3,9 @@ package roomescape.controller;
 import jakarta.validation.Valid;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,11 @@ public class ReservationController {
         ReservationCreateRequest reservationCreateRequest = ReservationCreateRequest.from(memberRequest, authInfo.id());
         ReservationResponse reservationResponse = reservationService.wait(reservationCreateRequest);
         return ResponseEntity.created(URI.create("/reservations" + reservationResponse.id())).body(reservationResponse);
+    }
+
+    @DeleteMapping("/reservations/waiting/{id}")
+    public ResponseEntity<Void> cancelWaiting(@PathVariable Long id) {
+        reservationService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -53,7 +53,7 @@ function requestDeleteWaiting(id) {
   /*
   TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
    */
-  const endpoint = '/reservations/' + id;
+  const endpoint = '/reservations/waiting/' + id;
   return fetch(endpoint, {
     method: 'DELETE'
   }).then(response => {
