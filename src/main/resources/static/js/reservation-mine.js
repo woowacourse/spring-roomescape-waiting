2e890af36504b3e63562d1acdfb92ec4c24ca316
seunghye218 <seunghye@student.42seoul.kr>
2024-05-23 22:38:32 +0900

    chore: 이전 단계 주석 제거

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*
-    TODO: [2단계] 내 예약 목록 조회 기능
-          endpoint 설정
-     */
     fetch('/reservations/me') // 내 예약 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
@@ -19,10 +15,6 @@ function render(data) {
     data.forEach(item => {
         const row = tableBody.insertRow();
 
-        /*
-        TODO: [2단계] 내 예약 목록 조회 기능
-              response 명세에 맞춰 값 설정
-         */
         const theme = item.theme;
         const date = item.date;
         const time = item.time;
