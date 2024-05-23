37667e7713c54a875897ac61e62436c1b4259447
robinjoon <robin980108@naver.com>
2024-05-23 16:48:51 +0900

    fix: ReservationWaiting 에서 N + 1 문제 수정
    - https://github.com/woowacourse/spring-roomescape-waiting/pull/101#discussion_r1610225388 반영

--- src/main/java/roomescape/domain/ReservationWaiting.java
+++ src/main/java/roomescape/domain/ReservationWaiting.java
@@ -5,6 +5,7 @@ import static roomescape.exception.ExceptionType.WAITING_WITHOUT_MEMBER;
 import static roomescape.exception.ExceptionType.WAITING_WITHOUT_RESERVATION;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,9 +21,9 @@ public class ReservationWaiting extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Reservation reservation;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member waitingMember;
 
     public ReservationWaiting(Reservation reservation, Member waitingMember) {
