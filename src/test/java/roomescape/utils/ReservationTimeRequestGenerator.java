13b8b00845b86d6221985e654a9b9f57e2417139
hangillee <skfcb10@naver.com>
2024-05-23 13:32:58 +0900

    refactor: 다른 유틸 클래스를 활용하도록 리팩토링

--- src/test/java/roomescape/utils/ReservationTimeRequestGenerator.java
+++ src/test/java/roomescape/utils/ReservationTimeRequestGenerator.java
@@ -4,23 +4,13 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import org.springframework.http.MediaType;
-import roomescape.core.dto.auth.TokenRequest;
 import roomescape.core.dto.reservationtime.ReservationTimeRequest;
 
 public class ReservationTimeRequestGenerator {
-    private static final String EMAIL = "test@email.com";
-    private static final String PASSWORD = "password";
     private static final String ACCESS_TOKEN;
 
     static {
-        ACCESS_TOKEN = RestAssured
-                .given().log().all()
-                .body(new TokenRequest(EMAIL, PASSWORD))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/login")
-                .then().log().cookies().extract().cookie("token");
+        ACCESS_TOKEN = AccessTokenGenerator.generate();
     }
 
     public static void generateOneMinuteAfter() {
