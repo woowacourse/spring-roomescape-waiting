d038b3362b6935ba5bdbebb335610fb9ca2f4623
donghoony <aru0504@naver.com>
2024-05-23 21:06:32 +0900

    refactor: `truncate.sql`에서 `id` 시작값 초기화를 통한 테스트 격리 강화

--- src/test/resources/truncate.sql
+++ src/test/resources/truncate.sql
@@ -4,4 +4,9 @@ truncate table reservation;
 truncate table theme;
 truncate table reservation_time;
 truncate table member;
+
+alter table reservation alter column id restart with 1;
+alter table theme alter column id restart with 1;
+alter table reservation_time alter column id restart with 1;
+alter table member alter column id restart with 1;
 set referential_integrity true;
