972e4c4d28025f5b8ba4716ec20c64a8b2fff286
pricelees <priceelees@gmail.com>
2024-05-23 16:31:07 +0900

    feat: 예약 대기 확인용 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,9 +14,15 @@ insert into member(name, email, password, role) values('2호', '2@2.2', '2', 'ME
 insert into member(name, email, password, role) values('3호', '3@3.3', '3', 'MEMBER');
 insert into member(name, email, password, role) values('4호', '4@4.4', '4', 'MEMBER');
 
+-- 예약
 insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(1, DATEADD('DAY', -1, CURRENT_DATE()) - 1 , 1, 1, 'CONFIRMED', CURRENT_TIMESTAMP());
 insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(2, DATEADD('DAY', -2, CURRENT_DATE()) -2 , 3, 2, 'CONFIRMED', CURRENT_TIMESTAMP());
 insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(3, DATEADD('DAY', -3, CURRENT_DATE()), 2, 2, 'CONFIRMED', CURRENT_TIMESTAMP());
 insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(4, DATEADD('DAY', -4, CURRENT_DATE()), 1, 2, 'CONFIRMED', CURRENT_TIMESTAMP());
 insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(5, DATEADD('DAY', -5, CURRENT_DATE()), 1, 3, 'CONFIRMED', CURRENT_TIMESTAMP());
+insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(2, DATEADD('DAY', 7, CURRENT_DATE()), 2, 4, 'CONFIRMED', CURRENT_TIMESTAMP());
 
+-- 예약 대기
+insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(3, DATEADD('DAY', 7, CURRENT_DATE()), 2, 4, 'WAITING', CURRENT_TIMESTAMP());
+insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(4, DATEADD('DAY', 7, CURRENT_DATE()), 2, 4, 'WAITING', CURRENT_TIMESTAMP());
+insert into reservation(member_id, date, time_id, theme_id, status, created_at) values(5, DATEADD('DAY', 7, CURRENT_DATE()), 2, 4, 'WAITING', CURRENT_TIMESTAMP());
