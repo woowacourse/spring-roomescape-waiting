0148096894eb1561751a98c4143f61cecf4c7e8c
Nam Gi Beom <rlqja0523@naver.com>
2024-05-25 02:29:11 +0900

    feat: auditing import

--- src/test/java/roomescape/domain/BaseRepositoryTest.java
+++ src/test/java/roomescape/domain/BaseRepositoryTest.java
@@ -3,12 +3,15 @@ package roomescape.domain;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import roomescape.config.JpaAuditingConfig;
 
 @DataJpaTest
+@Import(JpaAuditingConfig.class)
 public abstract class BaseRepositoryTest {
 
     @PersistenceContext
-    protected EntityManager em;
+    private EntityManager em;
 
     protected <T> T save(T entity) {
         em.persist(entity);
