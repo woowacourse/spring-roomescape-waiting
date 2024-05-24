4bce19104db53e1731e4fa7bc1d802a81022d7c4
pricelees <priceelees@gmail.com>
2024-05-24 22:35:04 +0900

    refactor: ObjectMapper 추가에 의한 DTO 에서의 시간 JsonFormat 제거

--- src/main/java/roomescape/service/dto/response/ReservationAvailabilityTimeResponse.java
+++ src/main/java/roomescape/service/dto/response/ReservationAvailabilityTimeResponse.java
@@ -1,6 +1,5 @@
 package roomescape.service.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import roomescape.domain.ReservationTime;
@@ -9,7 +8,6 @@ public record ReservationAvailabilityTimeResponse(
         @NotNull
         Long id,
         @NotNull
-        @JsonFormat(pattern = "HH:mm")
         LocalTime startAt,
         boolean booked) {
 
