8dd0eeb5e8ab7c4ca16b6903154cc90547f25f54
hyxrxn <gpfls981220@gmail.com>
2024-05-24 18:40:23 +0900

    refactor(data): 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ INSERT INTO theme (name, description, thumbnail) VALUES ('에버', '공포', 'ht
 INSERT INTO theme (name, description, thumbnail) VALUES ('배키', '스릴러', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 INSERT INTO member (name, email, password, role) VALUES ('에버', 'treeboss@gmail.com', 'treeboss123!', 'USER');
+INSERT INTO member (name, email, password, role) VALUES ('아토', 'atto@gmail.com', 'atto123!', 'USER');
 INSERT INTO member (name, email, password, role) VALUES ('관리자', 'admin@gmail.com', 'admin123!', 'ADMIN');
 
 INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-08', 1, 1, 1);
