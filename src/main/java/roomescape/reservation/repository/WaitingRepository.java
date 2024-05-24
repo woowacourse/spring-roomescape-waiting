5e417ec88a7e9ec0412957f6dddaa7a8186b7296
devhoya97 <diajbh123@gmail.com>
2024-05-24 18:16:53 +0900

    refactor: 1+N 문제를 해결할 수 있는 쿼리로 변경

--- src/main/java/roomescape/reservation/repository/WaitingRepository.java
+++ src/main/java/roomescape/reservation/repository/WaitingRepository.java
@@ -28,16 +28,17 @@ public interface WaitingRepository extends ListCrudRepository<Waiting, Long> {
 
     @Query("""
             SELECT new roomescape.reservation.domain.WaitingWithRank(
-                w,
-                (SELECT COUNT(w2) + 1
-                 FROM Waiting w2
-                 WHERE w2.theme = w.theme
-                   AND w2.date = w.date
-                   AND w2.reservationTime = w.reservationTime
-                   AND w2.id < w.id)
+                w1,
+                COUNT(w2) + 1
             )
-            FROM Waiting w
-            WHERE w.member.id = :memberId
+            FROM Waiting w1
+            LEFT JOIN Waiting w2
+              ON w1.theme = w2.theme
+             AND w1.date = w2.date
+             AND w1.reservationTime = w2.reservationTime
+             AND w2.id < w1.id
+            WHERE w1.member.id = :memberId
+            GROUP BY w1
             """)
     List<WaitingWithRank> findWaitingsWithRankByMemberId(Long memberId);
 }
