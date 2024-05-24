25d1ce0a07dd7984f6d8a3241560582763f5ba3d
nayonsoso <panda0329@naver.com>
2024-05-24 16:39:06 +0900

    test: 예약 대기 요청 테스트 추가

--- src/test/java/roomescape/acceptance/member/ReservationAddAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/member/ReservationAddAcceptanceTest.java
@@ -3,13 +3,11 @@ package roomescape.acceptance.member;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.*;
 import org.springframework.http.HttpStatus;
 import roomescape.acceptance.BaseAcceptanceTest;
 import roomescape.controller.exception.CustomExceptionResponse;
+import roomescape.domain.ReservationStatus;
 import roomescape.dto.request.MemberReservationRequest;
 import roomescape.dto.response.ReservationResponse;
 
@@ -20,67 +18,70 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static roomescape.acceptance.Fixture.customerToken;
-import static roomescape.acceptance.PreInsertedData.THEME_1;
-import static roomescape.acceptance.PreInsertedData.TIME_10_O0;
+import static roomescape.acceptance.PreInsertedData.*;
 
-@DisplayName("고객이 예약을 추가한다.")
 class ReservationAddAcceptanceTest extends BaseAcceptanceTest {
 
-    @DisplayName("정상 작동")
-    @Test
-    void addReservation_success() {
-        MemberReservationRequest requestBody = getRequestBody(
-                LocalDate.parse("2099-01-11")
-        );
+    @DisplayName("고객이 예약을 추가한다.")
+    @Nested
+    class addReservation {
 
-        sendPostRequest(requestBody)
-                .statusCode(HttpStatus.CREATED.value())
-                .header("location", containsString("/reservations/"))
-                .extract().as(ReservationResponse.class);
+        @DisplayName("정상 작동 - 예약을 추가한다.")
+        @Test
+        void addReservation_success() {
+            MemberReservationRequest requestBody = makeReservationOn(
+                    LocalDate.parse("2099-01-11")
+            );
+
+            ReservationResponse response = sendPostRequest(requestBody)
+                    .statusCode(HttpStatus.CREATED.value())
+                    .header("location", containsString("/reservations/"))
+                    .extract().as(ReservationResponse.class);
+
+            assertAll(
+                    () -> assertThat(response.id()).isEqualTo(CUSTOMER_1.getId()),
+                    () -> assertThat(response.reservationStatus().isReserved()).isTrue()
+            );
+        }
+
+        @DisplayName("예외 발생 - 과거 시간에 대한 예약 추가한다.")
+        @Test
+        void addReservation_forPastTime_fail() {
+            MemberReservationRequest reservationForPast = makeReservationOn(
+                    LocalDate.now().minusDays(1)
+            );
+
+            CustomExceptionResponse response = sendPostRequest(reservationForPast)
+                    .statusCode(HttpStatus.BAD_REQUEST.value())
+                    .extract().as(CustomExceptionResponse.class);
+
+            assertAll(
+                    () -> assertThat(response.title()).contains("허용되지 않는 작업입니다."),
+                    () -> assertThat(response.detail()).contains("지나간 시간에 대한 예약은 할 수 없습니다.")
+            );
+        }
+
+        @DisplayName("이미 예약이 존재한다면, 예약 대기를 걸어둔다.")
+        @Test
+        void addReservationWaiting_success() {
+            MemberReservationRequest requestBody = new MemberReservationRequest(
+                    RESERVATION_CUSTOMER1_THEME2_240501_1100.getDate(),
+                    RESERVATION_CUSTOMER1_THEME2_240501_1100.getReservationTime().getId(),
+                    RESERVATION_CUSTOMER1_THEME2_240501_1100.getTheme().getId()
+            );
+
+            ReservationResponse response = sendPostRequest(requestBody)
+                    .statusCode(HttpStatus.OK.value())
+                    .extract().as(ReservationResponse.class);
+
+            assertAll(
+                    () -> assertThat(response.id()).isEqualTo(CUSTOMER_1.getId()),
+                    () -> assertThat(response.reservationStatus().isWaiting()).isTrue()
+            );
+        }
     }
 
-    @DisplayName("예외 발생 - 과거 시간에 대한 예약 추가한다.")
-    @Test
-    void addReservation_forPastTime_fail() {
-        MemberReservationRequest reservationForPast = getRequestBody(
-                LocalDate.now().minusDays(1)
-        );
-
-        CustomExceptionResponse response = sendPostRequest(reservationForPast)
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .extract().as(CustomExceptionResponse.class);
-
-        assertAll(
-                () -> assertThat(response.title()).contains("허용되지 않는 작업입니다."),
-                () -> assertThat(response.detail()).contains("지나간 시간에 대한 예약은 할 수 없습니다.")
-        );
-    }
-
-    @DisplayName("예외 발생 - 이미 있는 예약을 추가한다.")
-    @TestFactory
-    Stream<DynamicTest> addReservation_alreadyExist_fail() {
-        MemberReservationRequest requestBody = getRequestBody(
-                LocalDate.parse("2099-01-11")
-        );
-
-        return Stream.of(
-                DynamicTest.dynamicTest("예약을 추가한다", () -> sendPostRequest(requestBody)),
-
-                DynamicTest.dynamicTest("동일한 예약을 추가한다", () -> {
-                            CustomExceptionResponse response = sendPostRequest(requestBody)
-                                    .statusCode(HttpStatus.BAD_REQUEST.value())
-                                    .extract().as(CustomExceptionResponse.class);
-                            assertAll(
-                                    () -> assertThat(response.title()).contains("허용되지 않는 작업입니다."),
-                                    () -> assertThat(response.detail()).contains("예약이 이미 존재합니다.")
-                            );
-
-                        }
-                )
-        );
-    }
-
-    private MemberReservationRequest getRequestBody(LocalDate date) {
+    private MemberReservationRequest makeReservationOn(LocalDate date) {
         return new MemberReservationRequest(
                 date,
                 TIME_10_O0.getId(),
