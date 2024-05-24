bfb919898e158443212e65cfd33541c33b97fa24
Nam Gi Beom <rlqja0523@naver.com>
2024-05-25 03:39:07 +0900

    feat: MemberFixture admin, user 추가

--- src/test/java/roomescape/support/fixture/MemberFixture.java
+++ src/test/java/roomescape/support/fixture/MemberFixture.java
@@ -6,6 +6,10 @@ import roomescape.domain.member.Role;
 
 public class MemberFixture {
 
+    public static final Member ADMIN = new Member("admin@email.com", "password", "관리자", Role.ADMIN);
+
+    public static final Member USER = new Member("user@email.com", "password", "유저", Role.USER);
+
     public static Member create() {
         return create("prin@email.com");
     }
