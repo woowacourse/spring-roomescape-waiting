0fa2bb76333202f4b5137c5f472b83ccfa5ec6f0
juha <khabh@naver.com>
2024-05-24 16:45:29 +0900

    test: 가독성을 위해 테스트 수정

--- src/test/java/roomescape/service/ReservationAndReservationWaitingServiceTest.java
+++ src/test/java/roomescape/service/ReservationAndReservationWaitingServiceTest.java
@@ -1,5 +1,6 @@
 package roomescape.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static roomescape.Fixture.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -39,14 +39,23 @@ class ReservationAndReservationWaitingServiceTest {
     @Autowired
     private ReservationWaitingRepository reservationWaitingRepository;
 
+    private Member member;
+    private Theme theme;
+    private ReservationTime time;
+    private ReservationDate date;
+
+    @BeforeEach
+    void init() {
+        member = memberRepository.save(VALID_MEMBER);
+        theme = themeRepository.save(VALID_THEME);
+        time = reservationTimeRepository.save(VALID_RESERVATION_TIME);
+        date = new ReservationDate(LocalDate.now().plusDays(2).toString());
+    }
+
     @Test
     @DisplayName("예약 대기가 존재하지 않는 경우 예약을 삭제한다.")
     void deleteReservationIfNoWaiting() {
-        Member member = memberRepository.save(VALID_MEMBER);
-        Theme theme = themeRepository.save(VALID_THEME);
-        ReservationTime time = reservationTimeRepository.save(VALID_RESERVATION_TIME);
-        String date = LocalDate.now().plusDays(2).toString();
-        Reservation reservation = reservationRepository.save(new Reservation(member, new ReservationDate(date), time, theme));
+        Reservation reservation = reservationRepository.save(new Reservation(member, date, time, theme));
 
         reservationAndWaitingService.deleteReservation(reservation.getId());
 
@@ -56,30 +65,17 @@ class ReservationAndReservationWaitingServiceTest {
     @Test
     @DisplayName("예약 대기가 존재하는 경우, 우선순위가 높은 대기를 예약으로 변경한다.")
     void updateReservation() {
-        Member reservedMember = memberRepository.save(VALID_MEMBER);
-        Member waitingMember = memberRepository.save(new Member(new MemberName("감자"),
-                new MemberEmail("111@aaa.com"),
-                VALID_USER_PASSWORD,
-                MemberRole.USER));
-        Theme theme = themeRepository.save(VALID_THEME);
-        ReservationTime time = reservationTimeRepository.save(VALID_RESERVATION_TIME);
-        ReservationDate date = new ReservationDate(LocalDate.now().plusDays(2).toString());
+        Member waitingMember = memberRepository.save(new Member(new MemberName("감자"), new MemberEmail("111@aaa.com"), VALID_USER_PASSWORD, MemberRole.USER));
         LocalDateTime createdDateTime = LocalDateTime.now().minusMonths(2);
-        Reservation reservation = reservationRepository.save(new Reservation(reservedMember, date, time, theme));
+        Reservation reservation = reservationRepository.save(new Reservation(member, date, time, theme));
         ReservationWaiting waiting = reservationWaitingRepository.save(new ReservationWaiting(createdDateTime, waitingMember, date, time, theme));
 
         reservationAndWaitingService.deleteReservation(reservation.getId());
 
         Reservation updatedReservation = reservationRepository.findByDateAndTimeIdAndThemeId(
-                reservation.getDate(),
-                reservation.getTime().getId(),
-                reservation.getTheme().getId()).orElseThrow();
-        assertAll(
-                () -> assertThat(reservationWaitingRepository.findById(waiting.getId())).isEmpty(),
-                () -> assertThat(updatedReservation.getDate().getDate()).isEqualTo(date.getDate()),
-                () -> assertThat(updatedReservation.getMember()).isEqualTo(waiting.getMember()),
-                () -> assertThat(updatedReservation.getTheme().getId()).isEqualTo(theme.getId()),
-                () -> assertThat(updatedReservation.getTime().getId()).isEqualTo(time.getId())
-        );
+                        reservation.getDate(), reservation.getTime().getId(), reservation.getTheme().getId())
+                .orElseThrow();
+
+        assertThat(updatedReservation.getMember()).isEqualTo(waiting.getMember());
     }
 }
