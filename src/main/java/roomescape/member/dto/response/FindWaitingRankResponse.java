4a311dced0c0c6b751c5bed03b96e5f4a1cbcfb8
haiseong <wjdgotjd9908@gmail.com>
2024-05-25 15:33:53 +0900

    fix: 필요없는 메서드 삭제

--- src/main/java/roomescape/member/dto/response/FindWaitingRankResponse.java
+++ src/main/java/roomescape/member/dto/response/FindWaitingRankResponse.java
@@ -2,19 +2,10 @@ package roomescape.member.dto.response;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import roomescape.reservation.model.Waiting;
 
 public record FindWaitingRankResponse(Long waitingId,
                                       String theme,
                                       LocalDate date,
                                       LocalTime time,
                                       Long waitingNumber) {
-    public static FindWaitingRankResponse from(Waiting waiting) {
-        return new FindWaitingRankResponse(
-                waiting.getId(),
-                waiting.getTheme().getName(),
-                waiting.getDate(),
-                waiting.getReservationTime().getStartAt(),
-                1L);
-    }
 }
