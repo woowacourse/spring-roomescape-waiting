f59ac4ab9dae4ac5a0b2ee76354eed605e0066b4
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-25 12:43:47 +0900

    fix(ReservationRepository): 예약된 시간 찾는 sql문 수정으로 예약 시간 못불러오는 에러 해결

--- src/main/java/roomescape/reservation/repository/ReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -5,12 +5,20 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import roomescape.reservation.domain.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByOrderByDetailDateAsc();
 
+    @Query("""
+            SELECT r
+            FROM Reservation r
+            WHERE r.detail.theme.id = :themeId
+            AND r.detail.date = :date
+            """
+    )
     List<Reservation> findAllByDetailTheme_IdAndDetailDate(Long themeId, LocalDate date);
 
     List<Reservation> findAllByMember_Id(Long memberId);
