533240d02da81cdc24aa2e1dfa71d4dc1ecea7cf
3juhwan <13selfesteem91@naver.com>
2024-05-25 21:07:37 +0900

    fix: 데이터 초기화 sql 수정

--- src/test/resources/init/truncate.sql
+++ src/test/resources/init/truncate.sql
@@ -4,6 +4,7 @@ truncate table reservation_detail;
 truncate table reservation_time;
 truncate table theme;
 truncate table members;
+truncate table reservation;
 
 alter table reservation_detail
     alter column id restart with 1;
@@ -12,6 +13,8 @@ alter table reservation_time
 alter table theme
     alter column id restart with 1;
 alter table members
+    alter column id restart with 1;
+alter table reservation
     alter column id restart with 1;
 
 set referential_integrity true;
