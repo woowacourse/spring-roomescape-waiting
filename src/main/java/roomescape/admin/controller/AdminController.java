f4d2b4028d228c1a60f2601ce95faa4aa27294ec
lemone <lejinhy@gmail.com>
2024-05-25 22:05:38 +0900

    feat(AdminController): 예약 대기 승인 api 추가

--- src/main/java/roomescape/admin/controller/AdminController.java
+++ src/main/java/roomescape/admin/controller/AdminController.java
@@ -7,33 +7,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.admin.dto.AdminReservationRequest;
 import roomescape.admin.dto.ReservationFilterRequest;
 import roomescape.auth.annotation.Auth;
 import roomescape.member.domain.MemberRole;
-import roomescape.registration.reservation.dto.ReservationResponse;
-import roomescape.registration.reservation.service.ReservationService;
-import roomescape.registration.waiting.dto.WaitingResponse;
-import roomescape.registration.waiting.service.WaitingService;
+import roomescape.registration.domain.reservation.dto.ReservationResponse;
+import roomescape.registration.domain.reservation.service.ReservationService;
+import roomescape.registration.domain.waiting.dto.WaitingResponse;
+import roomescape.registration.domain.waiting.service.WaitingService;
+import roomescape.registration.service.RegistrationService;
 
 @RestController
 @Auth(roles = MemberRole.ADMIN)
+@RequestMapping("/admin")
 public class AdminController {
 
+    private final RegistrationService registrationService;
     private final ReservationService reservationService;
     private final WaitingService waitingService;
 
-    public AdminController(ReservationService reservationService, WaitingService waitingService) {
+    public AdminController(RegistrationService registrationService, ReservationService reservationService,
+                           WaitingService waitingService) {
+        this.registrationService = registrationService;
         this.reservationService = reservationService;
         this.waitingService = waitingService;
     }
 
-    @PostMapping("/admin/reservations")
+    @PostMapping("/reservations")
     public ResponseEntity<Void> reservationSave(@RequestBody AdminReservationRequest adminReservationRequest) {
         reservationService.addAdminReservation(adminReservationRequest);
 
@@ -41,20 +48,26 @@ public class AdminController {
                 .build();
     }
 
-    @GetMapping("/admin/reservations")
+    @GetMapping("/reservations")
     public List<ReservationResponse> reservationFilteredList(
             @ModelAttribute ReservationFilterRequest reservationFilterRequest) {
         return reservationService.findFilteredReservations(reservationFilterRequest);
     }
 
-    @GetMapping("/admin/waitings")
+    @GetMapping("/waitings")
     public List<WaitingResponse> waitingList() {
         return waitingService.findWaitings();
     }
 
-    @DeleteMapping("/admin/waitings/{waitingId}")
+    @DeleteMapping("/waitings/{waitingId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void waitingReject(@PathVariable long waitingId) {
         waitingService.removeWaiting(waitingId);
+    }
+
+    @PatchMapping("/waitings/{waitingId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void waitingApprove(@PathVariable long waitingId) {
+        registrationService.approveWaitingToReservation(waitingId);
     }
 }
