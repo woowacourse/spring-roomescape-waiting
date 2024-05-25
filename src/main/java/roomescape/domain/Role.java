f11db02e92ea60aa63a2b0a7da0f2b84d4443d91
Zeus6768 <akaps6768@gmail.com>
2024-05-25 15:50:13 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/roomescape/domain/Role.java
+++ src/main/java/roomescape/domain/Role.java
@@ -1,17 +1,10 @@
 package roomescape.domain;
 
-import java.util.Map;
-
 public enum Role {
 
     ADMIN,
     CUSTOMER,
     ;
-
-    private static final Map<String, Role> CACHE = Map.of(
-            "ADMIN", ADMIN,
-            "CUSTOMER", CUSTOMER
-    );
 
     public boolean isAdmin() {
         return this == ADMIN;
