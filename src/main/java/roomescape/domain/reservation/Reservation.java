a857611859224c30eadc0fa4788f3c3495bdfe17
takoyakimchi <c4nyou@gmail.com>
2024-05-25 17:36:45 +0900

    refactor: 다대일 매핑에 lazy fetch 적용

--- src/main/java/roomescape/domain/reservation/Reservation.java
+++ src/main/java/roomescape/domain/reservation/Reservation.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class Reservation {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @NotNull
     @JoinColumn(name = "member_id")
     private Member member;
@@ -38,12 +39,12 @@ public class Reservation {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @NotNull
     @JoinColumn(name = "time_id")
     private ReservationTime time;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @NotNull
     @JoinColumn(name = "theme_id")
     private Theme theme;
