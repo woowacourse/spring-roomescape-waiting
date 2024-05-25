9f83473986e5dacccef4a02f2156a2b233343090
donghoony <aru0504@naver.com>
2024-05-26 03:11:35 +0900

    chore: 테스트 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -26,22 +26,15 @@ create table if not exists reservation_time
 
 create table if not exists reservation
 (
-    id         bigint    not null auto_increment,
-    member_id  bigint    not null,
-    date       date      not null,
-    time_id    bigint    not null,
-    theme_id   bigint    not null,
-    created_at timestamp not null,
+    id         bigint      not null auto_increment,
+    member_id  bigint      not null,
+    date       date        not null,
+    time_id    bigint      not null,
+    theme_id   bigint      not null,
+    created_at timestamp   not null,
+    status     varchar(10) not null,
     primary key (id),
     foreign key (member_id) references member (id),
     foreign key (time_id) references reservation_time (id),
     foreign key (theme_id) references theme (id)
-);
-
-create table if not exists reservation_status
-(
-    reservation_id bigint      not null,
-    status         varchar(20) not null,
-    primary key (reservation_id),
-    foreign key (reservation_id) references reservation (id)
 );
