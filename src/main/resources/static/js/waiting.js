9554e5cdd800da71a845fc5b10e03b14689b036a
lemone <lejinhy@gmail.com>
2024-05-25 22:14:58 +0900

    feat(waiting.js): 예약 대기 승인 api 호출을 위한 엔드포인트, http 메서드 종류 추가

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -54,12 +54,12 @@ function approve(event) {
     TODO: [4단계] 예약 대기 목록 관리 기능
           예약 대기 승인 API 호출
      */
-    const endpoint = '' + id;
+    const endpoint = '/admin/waitings/' + id;
     return fetch(endpoint, {
-        method: ''
+        method: 'PATCH'
     }).then(response => {
-        if (response.status === 200) return;
-        throw new Error('Delete failed');
+        if (response.status === 204) return;
+        throw new Error('Patch failed');
     }).then(() => location.reload());
 }
 
