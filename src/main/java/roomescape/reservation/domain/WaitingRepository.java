f9d9f0027d049952bcc77961dfcddddc89ea38d6
gyungchan Jo <namejgc@naver.com>
2024-05-25 17:57:15 +0900

    refactor: 쿼리 수정

--- src/main/java/roomescape/reservation/domain/WaitingRepository.java
+++ src/main/java/roomescape/reservation/domain/WaitingRepository.java
@@ -11,13 +11,13 @@ import java.util.Optional;
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
     @Query("SELECT new roomescape.reservation.domain.WaitingWithRank(" +
-            "    w, " +
-            "    (SELECT COUNT(w2) " +
-            "     FROM Waiting w2 " +
-            "     WHERE w2.theme = w.theme " +
-            "       AND w2.date = w.date " +
-            "       AND w2.time = w.time " +
-            "       AND w2.id < w.id)) " +
+            "w, " +
+            "(SELECT COUNT(w2) " +
+            "FROM Waiting w2 " +
+            "WHERE w2.theme = w.theme " +
+            "AND w2.date = w.date " +
+            "AND w2.time = w.time " +
+            "AND w2.id < w.id)) " +
             "FROM Waiting w " +
             "WHERE w.member = :member")
     WaitingWithRank findByMember(@Param(value = "member") Member member);
