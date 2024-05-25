e014fe24860313d9be1a323b8e0a18febe8b89e4
J-I-H-O <jeongjiho0731@gmail.com>
2024-05-25 18:23:50 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.ReservationSearchCondition;
 import roomescape.dto.ReservationRequest;
@@ -34,7 +33,6 @@ public class AdminController {
     }
 
     @GetMapping("/reservations")
-    @ResponseBody
     public List<ReservationResponse> search(@ModelAttribute ReservationSearchCondition condition) {
         return reservationService.findByMemberAndThemeBetweenDates(condition);
     }
