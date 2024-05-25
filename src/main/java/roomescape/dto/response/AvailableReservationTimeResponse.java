71f7dc74c13cc5f7c016372e29d3f928abba2f08
Zeus6768 <akaps6768@gmail.com>
2024-05-25 14:47:59 +0900

    style: 일관적인 줄바꿈 적용

--- src/main/java/roomescape/dto/response/AvailableReservationTimeResponse.java
+++ src/main/java/roomescape/dto/response/AvailableReservationTimeResponse.java
@@ -6,10 +6,16 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import roomescape.domain.ReservationTime;
 
-public record AvailableReservationTimeResponse(Long timeId, @JsonFormat(pattern = "HH:mm") LocalTime startAt, boolean alreadyBooked) {
-
-    public static AvailableReservationTimeResponse from(ReservationTime reservationTime, boolean alreadyBooked) {
-        return new AvailableReservationTimeResponse(reservationTime.getId(), reservationTime.getStartAt(),
-                alreadyBooked);
+public record AvailableReservationTimeResponse(
+        Long timeId,
+        @JsonFormat(pattern = "HH:mm") LocalTime startAt,
+        boolean isBooked
+) {
+    public static AvailableReservationTimeResponse from(ReservationTime reservationTime, boolean isBooked) {
+        return new AvailableReservationTimeResponse(
+                reservationTime.getId(),
+                reservationTime.getStartAt(),
+                isBooked
+        );
     }
 }
--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -4,8 +4,13 @@ import java.time.LocalDate;
 
 import roomescape.domain.Reservation;
 
-public record ReservationResponse(Long id, MemberPreviewResponse member, LocalDate date, ReservationTimeResponse time,
-                                  ThemeResponse theme) {
+public record ReservationResponse(
+        Long id,
+        MemberPreviewResponse member,
+        LocalDate date,
+        ReservationTimeResponse time,
+        ThemeResponse theme
+) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
