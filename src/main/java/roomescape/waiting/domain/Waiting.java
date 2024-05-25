441692cdb6f7dae837c397a7e824e5c808858af0
nhlee98 <e5slnh9876@gmail.com>
2024-05-26 03:05:25 +0900

    refactor(Waiting): 예약 대기 테이블에서도 unique 제약 조건을 설정

--- src/main/java/roomescape/waiting/domain/Waiting.java
+++ src/main/java/roomescape/waiting/domain/Waiting.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,7 +17,7 @@ import roomescape.member.domain.Member;
 import roomescape.reservation.domain.Reservation;
 
 @Entity
-@Table(name = "waiting")
+@Table(name = "waiting", uniqueConstraints = @UniqueConstraint(columnNames = {"reservation_id", "member_id"}))
 @EntityListeners(AuditingEntityListener.class)
 public class Waiting {
     @Id
