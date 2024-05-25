d5c3334942e4c796730888c2be430606bf1bad2b
lemone <lejinhy@gmail.com>
2024-05-25 22:14:13 +0900

    style(ReservationFilterRequest): 클래스와 메서드 사이에 공백 추가

--- src/main/java/roomescape/admin/dto/ReservationFilterRequest.java
+++ src/main/java/roomescape/admin/dto/ReservationFilterRequest.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import roomescape.admin.domain.FilterInfo;
 
 public record ReservationFilterRequest(Long memberId, Long themeId, LocalDate from, LocalDate to) {
+
     public FilterInfo toFilterInfo() {
         return new FilterInfo(memberId, themeId, from, to);
     }
