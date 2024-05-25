b044ab6c9ca817f503600dbcdb82d86210619377
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-05-25 22:15:46 +0900

    refactor: 성능을 위한 LAZY 로딩으로 수정

--- src/main/java/roomescape/domain/waiting/Waiting.java
+++ src/main/java/roomescape/domain/waiting/Waiting.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +22,7 @@ public class Waiting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Reservation reservation;
     @ManyToOne(optional = false)
     private Member member;
