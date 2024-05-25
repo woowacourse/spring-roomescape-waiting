c67bfb23d8a12d31c444d34a18a2d19d9eed9ccc
ehtjsv2 <ehtjsv2@naver.com>
2024-05-26 05:24:43 +0900

    refactor: ReservationMineResponse getter 제거

--- src/main/java/roomescape/domain/reservation/dto/ReservationWithOrderDto.java
+++ src/main/java/roomescape/domain/reservation/dto/ReservationWithOrderDto.java
@@ -1,24 +1,6 @@
 package roomescape.domain.reservation.dto;
 
-import java.time.LocalDate;
 import roomescape.domain.reservation.domain.reservation.Reservation;
-import roomescape.domain.reservation.domain.reservationTime.ReservationTime;
-import roomescape.domain.theme.domain.Theme;
 
 public record ReservationWithOrderDto(Reservation reservation, Long orderNumber) {
-    public Long getId() {
-        return reservation.getId();
-    }
-
-    public Theme getTheme() {
-        return reservation.getTheme();
-    }
-
-    public LocalDate getDate() {
-        return reservation.getDate();
-    }
-
-    public ReservationTime getTime() {
-        return reservation.getTime();
-    }
 }
--- src/main/java/roomescape/domain/reservation/dto/response/ReservationMineResponse.java
+++ src/main/java/roomescape/domain/reservation/dto/response/ReservationMineResponse.java
@@ -2,19 +2,27 @@ package roomescape.domain.reservation.dto.response;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import roomescape.domain.reservation.domain.reservation.Reservation;
 import roomescape.domain.reservation.dto.ReservationWithOrderDto;
 
 public record ReservationMineResponse(Long reservationId, String theme, LocalDate date, LocalTime time, String status) {
 
     public static ReservationMineResponse from(ReservationWithOrderDto reservationWithOrderDto) {
+        Reservation reservation = reservationWithOrderDto.reservation();
         if (reservationWithOrderDto.orderNumber() == 0) {
-            return new ReservationMineResponse(reservationWithOrderDto.getId(),
-                    reservationWithOrderDto.getTheme().getName(), reservationWithOrderDto.getDate(),
-                    reservationWithOrderDto.getTime().getStartAt(), "예약");
+            return new ReservationMineResponse(
+                    reservation.getId(),
+                    reservation.getTheme().getName(),
+                    reservation.getDate(),
+                    reservation.getTime().getStartAt(),
+                    "예약");
         }
         String status = reservationWithOrderDto.orderNumber() + "번째 예약대기";
-        return new ReservationMineResponse(reservationWithOrderDto.getId(),
-                reservationWithOrderDto.getTheme().getName(), reservationWithOrderDto.getDate(),
-                reservationWithOrderDto.getTime().getStartAt(), status);
+        return new ReservationMineResponse(
+                reservation.getId(),
+                reservation.getTheme().getName(),
+                reservation.getDate(),
+                reservation.getTime().getStartAt(),
+                status);
     }
 }
