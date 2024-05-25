035564f41fa5c18776ea6ee15aff27d481eaac44
zangsu <zangsu_@naver.com>
2024-05-25 23:58:49 +0900

    refactor: 예약 대기 데이터 삽입

--- src/main/resources/data-prod.sql
+++ src/main/resources/data-prod.sql
@@ -18,11 +18,15 @@ INSERT INTO member (NAME, ROLE, EMAIL, PASSWORD)
 VALUES ('admin', 'ADMIN', 'admin@email.com', 'password');
 INSERT INTO member (NAME, ROLE, EMAIL, PASSWORD)
 VALUES ('아서', 'USER', 'Hyunta@wooteco.com', 'KingArthur');
+INSERT INTO member (NAME, ROLE, EMAIL, PASSWORD)
+VALUES ('제이미', 'USER', 'jamie9504@wooteco.com', 'jamie9504');
 
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
 VALUES (CURRENT_DATE - 3, 1, 1, 1, '2024-04-02');
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
-VALUES (CURRENT_DATE - 3, 2, 1, 2, '2024-04-02');
+VALUES (CURRENT_DATE - 3, 2, 1, 2, '2024-04-02T11:20');
+INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
+VALUES (CURRENT_DATE - 3, 1, 1, 3, '2024-04-02T12:30');
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
 VALUES (CURRENT_DATE - 3, 3, 1, 1, '2024-04-02');
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
@@ -36,6 +40,12 @@ VALUES (CURRENT_DATE - 1, 1, 1, 1, '2024-04-02');
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
 VALUES (CURRENT_DATE, 1, 1, 1, '2024-04-02');
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
-VALUES (CURRENT_DATE + 1, 1, 2, 1, '2024-04-02');
+VALUES (CURRENT_DATE + 1, 1, 2, 1, '2024-04-02T10:30');
 INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
-VALUES (CURRENT_DATE + 2, 1, 2, 1, '2024-04-02');
+VALUES (CURRENT_DATE + 1, 1, 2, 2, '2024-04-02T10:40');
+INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
+VALUES (CURRENT_DATE + 2, 1, 2, 1, '2024-04-02T10:30');
+INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
+VALUES (CURRENT_DATE + 2, 1, 2, 2, '2024-04-02T11:30');
+INSERT INTO reservation (date, time_id, theme_id, member_id, created_at)
+VALUES (CURRENT_DATE + 2, 1, 2, 3, '2024-04-02T12:30');
