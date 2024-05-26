8bba8e2f552f1521797d5476c705199f139f9e46
lemone <lejinhy@gmail.com>
2024-05-26 18:02:09 +0900

    feat(ReservationExceptionCode): 예약을 취소하지 않았는데 예약 대기 승인할 경우 예외 추가

--- src/main/java/roomescape/exception/model/ReservationExceptionCode.java
+++ src/main/java/roomescape/exception/model/ReservationExceptionCode.java
@@ -9,7 +9,8 @@ public enum ReservationExceptionCode implements ExceptionCode {
     RESERVATION_DATE_IS_PAST_EXCEPTION(HttpStatus.BAD_REQUEST, "지난 날짜의 예약을 시도하였습니다."),
     THEME_INFO_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 테마 정보가 존재하지 않습니다."),
     MEMBER_INFO_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 유저 정보가 존재하지 않습니다."),
-    DATE_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 날짜 정보가 존재하지 않습니다.");
+    DATE_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 날짜 정보가 존재하지 않습니다."),
+    SAME_RESERVATION_EXCEPTION(HttpStatus.BAD_REQUEST, "날짜, 시간, 테마가 같은 예약이 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
