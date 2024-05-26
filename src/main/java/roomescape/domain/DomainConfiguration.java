112e65fbe849d489672efae641a6860adb4380ee
3juhwan <13selfesteem91@naver.com>
2024-05-26 21:31:44 +0900

    rename: 도메인 설정 클래스 이름과 위치 변경

--- src/main/java/roomescape/domain/DomainConfiguration.java
+++ src/main/java/roomescape/domain/DomainConfiguration.java
@@ -1,9 +1,9 @@
-package roomescape.domain.config;
+package roomescape.domain;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfiguration {
+public class DomainConfiguration {
 }
