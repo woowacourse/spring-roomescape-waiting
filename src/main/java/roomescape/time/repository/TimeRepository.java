cedf404d3e4888125db254a4c12c3bfc2a84205d
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-26 19:14:51 +0900

    refactor(TimeRepository): JpaRepository 로 변경

--- src/main/java/roomescape/time/repository/TimeRepository.java
+++ src/main/java/roomescape/time/repository/TimeRepository.java
@@ -3,11 +3,12 @@ package roomescape.time.repository;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import roomescape.time.domain.Time;
 
-public interface TimeRepository extends CrudRepository<Time, Long> {
+public interface TimeRepository extends JpaRepository<Time, Long> {
     List<Time> findAllByOrderByStartAtAsc();
 
     int countByStartAt(LocalTime startAt);
