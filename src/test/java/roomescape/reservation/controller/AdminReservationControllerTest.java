e8417dced27653ac6452b565ef9dca67a365aa02
jhon3242 <rldotnr2@gmail.com>
2024-05-26 13:01:45 +0900

    test: 잘못된 ID로 예약 삭제 요청에 대한 테스트 추가

--- src/test/java/roomescape/reservation/controller/AdminReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/AdminReservationControllerTest.java
@@ -3,7 +3,10 @@ package roomescape.reservation.controller;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import roomescape.auth.controller.dto.SignUpRequest;
 import roomescape.auth.service.TokenProvider;
 import roomescape.member.controller.dto.MemberResponse;
@@ -24,7 +27,7 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static roomescape.fixture.MemberFixture.getMemberAdmin;
 import static roomescape.fixture.MemberFixture.getMemberChoco;
 
-class AdminReservationSlotControllerTest extends ControllerTest {
+class AdminReservationControllerTest extends ControllerTest {
 
     @Autowired
     ReservationService reservationService;
@@ -48,9 +51,9 @@ class AdminReservationSlotControllerTest extends ControllerTest {
         token = tokenProvider.createAccessToken(getMemberAdmin().getEmail());
     }
 
-    @DisplayName("예약 목록 조회에 성공한다.")
+    @DisplayName("성공 : 예약 목록 조회에 성공한다.")
     @Test
-    void getReservationSlots() {
+    void getReservations() {
         //given & when & then
         RestAssured.given().log().all()
                 .cookie("token", token)
@@ -59,7 +62,7 @@ class AdminReservationSlotControllerTest extends ControllerTest {
                 .statusCode(200);
     }
 
-    @DisplayName("관리자가 예약을 생성 후, 예약을 삭제한다.")
+    @DisplayName("성공 : 관리자가 예약을 생성 후, 예약을 삭제한다.")
     @TestFactory
     Stream<DynamicTest> createAndDelete() {
         List<ReservationResponse> responses = new ArrayList<>();
@@ -105,7 +108,7 @@ class AdminReservationSlotControllerTest extends ControllerTest {
         );
     }
 
-    @DisplayName("예약 대기 목록을 조회한다.")
+    @DisplayName("성공 : 예약 대기 목록을 조회한다.")
     @Test
     void waitingReservation () {
         //given
@@ -116,5 +119,16 @@ class AdminReservationSlotControllerTest extends ControllerTest {
                 .when().get("/admin/reservations/waiting")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @DisplayName("실패 : 잘못된 id로 삭제 요청시 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {0L, -1L})
+    void deleteByWrongId(Long wrongId) {
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .when().delete("/admin/reservations/" + wrongId)
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
