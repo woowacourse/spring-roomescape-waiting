f07929a8ffa70697f888a3633de16b0daab69af5
takoyakimchi <c4nyou@gmail.com>
2024-05-26 15:12:20 +0900

    test: 웹 컨트롤러에서 JdbcTemplate 사용하지 않도록 수정

--- src/test/java/roomescape/controller/web/UserPageControllerTest.java
+++ src/test/java/roomescape/controller/web/UserPageControllerTest.java
@@ -7,24 +7,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import roomescape.controller.dto.LoginRequest;
+import roomescape.domain.member.Member;
+import roomescape.domain.member.Role;
+import roomescape.repository.MemberRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Sql(value = "/truncate.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 class UserPageControllerTest {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private MemberRepository memberRepository;
 
     private String userToken;
 
     @BeforeEach
     void setUpToken() {
-        jdbcTemplate.update(
-            "INSERT INTO member(name, email, password, role) VALUES ('러너덕', 'user@a.com', '123a!', 'USER')");
+        memberRepository.save(new Member("관리자", "admin@a.com", "123a!", Role.ADMIN));
+        memberRepository.save(new Member("사용자", "user@a.com", "123a!", Role.USER));
 
         LoginRequest user = new LoginRequest("user@a.com", "123a!");
 
