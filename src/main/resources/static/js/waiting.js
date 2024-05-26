6b4673b127cdd793bbbfce3cb221b9a762c46f2b
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-27 01:00:18 +0900

    feat(WaitingReservationController): 어드민은 예약 대기를 취소시킬 수 있다

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -33,12 +33,8 @@ function render(data) {
 
         const actionCell = row.insertCell(row.cells.length);
 
-        /*
-        TODO: [4단계] 예약 대기 관리 기능
-              예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
-         */
-        // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
-        // actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
+        actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
+        actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
 }
 
@@ -67,11 +63,11 @@ function deny(event) {
     TODO: [4단계] 예약 대기 목록 관리 기능
           예약 대기 거절 API 호출
      */
-    const endpoint = '' + id;
+    const endpoint = '/waiting-reservations/' + id;
     return fetch(endpoint, {
-        method: ''
+        method: 'DELETE'
     }).then(response => {
-        if (response.status === 200) return;
+        if (response.status === 204) return;
         throw new Error('Delete failed');
     }).then(() => location.reload());
 }
