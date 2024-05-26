99b97a78bc5e3b8b13bd30993cb4d75eb1c39efe
le2sky <leehaneul990623@gmail.com>
2024-05-26 09:54:57 +0900

    test: 누락된 도메인 로직 테스트 추가 작성

--- src/test/java/roomescape/domain/waiting/WaitingTest.java
+++ src/test/java/roomescape/domain/waiting/WaitingTest.java
@@ -1,9 +1,11 @@
 package roomescape.domain.waiting;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -28,5 +30,40 @@ class WaitingTest {
         // when, then
         assertThatThrownBy(() -> new Waiting(member, reservation))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 대기를_예약으로_승인() {
+        Member member1 = new Member("제이", "email@test.com", "password123", Role.ADMIN);
+        Member member2 = new Member("아톰", "email@test.com", "password123", Role.ADMIN);
+        LocalDate date = LocalDate.now().plusDays(1);
+        Theme theme = new Theme("테마", "테마에 대한 설명입니다.", "썸네일");
+        ReservationTime reservationTime = new ReservationTime(LocalTime.now());
+        Reservation reservation = new Reservation(date, reservationTime, theme, member1);
+        Waiting waiting = new Waiting(member2, reservation);
+
+        // when
+        waiting.approve();
+
+        // then
+        Member reservationOwner = reservation.getMember();
+        assertThat(reservationOwner).isEqualTo(member2);
+    }
+
+    @Test
+    void 대기의_소유자가_아닌지_검증() {
+        Member member1 = new Member("제이", "email@test.com", "password123", Role.ADMIN);
+        Member member2 = new Member("아톰", "email@test.com", "password123", Role.ADMIN);
+        LocalDate date = LocalDate.now().plusDays(1);
+        Theme theme = new Theme("테마", "테마에 대한 설명입니다.", "썸네일");
+        ReservationTime reservationTime = new ReservationTime(LocalTime.now());
+        Reservation reservation = new Reservation(date, reservationTime, theme, member1);
+        Waiting waiting = new Waiting(member2, reservation);
+
+        // when, then
+        Assertions.assertAll(
+                () -> assertThat(waiting.isNotOwner(member1)).isTrue(),
+                () -> assertThat(waiting.isNotOwner(member2)).isFalse()
+        );
     }
 }
