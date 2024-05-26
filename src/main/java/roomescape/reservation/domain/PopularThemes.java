4c6fcf5d22b273c5c0da2aaadb8ce12439402170
hoyeonyy <highoption@naver.com>
2024-05-27 00:15:01 +0900

    refactor : 인기 테마 동등성 보장

--- src/main/java/roomescape/reservation/domain/PopularThemes.java
+++ src/main/java/roomescape/reservation/domain/PopularThemes.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 
 public class PopularThemes {
 
-    private static final int POPULAR_THEME_SIZE = 10;
     private final List<Theme> populars;
 
     public PopularThemes(List<Theme> themes) {
@@ -15,16 +14,12 @@ public class PopularThemes {
     }
 
     private List<Theme> makePopularThemes(List<Theme> themes) {
-        Map<String, Long> countTheme = themes.stream()
-                .collect(Collectors.groupingBy(Theme::getName, Collectors.counting()));
-
-        return countTheme.entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder()))
-                .limit(POPULAR_THEME_SIZE)
-                .flatMap(entry -> themes.stream()
-                        .filter(theme -> theme.getName().equals(entry.getKey()))
-                        .findFirst()
-                        .stream())
+        return themes.stream()
+                .collect(Collectors.groupingBy(theme -> theme, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<Theme, Long>comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(Map.Entry::getKey)
                 .toList();
     }
 
--- src/main/java/roomescape/reservation/domain/Theme.java
+++ src/main/java/roomescape/reservation/domain/Theme.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Theme {
@@ -47,5 +48,22 @@ public class Theme {
 
     public boolean sameThemeId(Long id) {
         return this.id.equals(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Theme theme = (Theme) o;
+        return Objects.equals(id, theme.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
