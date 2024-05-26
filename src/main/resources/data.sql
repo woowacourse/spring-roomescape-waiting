e23f6101e3d9598c70fe3ee3a58cb42f749d1690
J-I-H-O <jeongjiho0731@gmail.com>
2024-05-26 13:43:28 +0900

    chore: 샘플 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,114 +1,75 @@
-
-
 -- 테마 목록 : 11개
 INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마1', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마2', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마3', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마4', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마5', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마6', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마7', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마8', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마9', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마10', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마11', '재밌는 테마입니다',
-        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
+VALUES ('테마1', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마2', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마3', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마4', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마5', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마6', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마7', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마8', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마9', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마10', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg'),
+       ('테마11', '재밌는 테마입니다', 'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_1280.jpg');
 
 -- 예약 시간 목록 : 5개
 INSERT INTO reservation_time (start_at)
-VALUES ('08:00');
-INSERT INTO reservation_time (start_at)
-VALUES ('10:00');
-INSERT INTO reservation_time (start_at)
-VALUES ('13:00');
-INSERT INTO reservation_time (start_at)
-VALUES ('21:00');
-INSERT INTO reservation_time (start_at)
-VALUES ('23:00');
+VALUES ('08:00'),
+       ('10:00'),
+       ('13:00'),
+       ('21:00'),
+       ('23:00');
 
 -- 유저 목록 : 2개
+INSERT INTO member(name, email, password, role)
+VALUES ('admin','admin@email.com', 'f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3', 'ADMIN'),
+       ('member1', 'email1@email.com', 'f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3', 'MEMBER'),
+       ('member2', 'email2@email.com', 'f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3', 'MEMBER'),
+       ('member3', 'email3@email.com', 'f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3', 'MEMBER');
 
-INSERT INTO member(name, email, password,role) VALUES ( 'member', 'email@email.com','f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3','MEMBER');
-INSERT INTO member(name, email, password,role) VALUES ( 'admin','email2@email.com','f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3','ADMIN');
-
-
--- 5,4,2,5,2,3,1,1,1,1,1
--- 내림차순 정렬 ID : 4/1, 2, 6, 3/5, 7/8/9/10/11
-
--- 테마 1 예약 목록 : 5개
+-- 8, 4, 2, 5, 2, 3, 1, 1, 1, 1, 1
+-- 내림차순 정렬 ID : 1, 4, 2, 6, 3, 5, 7, 8, 9, 10, 11
+-- 테마 1 예약 목록 : 8개
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 1, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 1, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 3, 1, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 4, 1, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 5, 1, 1, 'APPROVED');
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 1, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 2, 1, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 3, 1, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 4, 1, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 5, 1, 1, 'APPROVED'),
+       (DATEADD('DAY', 1, CURRENT_DATE), 1, 1, 2, 'APPROVED'),  -- 동일한 날짜/시간/테마에 member1 예약
+       (DATEADD('DAY', 1, CURRENT_DATE), 1, 1, 3, 'PENDING'),   -- 동일한 날짜/시간/테마에 member2 예약 대기
+       (DATEADD('DAY', 1, CURRENT_DATE), 1, 1, 4, 'PENDING');   -- 동일한 날짜/시간/테마에 member3 예약 대기
 
 -- 테마 2 예약 목록 : 4개
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 2, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 2, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 3, 2, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 4, 2, 1, 'APPROVED');
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 2, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 2, 2, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 3, 2, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 4, 2, 1, 'APPROVED');
 
 -- 테마 3 예약 목록 : 2개
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 3, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 3, 1, 'APPROVED');
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 3, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 2, 3, 1, 'APPROVED');
 
 -- 테마 4 예약 목록 : 5개
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 4, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 4, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 3, 4, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 4, 4, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 5, 4, 1, 'APPROVED');
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 4, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 2, 4, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 3, 4, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 4, 4, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 5, 4, 1, 'APPROVED');
 
 -- 테마 5 예약 목록 : 2개
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 5, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 5, 5, 1, 'APPROVED');
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 5, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 5, 5, 1, 'APPROVED');
 
 -- 테마 6 예약 목록 : 3개
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 6, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 6, 1, 'APPROVED');
-INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 3, 6, 1, 'APPROVED');
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 6, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 2, 6, 1, 'APPROVED'),
+       (DATEADD('DAY', -3, CURRENT_DATE), 3, 6, 1, 'APPROVED');
 
 -- 테마 7 예약 목록
 INSERT INTO reservation (date, time_id, theme_id, reservation_member_id, status)
