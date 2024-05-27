1862941faedd4d56e3ebdeee3efa42ad8dcc1cac
linirini <2001yerin@naver.com>
2024-05-27 15:37:34 +0900

    fix: not null 처리

--- src/main/java/roomescape/service/reservation/dto/AdminReservationRequest.java
+++ src/main/java/roomescape/service/reservation/dto/AdminReservationRequest.java
@@ -1,13 +1,13 @@
 package roomescape.service.reservation.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 public record AdminReservationRequest(
-        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate date,
-        @NotNull(message = "회원 ID를 입력해주세요.") Long memberId,
-        @NotNull(message = "시간 ID를 입력해주세요.") Long timeId,
-        @NotNull(message = "테마 ID를 입력해주세요.") Long themeId) {
+        @NotNull(message = "날짜를 입력해주세요.") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+        @NotNull(message = "회원 정보를 입력해주세요.") Long memberId,
+        @NotNull(message = "시간을 입력해주세요.") Long timeId,
+        @NotNull(message = "테마를 입력해주세요.") Long themeId) {
 }
