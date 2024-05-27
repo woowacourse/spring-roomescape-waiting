81cf30fdab2df6585d07098c8fd44974e7db93b6
SCY <helenason@naver.com>
2024-05-27 17:19:53 +0900

    refactor(GlobalExceptionHandler): 예외 응답 메시지 구체화

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -3,59 +3,85 @@ package roomescape.exception;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
-
-import java.time.format.DateTimeParseException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(NotFoundException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
     }
 
     @ExceptionHandler(value = {BadRequestException.class, DuplicatedException.class})
-    public ResponseEntity<String> handleBadRequestAndDuplicatedException(RuntimeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+    public ResponseEntity<ExceptionResponse> handleBadRequestAndDuplicatedException(RuntimeException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
 
-    @ExceptionHandler(value = DateTimeParseException.class)
-    public ResponseEntity<String> handleDateTimeParseException(DateTimeParseException exception) {
-        return new ResponseEntity<>("[ERROR] 유효하지 않은 요청입니다.", HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = AuthorizationException.class)
-    public ResponseEntity<String> handleAuthorizationException(AuthorizationException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ExceptionResponse> handleAuthorizationException(AuthorizationException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, "[REQUEST] 올바른 형태의 필드를 입력해주세요.");
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        String message = exception.getAllErrors().get(0).getDefaultMessage();
-        return new ResponseEntity<>("[REQUEST_ERROR] " + message, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ex.printStackTrace();
+        String message = ex.getAllErrors().get(0).getDefaultMessage();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, "[REQUEST BODY] " + message);
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
     }
 
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException exception) {
-        return new ResponseEntity<>("[REQUEST_ERROR] " + exception.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(value = HandlerMethodValidationException.class)
-    public ResponseEntity<String> handleHandlerMethodValidationException(HandlerMethodValidationException exception) {
-        return new ResponseEntity<>("[REQUEST_ERROR] " + exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, "[REQUEST PARAMETER] " + ex.getMessage());
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
     }
 
     @ExceptionHandler(value = IllegalStateException.class)
-    public ResponseEntity<String> handleIllegalStateException(IllegalStateException exception) {
-        return new ResponseEntity<>("[ERROR] " + exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionResponse> handleIllegalStateException(IllegalStateException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, "[ERROR] " + ex.getMessage());
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException ex) {
+        ex.printStackTrace();
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, "[SERVER] 관리자에게 문의해주세요.");
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
     }
 }
