68cf267a38ff21baa52bd4485d14f30a781b0ac0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-27 15:36:51 +0900

    refactor(BaseEntity): 엔티티 컬럼에 updatable, nullable 조건 추가

--- src/main/java/roomescape/common/model/BaseEntity.java
+++ src/main/java/roomescape/common/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package roomescape.common.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -12,8 +13,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
 }
