e8f36ffe61a3a1d4caca51de1fe32da35f796432
unifolio0 <boho82ha@gmail.com>
2024-05-28 10:54:53 +0900

    refactor: 클래스 분리

--- src/main/java/roomescape/presentation/admin/AdminPageController.java
+++ src/main/java/roomescape/presentation/admin/AdminPageController.java
@@ -1,4 +1,4 @@
-package roomescape.presentation;
+package roomescape.presentation.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
