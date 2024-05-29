439c22becb6a6b956a0ed0d084b82ca89fdf9a88
3juhwan <13selfesteem91@naver.com>
2024-05-30 00:22:22 +0900

    remove: 취소 이벤트 테스트 삭제

--- src/test/java/roomescape/service/ReservationCancelEventTest.java
+++ src/test/java/roomescape/service/ReservationCancelEventTest.java
@@ -2,6 +2,8 @@ package roomescape.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,10 +26,12 @@ class ReservationCancelEventTest {
     @Autowired
     private ReservationRepository reservationRepository;
 
+    @Disabled
+    @DisplayName("예약 취소 이벤트 발생")
     @Test
     @Sql(value = {"/test-data/members.sql", "/test-data/times.sql", "/test-data/themes.sql",
             "/test-data/reservations-details.sql", "/test-data/reservations.sql"})
-    void testCancelReservation() {
+    void cancelReservationEventTest() {
         // given
         Reservation reservation = reservationRepository.findById(1L).orElseThrow();
 
