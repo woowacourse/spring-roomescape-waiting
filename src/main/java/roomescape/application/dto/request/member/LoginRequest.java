55646be2e67d645a4c9d9c1e57aa2406945f8ac6
3juhwan <13selfesteem91@naver.com>
2024-05-30 01:59:37 +0900

    refactor: dto 중복 검증 제거

--- src/main/java/roomescape/application/dto/request/member/LoginRequest.java
+++ src/main/java/roomescape/application/dto/request/member/LoginRequest.java
@@ -1,13 +1,10 @@
 package roomescape.application.dto.request.member;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import roomescape.application.dto.validator.EmailConstraint;
 
 public record LoginRequest(
-        @Pattern(
-                regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
-                message = "이메일 또는 비밀번호를 형식에 맞춰 입력해주세요."
-        ) String email,
+        @EmailConstraint String email,
         @NotBlank(message = "이메일 또는 비밀번호를 형식에 맞춰 입력해주세요.") String password
 ) {
 }
--- src/main/java/roomescape/application/dto/request/member/SignupRequest.java
+++ src/main/java/roomescape/application/dto/request/member/SignupRequest.java
@@ -1,15 +1,12 @@
 package roomescape.application.dto.request.member;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import roomescape.application.dto.validator.EmailConstraint;
 import roomescape.domain.member.Member;
 
 public record SignupRequest(
         @NotBlank(message = "이름은 공백일 수 없습니다.") String name,
-        @Pattern(
-                regexp = "^[a-zA-Z0-9+._-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
-                message = "이메일 또는 비밀번호를 형식에 맞춰 입력해주세요."
-        ) String email,
+        @EmailConstraint String email,
         @NotBlank(message = "비밀번호는 공백일 수 없습니다.") String password
 ) {
 
