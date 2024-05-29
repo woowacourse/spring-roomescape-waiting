53825a30de6a6975e468afe451384fad96dce1a3
zangsu <zangsu_@naver.com>
2024-05-29 22:26:19 +0900

    refactor : EnableJpaAuditing 분리

--- src/test/java/roomescape/repository/ReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/ReservationRepositoryTest.java
@@ -10,13 +10,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import roomescape.Fixture;
+import roomescape.config.AuditingConfig;
 import roomescape.domain.Member;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.domain.Theme;
 
 @DataJpaTest
+@Import(AuditingConfig.class)
 class ReservationRepositoryTest {
 
     @Autowired
