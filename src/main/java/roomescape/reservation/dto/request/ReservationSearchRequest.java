e6f48d0f200f63b9b3cfa26ecbc5468fe0f3ef3b
seunghye218 <seunghye@student.42seoul.kr>
2024-05-29 09:00:19 +0900

    refactor: 어드민 날짜 제한 검색 정책 제거

--- src/main/java/roomescape/reservation/dto/request/ReservationSearchRequest.java
+++ src/main/java/roomescape/reservation/dto/request/ReservationSearchRequest.java
@@ -2,9 +2,7 @@ package roomescape.reservation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import roomescape.exception.ValidDateDuration;
 
-@ValidDateDuration
 @NotNull
 public record ReservationSearchRequest(
         Long themeId,
