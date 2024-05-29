817b9b5e5dc678f8efbfb1992d314f5ba1d1923a
hyxrxn <gpfls981220@gmail.com>
2024-05-29 21:52:36 +0900

    refactor(client): 구현 완료된 todo 제거

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -7,10 +7,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 function getReservations() {
-    /*
-    TODO: [2단계] 내 예약 목록 조회 기능
-          endpoint 설정
-     */
     return fetch('/reservations/mine') // 내 예약 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
@@ -20,7 +16,7 @@ function getReservations() {
 }
 
 function getWaitings() {
-    return fetch('/waitings/mine') // 내 예약 목록 조회 API 호출
+    return fetch('/waitings/mine') // 내 대기 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
             throw new Error('Read failed');
@@ -34,11 +30,6 @@ function render(data) {
 
     data.forEach(item => {
         const row = tableBody.insertRow();
-
-        /*
-        TODO: [2단계] 내 예약 목록 조회 기능
-              response 명세에 맞춰 값 설정
-         */
         const theme = item.theme;
         const date = item.date;
         const time = item.time;
@@ -49,9 +40,6 @@ function render(data) {
         row.insertCell(2).textContent = time;
         row.insertCell(3).textContent = status;
 
-        /*
-        TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
-         */
         if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
             const cancelCell = row.insertCell(4);
             const cancelButton = document.createElement('button');
@@ -68,9 +56,6 @@ function render(data) {
 }
 
 function requestDeleteWaiting(id) {
-    /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
-     */
     const endpoint = `/waitings/${id}`;
     return fetch(endpoint, {
         method: 'DELETE'
