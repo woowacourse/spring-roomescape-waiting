99a3a984f8acf1bfd9254cc9202918b2bede9a5e
Zeus6768 <akaps6768@gmail.com>
2024-06-02 15:28:41 +0900

    refactor: 불필요한 애너테이션 삭제

--- src/main/java/roomescape/repository/ReservationWaitingRepository.java
+++ src/main/java/roomescape/repository/ReservationWaitingRepository.java
@@ -4,13 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import roomescape.domain.Member;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationWaiting;
 
-@Repository
 public interface ReservationWaitingRepository extends JpaRepository<ReservationWaiting, Long> {
 
     List<ReservationWaiting> findAllByReservation(Reservation reservation);
--- src/main/java/roomescape/repository/ThemeRepository.java
+++ src/main/java/roomescape/repository/ThemeRepository.java
@@ -1,10 +1,8 @@
 package roomescape.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import roomescape.domain.Theme;
 
-@Repository
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
 }
