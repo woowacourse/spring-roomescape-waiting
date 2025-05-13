9dfbcd761e54aea9a80ca5c93965671c558ec857
Yehyeok Bang <qkddpgur318@gmail.com>
2025-05-13 16:13:59 +0900

    fix: 불필요한 import 문 제거
    Co-authored-by: abc5259 <dlwogns3413@naver.com>

--- src/main/java/roomescape/domain/reservation/ReservationTimeRepository.java
+++ src/main/java/roomescape/domain/reservation/ReservationTimeRepository.java
@@ -1,7 +1,5 @@
 package roomescape.domain.reservation;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationTimeRepository extends JpaRepository<ReservationTime, Long> {
