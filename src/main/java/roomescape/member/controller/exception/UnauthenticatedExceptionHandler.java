0e4e126713c9573179d21114fa1373c8e92e295e
(minjae8563) <(minjae8563@naver.com)>
2025-05-13 15:18:07 +0900

    refactor(UnauthenticatedExceptionHandler): 핸들러 우선 순위 설정

--- src/main/java/roomescape/member/controller/exception/UnauthenticatedExceptionHandler.java
+++ src/main/java/roomescape/member/controller/exception/UnauthenticatedExceptionHandler.java
@@ -1,11 +1,13 @@
 package roomescape.member.controller.exception;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.member.resolver.UnauthenticatedException;
 
 @RestControllerAdvice
+@Order(value = 1)
 public class UnauthenticatedExceptionHandler {
 
     @ExceptionHandler(UnauthenticatedException.class)
