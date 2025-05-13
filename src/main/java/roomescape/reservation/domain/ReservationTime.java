26c80289ef78f5bb783ee4bee97cdada35b83e45
sonjh919 <kingjh1125@gmail.com>
2025-05-13 15:40:20 +0900

    feat: ReservationTime jpa 변환

--- src/main/java/roomescape/reservation/domain/ReservationTime.java
+++ src/main/java/roomescape/reservation/domain/ReservationTime.java
@@ -1,11 +1,28 @@
 package roomescape.reservation.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalTime;
 
+@Entity
 public class ReservationTime {
 
-    private final Long id;
-    private final LocalTime startAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalTime startAt;
+
+    public ReservationTime() {
+    }
+
+    public ReservationTime(final LocalTime startAt) {
+        this.startAt = startAt;
+    }
 
     public ReservationTime(final Long id, final LocalTime startAt) {
         this.id = id;
