1f6cf33c2e2f80dd86bd8f8e60e5bdd3d945e530
sonjh919 <kingjh1125@gmail.com>
2025-05-13 15:52:29 +0900

    feat: Theme jpa 변환

--- src/main/java/roomescape/reservation/domain/Theme.java
+++ src/main/java/roomescape/reservation/domain/Theme.java
@@ -1,14 +1,38 @@
 package roomescape.reservation.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Theme {
 
-    private final Long id;
-    private final String name;
-    private final String description;
-    private final String thumbnail;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String thumbnail;
+
+    public Theme() {
+    }
 
     public Theme(Long id, String name, String description, String thumbnail) {
         this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public Theme(final String name, final String description, final String thumbnail) {
         this.name = name;
         this.description = description;
         this.thumbnail = thumbnail;
