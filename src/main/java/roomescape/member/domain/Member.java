7461ef1cb01fb4121e93ef8610ce1425658a86bf
sonjh919 <kingjh1125@gmail.com>
2025-05-13 15:52:35 +0900

    feat: Member jpa 변환

--- src/main/java/roomescape/member/domain/Member.java
+++ src/main/java/roomescape/member/domain/Member.java
@@ -1,14 +1,42 @@
 package roomescape.member.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
-    private final Long id;
-    private final String email;
-    private final String password;
-    private final String name;
-    private final Role role;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Role role;
+
+    public Member() {
+    }
 
     public Member(Long id, String email, String password, String name, Role role) {
         this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+
+    public Member(final String email, final String password, final String name, final Role role) {
         this.email = email;
         this.password = password;
         this.name = name;
