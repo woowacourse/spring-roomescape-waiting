11180d260ce2b1f3739c942736b97cb2f61156d3
2jin2.1031 <2jin2.1031@gmail.com>
2025-05-14 20:38:32 +0900

    test: 멤버 controller 테스트 추가

---
+++ src/test/java/roomescape/member/controller/MemberControllerTest.java
@@ -0,0 +1,92 @@
+package roomescape.member.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.admin.domain.dto.AdminReservationRequestDto;
+import roomescape.auth.JwtTokenProvider;
+import roomescape.auth.domain.dto.TokenResponseDto;
+import roomescape.auth.fixture.AuthFixture;
+import roomescape.auth.service.AuthService;
+import roomescape.member.domain.dto.ReservationWithBookStateDto;
+import roomescape.reservation.repository.ReservationRepository;
+import roomescape.reservationTime.ReservationTimeTestDataConfig;
+import roomescape.theme.ThemeTestDataConfig;
+import roomescape.theme.domain.Theme;
+import roomescape.user.AdminTestDataConfig;
+import roomescape.user.MemberTestDataConfig;
+import roomescape.user.domain.User;
+import roomescape.user.service.UserService;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes =
+        {MemberTestDataConfig.class, ThemeTestDataConfig.class, ReservationTimeTestDataConfig.class,})
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+class MemberControllerTest {
+
+    @Autowired
+    private UserService service;
+    @Autowired
+    private ReservationTimeTestDataConfig reservationTimeTestDataConfig;
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private MemberTestDataConfig memberTestDataConfig;
+    @Autowired
+    private ThemeTestDataConfig themeTestDataConfig;
+    @Autowired
+    private ReservationTimeTestDataConfig timeTestDataConfig;
+
+    private static LocalDate date;
+    private static User memberStatic;
+    private static TokenResponseDto memberTokenResponseDto;
+
+    @BeforeAll
+    public static void setUp(@Autowired AuthService authService,
+                             @Autowired MemberTestDataConfig memberTestDataConfig
+    ) {
+        date = LocalDate.now().plusDays(1);
+
+        memberStatic = memberTestDataConfig.getSavedMember();
+
+        memberTokenResponseDto = authService.login(
+                AuthFixture.createTokenRequestDto(memberStatic.getEmail(), memberStatic.getPassword()));
+    }
+
+    @DisplayName("유저의 예약 리스트 조회 기능 : 성공 시 200 OK 반환")
+    @Test
+    void findAllReservationsByMember_success_byMember() {
+        // given
+        Long themeId = themeTestDataConfig.getSavedId();
+        Long reservationTimeId = reservationTimeTestDataConfig.getSavedId();
+
+        AdminReservationRequestDto dto = new AdminReservationRequestDto(date,
+                themeId,
+                reservationTimeId,
+                memberStatic.getId());
+
+        String token = memberTokenResponseDto.accessToken();
+
+        // when
+        // then
+        RestAssured.given().log().all()
+                .cookies("token", token)
+                .contentType(ContentType.JSON)
+                .body(dto)
+                .when().get("/members/reservations-mine")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
