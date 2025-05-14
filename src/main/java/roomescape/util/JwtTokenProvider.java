ff0e2f943f61fc0fe87d2ebb20b352f9bacb6c37
supernovaMK <minkikim2002@gmail.com>
2025-05-14 16:47:08 +0900

    refactor: SecretKey 사용

--- src/main/java/roomescape/util/JwtTokenProvider.java
+++ src/main/java/roomescape/util/JwtTokenProvider.java
@@ -2,6 +2,7 @@ package roomescape.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.domain.Member;
@@ -9,23 +10,23 @@ import roomescape.domain.Member;
 @Component
 public class JwtTokenProvider {
 
-    private String secretKey;
+    private final SecretKey secretKey;
 
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
-        this.secretKey = secretKey;
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
     public String createToken(Member member) {
         String accessToken = Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getName())
-                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .signWith(secretKey)
                 .compact();
         return accessToken;
     }
 
     public Long findMemberIdByToken(String token) {
-        Long id = Long.valueOf(Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes())).build()
+        Long id = Long.valueOf(Jwts.parserBuilder().setSigningKey(secretKey).build()
                 .parseClaimsJws(token).getBody().getSubject());
         return id;
     }
