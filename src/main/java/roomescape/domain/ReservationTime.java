6d244d8458e146386d7a39151a58f5c0eabc4401
ChoiDongjun <adong0808@naver.com>
2025-05-14 14:32:32 +0900

    refactor: 예약 시간 검증 서비스로 이동

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import roomescape.exception.ReservationException;
 
 import java.time.LocalTime;
 import java.util.Objects;
@@ -12,27 +11,21 @@ import java.util.Objects;
 @Entity
 public class ReservationTime {
 
-    private static final LocalTime RESERVATION_START_TIME = LocalTime.of(12, 0);
-    private static final LocalTime RESERVATION_END_TIME = LocalTime.of(22, 0);
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalTime startAt;
 
-    public ReservationTime() {
-    }
-
-    public ReservationTime(LocalTime startAt) {
-        this(null, startAt);
+    protected ReservationTime() {
     }
 
     public ReservationTime(final Long id, final LocalTime startAt) {
-        if (startAt.isBefore(RESERVATION_START_TIME) || startAt.isAfter(RESERVATION_END_TIME)) {
-            throw new ReservationException("해당 시간은 예약 가능 시간이 아닙니다.");
-        }
         this.id = id;
         this.startAt = startAt;
+    }
+
+    public static ReservationTime createNew(final LocalTime startAt) {
+        return new ReservationTime(null, startAt);
     }
 
     public Long getId() {
--- src/main/java/roomescape/service/ReservationTimeService.java
+++ src/main/java/roomescape/service/ReservationTimeService.java
@@ -7,17 +7,22 @@ import roomescape.domain.ReservationTime;
 import roomescape.domain.ReservationTimeRepository;
 import roomescape.exception.DeletionNotAllowedException;
 import roomescape.exception.NotFoundReservationTimeException;
+import roomescape.exception.ReservationException;
 import roomescape.service.param.CreateReservationTimeParam;
 import roomescape.service.result.AvailableReservationTimeResult;
 import roomescape.service.result.ReservationTimeResult;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class ReservationTimeService {
+    // TODO: 정책으로 빼기
+    private static final LocalTime RESERVATION_START_TIME = LocalTime.of(12, 0);
+    private static final LocalTime RESERVATION_END_TIME = LocalTime.of(22, 0);
 
     private final ReservationTimeRepository reservationTimeRepository;
     private final ReservationRepository reservationRepository;
@@ -28,7 +33,12 @@ public class ReservationTimeService {
     }
 
     public ReservationTimeResult create(CreateReservationTimeParam createReservationTimeParam) {
-        ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(createReservationTimeParam.startAt()));
+        LocalTime startAt = createReservationTimeParam.startAt();
+        if (startAt.isBefore(RESERVATION_START_TIME) || startAt.isAfter(RESERVATION_END_TIME)) {
+            throw new ReservationException("해당 시간은 예약 가능 시간이 아닙니다.");
+        }
+
+        ReservationTime reservationTime = reservationTimeRepository.save(ReservationTime.createNew(startAt));
         return ReservationTimeResult.from(reservationTime);
     }
 
