a831f0d306d9da01967e750fedfc982490be81ea
soeun2537 <soeun2537@gmail.com>
2025-05-14 16:15:04 +0900

    refactor: 쿼리 JPQL로 변경

--- src/main/java/roomescape/reservation/repository/ReservationTimeRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationTimeRepository.java
@@ -11,30 +11,15 @@ public interface ReservationTimeRepository extends JpaRepository<ReservationTime
 
     List<ReservationTime> findAll();
 
-    // TODO: 테스트 코드 짜기, 응답 DTO로 변환 가능한지 확인
-//    @Query(value = """
-//            SELECT rt.id, rt.startAt
-//            FROM ReservationTime rt
-//            WHERE rt.id NOT IN (
-//                SELECT rt.id
-//                FROM Reservation r
-//                WHERE r.date = :date AND r.theme.id = :themeId
-//            )
-//            """)
-//    List<ReservationTime> findAvailableTimes(@Param("date") LocalDate date, @Param("themeId") Long themeId);
-
     @Query(value = """
-            SELECT
-                rt.id,
-                rt.start_at
-            FROM reservation_time rt
-            JOIN (
-                SELECT id, time_id
-                FROM reservation
-                WHERE date = :date AND theme_id = :themeId
-            ) r ON r.time_id = rt.id
-            ORDER BY rt.start_at
-            """, nativeQuery = true)
+            SELECT rt
+            FROM ReservationTime rt
+            WHERE rt.id IN (
+                SELECT r.time.id
+                FROM Reservation r
+                WHERE r.date = :date AND r.theme.id = :themeId
+            )
+            """)
     List<ReservationTime> findAllReservedTimeByDateAndThemeId(@Param("date") LocalDate date,
                                                               @Param("themeId") Long themeId);
 }
--- src/main/java/roomescape/theme/repository/ThemeRepository.java
+++ src/main/java/roomescape/theme/repository/ThemeRepository.java
@@ -5,32 +5,24 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import roomescape.theme.entity.Theme;
 
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
 
     List<Theme> findAll();
 
-    // TODO: 쿼리 변경
-    @Query(value = """
-            SELECT
-                t.id,
-                t.name,
-                t.description,
-                t.thumbnail
-            FROM theme t
-            LEFT JOIN (
-                SELECT
-                    theme_id,
-                    COUNT(*) as cnt
-                FROM reservation
-                WHERE date BETWEEN :startDate AND :endDate
-                GROUP BY theme_id
-            ) r_stats ON t.id = r_stats.theme_id
-            ORDER BY cnt DESC, theme_id DESC
-            FETCH FIRST :limit ROWS ONLY
-            """, nativeQuery = true)
-    List<Theme> findPopularDescendingUpTo(LocalDate startDate, LocalDate endDate, int limit);
+    @Query("""
+            SELECT t
+            FROM Theme t
+            LEFT JOIN Reservation r ON r.theme = t AND r.date BETWEEN :startDate AND :endDate
+            GROUP BY t
+            ORDER BY COUNT(r) DESC, t.id DESC
+            LIMIT :limit
+            """)
+    List<Theme> findPopularDescendingUpTo(@Param("startDate") LocalDate startDate,
+                                          @Param("endDate") LocalDate endDate,
+                                          @Param("limit") int limit);
 
     Optional<Theme> findByName(String name);
 }
