070d537fdf7e8707bc6c9f1018d2edaaf4fec14c
ChoiDongjun <adong0808@naver.com>
2025-05-14 15:26:33 +0900

    test: MemberService 테스트 수정

--- src/test/java/roomescape/service/MemberServiceTest.java
+++ src/test/java/roomescape/service/MemberServiceTest.java
@@ -1,11 +1,16 @@
 package roomescape.service;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import roomescape.TestFixture;
+import roomescape.domain.Member;
+import roomescape.domain.MemberRepository;
 import roomescape.domain.MemberRole;
 import roomescape.exception.NotFoundMemberException;
 import roomescape.exception.UnAuthorizedException;
-import roomescape.fake.FakeMemberRepository;
-import roomescape.persistence.query.CreateMemberQuery;
 import roomescape.service.param.LoginMemberParam;
 import roomescape.service.param.RegisterMemberParam;
 import roomescape.service.result.MemberResult;
@@ -13,10 +18,16 @@ import roomescape.service.result.MemberResult;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
 class MemberServiceTest {
 
-    private final FakeMemberRepository memberRepository = new FakeMemberRepository();
-    private final MemberService memberService = new MemberService(memberRepository);
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     void 로그인_시_존재하지_않는_email로_로그인_시_예외() {
@@ -32,10 +43,10 @@ class MemberServiceTest {
     @Test
     void 로그인_시_email에_대해_password가_일치하지_않으면_예외() {
         //given
-        memberRepository.create(new CreateMemberQuery("name1", MemberRole.USER, "email1", "password1"));
+        Member member = memberRepository.save(TestFixture.createDefaultMember());
 
         //when & then
-        assertThatThrownBy(() -> memberService.login(new LoginMemberParam("email1", "password2")))
+        assertThatThrownBy(() -> memberService.login(new LoginMemberParam(member.getEmail(), "password2")))
                 .isInstanceOf(UnAuthorizedException.class)
                 .hasMessage("비밀 번호가 일치하지 않습니다.");
     }
@@ -43,26 +54,27 @@ class MemberServiceTest {
     @Test
     void email과_password로_로그인_가능() {
         //given
-        memberRepository.create(new CreateMemberQuery("name1", MemberRole.USER, "email1", "password1"));
-        LoginMemberParam loginMemberParam = new LoginMemberParam("email1", "password1");
+        Member member = memberRepository.save(TestFixture.createDefaultMember());
+        LoginMemberParam loginMemberParam = new LoginMemberParam(member.getEmail(), member.getPassword());
 
         //when & then
-        assertThat(memberService.login(loginMemberParam)).isEqualTo(new MemberResult(1L, "name1", MemberRole.USER, "email1"));
+        assertThat(memberService.login(loginMemberParam)).isEqualTo(new MemberResult(member.getId(), member.getName(), MemberRole.USER, member.getEmail()));
     }
 
     @Test
     void 멤버를_생성할_수_있다() {
-        assertThat(memberService.create(new RegisterMemberParam("name1", "email1", "password1")))
-                .isEqualTo(new MemberResult(1L, "name1", MemberRole.USER, "email1"));
+        MemberResult memberResult = memberService.create(new RegisterMemberParam("name1", "email1", "password1"));
+        assertThat(memberResult)
+                .isEqualTo(new MemberResult(memberResult.id(), "name1", MemberRole.USER, "email1"));
     }
 
     @Test
     void id를_통해_멤버를_찾을_수_있다() {
         //given
-        memberRepository.create(new CreateMemberQuery("name1", MemberRole.USER, "email1", "password1"));
+        Member member = memberRepository.save(TestFixture.createDefaultMember());
 
         //when & then
-        assertThat(memberService.findById(1L)).isEqualTo(new MemberResult(1L, "name1", MemberRole.USER, "email1"));
+        assertThat(memberService.findById(1L)).isEqualTo(new MemberResult(1L, member.getName(), MemberRole.USER, member.getEmail()));
     }
 
     @Test
