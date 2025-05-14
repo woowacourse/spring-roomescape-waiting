135e9e59b2d238bd82943c2ee14f6140c968e0c3
RIWON <121172065+awrion3@users.noreply.github.com>
2025-05-14 14:51:08 +0900

    feat: ReservationTime 엔티티 매핑

--- src/main/java/roomescape/reservationTime/domain/ReservationTime.java
+++ src/main/java/roomescape/reservationTime/domain/ReservationTime.java
@@ -1,29 +1,31 @@
 package roomescape.reservationTime.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalTime;
 import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor
 public class ReservationTime {
 
-    private final Long id;
-    private final LocalTime startAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalTime startAt;
 
     public ReservationTime(LocalTime startAt) {
-        this.id = null;
         this.startAt = Objects.requireNonNull(startAt);
     }
 
     public ReservationTime(Long id, LocalTime startAt) {
         this.id = Objects.requireNonNull(id);
         this.startAt = Objects.requireNonNull(startAt);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public LocalTime getStartAt() {
-        return startAt;
     }
 
     @Override
