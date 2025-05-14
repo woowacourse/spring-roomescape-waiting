1fff2644e78e36d56bfc74f06d2aa015d73040ec
Minuring <mw9505@gmail.com>
2025-05-14 11:01:30 +0900

    feat: 초기 데이터 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -26,6 +27,7 @@ public class Reservation {
     private User user;
     private LocalDate date;
     @ManyToOne
+    @JoinColumn(name = "time_id")
     private TimeSlot timeSlot;
     @ManyToOne
     private Theme theme;
