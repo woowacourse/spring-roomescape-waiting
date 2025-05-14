d6fd71dc28867065c1d0d26c7f92a8fff5ba4327
junhaa <2171326@hansung.ac.kr>
2025-05-14 14:53:55 +0900

    chore: 썸네일 URL 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,12 +13,17 @@ INSERT INTO reservation_times (start_at)
 VALUES ('15:00');
 
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('a헤일러의 디버깅 교실', '재밌어요', 'debug.jpg');
+VALUES ('a헤일러의 디버깅 교실', '재밌어요',
+        'https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Ftechcourse-storage.s3.ap-northeast-2.amazonaws.com%2Fwooteco.webp&blockId=202af4c7-6ac9-4e3a-bb4e-6a94cca88d48');
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('b우가의 레이어드 아키텍처', '우가우가', 'layered.jpg');
+VALUES ('b우가의 레이어드 아키텍처', '우가우가',
+        'https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Ftechcourse-storage.s3.ap-northeast-2.amazonaws.com%2Fwooteco.webp&blockId=202af4c7-6ac9-4e3a-bb4e-6a94cca88d48');
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('테마명1', '설명1', '썸네일1');
+VALUES ('테마명1', '설명1',
+        'https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Ftechcourse-storage.s3.ap-northeast-2.amazonaws.com%2Fwooteco.webp&blockId=202af4c7-6ac9-4e3a-bb4e-6a94cca88d48');
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('테마명2', '설명2', '썸네일2');
+VALUES ('테마명2', '설명2',
+        'https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Ftechcourse-storage.s3.ap-northeast-2.amazonaws.com%2Fwooteco.webp&blockId=202af4c7-6ac9-4e3a-bb4e-6a94cca88d48');
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('테마명3', '설명', '썸네일');
+VALUES ('테마명3', '설명',
+        'https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Ftechcourse-storage.s3.ap-northeast-2.amazonaws.com%2Fwooteco.webp&blockId=202af4c7-6ac9-4e3a-bb4e-6a94cca88d48');
