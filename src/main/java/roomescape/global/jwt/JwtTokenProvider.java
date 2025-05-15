1277a21dd6a2832f0a868ae6630d2eaf5017da9e
Son JunHyung <kingjh1125@gmail.com>
2025-05-16 00:19:05 +0900

    refactor: jwt 버전 최신화

--- src/main/java/roomescape/global/jwt/JwtTokenProvider.java
+++ src/main/java/roomescape/global/jwt/JwtTokenProvider.java
@@ -2,13 +2,16 @@ package roomescape.global.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Jwts.SIG;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import java.util.Date;
+import javax.crypto.SecretKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,13 +23,9 @@ import roomescape.member.domain.Role;
 @Component
 public class JwtTokenProvider implements TokenProvider {
     private static final String JWT_EXCEPTION_MESSAGE = "잘못된 로그인 시도입니다. 다시 시도해 주세요.";
-
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-    @Value("${security.jwt.token.expire-length}")
-    private long validityInMilliseconds;
-
-    private final static String PREFIX = "token=";
+    private static final SecretKey SECRET_KEY = SIG.HS256.key().build();
+    private static final long validityInMilliseconds = 3600000;
+    private static final String PREFIX = "token=";
 
     @Override
     public String createToken(Member member) {
@@ -36,9 +35,8 @@ public class JwtTokenProvider implements TokenProvider {
         return PREFIX + Jwts.builder()
                 .claim("id", member.getId())
                 .claim("role", member.getRole())
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .expiration(validity)
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
@@ -61,8 +59,8 @@ public class JwtTokenProvider implements TokenProvider {
         }
 
         try {
-            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            return Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+        } catch (SecurityException | MalformedJwtException e) {
             logger.warn("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
             throw new JwtException(JWT_EXCEPTION_MESSAGE);
         } catch (ExpiredJwtException e) {
