a16c37dd10550bb195dc6ef2540fd14a192acae5
saera <yook.saera@gmail.com>
2025-05-15 13:23:23 +0900

    refactor: 예약 테마 엔티티의 양방향 연관관계 매핑 제거
    Co-authored-by: ppparkta <pppppark2@gmail.com>

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -4,9 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,9 +15,6 @@ public class Theme {
     private String name;
     private String description;
     private String thumbnail;
-
-    @OneToMany(mappedBy = "theme")
-    private List<Reservation> reservation = new ArrayList<>();
 
     public Theme() {
     }
@@ -58,10 +52,6 @@ public class Theme {
 
     public String getThumbnail() {
         return thumbnail;
-    }
-
-    public List<Reservation> getReservation() {
-        return reservation;
     }
 
     @Override
--- src/main/java/roomescape/domain/repository/ThemeRepository.java
+++ src/main/java/roomescape/domain/repository/ThemeRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.data.repository.query.Param;
+import roomescape.domain.Reservation;
 import roomescape.domain.Theme;
 
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
