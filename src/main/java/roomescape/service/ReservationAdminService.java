79be0d20ef0c09e2a79f226dcd1f56cccb61a1ce
Lee Dong Yeop <s01054830455@gmail.com>
2025-05-15 15:48:38 +0900

    remove: 사용하지 않는 dao 제거

--- src/main/java/roomescape/service/ReservationAdminService.java
+++ src/main/java/roomescape/service/ReservationAdminService.java
@@ -3,28 +3,29 @@ package roomescape.service;
 import java.time.LocalDate;
 import org.springframework.stereotype.Service;
 import roomescape.common.exception.NotFoundException;
-import roomescape.dao.ReservationDao;
 import roomescape.dto.request.ReservationAdminRegisterDto;
 import roomescape.model.Member;
 import roomescape.model.Reservation;
 import roomescape.model.ReservationTime;
 import roomescape.model.Theme;
 import roomescape.repository.MemberRepository;
+import roomescape.repository.ReservationRepository;
 import roomescape.repository.ReservationTimeRepository;
 import roomescape.repository.ThemeRepository;
 
 @Service
 public class ReservationAdminService {
 
-    private final ReservationDao reservationDao;
+    private final ReservationRepository reservationRepository;
     private final ThemeRepository themeRepository;
     private final ReservationTimeRepository reservationTimeRepository;
     private final MemberRepository memberRepository;
 
-    public ReservationAdminService(final ReservationDao reservationDao, final ThemeRepository themeRepository,
-                                   final ReservationTimeRepository reservationTimeRepository,
-                                   final MemberRepository memberRepository) {
-        this.reservationDao = reservationDao;
+    public ReservationAdminService(ReservationRepository reservationRepository,
+                                   ThemeRepository themeRepository,
+                                   ReservationTimeRepository reservationTimeRepository,
+                                   MemberRepository memberRepository) {
+        this.reservationRepository = reservationRepository;
         this.themeRepository = themeRepository;
         this.reservationTimeRepository = reservationTimeRepository;
         this.memberRepository = memberRepository;
@@ -37,20 +38,20 @@ public class ReservationAdminService {
 
         Reservation reservation = new Reservation(registerDto.date(), reservationTime, theme, member, LocalDate.now());
 
-        reservationDao.saveReservation(reservation);
+        reservationRepository.save(reservation);
     }
 
-    private Theme findThemeById(final Long id) {
+    private Theme findThemeById(Long id) {
         return themeRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("해당 id 를 가진 테마는 존재하지 않습니다."));
     }
 
-    private ReservationTime findReservationTimeById(final Long id) {
+    private ReservationTime findReservationTimeById(Long id) {
         return reservationTimeRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("해당 id 를 가진 예약 시각은 존재하지 않습니다."));
     }
 
-    private Member findMemberById(final Long id) {
+    private Member findMemberById(Long id) {
         return memberRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("해당 id 를 가진 회원은 존재하지 않습니다."));
     }
