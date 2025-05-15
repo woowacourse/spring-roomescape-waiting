5ddb31a45f08497c8220ee8747219e5d663bae57
yeonnhuu <yeonnhuu@gmail.com>
2025-05-15 17:25:00 +0900

    :recycle: refactor: SQL 스크립트 가독성 개선

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,20 +1,18 @@
-DROP TABLE reservation IF EXISTS;
-DROP TABLE member IF EXISTS;
-DROP TABLE theme IF EXISTS;
-DROP TABLE reservation_time IF EXISTS;
+DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS theme;
+DROP TABLE IF EXISTS reservation_time;
 
-CREATE TABLE member
-(
+CREATE TABLE member (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL,
+    role     VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE theme
-(
+CREATE TABLE theme (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
@@ -22,23 +20,21 @@ CREATE TABLE theme
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation_time
-(
+CREATE TABLE reservation_time (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation
-(
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    date     VARCHAR(255) NOT NULL,
-    member_id BIGINT,
-    time_id  BIGINT,
-    theme_id BIGINT,
-    status ENUM('RESERVED'),
+CREATE TABLE reservation (
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    date       VARCHAR(255) NOT NULL,
+    member_id  BIGINT,
+    time_id    BIGINT,
+    theme_id   BIGINT,
+    status     ENUM('RESERVED') NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (time_id)   REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id)  REFERENCES theme (id)
 );
--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,31 +1,39 @@
-INSERT INTO member(name, email, password, role) VALUES('레오', 'admin@gmail.com', 'qwer!', 'ADMIN');
+INSERT INTO member (name, email, password, role)
+VALUES
+    ('레오', 'admin@gmail.com', 'qwer!', 'ADMIN');
 
-INSERT INTO reservation_time(start_at) VALUES('10:00');
-INSERT INTO reservation_time(start_at) VALUES('15:00');
-INSERT INTO reservation_time(start_at) VALUES('16:00');
+INSERT INTO reservation_time (start_at)
+VALUES
+    ('10:00'),
+    ('15:00'),
+    ('16:00');
 
-INSERT INTO theme(name, description, thumbnail) VALUES('테마1', '테마1입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마2', '테마2입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마3', '테마3입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마4', '테마4입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마5', '테마5입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마6', '테마6입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마7', '테마7입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마8', '테마8입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마9', '테마9입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마10', '테마10입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme(name, description, thumbnail) VALUES('테마11', '테마11입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES
+    ('테마1',  '테마1입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마2',  '테마2입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마3',  '테마3입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마4',  '테마4입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마5',  '테마5입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마6',  '테마6입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마7',  '테마7입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마8',  '테마8입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마9',  '테마9입니다.',  'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마10', '테마10입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+    ('테마11', '테마11입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-30', 1, 1, 11, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-30', 1, 2, 11, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-29', 1, 1, 11, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-29', 1, 2, 9, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-28', 1, 2, 9, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-28', 1, 1, 8, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-27', 1, 1, 1, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-27', 1, 2, 2, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-26', 1, 1, 3, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-26', 1, 2, 4, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-25', 1, 1, 5, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-25', 1, 2, 6, 'RESERVED');
-INSERT INTO reservation(date, member_id, time_id, theme_id, status) VALUES('2025-04-24', 1, 1, 7, 'RESERVED');
+INSERT INTO reservation (date, member_id, time_id, theme_id, status)
+VALUES
+    ('2025-04-30', 1, 1, 11, 'RESERVED'),
+    ('2025-04-30', 1, 2, 11, 'RESERVED'),
+    ('2025-04-29', 1, 1, 11, 'RESERVED'),
+    ('2025-04-29', 1, 2,  9, 'RESERVED'),
+    ('2025-04-28', 1, 2,  9, 'RESERVED'),
+    ('2025-04-28', 1, 1,  8, 'RESERVED'),
+    ('2025-04-27', 1, 1,  1, 'RESERVED'),
+    ('2025-04-27', 1, 2,  2, 'RESERVED'),
+    ('2025-04-26', 1, 1,  3, 'RESERVED'),
+    ('2025-04-26', 1, 2,  4, 'RESERVED'),
+    ('2025-04-25', 1, 1,  5, 'RESERVED'),
+    ('2025-04-25', 1, 2,  6, 'RESERVED'),
+    ('2025-04-24', 1, 1,  7, 'RESERVED');
