aa6c20a3e97cbee699c1030fe6ed922fc6a8e85a
yesjuhee <juhee0924@g.skku.edu>
2025-05-15 15:35:02 +0900

    refactor: 요청 DTO의 검증 방식 통일

--- src/main/java/roomescape/theme/dto/CreateThemeRequest.java
+++ src/main/java/roomescape/theme/dto/CreateThemeRequest.java
@@ -1,12 +1,12 @@
 package roomescape.theme.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import roomescape.theme.domain.Theme;
 
 public record CreateThemeRequest(
-        @NotNull(message = "테마명을 입력해주세요.") String name,
-        @NotNull(message = "테마소개를 입력해주세요.") String description,
-        @NotNull(message = "썸네일을 입력해주세요.") String thumbnail
+        @NotBlank(message = "테마명을 입력해주세요.") String name,
+        @NotBlank(message = "테마소개를 입력해주세요.") String description,
+        @NotBlank(message = "썸네일을 입력해주세요.") String thumbnail
 ) {
 
     public Theme convertToTheme() {
