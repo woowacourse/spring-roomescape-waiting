17c3fe0b7bf429ff5449f6a12de9f635bf01e644
Son JunHyung <kingjh1125@gmail.com>
2025-05-16 01:06:42 +0900

    refactor: 중복 코드 제거

--- src/test/java/roomescape/member/presentation/fixture/MemberFixture.java
+++ src/test/java/roomescape/member/presentation/fixture/MemberFixture.java
@@ -9,34 +9,29 @@ public class MemberFixture {
 
     private static final String USER_EMAIL = "user@user.com";
     private static final String USER_PASSWORD = "user";
-    private static final String USER_NAME = "유저";
     private static final String ADMIN_EMAIL = "admin@admin.com";
     private static final String ADMIN_PASSWORD = "admin";
-    private static final String ADMIN = "어드민";
 
     public TokenRequest createLoginRequest(String email, String password) {
         return new TokenRequest(email, password);
     }
 
     public Map<String, String> loginUser() {
-        TokenRequest tokenRequest = createLoginRequest(USER_EMAIL, USER_PASSWORD);
-
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(tokenRequest)
-                .when().post("/login")
-                .then().log().all()
-                .extract().cookies();
+        return login(USER_EMAIL, USER_PASSWORD);
     }
 
     public Map<String, String> loginAdmin() {
-        TokenRequest tokenRequest = createLoginRequest(ADMIN_EMAIL, ADMIN_PASSWORD);
+        return login(ADMIN_EMAIL, ADMIN_PASSWORD);
+    }
 
-        return RestAssured.given().log().all()
+    private Map<String, String> login(String email, String password) {
+        TokenRequest tokenRequest = createLoginRequest(email, password);
+
+        return RestAssured.given().log().ifValidationFails()
                 .contentType(ContentType.JSON)
                 .body(tokenRequest)
                 .when().post("/login")
-                .then().log().all()
+                .then().log().ifValidationFails()
                 .extract().cookies();
     }
 
