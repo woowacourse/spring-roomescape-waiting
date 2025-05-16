4d5ca5109908e0935c473decf2776f07bd33e7df
JO-eusan <goodsan0330@gmail.com>
2025-05-17 00:19:37 +0900

    feat: 이메일 unique 조건 추가

--- src/main/java/roomescape/entity/Member.java
+++ src/main/java/roomescape/entity/Member.java
@@ -1,5 +1,6 @@
 package roomescape.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -14,6 +15,8 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
