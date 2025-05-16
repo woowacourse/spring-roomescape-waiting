51821617a2f3271a759e0c9c60deb8c769cf7820
Jin409 <sualng123@gmail.com>
2025-05-16 16:56:19 +0900

    add: null 이 되어서는 안되는 필드들에 대해 `@Column(nullable = false)` 설정

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -1,5 +1,6 @@
 package roomescape.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private LocalDate date;
 
     @ManyToOne
