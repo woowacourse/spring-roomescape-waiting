5adcaecb62bf94b75831ef35373dacb71e376e54
MinJun Choi <mj043000@naver.com>
2025-05-16 15:10:40 +0900

    refactor: Param 어노테이션 추가

--- src/main/java/roomescape/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/roomescape/reservation/domain/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import roomescape.reservation.domain.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -17,35 +18,39 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findAllWithAssociations();
 
     @Query("""
-              SELECT r FROM Reservation r
-              JOIN FETCH r.time
-              WHERE r.date = :date AND r.theme.id = :themeId
+            SELECT r FROM Reservation r
+            JOIN FETCH r.time
+            WHERE r.date = :date AND r.theme.id = :themeId
             """)
-    List<Reservation> findByDateAndThemeIdWithAssociations(LocalDate date, Long themeId);
+    List<Reservation> findByDateAndThemeIdWithAssociations(
+            @Param("date") LocalDate date,
+            @Param("themeId") Long themeId
+    );
 
     @Query("""
-              SELECT r FROM Reservation r
-              JOIN FETCH r.time
-              JOIN FETCH r.theme
-              JOIN FETCH r.member
-              WHERE (:themeId IS NULL OR r.theme.id = :themeId)
-                AND (:memberId IS NULL OR r.member.id = :memberId)
-                AND (:from IS NULL OR r.date >= :from)
-                AND (:to IS NULL OR r.date <= :to)
+            SELECT r FROM Reservation r
+            JOIN FETCH r.time
+            JOIN FETCH r.theme
+            JOIN FETCH r.member
+            WHERE (:themeId IS NULL OR r.theme.id = :themeId)
+              AND (:memberId IS NULL OR r.member.id = :memberId)
+              AND (:from IS NULL OR r.date >= :from)
+              AND (:to IS NULL OR r.date <= :to)
             """)
     List<Reservation> findByFilteringWithAssociations(
-            Long themeId,
-            Long memberId,
-            LocalDate from,
-            LocalDate to);
+            @Param("themeId") Long themeId,
+            @Param("memberId") Long memberId,
+            @Param("from") LocalDate from,
+            @Param("to") LocalDate to
+    );
 
     @Query("""
-              SELECT r FROM Reservation r
-              JOIN FETCH r.theme
-              JOIN FETCH r.time
-              WHERE r.member.id = :memberId
+            SELECT r FROM Reservation r
+            JOIN FETCH r.theme
+            JOIN FETCH r.time
+            WHERE r.member.id = :memberId
             """)
-    List<Reservation> findByMemberIdWithAssociations(Long memberId);
+    List<Reservation> findByMemberIdWithAssociations(@Param("memberId") Long memberId);
 
     boolean existsByTimeId(Long timeId);
 
