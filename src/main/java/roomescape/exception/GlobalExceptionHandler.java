0f832afaa8b56837c9042bf7e5da6d3a003993f4
ryan-dia <cjfdnjs158@naver.com>
2025-05-17 01:43:57 +0900

    refactor: 중복 코드 메서드화

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -15,47 +16,48 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ReservationException.class, IllegalArgumentException.class})
-    public ResponseEntity<Object> handleBadRequestException(final Exception e, final WebRequest request) {
-        final ProblemDetail body = super.createProblemDetail(e, HttpStatus.BAD_REQUEST, e.getMessage(), null,
-                null, request);
-        return super.handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    public ResponseEntity<Object> handleBadRequestException(final RuntimeException e, final WebRequest request) {
+        return buildResponseEntity(e, HttpStatus.BAD_REQUEST, e.getMessage(), request);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Object> handleNotFoundException(final Exception e, final WebRequest request) {
-        final ProblemDetail body = super.createProblemDetail(e, HttpStatus.NOT_FOUND, e.getMessage(), null,
-                null, request);
-        return super.handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    public ResponseEntity<Object> handleNotFoundException(final NotFoundException e, final WebRequest request) {
+        return buildResponseEntity(e, HttpStatus.NOT_FOUND, e.getMessage(), request);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<Object> handleUnauthorizedException(final Exception e, final WebRequest request) {
-        final ProblemDetail body = super.createProblemDetail(e, HttpStatus.UNAUTHORIZED, e.getMessage(), null,
-                null, request);
+    public ResponseEntity<Object> handleUnauthorizedException(final UnauthorizedException e,
+                                                              final WebRequest request) {
         log.error(e.getMessage(), e);
-        return super.handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+        return buildResponseEntity(e, HttpStatus.UNAUTHORIZED, e.getMessage(), request);
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<Object> handleForbiddenException(final Exception e, final WebRequest request) {
-        final ProblemDetail body = super.createProblemDetail(e, HttpStatus.FORBIDDEN, e.getMessage(), null,
-                null, request);
-        return super.handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+    public ResponseEntity<Object> handleForbiddenException(final ForbiddenException e, final WebRequest request) {
+        return buildResponseEntity(e, HttpStatus.FORBIDDEN, e.getMessage(), request);
     }
 
     @ExceptionHandler(TokenCreationException.class)
-    public ResponseEntity<Object> handleTokenCreationException(final Exception e, final WebRequest request) {
-        final ProblemDetail body = super.createProblemDetail(e, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
-                null, null, request);
-        log.error("JWT 토큰 생성 실패: " + e.getMessage(), e);
-        return super.handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    public ResponseEntity<Object> handleTokenCreationException(final TokenCreationException e,
+                                                               final WebRequest request) {
+        log.error(e.getMessage(), e);
+        return buildResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR, "토큰 생성 중 오류가 발생했습니다.", request);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleInternalServerException(final Exception e, final WebRequest request) {
         log.error(e.getMessage(), e);
-        final ProblemDetail body = super.createProblemDetail(e, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.",
-                null, null, request);
-        return super.handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return buildResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", request);
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(
+            final Exception e,
+            final HttpStatus status,
+            final String message,
+            final WebRequest request
+    ) {
+        final String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        final ProblemDetail body = super.createProblemDetail(e, status, message, path, null, request);
+        return super.handleExceptionInternal(e, body, new HttpHeaders(), status, request);
     }
 }
