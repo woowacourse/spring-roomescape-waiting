5d343c142f029be9c55467ff205fd7d591b00674
taek2222 <oot309@daum.net>
2025-05-16 21:13:49 +0900

    feat: Member 엔티티에 SEQUENCE 기반 ID 생성 전략 적용

--- src/main/java/roomescape/member/domain/Member.java
+++ src/main/java/roomescape/member/domain/Member.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Id;
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false)
--- src/test/resources/testData.sql
+++ src/test/resources/testData.sql
@@ -1,3 +1,3 @@
-insert into member (email, password, name, role)
-values ('user@user.com', 'user', '유저', 'USER'),
-       ('admin@admin.com', 'admin', '어드민', 'ADMIN');
+insert into member (id, email, password, name, role)
+values (1L, 'user@user.com', 'user', '유저', 'USER'),
+       (2L, 'admin@admin.com', 'admin', '어드민', 'ADMIN');
