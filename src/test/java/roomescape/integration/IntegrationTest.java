a03ad119f92f038ef8f68e43c11ebf3481e22d82
ljhee92 <weilainib7@gmail.com>
2025-05-17 11:25:52 +0900

    test: 통합테스트 클래스명 변경

--- src/test/java/roomescape/integration/IntegrationTest.java
+++ src/test/java/roomescape/integration/IntegrationTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class ReservationTest extends BaseTest {
+class IntegrationTest extends BaseTest {
 
     @LocalServerPort
     private int port;
