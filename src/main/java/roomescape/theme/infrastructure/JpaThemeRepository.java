81b2e92fb47cf8fe93ab49841e28fe7c965747be
threepebbles <threepebbles@naver.com>
2025-05-17 21:46:50 +0900

    chore: 불필요한 개행 제거

--- src/main/java/roomescape/theme/infrastructure/JpaThemeRepository.java
+++ src/main/java/roomescape/theme/infrastructure/JpaThemeRepository.java
@@ -11,23 +11,22 @@ public interface JpaThemeRepository extends JpaRepository<Theme, Long> {
     boolean existsByName(String name);
 
     @Query(value = """
-                SELECT
-                    t.id AS id,
-                    t.name AS name,
-                    t.description AS description,
-                    t.thumbnail AS thumbnail,
-                    COUNT(r.id) AS reservation_count
-                FROM themes t
-                LEFT JOIN reservations r 
-                    ON t.id = r.theme_id AND r.date BETWEEN ? AND ?
-                GROUP BY t.id, t.name, t.description, t.thumbnail
-                ORDER BY reservation_count DESC
-                LIMIT ?;
-                """, nativeQuery = true)
+            SELECT
+                t.id AS id,
+                t.name AS name,
+                t.description AS description,
+                t.thumbnail AS thumbnail,
+                COUNT(r.id) AS reservation_count
+            FROM themes t
+            LEFT JOIN reservations r 
+                ON t.id = r.theme_id AND r.date BETWEEN ? AND ?
+            GROUP BY t.id, t.name, t.description, t.thumbnail
+            ORDER BY reservation_count DESC
+            LIMIT ?;
+            """, nativeQuery = true)
     List<Theme> findTopNThemesByReservationCountInDateRange(
             final LocalDate dateFrom,
             final LocalDate dateTo,
             final int limit
     );
-
 }
