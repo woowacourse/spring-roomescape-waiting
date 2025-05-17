a3adfcd40e6a8f754b2159caab45c3a879f70abf
ljhee92 <weilainib7@gmail.com>
2025-05-17 10:47:07 +0900

    refactor: 서버 에러 메시지 수정

--- src/main/java/roomescape/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/presentation/exception/GlobalExceptionHandler.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "알 수 없는 문제 발생했습니다.";
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "알 수 없는 문제가 발생했습니다.";
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
