ba864c1188e3768d6c0c07be09004b04817b137b
ljhee92 <weilainib7@gmail.com>
2025-05-17 10:38:08 +0900

    refactor: request DTO Email 형식 검증 어노테이션 추가, 라인 정리

--- src/main/java/roomescape/presentation/dto/request/AvailableReservationTimeRequest.java
+++ src/main/java/roomescape/presentation/dto/request/AvailableReservationTimeRequest.java
@@ -5,7 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record AvailableReservationTimeRequest(
-        @NotNull(message = "날짜는 필수 선택 사항입니다.") LocalDate date,
-        @NotNull(message = "테마는 필수 선택 사항입니다.") Long themeId
+        @NotNull(message = "날짜는 필수 선택 사항입니다.")
+        LocalDate date,
+
+        @NotNull(message = "테마는 필수 선택 사항입니다.")
+        Long themeId
 ) {
 }
--- src/main/java/roomescape/presentation/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/presentation/dto/request/ReservationCreateRequest.java
@@ -5,8 +5,13 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ReservationCreateRequest(
-        @NotNull(message = "예약 일자는 필수입니다.") LocalDate date,
-        @NotNull(message = "예약 시간은 필수입니다.") Long timeId,
-        @NotNull(message = "예약 테마는 필수입니다.") Long themeId
+        @NotNull(message = "예약 일자는 필수입니다.")
+        LocalDate date,
+
+        @NotNull(message = "예약 시간은 필수입니다.")
+        Long timeId,
+
+        @NotNull(message = "예약 테마는 필수입니다.")
+        Long themeId
 ) {
 }
