c92a90f05ea33e68e43c465643f34d69596d88c5
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-17 13:19:37 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -2,11 +2,8 @@ package roomescape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import roomescape.infrastructure.auth.config.WebMvcConfiguration;
 
 @SpringBootApplication
-@Import(WebMvcConfiguration.class)
 public class RoomescapeApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
