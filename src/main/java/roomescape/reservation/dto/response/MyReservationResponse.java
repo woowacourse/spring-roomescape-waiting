033a1d0fabfb3a3fcbf1782bf141e1d5d859b652
wogus <ind07152@naver.com>
2025-05-17 23:50:44 +0900

    refactor: MyReservationResponse 필드 타입 변경
    - String -> LocalDate, LocalTime

--- src/main/java/roomescape/reservation/dto/response/MyReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/response/MyReservationResponse.java
@@ -1,20 +1,22 @@
 package roomescape.reservation.dto.response;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import roomescape.reservation.domain.Reservation;
 
 public record MyReservationResponse(
         Long reservationId,
         String theme,
-        String date,
-        String time,
+        LocalDate date,
+        LocalTime time,
         String status
 ) {
     public static MyReservationResponse from(Reservation reservation) {
         return new MyReservationResponse(
                 reservation.getId(),
                 reservation.themeName(),
-                reservation.getDate().toString(),
-                reservation.reservationTime().toString(),
+                reservation.getDate(),
+                reservation.reservationTime(),
                 "예약"
         );
     }
