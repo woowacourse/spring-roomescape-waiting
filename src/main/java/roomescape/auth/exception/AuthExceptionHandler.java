6c77a916463d74cc121a897ab139eceeb3f4294b
GiSeok Kang <ime.rokz@gmail.com>
2025-05-18 18:41:51 +0900

    refactor: AuthExceptionHandler 패키지 이동

--- src/main/java/roomescape/auth/exception/AuthExceptionHandler.java
+++ src/main/java/roomescape/auth/exception/AuthExceptionHandler.java
@@ -1,12 +1,10 @@
-package roomescape.common.exception.handler;
+package roomescape.auth.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import roomescape.auth.exception.ForbiddenException;
-import roomescape.auth.exception.UnauthorizedException;
 import roomescape.common.exception.handler.dto.ExceptionResponse;
 
 @RestControllerAdvice
