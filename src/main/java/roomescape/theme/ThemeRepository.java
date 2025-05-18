d8328dfcfa96781be1b25628d14406e652508566
seaniiio <cnc4750@naver.com>
2025-05-18 15:11:36 +0900

    refactor(ThemeRepository): `@Query`에서 native query 대신 JPQL 사용하도록 수정

--- src/main/java/roomescape/theme/ThemeRepository.java
+++ src/main/java/roomescape/theme/ThemeRepository.java
@@ -8,15 +8,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
-    @Query(nativeQuery = true,
-            value = """
-                            SELECT t.id, t.name, t.description, t.thumbnail
-                            FROM theme t INNER JOIN reservation r
-                                ON t.id=r.theme_id
-                            WHERE r.date BETWEEN :from AND :to
-                            GROUP BY t.id, t.name, t.description, t.thumbnail
-                            ORDER BY COUNT(*) DESC
-                            LIMIT :size
-                    """)
+    @Query(value = """
+                    SELECT t
+                    FROM Theme t INNER JOIN Reservation r ON t.id = r.theme.id
+                    WHERE r.date BETWEEN :from AND :to
+                    GROUP BY t.id, t.name, t.description, t.thumbnail
+                    ORDER BY COUNT(*) DESC
+                    LIMIT :size
+            """)
     List<Theme> findAllOrderByRank(@Param("from") LocalDate from, @Param("to") LocalDate to, @Param("size") int size);
 }
