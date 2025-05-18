918a6043ca2b73646dc376261d2496ee2f4ab09e
mimi <110226866+hongkikii@users.noreply.github.com>
2025-05-18 22:01:40 +0900

    refactor: @JoinColumn 명시

--- src/main/java/roomescape/business/domain/Reservation.java
+++ src/main/java/roomescape/business/domain/Reservation.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
@@ -21,12 +22,15 @@ public class Reservation {
     private LocalDate date;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_time_id")
     private ReservationTime reservationTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id")
     private Theme theme;
 
     public Reservation(final Long id, final LocalDate date, final Member member, final ReservationTime reservationTime,
