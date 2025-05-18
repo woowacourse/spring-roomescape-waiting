14ae1b027ff6cd24656574020f67211a3ff95df0
JO-eusan <goodsan0330@gmail.com>
2025-05-19 01:36:40 +0900

    refactor: 예약 엔티티 지연 로딩 전략을 즉시 로딩 전략으로 변경

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -3,22 +3,20 @@ package roomescape.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import roomescape.entity.Reservation;
-import roomescape.entity.ReservationTime;
-import roomescape.entity.Theme;
 
 public record ReservationResponse(
     Long id,
     MemberResponse member,
     @JsonFormat(pattern = "yyyy-MM-dd") LocalDate date,
-    ReservationTime time,
-    Theme theme) {
+    TimeResponse time,
+    ThemeResponse theme) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
             reservation.getId(),
             MemberResponse.from(reservation.getMember()),
             reservation.getDate(),
-            reservation.getTime(),
-            reservation.getTheme());
+            TimeResponse.from(reservation.getTime()),
+            ThemeResponse.from(reservation.getTheme()));
     }
 }
--- src/test/java/roomescape/controller/api/AdminReservationControllerTest.java
+++ src/test/java/roomescape/controller/api/AdminReservationControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class AdminReservationControllerTest {
+class AdminReservationControllerTest {
 
     String token;
 
