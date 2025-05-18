51ca00d1c4da7d320b5fdea51a24e4d48d4d7483
kjungw <sagakkb@gmail.com>
2025-05-18 15:12:24 +0900

    test: 레파지토리 테스트 추가

---
+++ src/test/java/roomescape/repository/ReservationTimeRepositoryTest.java
@@ -0,0 +1,62 @@
+package roomescape.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static roomescape.test.fixture.DateFixture.NEXT_DAY;
+
+import java.time.LocalTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationStatus;
+import roomescape.domain.ReservationTime;
+import roomescape.domain.Role;
+import roomescape.domain.Theme;
+import roomescape.domain.User;
+import roomescape.dto.business.ReservationTimeWithBookState;
+
+@DataJpaTest
+class ReservationTimeRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private ReservationTimeRepository reservationTimeRepository;
+
+    @DisplayName("특정 테마와 날짜의 예약시간을 예약 여부와 함께 조회할 수 있다.")
+    void canFindReservationTimesWithBookState() {
+        // given
+        User member = entityManager.persist(
+                User.createWithoutId(Role.ROLE_MEMBER, "회원", "member@test.com", "password123"));
+        Theme theme = entityManager.persist(
+                Theme.createWithoutId("테마", "테마 설명", "thumbnail.jpg"));
+
+        ReservationTime timeAt10 = entityManager.persist(ReservationTime.createWithoutId(LocalTime.of(10, 0)));
+        ReservationTime timeAt11 = entityManager.persist(ReservationTime.createWithoutId(LocalTime.of(11, 0)));
+        ReservationTime timeAt12 = entityManager.persist(ReservationTime.createWithoutId(LocalTime.of(12, 0)));
+
+        entityManager.persist(Reservation.createWithoutId(
+                NEXT_DAY, ReservationStatus.BOOKED, timeAt10, theme, member));
+        entityManager.persist(Reservation.createWithoutId(
+                NEXT_DAY, ReservationStatus.BOOKED, timeAt11, theme, member));
+
+        entityManager.flush();
+
+        // when
+        List<ReservationTimeWithBookState> timesWithBookState =
+                reservationTimeRepository.findReservationTimesWithBookState(theme, NEXT_DAY);
+
+        // then
+        assertAll(
+                () -> assertThat(timesWithBookState)
+                        .extracting(ReservationTimeWithBookState::id)
+                        .containsExactly(timeAt10.getId(), timeAt11.getId(), timeAt12.getId()),
+                () -> assertThat(timesWithBookState)
+                        .extracting(ReservationTimeWithBookState::alreadyBooked)
+                        .containsExactly(true, true, false)
+        );
+    }
+}
