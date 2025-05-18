4bda91511b0d577b7807ed88723d83ee17b6d38c
Mint <audwl03071@gmail.com>
2025-05-19 00:49:53 +0900

    feat: exists를 사용하여 쿼리 최적화

--- src/main/java/roomescape/reservationtime/repository/JpaReservationTimeRepository.java
+++ src/main/java/roomescape/reservationtime/repository/JpaReservationTimeRepository.java
@@ -1,10 +1,12 @@
 package roomescape.reservationtime.repository;
 
 import java.time.LocalTime;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import roomescape.reservationtime.domain.ReservationTime;
 
 public interface JpaReservationTimeRepository extends ListCrudRepository<ReservationTime, Long> {
 
+    @Query("SELECT EXISTS (SELECT 1 FROM ReservationTime rt WHERE rt.startAt = :time) ")
     boolean existsByStartAt(LocalTime time);
 }
