d8b31be4259c9ec2e977c639ad2fc3fd97c45e57
송선권 <songsunkook@gmail.com>
2025-05-18 15:53:43 +0900

    [1 - 2단계 - 방탈출 예약 대기] 모코(송선권) 미션 제출합니다. (#217)
    * chore: 이전 미션 코드 가져오기
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * feat: Jdbc를 JPA로 마이그레이션
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * test: 테스트에 JPA 적용
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * feat: 내 예약 조회 API 구현
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * feat: 내 예약 페이지 구현
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * test: ReservationTimeService 테스트 추가
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * feat: reservationStatus 필드 추가
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * feat: resourceBundle 추가
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: 불필요한 메서드 제거
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * test: 예외처리를 NullPointerException으로 변경
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: Reservation 정적 팩터리 메서드 적용
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: Theme 정적 팩터리 메서드 적용
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: ReservationTime 정적 팩터리 메서드 적용
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: Member 정적 팩터리 메서드 적용
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: 어드민 권한 확인 책임 서비스로 이동
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * fix: 어드민 권한 확인 에러 수정
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * style: 디렉토리 이름 변경
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: 로깅을 GlobalExceptionHandler가 처리하도록 수정
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * style: 디렉토리 도메인으로 이동
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: 엔티티 필드에서 NonNull 제거
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * feat: 쿠키 생성할 때 maxAge 설정
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: Cookie 관리 클래스 추가
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * style: auth.infrastructure 패키지 구체화
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * style: 불필요한 주석 제
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * style: 코드 포맷팅
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: resourceBundle을 ReservationS
    로 이동
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * chore: 비즈니스 로직 코드와 테스트 코드 application.yml 분리
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * test: MemberServiceTest에서 mock을 DataJpaTest로 변경
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * docs: 기능 목록 업데이트
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>
    
    * refactor: Member.email에 unique 제약조건 명시
    
    * test: 내 모든 예약 조회 테스트 추가
    
    * test: 잘못된 테스트 메서드명 수정
    
    * refactor: 예약 목록 조회 시 쿼리 효율 개선
    
    ---------
    
    Co-authored-by: ryan-dia <cjfdnjs158@naver.com>

---
+++ src/main/java/roomescape/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package roomescape.member.repository;
+
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import roomescape.member.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmailAndPassword(final String email, final String password);
+
+    boolean existsByEmail(final String email);
+}
---
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -0,0 +1,95 @@
+package roomescape.reservation.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import roomescape.exception.ReservationException;
+import roomescape.member.domain.Member;
+import roomescape.reservationtime.domain.ReservationTime;
+import roomescape.theme.domain.Theme;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private ReservationTime time;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Theme theme;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Member member;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status = ReservationStatus.BOOKED;
+
+    @Builder
+    private Reservation(
+            final Long id,
+            @NonNull final LocalDate date,
+            @NonNull final ReservationTime time,
+            @NonNull final Theme theme,
+            @NonNull final Member member,
+            @NonNull final ReservationStatus status
+    ) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.theme = theme;
+        this.member = member;
+        this.status = status;
+    }
+
+    public static Reservation booked(
+            final LocalDate date,
+            final ReservationTime reservationTime,
+            final Theme theme,
+            final Member member
+    ) {
+        return builder()
+                .id(null)
+                .date(date)
+                .time(reservationTime)
+                .theme(theme)
+                .member(member)
+                .status(ReservationStatus.BOOKED)
+                .build();
+    }
+
+    @PrePersist
+    private void validateFutureOrPresent() {
+        final LocalDateTime reservationDateTime = LocalDateTime.of(date, time.getStartAt());
+        final LocalDateTime currentDateTime = LocalDateTime.now();
+        if (reservationDateTime.isBefore(currentDateTime)) {
+            throw new ReservationException("예약은 현재 시간 이후로 가능합니다.");
+        }
+    }
+}
