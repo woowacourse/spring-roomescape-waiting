c7a99dd77cc6ef434bfe0f003ed4d2d5e0086e08
노주희 <juhee0924@g.skku.edu>
2025-05-19 01:46:32 +0900

    [방탈출 예약 대기 STEP1] 노랑(노주희) 미션 제출합니다. (#216)
    * chore: first migration commit
    
    * chore: 의존성 추가 및 환경 설정
    
    * feat: JPA 엔티티 설정
    
    * chore: 테스트 코드 DB 환경 격리
    
    * refactor: `ReservationTimeRepository`를 Jdbc에서 JPA로 변경
    
    * refactor: `ReservationTimeRepository`를 Jdbc에서 JPA 변경으로 인한 불필요한 메서드 삭제
    
    * refactor: `MemberRepository`를 Jdbc에서 JPA 변경
    
    - 리플렉션 오류로 인한 `memberName` 필드명 수정
    
    * chore: 데이터 스키마 변경에 따라 `data.sql` 수정
    
    * refactor: 어드민의 예약 조회 엔드포인트 변경
    
    * refactor: `ThemeRepository`를 Jdbc에서 JPA 변경
    
    * refactor: `ReservationRepository`를 Jdbc에서 JPA 변경
    
    * refactor: 예약 가능 시간 사용 시 `ReservationTimeRepository`를 사용하도록 변경
    
    * docs: 프로그램 설명 문서 추가
    
    * chore: 클라이언트 코드 추가
    
    * feat: 내 예약 조회 API 구현
    
    * refactor: 권한 확인 기능을 서비스로 리팩터링
    
    * refactor: 테스트 추가 및 오류 수정
    
    * refactor: 불필요한 생성자 삭제
    
    * refactor: 게터 사용 방식 통일
    
    - 엔티티에서 값 객체를 꺼낼 경우 객체 그대로 반환하도록 수정
    - 값 객체에서 원시값을 꺼낼 경우 메서드명을 `getValue()`로 통일
    
    * refactor: 요청 DTO의 검증 방식 통일
    
    * refactor: 불필요한 래퍼 타입을 원시타입으로 변경
    
    * refactor: `FakeRepository` 메서드 순서 정렬 및 사용하지 않는 메서드 예외 처리
    
    * chore: `test-schema.sql` 삭제
    
    * fix: 꺠지는 테스트 수정
    
    * test: 인기 테마 조회시 항상 오늘 날짜를 기준으로 생성되도록 테스트용 데이터 수정
    
    * refactor: 엔티티 생성자 리팩터링
    
    - 기본 생성자의 접근제어자를 `protected`로 수정
    - 사용하지 않는 생성자 삭제
    - 불필요한 검증 제거
    
    * refactor: 엔티티의 final 키워드 제거
    
    * refactor: VO의 기본 생성자 접근제어자를 `protected`로 변경
    
    * refactor: `@Embedded` 어노테이션 추가 및 `Password` VO 추가
    
    * refactor: `MemberEmail`, `Password`의 형식 검증 리팩터링
    
    * fix: `Member` 필드 오타 수정
    
    * fix: 변경사항 모두 수정

---
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -0,0 +1,100 @@
+package roomescape.reservation.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import roomescape.global.exception.custom.BadRequestException;
+import roomescape.member.domain.Member;
+import roomescape.theme.domain.Theme;
+import roomescape.time.domain.ReservationTime;
+
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    @ManyToOne
+    private ReservationTime time;
+
+    @ManyToOne
+    private Member member;
+    
+    @ManyToOne
+    private Theme theme;
+
+    public Reservation(final Long id, final Member member, final LocalDate date, final ReservationTime time,
+                       final Theme theme) {
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.member = member;
+        this.theme = theme;
+    }
+
+    public static Reservation register(final Member member, final LocalDate date,
+                                       final ReservationTime time, final Theme theme) {
+        boolean isBefore = false;
+        final LocalDateTime other = LocalDateTime.now();
+        if (date.isBefore(other.toLocalDate())) {
+            isBefore = true;
+        }
+        if (date.equals(other.toLocalDate())) {
+            isBefore = time.isBeforeOrEqual(other.toLocalTime());
+        }
+        if (isBefore) {
+            throw new BadRequestException("지나간 날짜와 시간은 예약 불가합니다.");
+        }
+        return new Reservation(null, member, date, time, theme);
+    }
+
+    protected Reservation() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public ReservationTime getTime() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Reservation that = (Reservation) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
