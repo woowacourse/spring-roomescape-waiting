b1547ee8e3aa9f9efce7b8ab02d6ecbf338a5e9d
DEV_WOONG <ywcsuwon@gmail.com>
2025-05-18 21:55:21 +0900

    refactor: 쿼리문 변경

--- src/main/java/roomescape/repository/JpaReservationRepository.java
+++ src/main/java/roomescape/repository/JpaReservationRepository.java
@@ -5,9 +5,28 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import roomescape.entity.Reservation;
 
 public interface JpaReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query(
+            """
+             SELECT r FROM Reservation r
+             WHERE r.theme.id = :themeId
+             AND r.member.id = :memberId
+             AND r.date > :dateFrom
+             AND r.date < :dateTo
+            """
+    )
+    Optional<List<Reservation>> findReservationsByFilters(
+            @Param("themeId") long themeId,
+            @Param("memberId") long memberId,
+            @Param("dateFrom") LocalDate dateFrom,
+            @Param("dateTo") LocalDate dateTo
+    );
+
 
     Optional<List<Reservation>> findByMemberId(long memberId);
 
