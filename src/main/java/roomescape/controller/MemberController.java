216fbb1d6b8a5ebfdc8a981f02fc95ee5be34f6c
YuyoungRhee <bbclara@naver.com>
2025-05-19 20:20:51 +0900

    refactor(MemberController): 회원가입 성공 응답을 201로 변경

--- src/main/java/roomescape/controller/MemberController.java
+++ src/main/java/roomescape/controller/MemberController.java
@@ -1,15 +1,20 @@
 package roomescape.controller;
 
+import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.controller.request.RegisterMemberRequest;
 import roomescape.controller.response.MemberResponse;
 import roomescape.controller.response.RegisterUserResponse;
 import roomescape.service.MemberService;
 import roomescape.service.param.RegisterMemberParam;
 import roomescape.service.result.MemberResult;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/members")
@@ -21,11 +26,12 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<RegisterUserResponse> signup(@RequestBody final RegisterMemberRequest registerMemberRequest) {
+    public RegisterUserResponse signup(@RequestBody final RegisterMemberRequest registerMemberRequest) {
         RegisterMemberParam registerMemberParam = registerMemberRequest.toServiceParam();
         MemberResult memberResult = memberService.create(registerMemberParam);
-        return ResponseEntity.ok(RegisterUserResponse.from(memberResult));
+        return RegisterUserResponse.from(memberResult);
     }
 
     @GetMapping
