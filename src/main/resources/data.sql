6361841fe39d18684cba79b5483e2cb60238c026
AppleMint98 <applemint9821@gmail.com>
2025-05-19 20:24:18 +0900

    feat(data.sql): 초기 데이터 삽입을 위한 SQL 스크립트 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO MEMBER (NAME, ROLE, EMAIL, PASSWORD)
+VALUES ('admin', 'ADMIN', 'admin@email.com', 'Admin123!@#'),
+       ('제프리', 'USER', 'jeffrey@email.com', 'jeffrey123!@#');
+
+INSERT INTO RESERVATION_TIME(START_AT)
+values ('12:30'),
+       ('11:40'),
+       ('13:50');
+
+INSERT INTO THEME (NAME, DESCRIPTION, THUMBNAIL)
+VALUES ('FILM BY EDDY', 'FILM BY EDDY',
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('FILM BY STEVE', 'FILM BY STEVE',
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('FILM BY BOB', 'FILM BY BOB',
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+
