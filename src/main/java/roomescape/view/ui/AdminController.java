3111b5e4f9409aa3b8dd4606a943f898f1218304
threepebbles <threepebbles@naver.com>
2025-05-19 20:46:17 +0900

    refactor: AdminController 클래스에 RequiresRole 어노테이션을 적용하도록 리팩터링

--- src/main/java/roomescape/view/ui/AdminController.java
+++ src/main/java/roomescape/view/ui/AdminController.java
@@ -1,35 +1,33 @@
 package roomescape.view.ui;
 
+import static roomescape.auth.domain.AuthRole.ADMIN;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import roomescape.auth.domain.AuthRole;
 import roomescape.auth.domain.RequiresRole;
 
 @Controller
 @RequestMapping("/admin")
+@RequiresRole(authRoles = ADMIN)
 public class AdminController {
 
     @GetMapping
-    @RequiresRole(authRoles = AuthRole.ADMIN)
     public String home() {
         return "admin/index";
     }
 
     @GetMapping("/reservation")
-    @RequiresRole(authRoles = AuthRole.ADMIN)
     public String reservation() {
         return "admin/reservation-new";
     }
 
     @GetMapping("/time")
-    @RequiresRole(authRoles = AuthRole.ADMIN)
     public String time() {
         return "admin/time";
     }
 
     @GetMapping("/theme")
-    @RequiresRole(authRoles = AuthRole.ADMIN)
     public String theme() {
         return "admin/theme";
     }
