79d93d38230a80d0dded65c2874a50ed0b9aac94
juno <wnsgh732@gmail.com>
2025-05-19 20:13:36 +0900

    test: Reservation에 추가한 메서드에 대한 단위테스트 추가

--- src/test/java/roomescape/domain/entity/ReservationTest.java
+++ src/test/java/roomescape/domain/entity/ReservationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.ReservationTestFixture;
+import roomescape.member.model.Member;
+import roomescape.member.model.Role;
 import roomescape.reservation.model.dto.ReservationDetails;
 import roomescape.reservation.model.entity.Reservation;
 import roomescape.reservation.model.entity.ReservationTheme;
@@ -24,8 +27,7 @@ class  ReservationTest {
     void createFutureReservationSuccess() {
         //given
         ReservationDetails details = new ReservationDetails(
-                "웨이드",
-                1L,
+                new Member("1234", Role.USER, "a@a.com", "웨이드"),
                 LocalDate.now().plusDays(10),
                 new ReservationTime(LocalTime.of(10, 0)),
                 new ReservationTheme("테마 이름", "테마 설명", "테마 url")
@@ -36,7 +38,7 @@ class  ReservationTest {
 
         //then
         assertThat(reservation.getDate()).isEqualTo(details.date());
-        assertThat(reservation.getMemberId()).isEqualTo(details.memberId());
+        assertThat(reservation.getMember()).isEqualTo(details.member());
     }
 
     @DisplayName("예약 생성시 예약 시간이 과거 시간이면 예외를 발생시킨다")
@@ -44,8 +46,7 @@ class  ReservationTest {
     void createFutureReservationExceptionIfPastGetTime() {
         //given
         ReservationDetails details = new ReservationDetails(
-                "홍길동",
-                1L,
+                new Member("1234", Role.USER, "a@a.com", "홍길동"),
                 LocalDate.now().minusDays(1),
                 new ReservationTime(LocalTime.of(10, 0)),
                 new ReservationTheme("테마 이름", "테마 설명", "테마 url")
@@ -54,5 +55,25 @@ class  ReservationTest {
         //when & then
         assertThatThrownBy(() -> Reservation.createFutureReservation(details))
                 .isInstanceOf(InvalidReservationTimeException.class);
+    }
+
+    @DisplayName("예약 날짜와 시간을 LocalDateTime으로 반환한다 ")
+    @Test
+    void returnLocalDateTime() {
+        //given
+        LocalTime time = LocalTime.of(10, 0);
+        LocalDate date = LocalDate.now().plusDays(10);
+        ReservationDetails details = new ReservationDetails(
+            new Member("1234", Role.USER, "a@a.com", "웨이드"),
+            date,
+            new ReservationTime(time),
+            new ReservationTheme("테마 이름", "테마 설명", "테마 url")
+        );
+
+        //when
+        Reservation reservation = Reservation.createFutureReservation(details);
+
+        //then
+        assertThat(reservation.getReservationDateTime()).isEqualTo(LocalDateTime.of(date, time));
     }
 }
