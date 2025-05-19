2c279739a0e55ca0e90e219a780d738df54ed9dc
saera <yook.saera@gmail.com>
2025-05-20 04:48:25 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/roomescape/domain/repository/MemberRepository.java
+++ src/main/java/roomescape/domain/repository/MemberRepository.java
@@ -1,6 +1,5 @@
 package roomescape.domain.repository;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
