9fc06ec4c7db0c5607180ee9ed36ffcbccba8925
ChoiDongjun <adong0808@naver.com>
2025-05-21 01:48:18 +0900

    feat: 예약 대기 날짜 필드 추가

--- src/main/java/roomescape/domain/Waiting.java
+++ src/main/java/roomescape/domain/Waiting.java
@@ -2,6 +2,7 @@ package roomescape.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
@@ -14,27 +15,31 @@ public class Waiting {
     @ManyToOne
     private Member member;
 
+    private LocalDate date;
+
     @ManyToOne
     private ReservationTime time;
 
     @ManyToOne
     private Theme theme;
 
+    @Column(name = "waiting_order")
     private int order;
 
     protected Waiting() {
     }
 
-    private Waiting(Long id, Member member, ReservationTime time, Theme theme, int order) {
+    private Waiting(Long id, Member member, LocalDate date, ReservationTime time, Theme theme, int order) {
         this.id = id;
         this.member = member;
+        this.date = date;
         this.time = time;
         this.theme = theme;
         this.order = order;
     }
 
-    public static Waiting createNew(Member member, ReservationTime time, Theme theme, int waitingOrder) {
-        return new Waiting(null, member, time, theme, waitingOrder);
+    public static Waiting createNew(Member member, LocalDate date, ReservationTime time, Theme theme, int waitingOrder) {
+        return new Waiting(null, member, date, time, theme, waitingOrder);
     }
 
     public Long getId() {
@@ -43,6 +48,10 @@ public class Waiting {
 
     public Member getMember() {
         return member;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     public ReservationTime getTime() {
@@ -60,11 +69,11 @@ public class Waiting {
     @Override
     public boolean equals(final Object o) {
         if (!(o instanceof Waiting waiting)) return false;
-        return order == waiting.order && Objects.equals(id, waiting.id) && Objects.equals(member, waiting.member) && Objects.equals(time, waiting.time) && Objects.equals(theme, waiting.theme);
+        return order == waiting.order && Objects.equals(id, waiting.id) && Objects.equals(member, waiting.member) && Objects.equals(date, waiting.date) && Objects.equals(time, waiting.time) && Objects.equals(theme, waiting.theme);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, member, time, theme, order);
+        return Objects.hash(id, member, date, time, theme, order);
     }
 }
