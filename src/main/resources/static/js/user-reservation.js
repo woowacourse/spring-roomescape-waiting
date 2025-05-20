de95e42cff49de21292f81d1d39cc4f0e3b7816f
Yehyeok Bang <qkddpgur318@gmail.com>
2025-05-20 22:18:12 +0900

    refactor: 클라이언트 코드 완료된 TODO 제거

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -39,11 +39,6 @@ function renderTheme(themes) {
   themes.forEach(theme => {
     const name = theme.name;
     const themeId = theme.id;
-    /*
-    TODO: [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-          createSlot('theme', theme name, theme id) 형태로 호출 - 완료
-    */
     themeSlots.appendChild(createSlot('theme', name, themeId));
   });
 }
@@ -88,10 +83,6 @@ function checkDateAndTheme() {
 }
 
 function fetchAvailableTimes(date, themeId) {
-  /*
-  TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
-        요청 포맷에 맞게 설정 -완료
-  */
   fetch(`/times/available?date=${date}&themeId=${themeId}`, { // 예약 가능 시간 조회 API endpoint
     method: 'GET',
     headers: {
@@ -117,10 +108,6 @@ function renderAvailableTimes(times) {
     return;
   }
   times.forEach(time => {
-    /*
-    TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정 - 완료
-    */
     const startAt = time.startAt;
     const timeId = time.timeId;
     const alreadyBooked = time.booked;
