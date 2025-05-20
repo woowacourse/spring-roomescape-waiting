a0c1809cfa853b62b0428265bf062d8e1fe82be9
songsunkook <songsunkook@gmail.com>
2025-05-20 20:14:13 +0900

    feat: 예약 대기 관리 페이지 정상 출력

--- src/main/java/roomescape/reservation/controller/AdminReservationController.java
+++ src/main/java/roomescape/reservation/controller/AdminReservationController.java
@@ -1,13 +1,14 @@
 package roomescape.reservation.controller;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import roomescape.reservation.dto.AdminReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
 import roomescape.reservation.service.ReservationService;
@@ -18,6 +19,13 @@ import roomescape.reservation.service.ReservationService;
 public class AdminReservationController {
 
     private final ReservationService reservationService;
+
+    /**
+     TODO 4단계
+     ### 예약 대기 취소(어드민)
+     ### 예약 대기 승인(어드민)
+     - 예약 상태인 예약을 취소하면 자동으로 1번 대기 요청이 승인됩니다.
+     */
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
