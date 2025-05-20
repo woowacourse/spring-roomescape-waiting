f970854bc0a91997bb51a218bbea1cdf213569ff
eunseongu <rndmstjs75@gmail.com>
2025-05-20 17:02:38 +0900

    test: 인기 테마 조회 시 오늘 날짜를 기준으로 조회되도록 테스트 데이터 수정

--- src/test/resources/test-popular-theme-data.sql
+++ src/test/resources/test-popular-theme-data.sql
@@ -9,30 +9,30 @@ VALUES ('테마4', '테마4입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc
 
 -- 2 -> 1 -> 3 인기 테마
 
--- 테마1은 0501 ~ 0503 동안 2번 예약됨
+-- 테마1은 최근 일주일 동안 2번 예약됨
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-05-11', 1, 1, 1);
+VALUES (DATEADD('DAY', -1, CURRENT_DATE), 1, 1, 1);
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-05-12', 1, 1, 1);
+VALUES (DATEADD('DAY', -2, CURRENT_DATE), 1, 1, 1);
 
--- 테마2는 0501 ~ 0503 동안 3번 예약됨
+-- 테마2는 최근 일주일 동안 3번 예약됨
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-05-11', 2, 1, 2);
+VALUES (DATEADD('DAY', -1, CURRENT_DATE), 2, 1, 2);
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-05-12', 2, 1, 2);
+VALUES (DATEADD('DAY', -2, CURRENT_DATE), 2, 1, 2);
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-05-13', 1, 1, 2);
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 1, 2);
 
--- 테마3은 0501 ~ 0503 동안 1번 예약됨
+-- 테마3은 최근 일주일 동안 1번 예약됨
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-05-11', 3, 1, 3);
+VALUES (DATEADD('DAY', -1, CURRENT_DATE), 3, 1, 3);
 
--- 테마4는 0401 ~ 0404 동안 4번 예약됨
+-- 테마4는 4번 예약됐지만, 최근 일주일보다 과거의 시점임
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-04-11', 1, 1, 4);
+VALUES (DATEADD('DAY', -8, CURRENT_DATE), 1, 1, 4);
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-04-12', 1, 1, 4);
+VALUES (DATEADD('DAY', -9, CURRENT_DATE), 1, 1, 4);
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-04-13', 1, 1, 4);
+VALUES (DATEADD('DAY', -10, CURRENT_DATE), 1, 1, 4);
 INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES ('2025-04-14', 1, 1, 4);
+VALUES (DATEADD('DAY', -11, CURRENT_DATE), 1, 1, 4);
