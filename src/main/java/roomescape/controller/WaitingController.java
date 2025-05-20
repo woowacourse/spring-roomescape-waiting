d5036e964ab9127bc5352ec7c875d4dd8eb19af4
supernovaMK <minkikim2002@gmail.com>
2025-05-20 20:36:19 +0900

    feat: 예약 대기 컨트롤러 추가

---
+++ src/main/java/roomescape/controller/WaitingController.java
@@ -0,0 +1,31 @@
+package roomescape.controller;
+
+import jakarta.validation.Valid;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.Member;
+import roomescape.dto.reservation.ReservationRequest;
+import roomescape.dto.waiting.WaitingResponse;
+import roomescape.service.waiting.WaitingService;
+
+@RestController
+@RequestMapping("waiting")
+public class WaitingController {
+
+    private final WaitingService waitingService;
+
+    public WaitingController(WaitingService waitingService) {
+        this.waitingService = waitingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<WaitingResponse> createWaiting(@Valid @RequestBody ReservationRequest request,
+                                                         Member member) {
+        WaitingResponse waitingResponse = waitingService.create(request, member);
+        return ResponseEntity.created(URI.create("/waiting/" + waitingResponse.id())).body(waitingResponse);
+    }
+}
