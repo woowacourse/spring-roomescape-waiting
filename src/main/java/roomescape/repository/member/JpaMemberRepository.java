d3f87f0fb92b9c6ddf3eccaf15665ec8b292f7fa
kimdev <kim__dev@hufs.ac.kr>
2025-05-20 13:27:19 +0900

    refactor: JpaRepository를 상속받은 레포지토리 클래스에 붙은 @Repository 애너테이션 삭제

--- src/main/java/roomescape/repository/member/JpaMemberRepository.java
+++ src/main/java/roomescape/repository/member/JpaMemberRepository.java
@@ -2,10 +2,8 @@ package roomescape.repository.member;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import roomescape.domain.member.Member;
 
-@Repository
 public interface JpaMemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
--- src/main/java/roomescape/repository/reservationtime/JpaReservationTimeRepository.java
+++ src/main/java/roomescape/repository/reservationtime/JpaReservationTimeRepository.java
@@ -2,10 +2,8 @@ package roomescape.repository.reservationtime;
 
 import java.time.LocalTime;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import roomescape.domain.reservationtime.ReservationTime;
 
-@Repository
 public interface JpaReservationTimeRepository extends JpaRepository<ReservationTime, Long> {
 
     boolean existsByTime(LocalTime time);
