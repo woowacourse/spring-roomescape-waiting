12dc1572381e0ecdbb74b50fc0033fbd06f2bb5f
taek2222 <oot309@daum.net>
2025-05-20 20:40:18 +0900

    refactor: 불필요한 유효성 검사 어노테이션 제거

--- src/main/java/roomescape/reservation/presentation/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/presentation/dto/ReservationRequest.java
@@ -1,8 +1,6 @@
 package roomescape.reservation.presentation.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class ReservationRequest {
