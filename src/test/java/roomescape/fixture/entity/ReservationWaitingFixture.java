5addf41345396a224c9ea7b5cfdada2e2f4d3106
7i.w__k <tizmfns1218@Naver.com>
2025-05-20 20:18:36 +0900

    feat: 엔티티 fixture 추가

---
+++ src/test/java/roomescape/fixture/entity/ReservationWaitingFixture.java
@@ -0,0 +1,25 @@
+package roomescape.fixture.entity;
+
+import roomescape.member.domain.Member;
+import roomescape.reservation.domain.ReservationDateTime;
+import roomescape.reservation.domain.ReservationWaiting;
+import roomescape.theme.domain.Theme;
+
+public class ReservationWaitingFixture {
+
+    public static ReservationWaiting create() {
+        return ReservationWaiting.builder()
+                .reserver(MemberFixture.createUser())
+                .reservationDatetime(ReservationDateTimeFixture.create())
+                .theme(ThemeFixture.create())
+                .build();
+    }
+
+    public static ReservationWaiting create(Member member, ReservationDateTime dateTime, Theme theme) {
+        return ReservationWaiting.builder()
+                .reserver(member)
+                .reservationDatetime(dateTime)
+                .theme(theme)
+                .build();
+    }
+} 
