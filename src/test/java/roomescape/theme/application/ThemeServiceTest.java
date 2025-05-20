fa355f29b1cc3f52291a6a63e4e6cc0c94ec47ab
threepebbles <threepebbles@naver.com>
2025-05-20 20:55:33 +0900

    test(ThemeService): 인기 테마 목록 조회 기능 테스트 추가

--- src/test/java/roomescape/theme/application/ThemeServiceTest.java
+++ src/test/java/roomescape/theme/application/ThemeServiceTest.java
@@ -1,9 +1,12 @@
 package roomescape.theme.application;
 
 
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static roomescape.fixture.domain.MemberFixture.notSavedMember1;
+
+import java.time.LocalDate;
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import roomescape.exception.resource.AlreadyExistException;
 import roomescape.fixture.config.TestConfig;
+import roomescape.fixture.domain.ReservationTimeFixture;
+import roomescape.fixture.domain.ThemeFixture;
+import roomescape.member.domain.Member;
+import roomescape.member.domain.MemberRepository;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.domain.ReservationRepository;
+import roomescape.reservation.domain.ReservationStatus;
+import roomescape.reservation.domain.ReservationTime;
+import roomescape.reservation.domain.ReservationTimeRepository;
 import roomescape.theme.domain.Theme;
 import roomescape.theme.domain.ThemeRepository;
 import roomescape.theme.ui.dto.CreateThemeRequest;
@@ -27,6 +39,15 @@ class ThemeServiceTest {
 
     @Autowired
     private ThemeRepository themeRepository;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private ReservationTimeRepository reservationTimeRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     void 테마를_저장한다() {
@@ -75,7 +96,7 @@ class ThemeServiceTest {
         final List<ThemeResponse> themes = themeService.findAll();
 
         // then
-        SoftAssertions.assertSoftly(softly -> {
+        assertSoftly(softly -> {
             softly.assertThat(themes).hasSize(2);
             softly.assertThat(themes.get(0).name()).isEqualTo(name1);
             softly.assertThat(themes.get(0).description()).isEqualTo(description1);
@@ -100,5 +121,73 @@ class ThemeServiceTest {
         // when & then
         Assertions.assertThatThrownBy(() -> themeService.create(request))
                 .isInstanceOf(AlreadyExistException.class);
+    }
+
+    @Test
+    void 인기_테마_목록을_조회한다() {
+        // given
+        final LocalDate now = LocalDate.now();
+        final LocalDate weekAgo = now.minusDays(7);
+
+        final int themeSize = 7;
+        final List<Theme> themes = ThemeFixture.notSavedThemes(themeSize);
+        for (final Theme theme : themes) {
+            themeRepository.save(theme);
+        }
+
+        final List<ReservationTime> times = ReservationTimeFixture.notSavedReservationTimes(11);
+        for (final ReservationTime time : times) {
+            reservationTimeRepository.save(time);
+        }
+
+        final Member member = memberRepository.save(notSavedMember1());
+
+        // theme.get(i) 테마에 예약 themeCounts.get(i)개 추가
+        final List<Integer> themeCounts = List.of(5, 3, 4, 6, 2);
+        for (int themeIndex = 0; themeIndex < 5; themeIndex++) {
+            for (int timeIndex = 0; timeIndex < themeCounts.get(themeIndex); timeIndex++) {
+                reservationRepository.save(
+                        new Reservation(
+                                now.minusDays(themeIndex), times.get(timeIndex), themes.get(themeIndex), member,
+                                ReservationStatus.CONFIRMED
+                        )
+                );
+            }
+        }
+
+        // theme.get(5)는 weekAgo보다 이전 날짜로 예약 10개 추가 -> weekAgo~now 기간에는 예약 0개로 취급되어야 함
+        for (int timeIndex = 0; timeIndex < 10; timeIndex++) {
+            reservationRepository.save(
+                    new Reservation(
+                            weekAgo.minusDays(2), times.get(timeIndex), themes.get(5), member,
+                            ReservationStatus.CONFIRMED
+                    )
+            );
+        }
+
+        // theme.get(6) 테마는 now날짜에 예약 1개, now보다 이후 날짜에 예약 10개 -> weekAgo~now 기간에는 예약 1개로 취급되어야 함
+        for (int timeIndex = 0; timeIndex < 11; timeIndex++) {
+            reservationRepository.save(
+                    new Reservation(
+                            now.plusDays(timeIndex), times.get(timeIndex), themes.get(6), member,
+                            ReservationStatus.CONFIRMED
+                    )
+            );
+        }
+
+        // when
+        final List<ThemeResponse> popularThemes = themeService.findPopularThemes();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(popularThemes).hasSize(themes.size());
+            softly.assertThat(popularThemes.get(0).id()).isEqualTo(themes.get(3).getId());
+            softly.assertThat(popularThemes.get(1).id()).isEqualTo(themes.get(0).getId());
+            softly.assertThat(popularThemes.get(2).id()).isEqualTo(themes.get(2).getId());
+            softly.assertThat(popularThemes.get(3).id()).isEqualTo(themes.get(1).getId());
+            softly.assertThat(popularThemes.get(4).id()).isEqualTo(themes.get(4).getId());
+            softly.assertThat(popularThemes.get(5).id()).isEqualTo(themes.get(6).getId());
+            softly.assertThat(popularThemes.get(6).id()).isEqualTo(themes.get(5).getId());
+        });
     }
 }
