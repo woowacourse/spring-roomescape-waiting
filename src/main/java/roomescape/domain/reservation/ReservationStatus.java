3203a25bf202ee330d77c6bb09640665d5b889e1
RaZel713 <razel7130@gmail.com>
2025-05-20 13:42:31 +0900

    refactor: 미사용 ReservationStatus 제거

--- src/main/java/roomescape/domain/reservation/ReservationStatus.java
+++ src/main/java/roomescape/domain/reservation/ReservationStatus.java
@@ -2,7 +2,5 @@ package roomescape.domain.reservation;
 
 public enum ReservationStatus {
     RESERVED,
-    HOLD,     // 예약 대기 상태 (추후 추가될 기능에서 사용)
-    REJECTED, // 예약 거절 상태 (추후 추가될 기능에서 사용)
-    OVERDUE;  // 예약 만료 상태 (추후 추가될 기능에서 사용)
+    ;
 }
