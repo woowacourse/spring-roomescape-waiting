2b05c4015ae71060e5cdd724e5fb9d98571b275d
saera <yook.saera@gmail.com>
2025-05-22 00:01:58 +0900

    refactor: 불필요한 애너테이션 삭제

--- src/main/java/roomescape/domain/repository/MemberRepository.java
+++ src/main/java/roomescape/domain/repository/MemberRepository.java
@@ -2,10 +2,8 @@ package roomescape.domain.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import roomescape.domain.entity.Member;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmailAndPassword(String email, String password);
