83e6083d9cc4bf467c73c30c30ba16b3e45c2da8
supernovaMK <minkikim2002@gmail.com>
2025-05-21 13:27:02 +0900

    feat: 예약 대기 취소 기능 구현

--- src/main/java/roomescape/controller/WaitingController.java
+++ src/main/java/roomescape/controller/WaitingController.java
@@ -3,6 +3,8 @@ package roomescape.controller;
 import jakarta.validation.Valid;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,11 @@ public class WaitingController {
                                                          Member member) {
         WaitingResponse waitingResponse = waitingService.create(request, member);
         return ResponseEntity.created(URI.create("/waiting/" + waitingResponse.id())).body(waitingResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWaiting(@PathVariable Long id) {
+        waitingService.deleteWaitingById(id);
+        return ResponseEntity.ok().build();
     }
 }
