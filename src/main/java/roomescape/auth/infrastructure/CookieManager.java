5e4705280a22897eda98ccf9895a5cc75340e5be
Mint <audwl03071@gmail.com>
2025-05-21 14:27:54 +0900

    chore: cookie 보안 설정 추가

--- src/main/java/roomescape/auth/infrastructure/CookieManager.java
+++ src/main/java/roomescape/auth/infrastructure/CookieManager.java
@@ -1,24 +1,38 @@
 package roomescape.auth.infrastructure;
 
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @Component
 public class CookieManager {
+
+    @Value("${cookie.domain}")
+    private String domain;
+
+    @Value("${cookie.max-age}")
+    private long maxAge;
 
     public ResponseCookie makeCookie(final String name, final String value) {
         return ResponseCookie.from(name, value)
                 .httpOnly(true)
+                .sameSite("Strict")
                 .path("/")
+                .domain(domain)
+                .maxAge(maxAge)
                 .build();
     }
 
     public void deleteCookie(final HttpServletResponse response, final String name) {
         ResponseCookie cookie = ResponseCookie.from(name, "")
                 .httpOnly(true)
+                .sameSite("Strict")
                 .path("/")
+                .domain(domain)
+                .maxAge(0)
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
