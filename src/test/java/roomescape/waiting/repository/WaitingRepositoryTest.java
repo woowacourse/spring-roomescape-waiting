d38d795705c5dd8d678a0955c573ff3843bd0630
7i.w__k <tizmfns1218@Naver.com>
2025-05-21 20:49:54 +0900

    test: WaitingRepository

--- src/test/java/roomescape/waiting/repository/WaitingRepositoryTest.java
+++ src/test/java/roomescape/waiting/repository/WaitingRepositoryTest.java
@@ -1,5 +1,6 @@
 package roomescape.waiting.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
@@ -10,10 +11,10 @@ import roomescape.fixture.db.ReservationDateTimeDbFixture;
 import roomescape.fixture.db.ThemeDbFixture;
 import roomescape.member.domain.Member;
 import roomescape.reservation.domain.ReservationDateTime;
+import roomescape.theme.domain.Theme;
 import roomescape.waiting.controller.response.WaitingInfoResponse;
 import roomescape.waiting.domain.Waiting;
 import roomescape.waiting.dto.WaitingWithRank;
-import roomescape.theme.domain.Theme;
 
 @CustomJpaTest
 class WaitingRepositoryTest {
@@ -157,6 +158,85 @@ class WaitingRepositoryTest {
         SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(소유자_확인).isTrue();
             softly.assertThat(다른_회원_확인).isFalse();
+        });
+    }
+
+    @Test
+    void 날짜와_시간으로_대기_예약_존재_여부를_확인할_수_있다() {
+        // given
+        Member 유저1 = memberDbFixture.유저1_생성();
+        Theme 공포 = themeDbFixture.공포();
+        ReservationDateTime 내일_열시 = reservationDateTimeDbFixture.내일_열시();
+
+        Waiting waiting = Waiting.builder()
+                .reserver(유저1)
+                .reservationDatetime(내일_열시)
+                .theme(공포)
+                .build();
+        waitingRepository.save(waiting);
+
+        // when
+        boolean 존재함 = waitingRepository.existsByDateAndTimeId(
+                내일_열시.getDate(),
+                내일_열시.getReservationTime().getId()
+        );
+        boolean 존재하지않음 = waitingRepository.existsByDateAndTimeId(
+                내일_열시.getDate().plusDays(1),
+                내일_열시.getReservationTime().getId()
+        );
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(존재함).isTrue();
+            softly.assertThat(존재하지않음).isFalse();
+        });
+    }
+
+    @Test
+    void 예약_대기_중_ID가_제일_작은_엔티티를_가져온다() {
+        // given
+        Member 유저1 = memberDbFixture.유저1_생성();
+        Member 유저2 = memberDbFixture.유저2_생성();
+        Theme 공포 = themeDbFixture.공포();
+        ReservationDateTime 내일_열시 = reservationDateTimeDbFixture.내일_열시();
+        ReservationDateTime 내일_열한시 = reservationDateTimeDbFixture.내일_열한시();
+
+        LocalDate date = 내일_열시.getDate();
+        Long timeId = 내일_열시.getTimeId();
+
+        Waiting waiting1 = Waiting.builder()
+                .reserver(유저2)
+                .reservationDatetime(내일_열시)
+                .theme(공포)
+                .build();
+
+        waitingRepository.save(waiting1);
+
+        Waiting waiting2 = Waiting.builder()
+                .reserver(유저1)
+                .reservationDatetime(내일_열시)
+                .theme(공포)
+                .build();
+
+        waitingRepository.save(waiting2);
+
+        Waiting waiting3 = Waiting.builder()
+                .reserver(유저1)
+                .reservationDatetime(내일_열한시)
+                .theme(공포)
+                .build();
+
+        waitingRepository.save(waiting3);
+
+        // when
+        Waiting firstWaiting = waitingRepository.findByDateAndTimeId(date, timeId).getFirst();
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(firstWaiting).isNotNull();
+            softly.assertThat(firstWaiting.getReservationDatetime().getDate()).isEqualTo(date);
+            softly.assertThat(firstWaiting.getReservationDatetime().getTimeId()).isEqualTo(timeId);
+            softly.assertThat(firstWaiting.getId()).isEqualTo(waiting1.getId());
         });
     }
 }
