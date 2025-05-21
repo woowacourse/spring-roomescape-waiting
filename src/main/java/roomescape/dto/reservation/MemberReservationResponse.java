6b04675c8b89aeec29da1cb6277b6d12b35af8d5
rladmstn <oz33177351@gmail.com>
2025-05-21 16:51:20 +0900

    refactor: 예약 상태 메세지 수정

--- src/main/java/roomescape/dto/reservation/MemberReservationResponse.java
+++ src/main/java/roomescape/dto/reservation/MemberReservationResponse.java
@@ -3,20 +3,27 @@ package roomescape.dto.reservation;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import roomescape.domain.Reservation;
+import roomescape.domain.ReservationStatus;
 
-public record MemberReservationResponse(Long reservationId,
+public record MemberReservationResponse(Long id,
                                         String theme,
                                         LocalDate date,
                                         LocalTime time,
                                         String status) {
 
     public static MemberReservationResponse from(Reservation reservation) {
+        ReservationStatus status = reservation.getStatus();
+        String statusMessage = status.getStatus().getValue();
+
+        if (status.getStatus().isWaiting()) {
+            statusMessage = String.format("%d번째 예약대기", status.getPriority());
+        }
         return new MemberReservationResponse(
                 reservation.getId(),
                 reservation.getTheme().getName(),
                 reservation.getDate(),
                 reservation.getTime().getStartAt(),
-                "예약"
+                statusMessage
         );
     }
 }
