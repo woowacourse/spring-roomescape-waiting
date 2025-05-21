89d506f8c9ff81e8ad0af5d8624e29ac91f35b4f
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-21 10:51:08 +0900

    feat: AuthService 추가 및 Facade 에서 사용하도록 구현

---
+++ src/main/java/roomescape/auth/service/AuthService.java
@@ -0,0 +1,30 @@
+package roomescape.auth.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.auth.dto.request.LoginRequest;
+import roomescape.auth.infrastructure.AuthorizationPayload;
+import roomescape.auth.infrastructure.AuthorizationPrincipal;
+import roomescape.auth.infrastructure.provider.AuthorizationProvider;
+import roomescape.exception.UnauthorizedException;
+import roomescape.member.domain.Member;
+
+@Service
+public class AuthService {
+
+    private final AuthorizationProvider authorizationProvider;
+
+    public AuthService(AuthorizationProvider authorizationProvider) {
+        this.authorizationProvider = authorizationProvider;
+    }
+
+    public AuthorizationPrincipal login(Member member, LoginRequest request) {
+        validateInvalidLogin(member, request);
+        return authorizationProvider.createPrincipal(AuthorizationPayload.fromMember(member));
+    }
+
+    public void validateInvalidLogin(Member member, LoginRequest request) {
+        if (!member.checkInvalidLogin(request.email(), request.password())) {
+            throw new UnauthorizedException("이메일 혹은 비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
--- src/main/java/roomescape/auth/service/AuthServiceFacade.java
+++ src/main/java/roomescape/auth/service/AuthServiceFacade.java
@@ -2,10 +2,8 @@ package roomescape.auth.service;
 
 import org.springframework.stereotype.Service;
 import roomescape.auth.dto.request.LoginRequest;
-import roomescape.auth.infrastructure.AuthorizationPayload;
 import roomescape.auth.infrastructure.AuthorizationPrincipal;
 import roomescape.auth.infrastructure.methodargument.MemberPrincipal;
-import roomescape.auth.infrastructure.provider.AuthorizationProvider;
 import roomescape.exception.NotFoundException;
 import roomescape.exception.UnauthorizedException;
 import roomescape.member.domain.Member;
@@ -14,28 +12,22 @@ import roomescape.member.service.MemberService;
 @Service
 public class AuthServiceFacade {
 
+    private final AuthService authService;
     private final MemberService memberService;
-    private final AuthorizationProvider authorizationProvider;
 
     public AuthServiceFacade(
-        MemberService memberService,
-        AuthorizationProvider authorizationProvider
+        AuthService authService,
+        MemberService memberService
     ) {
+        this.authService = authService;
         this.memberService = memberService;
-        this.authorizationProvider = authorizationProvider;
     }
 
     public AuthorizationPrincipal login(LoginRequest request) {
         Member member = memberService.findByEmail(request.email())
             .orElseThrow(() -> new UnauthorizedException("인증되지 않은 유저 정보입니다."));
-        if (checkInvalidLogin(member, request)) {
-            throw new UnauthorizedException("인증되지 않은 유저 정보입니다.");
-        }
-        return authorizationProvider.createPrincipal(AuthorizationPayload.fromMember(member));
-    }
 
-    public boolean checkInvalidLogin(Member member, LoginRequest request) {
-        return member.checkInvalidLogin(request.email(), request.password());
+        return authService.login(member, request);
     }
 
     public void validateMemberExistence(MemberPrincipal memberPrincipal) {
