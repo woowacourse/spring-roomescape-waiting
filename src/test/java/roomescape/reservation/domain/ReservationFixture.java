bf9112f9443ede138c62e7fcd4e6b8a10ac8f94b
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-21 13:57:57 +0900

    feat: 예약 Fixture 구현

---
+++ src/test/java/roomescape/reservation/domain/ReservationFixture.java
@@ -0,0 +1,33 @@
+package roomescape.reservation.domain;
+
+import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.member.domain.MemberRole;
+import roomescape.member.fixture.MemberFixture;
+import roomescape.reservationtime.domain.ReservationTimeFixture;
+import roomescape.theme.domain.ThemeFixture;
+
+public class ReservationFixture {
+    private static final AtomicLong identifier = new AtomicLong(1L);
+
+    public static Reservation create() {
+        long id = identifier.getAndIncrement();
+        return new Reservation(
+            id,
+            LocalDate.now(),
+            ReservationTimeFixture.create(),
+            ThemeFixture.create(),
+            MemberFixture.create(MemberRole.USER)
+        );
+    }
+
+    public static Reservation createWithoutId() {
+        long id = identifier.getAndIncrement();
+        return new Reservation(
+            LocalDate.now(),
+            ReservationTimeFixture.create(),
+            ThemeFixture.create(),
+            MemberFixture.createWithoutId(MemberRole.USER)
+        );
+    }
+}
