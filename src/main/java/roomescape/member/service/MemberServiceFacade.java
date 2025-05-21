fc289ebb286904fce79bca6e23baca78fba3a3f9
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-21 11:47:28 +0900

    remove: 사용하지 않는 메서드 제거

--- src/main/java/roomescape/member/service/MemberServiceFacade.java
+++ src/main/java/roomescape/member/service/MemberServiceFacade.java
@@ -1,13 +1,10 @@
 package roomescape.member.service;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import roomescape.auth.dto.request.MemberSignUpRequest;
 import roomescape.auth.dto.response.MemberSignUpResponse;
-import roomescape.auth.infrastructure.methodargument.MemberPrincipal;
-import roomescape.member.domain.Member;
 import roomescape.member.dto.response.MemberNameSelectResponse;
 
 @Service
@@ -24,27 +21,7 @@ public class MemberServiceFacade {
         return memberService.signup(request);
     }
 
-    public Member findMemberById(Long id) {
-        return memberService.findExistingMemberById(id);
-    }
-
-    public Member findExistingMemberByPrincipal(MemberPrincipal memberPrincipal) {
-        return memberService.findExistingMemberByPrincipal(memberPrincipal);
-    }
-
-    public Optional<Member> findByEmail(String email) {
-        return memberService.findByEmail(email);
-    }
-
-    public boolean isExistMemberById(Long id) {
-        return memberService.isExistMemberById(id);
-    }
-
     public List<MemberNameSelectResponse> findMemberNames() {
         return memberService.findMemberNames();
-    }
-
-    public boolean existsByName(String name) {
-        return memberService.existsByName(name);
     }
 }
