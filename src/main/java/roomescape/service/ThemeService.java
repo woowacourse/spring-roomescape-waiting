88cb21388f9ca5bca0b299bd9d9b64217c1ad2db
jhan0121 <jhan0121@gmail.com>
2025-05-21 11:50:28 +0900

    feat: 특정 기간 내 테마 랭킹 로직 오류 수정

--- src/main/java/roomescape/service/ThemeService.java
+++ src/main/java/roomescape/service/ThemeService.java
@@ -3,6 +3,7 @@ package roomescape.service;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.domain.ThemeRanking;
 import roomescape.dto.request.CreateThemeRequest;
 import roomescape.entity.Reservation;
@@ -12,6 +13,7 @@ import roomescape.repository.ReservationRepository;
 import roomescape.repository.ThemeRepository;
 
 @Service
+@Transactional
 public class ThemeService {
 
     private static final int THEME_RANKING_END_RANGE = 7;
@@ -28,7 +30,7 @@ public class ThemeService {
     public Theme addTheme(CreateThemeRequest request) {
         Theme theme = request.toTheme();
         boolean existsByName = themeRepository.existsByName(theme.getName());
-        if (existsByName){
+        if (existsByName) {
             throw new InvalidThemeException("중복된 테마 이름입니다.");
         }
         return themeRepository.save(theme);
@@ -47,7 +49,7 @@ public class ThemeService {
 
     public List<Theme> getRankingThemes(LocalDate originDate) {
         LocalDate end = originDate.minusDays(THEME_RANKING_START_RANGE);
-        LocalDate start = end.minusDays(THEME_RANKING_END_RANGE);
+        LocalDate start = originDate.minusDays(THEME_RANKING_END_RANGE);
 
         List<Reservation> inRangeReservations = reservationRepository.findAllByDateBetween(start, end);
 
