637f3d6ccf1d6feb5b4067eed91ae6172b675a27
julie <7jujubuilds@gmail.com>
2025-05-21 21:29:37 +0900

    refactor(reservation): Reservation 조회 쿼리에 JOIN FETCH 적용

--- src/main/java/roomescape/reservation/repository/ReservationListCrudRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationListCrudRepository.java
@@ -4,10 +4,21 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.lang.NonNull;
 import roomescape.reservation.domain.Reservation;
 
 public interface ReservationListCrudRepository extends ListCrudRepository<Reservation, Long>, JpaSpecificationExecutor<Reservation> {
+
+    @NonNull
+    @Query("""
+            SELECT r FROM Reservation r
+            JOIN FETCH r.member
+            JOIN FETCH r.time
+            JOIN FETCH r.theme
+            """)
+    List<Reservation> findAll();
 
     List<Reservation> findByDateAndThemeId(LocalDate date, Long themeId);
 
