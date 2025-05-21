bccbe0836f52407f80bfbf604ec5bcf91009b06e
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-21 11:08:35 +0900

    test: AuthService 테스트 작성

---
+++ src/test/java/roomescape/auth/service/AuthServiceTest.java
@@ -0,0 +1,33 @@
+package roomescape.auth.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import roomescape.auth.dto.request.LoginRequest;
+import roomescape.auth.infrastructure.AuthorizationPrincipal;
+import roomescape.member.domain.Member;
+import roomescape.member.domain.MemberRole;
+import roomescape.member.fixture.MemberFixture;
+
+@SpringBootTest
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    void 로그인을_할_수_있다() {
+
+        // given
+        Member member = MemberFixture.createMember(MemberRole.USER);
+        LoginRequest request = new LoginRequest(member.getEmail(), member.getPassword());
+
+        // when
+        AuthorizationPrincipal authorizationPrincipal = authService.login(member, request);
+
+        // then
+        assertThat(authorizationPrincipal).isNotNull();
+    }
+}
