15047a54aecab950cfa8986bfe41a3abb77e8de1
jhpark1227 <junhyeok0246@naver.com>
2025-05-21 14:28:03 +0900

    test: WaitingController 단위테스트 작성

--- src/test/java/roomescape/unit/reservation/presentation/WaitingControllerTest.java
+++ src/test/java/roomescape/unit/reservation/presentation/WaitingControllerTest.java
@@ -1,6 +1,7 @@
 package roomescape.unit.reservation.presentation;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -10,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,9 +20,14 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import roomescape.auth.infrastructure.JwtTokenProvider;
 import roomescape.auth.presentation.AuthorizationExtractor;
+import roomescape.member.domain.Member;
+import roomescape.reservation.domain.Theme;
+import roomescape.reservation.domain.TimeSlot;
+import roomescape.reservation.domain.Waiting;
 import roomescape.reservation.dto.request.WaitingRequest;
 import roomescape.reservation.dto.response.ReservationTimeResponse;
 import roomescape.reservation.dto.response.WaitingResponse;
+import roomescape.reservation.dto.response.WaitingWithRankResponse;
 import roomescape.reservation.presentation.WaitingController;
 import roomescape.reservation.service.WaitingService;
 
@@ -54,6 +61,34 @@ public class WaitingControllerTest {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", "/api/waiting/1"))
+                .andExpect(content().string(objectMapper.writeValueAsString(response)));
+    }
+
+    @Test
+    void 사용자의_대기목록을_조회한다() throws Exception {
+        // given
+        Waiting waiting = Waiting.builder()
+                .date(LocalDate.of(2025, 1, 1))
+                .timeSlot(TimeSlot.builder().startAt(LocalTime.of(9, 0)).build())
+                .member(
+                        Member.builder()
+                                .id(1L)
+                                .name("name1").bulid()
+                ).theme(
+                        Theme.builder()
+                                .name("theme1")
+                                .description("desc")
+                                .thumbnail("thumb").build()
+                ).build();
+        WaitingWithRankResponse waitingWithRankResponse = new WaitingWithRankResponse(waiting, 1L);
+        List<WaitingWithRankResponse> response = List.of(waitingWithRankResponse);
+        given(tokenProvider.extractSubject("accessToken")).willReturn("1");
+        given(waitingService.findWaitingByMemberId(1L)).willReturn(response);
+        // when & then
+        mockMvc.perform(get("/api/waiting/my")
+                        .cookie(new Cookie("token", "accessToken"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andExpect(content().string(objectMapper.writeValueAsString(response)));
     }
 }
