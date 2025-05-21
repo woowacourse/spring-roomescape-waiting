4caa072b729ca731a72260547574775d64ac79b9
GiSeok Kang <ime.rokz@gmail.com>
2025-05-21 22:40:18 +0900

    feat: 예외 처리 추가 및 코드 리팩토링
    - UnauthorizedException, ForbiddenException에 대한 예외 처리 추가
    - 공통 예외 응답 생성을 위한 `createErrorResponse` 메서드 추가
    - 코드 가독성을 위한 형식 및 구조 개선

--- src/main/java/roomescape/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/exception/handler/GlobalExceptionHandler.java
@@ -6,27 +6,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.auth.exception.ForbiddenException;
+import roomescape.auth.exception.UnauthorizedException;
 import roomescape.common.exception.BusinessException;
 import roomescape.common.exception.handler.dto.ExceptionResponse;
-import roomescape.member.exception.EmailException;
-import roomescape.member.exception.NameException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ExceptionResponse> handleNullPointer(final BusinessException exception, final HttpServletRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                HttpStatus.BAD_REQUEST.value(), "[ERROR] " + exception.getMessage(), request.getRequestURI()
+    public ResponseEntity<ExceptionResponse> handleNullPointer(final BusinessException exception,
+                                                               final HttpServletRequest request) {
+        ExceptionResponse exceptionResponse = createErrorResponse(
+            HttpStatus.BAD_REQUEST, exception.getMessage(), request.getRequestURI()
         );
 
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ExceptionResponse> handleUnauthorize(final UnauthorizedException exception, final HttpServletRequest request) {
+        ExceptionResponse exceptionResponse = createErrorResponse(
+            HttpStatus.UNAUTHORIZED, exception.getMessage(), request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ExceptionResponse> handleForbidden(final ForbiddenException exception, final HttpServletRequest request) {
+        ExceptionResponse exceptionResponse = createErrorResponse(
+            HttpStatus.FORBIDDEN, exception.getMessage(), request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).body(exceptionResponse);
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ExceptionResponse> handleIllegalArgument(final IllegalArgumentException exception, final HttpServletRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                HttpStatus.BAD_REQUEST.value(), "[ERROR] " + exception.getMessage(), request.getRequestURI()
+    public ResponseEntity<ExceptionResponse> handleIllegalArgument(final IllegalArgumentException exception,
+                                                                   final HttpServletRequest request) {
+        ExceptionResponse exceptionResponse = createErrorResponse(
+            HttpStatus.BAD_REQUEST, exception.getMessage(), request.getRequestURI()
         );
 
         return ResponseEntity.badRequest().body(exceptionResponse);
@@ -34,28 +54,37 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadable(
-            final HttpMessageNotReadableException exception, final HttpServletRequest request
+        final HttpMessageNotReadableException exception, final HttpServletRequest request
     ) {
         Throwable rootCause = exception.getRootCause();
         if (rootCause instanceof IllegalArgumentException) {
-            ExceptionResponse exceptionResponse = new ExceptionResponse(
-                    HttpStatus.BAD_REQUEST.value(), "[ERROR] " + rootCause.getMessage(), request.getRequestURI()
+            ExceptionResponse exceptionResponse = createErrorResponse(
+                HttpStatus.BAD_REQUEST, rootCause.getMessage(), request.getRequestURI()
             );
+
             return ResponseEntity.badRequest().body(exceptionResponse);
         }
 
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                HttpStatus.BAD_REQUEST.value(), "[ERROR] 요청 입력이 잘못되었습니다.", request.getRequestURI()
+        ExceptionResponse exceptionResponse = createErrorResponse(
+            HttpStatus.BAD_REQUEST, "요청 입력이 잘못되었습니다.", request.getRequestURI()
         );
+
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleException(final Exception exception, final HttpServletRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                HttpStatus.BAD_REQUEST.value(), "[ERROR] " + exception.getMessage(), request.getRequestURI()
+    public ResponseEntity<ExceptionResponse> handleException(final Exception exception,
+                                                             final HttpServletRequest request) {
+        ExceptionResponse exceptionResponse = createErrorResponse(
+            HttpStatus.BAD_REQUEST, exception.getMessage(), request.getRequestURI()
         );
 
         return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    private ExceptionResponse createErrorResponse(final HttpStatus httpStatus, final String msg, final String uri) {
+        return new ExceptionResponse(
+            httpStatus.value(), "[ERROR] " + msg, uri
+        );
     }
 }
