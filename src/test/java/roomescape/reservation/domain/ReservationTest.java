0f7e1e14502d811317a2df60e10d4be32d1a2cb2
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-21 13:58:06 +0900

    test: Reservation 테스트 작성

---
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -0,0 +1,23 @@
+package roomescape.reservation.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ReservationTest {
+
+    @Test
+    void PK_와_함께_생성된다() {
+        // given
+        Long primaryKey = 1L;
+        Reservation reservationWithoutPrimaryKey = ReservationFixture.createWithoutId();
+
+        // when
+        Reservation reservation = Reservation.generateWithPrimaryKey(reservationWithoutPrimaryKey, primaryKey);
+
+        // then
+        assertThat(reservationWithoutPrimaryKey.getId()).isNull();
+        assertEquals(primaryKey, reservation.getId());
+    }
+}
