2460458ae5416ca4ceb9c40a865482a77c7c3f26
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-05-22 20:48:34 +0900

    refactor: 조건에 따른 예약 조회 JPQL 문 수정

--- src/main/java/roomescape/reservation/repository/jpa/JpaReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/jpa/JpaReservationRepository.java
@@ -1,32 +1,28 @@
 package roomescape.reservation.repository.jpa;
 
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import roomescape.member.domain.Member;
 import roomescape.reservation.domain.Reservation;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface JpaReservationRepository extends JpaRepository<Reservation, Long> {
 
     boolean existsByTimeId(Long id);
 
     @Query("""
-            SELECT r FROM Reservation r
-            WHERE (:themeId IS NULL OR r.theme.id = :themeId)
-              AND (:memberId IS NULL OR r.member.id = :memberId)
-              AND (
-                (:dateFrom IS NULL AND :dateTo IS NULL)
-                OR (:dateFrom IS NULL AND r.date <= :dateTo)
-                OR (:dateTo IS NULL AND r.date >= :dateFrom)
-                OR (r.date BETWEEN :dateFrom AND :dateTo)
-              )
-        """)
+                SELECT r FROM Reservation r
+                WHERE (:themeId IS NULL OR r.theme.id = :themeId)
+                  AND (:memberId IS NULL OR r.member.id = :memberId)
+                  AND ((:dateFrom IS NULL OR r.date >= :dateFrom) AND (:dateTo IS NULL OR r.date <= :dateTo))
+            """)
     List<Reservation> findByMemberAndThemeAndVisitDateBetween(
-        Long themeId,
-        Long memberId,
-        LocalDate dateFrom,
-        LocalDate dateTo
+            Long themeId,
+            Long memberId,
+            LocalDate dateFrom,
+            LocalDate dateTo
     );
 
     List<Reservation> findAllByMember(Member member);
--- src/main/java/roomescape/theme/repository/jpa/JpaThemeRepository.java
+++ src/main/java/roomescape/theme/repository/jpa/JpaThemeRepository.java
@@ -1,23 +1,24 @@
 package roomescape.theme.repository.jpa;
 
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import roomescape.theme.domain.Theme;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface JpaThemeRepository extends JpaRepository<Theme, Long> {
 
     @Query("""
-            SELECT t
-            FROM Theme t
-            WHERE t.id IN (
-                SELECT r.theme.id
-                FROM Reservation r
-                WHERE r.date BETWEEN :fromDate AND :toDate
-                GROUP BY r.theme.id
-                ORDER BY COUNT(r) DESC
-            )
-        """)
+                SELECT t
+                FROM Theme t
+                WHERE t.id IN (
+                    SELECT r.theme.id
+                    FROM Reservation r
+                    WHERE r.date BETWEEN :fromDate AND :toDate
+                    GROUP BY r.theme.id
+                    ORDER BY COUNT(r) DESC
+                )
+            """)
     List<Theme> findTopByReservationCountDesc(LocalDate fromDate, LocalDate toDate, long listNum);
 }
