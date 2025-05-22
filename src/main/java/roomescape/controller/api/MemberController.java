8d9d10b5873b779c6b3c71527f2d49a1df81e2b1
Han <jumdo12@gmail.com>
2025-05-22 13:44:28 +0900

    refactor(MemberController): 중복 엔드포인트 RequestMapping으로 제거

--- src/main/java/roomescape/controller/api/MemberController.java
+++ src/main/java/roomescape/controller/api/MemberController.java
@@ -2,16 +2,14 @@ package roomescape.controller.api;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import roomescape.dto.auth.SignUpRequestDto;
 import roomescape.dto.member.MemberResponseDto;
 import roomescape.dto.member.MemberSignupResponseDto;
 import roomescape.service.MemberService;
 
 @RestController
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
@@ -20,13 +18,13 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/members")
+    @GetMapping
     public ResponseEntity<List<MemberResponseDto>> getMembers() {
         List<MemberResponseDto> allMembers = memberService.findAllMembers();
         return ResponseEntity.ok(allMembers);
     }
 
-    @PostMapping("/members")
+    @PostMapping
     public ResponseEntity<MemberSignupResponseDto> signup(@RequestBody SignUpRequestDto requestDto) {
         MemberSignupResponseDto memberSignupResponseDto = memberService.registerMember(requestDto);
         return ResponseEntity.ok().body(memberSignupResponseDto);
