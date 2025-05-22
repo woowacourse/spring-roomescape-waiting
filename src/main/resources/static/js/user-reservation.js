dc6ea2259333ba50a82413d4cb93d05fe024645f
mimi <110226866+hongkikii@users.noreply.github.com>
2025-05-22 18:58:46 +0900

    style: 주석 제거

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -154,7 +154,7 @@ function onWaitButtonClick() {
         };
 
         /*
-        TODO: [3단계] 예약 대기 생성 요청 API 호출
+        [3단계] 예약 대기 생성 요청 API 호출
          */
         fetch('/waitings', {
             method: 'POST',
