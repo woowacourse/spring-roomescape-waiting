d3b64e09cf7858926382c16c0645b3493835948d
bowook <mbosx316@naver.com>
2025-05-22 17:24:41 +0900

    feat: Waiting 기능 추가

--- src/main/java/roomescape/reservation/domain/Waiting.java
+++ src/main/java/roomescape/reservation/domain/Waiting.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import roomescape.common.exception.WaitingException;
 import roomescape.member.domain.Member;
 import roomescape.theme.domain.Theme;
 
@@ -33,10 +34,51 @@ public class Waiting {
     @ManyToOne(fetch = FetchType.LAZY)
     private Theme theme;
 
-    public Waiting(final Member member, final ReservationTime time, final Theme theme, final LocalDate date) {
+    public Waiting(final Long id, final Member member, final ReservationTime time, final Theme theme,
+                   final LocalDate date) {
+        validateMember(member);
+        validateDate(date);
+        validateTime(time);
+        validateTheme(theme);
+        this.id = id;
         this.member = member;
         this.date = date;
         this.time = time;
         this.theme = theme;
+    }
+
+    public Waiting(final Member member, final ReservationTime time, final Theme theme, final LocalDate date) {
+        validateMember(member);
+        validateDate(date);
+        validateTime(time);
+        validateTheme(theme);
+        this.member = member;
+        this.date = date;
+        this.time = time;
+        this.theme = theme;
+    }
+
+    private void validateMember(final Member member) {
+        if (member == null) {
+            throw new WaitingException("Member cannot be null");
+        }
+    }
+
+    private void validateDate(final LocalDate date) {
+        if (date == null) {
+            throw new WaitingException("Date cannot be null");
+        }
+    }
+
+    private void validateTime(final ReservationTime time) {
+        if (time == null) {
+            throw new WaitingException("Time cannot be null");
+        }
+    }
+
+    private void validateTheme(final Theme theme) {
+        if (theme == null) {
+            throw new WaitingException("Theme cannot be null");
+        }
     }
 }
