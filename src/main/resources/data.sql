2330190e767fb371bd055bbe85677e7f903b4588
LJM\eueo8 <eueo8259@gmail.com>
2025-05-22 11:44:07 +0900

    chore: 테이블 및 예시 sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,36 @@
-DELETE
-FROM reservation;
-DELETE
-FROM reservation_time;
-DELETE
-FROM theme;
-DELETE
-FROM member;
+DELETE FROM reservation;
+DELETE FROM reservation_time;
+DELETE FROM theme;
+DELETE FROM member;
 
--- member 데이터
 INSERT INTO member(name, email, password, role)
 VALUES ('훌라', 'test@test.com', 'test', 'USER'),
-       ('어드민', 'admin@admin.com', 'admin', 'ADMIN');
+       ('어드민', 'admin@admin.com', 'admin', 'ADMIN'),
+       ('김모험', 'qwe@qwe.com', 'qwe', 'USER'),
+       ('이추리', 'detective@escape.com', 'password', 'USER');
 
--- theme 데이터
 INSERT INTO theme (name, description, thumbnail)
-VALUES ('공포의 방', '소름 끼치는 공포 테마', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('추리의 방', '논리력으로 푸는 추리 테마', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('시간 여행자', '과거와 미래를 넘나드는 방 탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+VALUES ('공포의 방', '소름 끼치는 공포 테마', 'url'),
+       ('추리의 방', '논리력으로 푸는 추리 테마', 'url'),
+       ('시간 여행자', '과거와 미래를 넘나드는 방 탈출', 'url');
 
--- reservation_time 데이터
 INSERT INTO reservation_time (start_at)
-VALUES ('10:00'),
-       ('12:00'),
-       ('14:00'),
-       ('16:00');
+VALUES ('10:00'), ('12:00'), ('14:00'), ('16:00');
+
+INSERT INTO reservation (member_id, date, time_id, theme_id, type)
+VALUES
+(1, '2025-05-25', 1, 1, 'confirm'),
+(1, '2025-05-26', 2, 1, 'confirm'),
+(3, '2025-06-01', 3, 2, 'confirm'),
+(4, '2025-06-02', 1, 3, 'confirm'),
+(3, '2025-06-03', 2, 2, 'confirm'),
+(4, '2025-06-03', 4, 3, 'confirm'),
+(3, '2025-06-04', 3, 1, 'confirm'),
+(4, '2025-06-05', 4, 2, 'confirm');
+
+INSERT INTO reservation (member_id, date, time_id, theme_id, type)
+VALUES
+(2, '2025-05-25', 1, 1, 'waiting'),
+(2, '2025-05-26', 2, 1, 'waiting'),
+(1, '2025-06-03', 2, 2, 'waiting'),
+(2, '2025-06-05', 4, 2, 'waiting');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,51 +1,38 @@
-CREATE TABLE IF NOT EXISTS theme
-(
-    id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS theme (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    thumbnail   VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS reservation_time
-(
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS member
-(
-    id      BIGINT  NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255)   NOT NULL,
-    email   VARCHAR(255)    NOT NULL,
-    password    VARCHAR(255)    NOT NULL,
-    role    VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS reservation
-(
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    member_id   BIGINT NOT NULL,
-    date     VARCHAR(255) NOT NULL,
-    time_id  BIGINT,
-    theme_id BIGINT,
+    thumbnail VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS waiting
-(
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    member_id   BIGINT NOT NULL,
-    date     VARCHAR(255) NOT NULL,
-    time_id  BIGINT,
-    theme_id BIGINT,
+CREATE TABLE IF NOT EXISTS reservation_time (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    UNIQUE (start_at)
+);
+
+CREATE TABLE IF NOT EXISTS member (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
+
+CREATE TABLE IF NOT EXISTS reservation (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    member_id BIGINT NOT NULL,
+    date DATE NOT NULL,
+    time_id BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
+    type VARCHAR(31) NOT NULL, -- @DiscriminatorColumn(name = "type")
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time(id),
+    FOREIGN KEY (theme_id) REFERENCES theme(id)
 );
