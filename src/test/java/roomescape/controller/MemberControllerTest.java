e0cf81b24d581fb54a2f7190485d02f4ba59a456
supernovaMK <minkikim2002@gmail.com>
2025-05-22 17:28:04 +0900

    test: 유저 추가 및 조회 테스트 구현

---
+++ src/test/java/roomescape/controller/MemberControllerTest.java
@@ -0,0 +1,62 @@
+package roomescape.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import roomescape.domain.Member;
+import roomescape.domain.enums.Role;
+import roomescape.dto.signup.SignupRequest;
+import roomescape.repository.member.MemberRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원 전체 조회 테스트")
+    void getAllMember() throws Exception {
+        // given
+        Member member = new Member(null, "슬링키", "이메일", "비밀번호", Role.USER);
+        memberRepository.save(member);
+        // when & then
+        mockMvc.perform(get("/members"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id").exists())
+                .andExpect(jsonPath("$.length()").value(1));
+    }
+
+    @Test
+    @DisplayName("회원 추가 테스트")
+    void addMember() throws Exception {
+        // given
+        SignupRequest request = new SignupRequest("email", "password", "테스트유저");
+        // when & then
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.name").value("테스트유저"));
+    }
+} 
