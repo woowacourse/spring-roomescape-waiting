57ed713d334b27b26aa2138b5492c48d9e226dba
RaZel713 <razel7130@gmail.com>
2025-05-22 21:05:13 +0900

    feat: 내 예약 목록에서 조회 기능 구현

--- src/main/java/roomescape/presentation/rest/UserController.java
+++ src/main/java/roomescape/presentation/rest/UserController.java
@@ -3,6 +3,7 @@ package roomescape.presentation.rest;
 import static org.springframework.http.HttpStatus.CREATED;
 
 import jakarta.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.application.ReservationService;
 import roomescape.application.UserService;
+import roomescape.application.WaitingService;
 import roomescape.domain.reservation.Reservation;
 import roomescape.domain.user.User;
+import roomescape.domain.waiting.Waiting;
 import roomescape.presentation.auth.Authenticated;
 import roomescape.presentation.request.SignupRequest;
 import roomescape.presentation.response.UserReservationResponse;
@@ -25,10 +28,14 @@ public class UserController {
 
     private final UserService userService;
     private final ReservationService reservationService;
+    private final WaitingService waitingService;
 
-    public UserController(final UserService userService, final ReservationService reservationService) {
+    public UserController(final UserService userService,
+                          final ReservationService reservationService,
+                          final WaitingService waitingService) {
         this.userService = userService;
         this.reservationService = reservationService;
+        this.waitingService = waitingService;
     }
 
     @PostMapping
@@ -42,9 +49,21 @@ public class UserController {
     @GetMapping("/reservations")
     public List<UserReservationResponse> readAllReservationsByUser(@Authenticated final User user) {
         List<Reservation> reservations = reservationService.findReservationsByUserId(user.id());
+        List<Waiting> waitings = waitingService.findWaitingByUserId(user.id());
 
-        return reservations.stream()
-                .map(UserReservationResponse::from)
+        List<UserReservationResponse> userReservationResponses = new ArrayList<>();
+
+        List<UserReservationResponse> reservedResponse = reservations.stream()
+                .map(UserReservationResponse::fromReservation)
                 .toList();
+
+        List<UserReservationResponse> waitingResponse = waitings.stream()
+                .map(UserReservationResponse::fromWaiting)
+                .toList();
+
+        userReservationResponses.addAll(reservedResponse);
+        userReservationResponses.addAll(waitingResponse);
+
+        return userReservationResponses;
     }
 }
