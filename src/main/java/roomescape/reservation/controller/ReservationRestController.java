dd9f08dd4ad7277b1a9e953e578c2baef567296c
bowook <mbosx316@naver.com>
2025-05-22 13:29:11 +0900

    feat : ReservationRestController API 추가
    - /mine
    - /waiting

--- src/main/java/roomescape/reservation/controller/ReservationRestController.java
+++ src/main/java/roomescape/reservation/controller/ReservationRestController.java
@@ -1,5 +1,6 @@
 package roomescape.reservation.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.member.domain.Member;
 import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.domain.Waiting;
 import roomescape.reservation.dto.AvailableReservationTimeRequest;
 import roomescape.reservation.dto.AvailableReservationTimeResponse;
 import roomescape.reservation.dto.CreateReservationRequest;
 import roomescape.reservation.dto.CreateReservationResponse;
+import roomescape.reservation.dto.CreateWaitingRequest;
+import roomescape.reservation.dto.CreateWaitingResponse;
 import roomescape.reservation.dto.ReservationMineResponse;
 import roomescape.reservation.service.ReservationService;
 
@@ -74,13 +78,50 @@ public class ReservationRestController {
         );
     }
 
+    //TODO : 몇 번째 예약 대기인지,,,
+    //TODO : 그러면, 서비스에서 총 몇개인지 파악을 해야함
+    //그러면, waiting에 저장된 애들을 (테마 & 날짜 & 시간) 기준으로 일치하는 애들끼리
+    //그 안에서 id를 기준으로 내림차순 정렬을 해서 member몇 번째에 있는지 파악해야함
     @GetMapping("/mine")
     public ResponseEntity<List<ReservationMineResponse>> getMyReservations(final Member member) {
-        final List<Reservation> reservations = reservationService.findByMember(member);
-        final List<ReservationMineResponse> reservationMineResponses = reservations.stream()
+        final List<ReservationMineResponse> reservationMineResponses = reservationService.findByMember(member)
+                .stream()
                 .map(ReservationMineResponse::from)
                 .toList();
+        final List<ReservationMineResponse> waitingMineResponses = reservationService.findWaitingByMember(member)
+                .stream()
+                .map(waiting -> {
+                    final long rank = reservationService.getRankInWaiting(waiting);
+                    return ReservationMineResponse.from(waiting, rank);
+                })
+                .toList();
 
-        return ResponseEntity.ok(reservationMineResponses);
+        final List<ReservationMineResponse> combined = new ArrayList<>();
+        combined.addAll(reservationMineResponses);
+        combined.addAll(waitingMineResponses);
+
+        return ResponseEntity.ok(combined);
+    }
+
+    @PostMapping("/waiting")
+    public ResponseEntity<CreateWaitingResponse> createWaitingReservation(
+            @RequestBody final CreateWaitingRequest createWaitingRequest,
+            final Member member
+    ) {
+        final Waiting savedWaiting = reservationService.createWaitingReservation(
+                member,
+                createWaitingRequest.date(),
+                createWaitingRequest.time(),
+                createWaitingRequest.theme()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(CreateWaitingResponse.from(savedWaiting));
+    }
+
+    @DeleteMapping("/waiting/{id}")
+    public ResponseEntity<Void> deleteWaitingReservation(@PathVariable final Long id) {
+        reservationService.deleteWaitingById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
