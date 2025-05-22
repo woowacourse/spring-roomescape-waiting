5971b09cfc6b6133d182680a8b457deaa0522370
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-22 19:13:16 +0900

    chore: 클라이언트 코드 수정

--- src/test/java/roomescape/controller/api/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/api/ReservationControllerTest.java
@@ -48,86 +48,86 @@ class ReservationControllerTest {
 //    @DisplayName("예약 조회, 생성 및 삭제")
 //    class ReservationPostTest {
 
-        String loginToken;
+    String loginToken;
 
-        @Autowired
-        JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
 
-        @BeforeEach
-        void setUp(){
-            loginToken = jwtTokenProvider.createToken(
-                    new Member(1L, "가이온", "hello@woowa.com", Role.USER, "password"));
-        }
-
-        @DisplayName("Reservation 목록 내용 갯수를 검사한다")
-        @Test
-        void reservationTest() {
-            RestAssured.given().log().all()
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(1));
-        }
-
-        @DisplayName("Reservation을 생성한다")
-        @Test
-        void addReservationTest() {
-            Map<String, Object> params = new HashMap<>();
-            params.put("name", "브라운");
-            params.put("date", "2030-08-05");
-            params.put("timeId", 1);
-            params.put("themeId", 1);
-
-            RestAssured.given().log().all()
-                    .cookie("token", loginToken)
-                    .contentType(ContentType.JSON)
-                    .body(params)
-                    .when().post("/reservations")
-                    .then().log().all()
-                    .statusCode(201)
-                    .body("id", is(2));
-
-            RestAssured.given().log().all()
-                    .cookie("token", loginToken)
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(2));
-        }
-
-        @DisplayName("예약이 존재하면 삭제할 수 있다")
-        @Test
-        void deleteReservationTest() {
-            RestAssured.given().log().all()
-                    .cookie("token", loginToken)
-                    .when().delete("/reservations/1")
-                    .then().log().all()
-                    .statusCode(204);
-
-            RestAssured.given().log().all()
-                    .cookie("token", loginToken)
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(0));
-        }
-
-        @DisplayName("존재하지 않는 예약을 삭제할 수 없다")
-        @Test
-        void invalidReservationIdDeleteTest() {
-            RestAssured.given().cookie("token", loginToken).log().all()
-                    .when().delete("/reservations/5")
-                    .then().log().all()
-                    .statusCode(404);
-        }
-
-        @DisplayName("자신의 예약 정보를 불러올 수 있다")
-        @Test
-        void myReservationTest() {
-            RestAssured.given().log().all()
-                    .cookie("token", loginToken)
-                    .when().get("/reservations/me")
-                    .then().log().all().statusCode(200);
-        }
+    @BeforeEach
+    void setUp() {
+        loginToken = jwtTokenProvider.createToken(
+                new Member(1L, "가이온", "hello@woowa.com", Role.USER, "password"));
     }
+
+    @DisplayName("Reservation 목록 내용 갯수를 검사한다")
+    @Test
+    void reservationTest() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @DisplayName("Reservation을 생성한다")
+    @Test
+    void addReservationTest() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2030-08-05");
+        params.put("timeId", 1);
+        params.put("themeId", 1);
+
+        RestAssured.given().log().all()
+                .cookie("token", loginToken)
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(2));
+
+        RestAssured.given().log().all()
+                .cookie("token", loginToken)
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+
+    @DisplayName("예약이 존재하면 삭제할 수 있다")
+    @Test
+    void deleteReservationTest() {
+        RestAssured.given().log().all()
+                .cookie("token", loginToken)
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given().log().all()
+                .cookie("token", loginToken)
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @DisplayName("존재하지 않는 예약을 삭제할 수 없다")
+    @Test
+    void invalidReservationIdDeleteTest() {
+        RestAssured.given().cookie("token", loginToken).log().all()
+                .when().delete("/reservations/5")
+                .then().log().all()
+                .statusCode(404);
+    }
+
+    @DisplayName("자신의 예약 정보를 불러올 수 있다")
+    @Test
+    void myReservationTest() {
+        RestAssured.given().log().all()
+                .cookie("token", loginToken)
+                .when().get("/reservations/me")
+                .then().log().all().statusCode(200);
+    }
+//}
 }
