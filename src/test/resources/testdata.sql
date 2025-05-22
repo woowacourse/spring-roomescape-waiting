ddf47b092954373d9f4bc26ec8171a9409dc5e62
RaZel713 <razel7130@gmail.com>
2025-05-22 21:46:28 +0900

    test: 예약 데이터에서 상태 필드 제거

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -18,12 +18,12 @@ VALUES ('레벨1 탈출', '우테코 레벨1을 탈출하는 내용입니다.', 
        ('레벨4 탈출', '우테코 레벨4를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
        ('레벨5 탈출', '우테코 레벨를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO RESERVATION (user_id, date, time_slot_id, theme_id, status)
-VALUES (1, '2025-05-04', 1, 1, 'RESERVED'),
-       (1, '2025-05-04', 5, 2, 'RESERVED'),
-       (1, '2025-05-04', 6, 2, 'RESERVED'),
-       (2, '2025-05-05', 3, 3, 'RESERVED'),
-       (2, '2025-05-05', 1, 3, 'RESERVED'),
-       (2, '2025-05-04', 2, 3, 'RESERVED'),
-       (3, '2025-05-04', 1, 4, 'RESERVED'),
-       (3, '2025-05-04', 1, 5, 'RESERVED');
+INSERT INTO RESERVATION (user_id, date, time_slot_id, theme_id)
+VALUES (1, '2025-05-04', 1, 1),
+       (1, '2025-05-04', 5, 2),
+       (1, '2025-05-04', 6, 2),
+       (2, '2025-05-05', 3, 3),
+       (2, '2025-05-05', 1, 3),
+       (2, '2025-05-04', 2, 3),
+       (3, '2025-05-04', 1, 4),
+       (3, '2025-05-04', 1, 5);
