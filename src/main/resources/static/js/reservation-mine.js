9867c9d32061d93acc7754360f578e1f49e62054
sonjh919 <kingjh1125@gmail.com>
2025-05-22 09:57:36 +0900

    feat: 구현 완료한 view 체크

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -34,7 +34,7 @@ function render(data) {
         row.insertCell(3).textContent = status;
 
         /*
-        TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
+        DONE: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
          */
         if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
             const cancelCell = row.insertCell(4);
@@ -54,7 +54,7 @@ function render(data) {
 function requestDeleteWaiting(id) {
     console.log(id)
     /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
+    DONE: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
      */
     return fetch(`/reservations/waiting/${id}`, {
         method: 'DELETE'
--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     /*
-    TODO: [4단계] 예약 대기 관리 기능
+    DONE: [4단계] 예약 대기 관리 기능
           예약 대기 목록 조회 endpoint 설정
      */
     fetch('/admin/reservations/waiting') // 내 예약 목록 조회 API 호출
@@ -38,11 +38,11 @@ function render(data) {
         const actionCell = row.insertCell(row.cells.length);
 
         /*
-        TODO: [4단계] 예약 대기 관리 기능
+        DONE: [4단계] 예약 대기 관리 기능
               예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
          */
-        // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
-        // actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
+        actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
+        actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
 }
 
