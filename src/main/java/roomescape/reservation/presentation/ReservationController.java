c7cf43a9f2549f1c53b2e7608d2ac9ca71dff62b
wogus <ind07152@naver.com>
2025-05-22 17:52:39 +0900

    feat: 대기 예약 생성 api 추가

--- src/main/java/roomescape/reservation/presentation/ReservationController.java
+++ src/main/java/roomescape/reservation/presentation/ReservationController.java
@@ -1,7 +1,5 @@
 package roomescape.reservation.presentation;
 
-import static roomescape.reservation.presentation.ReservationController.RESERVATION_BASE_URL;
-
 import jakarta.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.time.format.DateTimeParseException;
@@ -14,18 +12,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.member.argumentResolver.Authentication;
 import roomescape.common.exceptionHandler.dto.ExceptionResponse;
+import roomescape.member.argumentResolver.Authentication;
+import roomescape.member.dto.request.LoginMember;
 import roomescape.reservation.dto.request.ReservationConditionRequest;
 import roomescape.reservation.dto.request.ReservationRequest;
+import roomescape.reservation.dto.request.ReservationWaitingRequest;
 import roomescape.reservation.dto.response.MyReservationResponse;
 import roomescape.reservation.dto.response.ReservationResponse;
 import roomescape.reservation.service.ReservationService;
 
 @RestController
-@RequestMapping(RESERVATION_BASE_URL)
 public class ReservationController {
 
     public static final String RESERVATION_BASE_URL = "/reservations";
@@ -37,22 +35,33 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping
+    // TODO: 대기 상태 추가
+    @GetMapping(RESERVATION_BASE_URL)
     public ResponseEntity<List<ReservationResponse>> getReservations(
             @ModelAttribute ReservationConditionRequest request) {
         List<ReservationResponse> response = reservationService.getReservations(request);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping
+    @PostMapping(RESERVATION_BASE_URL)
     public ResponseEntity<ReservationResponse> createReservation(@RequestBody final ReservationRequest request,
-                                                                 @Authentication final roomescape.member.dto.request.LoginMember loginMember) {
+                                                                 @Authentication final LoginMember loginMember) {
         ReservationResponse response = reservationService.createReservation(request, loginMember.id());
         URI locationUri = URI.create(RESERVATION_BASE_URL + SLASH + response.id());
         return ResponseEntity.created(locationUri).body(response);
     }
 
-    @DeleteMapping("/{id}")
+    // TODO: ReservationResponse 통일
+    @PostMapping("/waiting-reservations")
+    public ResponseEntity<ReservationResponse> createWaitingReservation(
+            @RequestBody final ReservationWaitingRequest request,
+            @Authentication final LoginMember loginMember) {
+        ReservationResponse response = reservationService.createWaitingReservation(request, loginMember.id());
+        URI locationUri = URI.create(RESERVATION_BASE_URL + SLASH + response.id());
+        return ResponseEntity.created(locationUri).body(response);
+    }
+
+    @DeleteMapping(RESERVATION_BASE_URL + "/{id}")
     public ResponseEntity<Void> deleteReservationById(@PathVariable("id") final Long id) {
         reservationService.deleteReservationById(id);
         return ResponseEntity.noContent().build();
@@ -66,8 +75,8 @@ public class ReservationController {
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
-    @GetMapping("/mine")
-    public ResponseEntity<List<MyReservationResponse>> getMyReservations(@Authentication roomescape.member.dto.request.LoginMember loginMember) {
+    @GetMapping(RESERVATION_BASE_URL + "/mine")
+    public ResponseEntity<List<MyReservationResponse>> getMyReservations(@Authentication LoginMember loginMember) {
         List<MyReservationResponse> myReservationResponses = reservationService.getMyReservations(loginMember.id());
         return ResponseEntity.ok().body(myReservationResponses);
     }
