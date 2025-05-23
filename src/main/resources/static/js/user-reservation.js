79f970379b8a906db9b1d94abf7fe44946b659de
byesol <bsj5967@naver.com>
2025-05-23 09:19:18 +0900

    feat : [3단계] 예약 대기 기능 클라이언트 코드

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -182,14 +182,14 @@ function onWaitButtonClick() {
     if (selectedDate && selectedThemeId && selectedTimeId) {
         const reservationData = {
             date: selectedDate,
-            theme: selectedThemeId,
-            time: selectedTimeId
+            timeId: selectedTimeId,
+            themeId: selectedThemeId
         };
 
         /*
         TODO: [3단계] 예약 대기 생성 요청 API 호출
          */
-        fetch('', {
+        fetch('/waiting', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
