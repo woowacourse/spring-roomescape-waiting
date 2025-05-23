6964673f5a49b0bd4640bb0c7b930526f04902ca
juno <wnsgh732@gmail.com>
2025-05-23 16:45:34 +0900

    feat: 예약 대기 관리 어드민 페이지 추가

--- src/main/java/roomescape/reservation/model/entity/Reservation.java
+++ src/main/java/roomescape/reservation/model/entity/Reservation.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
