43741fec7b126bd1b795c03f7ff02fa7d9abc6d6
Minuring <mw9505@gmail.com>
2025-05-23 20:33:59 +0900

    feat: 어드민 예약 대기 거절 구현

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -19,22 +19,19 @@ function render(data) {
   data.forEach(item => {
     const row = tableBody.insertRow();
 
-    /*
-    TODO: [4단계] 예약 대기 관리 기능
-          예약 대기 목록 조회 response 명세에 맞춰 값 설정
-     */
     const order = item.order;
+    const id = item.id;
     const name = item.user.name;
     const theme = item.theme.name;
     const date = item.date;
     const startAt = item.time.startAt;
 
+    row.dataset.id = id;
     row.insertCell(0).textContent = order;         // 예약 대기 순번
     row.insertCell(1).textContent = name;          // 예약자명
     row.insertCell(2).textContent = theme;         // 테마명
     row.insertCell(3).textContent = date;          // 예약 날짜
     row.insertCell(4).textContent = startAt;       // 시작 시간
-
     const actionCell = row.insertCell(row.cells.length);
 
     /*
@@ -42,7 +39,7 @@ function render(data) {
           예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
      */
     // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
-    // actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
+    actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
   });
 }
 
@@ -65,17 +62,17 @@ function approve(event) {
 
 function deny(event) {
   const row = event.target.closest('tr');
-  const id = row.cells[0].textContent;
+  const id = row.dataset.id;
 
   /*
   TODO: [4단계] 예약 대기 목록 관리 기능
         예약 대기 거절 API 호출
    */
-  const endpoint = '' + id;
+  const endpoint = '/reservations/' + id;
   return fetch(endpoint, {
-    method: ''
+    method: 'DELETE'
   }).then(response => {
-    if (response.status === 200) return;
+    if (response.status === 204) return;
     throw new Error('Delete failed');
   }).then(() => location.reload());
 }
