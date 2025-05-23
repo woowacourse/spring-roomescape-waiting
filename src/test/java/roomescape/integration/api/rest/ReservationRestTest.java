7dd9e8330ad5f7d871d1237fd86eec584e5f4443
HyeonsuLee <leehyeonsu4888@naver.com>
2025-05-23 16:41:05 +0900

    test: 예약 대기 관련 e2e테스트 작성

--- src/test/java/roomescape/integration/api/rest/ReservationRestTest.java
+++ src/test/java/roomescape/integration/api/rest/ReservationRestTest.java
@@ -9,6 +9,7 @@ import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import roomescape.common.RestAssuredTestBase;
 import roomescape.integration.api.RestLoginMember;
@@ -128,4 +129,81 @@ class ReservationRestTest extends RestAssuredTestBase {
                 .body("[0].time", is("10:00"))
                 .body("[0].status", is("예약"));
     }
+
+    @Test
+    void 예약_대기를_한다() {
+        Map<String, Object> request = Map.of(
+                "date", LocalDate.now(FIXED_CLOCK).plusDays(1).toString(),
+                "timeId", timeId,
+                "themeId", themeId
+        );
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("JSESSIONID", restLoginMember.sessionId())
+                .body(request)
+                .when().post("/reservations/wait")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(1))
+                .body("name", is("홍길동"))
+                .body("date", is(RESERVATION_DATE.toString()))
+                .body("time.startAt", is("10:00"))
+                .body("theme.name", is("어드벤처"));
+    }
+
+    @Test
+    void 예약_대기를_승인한다() {
+        예약_대기를_한다();
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("JSESSIONID", restLoginMember.sessionId())
+                .when().post("/reservations/wait/{id}/approve", 1L)
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(1))
+                .body("name", is("홍길동"))
+                .body("date", is(RESERVATION_DATE.toString()))
+                .body("time.startAt", is("10:00"))
+                .body("theme.name", is("어드벤처"));
+    }
+
+    @Test
+    void 예약_대기를_취소한다() {
+        예약_대기를_한다();
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("JSESSIONID", restLoginMember.sessionId())
+                .when().delete("/reservations/wait/{id}/cancel", 1L)
+                .then().log().all()
+                .statusCode(204);
+    }
+
+
+    // TODO: 응답 어떻게 구성할지 고민해보기
+    @Test
+    @DisplayName("예약, 예약 대기가 모두 있는 경우")
+    void 내_예약을_조회한다2() {
+        예약을_생성한다();
+        예약_대기를_한다();
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("JSESSIONID", restLoginMember.sessionId())
+                .when().delete("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1))
+                .body("[0].reservationId", is(1))
+                .body("[0].theme", is(THEME_NAME))
+                .body("[0].date", is(RESERVATION_DATE.toString()))
+                .body("[0].time", is("10:00"))
+                .body("[0].status", is("예약"))
+                .body("[1].reservationId", is(1))
+                .body("[1].theme", is(THEME_NAME))
+                .body("[1].date", is(RESERVATION_DATE.toString()))
+                .body("[1].time", is("10:00"))
+                .body("[1].status", is("예약대기"));
+
+    }
+
+
 }
