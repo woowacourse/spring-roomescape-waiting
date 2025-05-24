69789c8e15b100a15b590fbbbbee080e9926c148
JO-eusan <goodsan0330@gmail.com>
2025-05-24 21:19:56 +0900

    refactor: 테스트 픽스쳐 함수 분리

--- src/test/java/roomescape/repository/jpa/JpaMemberRepositoryTest.java
+++ src/test/java/roomescape/repository/jpa/JpaMemberRepositoryTest.java
@@ -6,25 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.entity.Member;
 import roomescape.exception.custom.NotFoundException;
 
 @DataJpaTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class JpaMemberRepositoryTest {
 
     @Autowired
-    private JpaMemberRepository jpaMemberRepository;
+    private JpaMemberRepository memberRepository;
 
     @Test
     @DisplayName("이메일이 존재한다면 조회할 수 있다.")
     void findMemberByExistedEmail() {
         Member member = Member.createUser("이름", "이메일", "비밀번호");
-        jpaMemberRepository.save(member);
+        memberRepository.save(member);
 
         String email = member.getEmail();
-        Member expected = jpaMemberRepository.findByEmail(email)
+        Member expected = memberRepository.findByEmail(email)
             .orElseThrow(() -> new NotFoundException("member"));
 
         assertThat(expected.getEmail()).isEqualTo(email);
@@ -34,9 +32,9 @@ class JpaMemberRepositoryTest {
     @DisplayName("해당 이메일이 없다면 true를 반환한다.")
     void existsByEmail() {
         Member member = Member.createUser("이름", "이메일", "비밀번호");
-        jpaMemberRepository.save(member);
+        memberRepository.save(member);
 
-        assertThat(jpaMemberRepository.existsByEmail(member.getEmail())).isTrue();
+        assertThat(memberRepository.existsByEmail(member.getEmail())).isTrue();
     }
 
     @Test
@@ -44,6 +42,6 @@ class JpaMemberRepositoryTest {
     void notExistsByEmail() {
         Member member = Member.createUser("이름", "이메일", "비밀번호");
 
-        assertThat(jpaMemberRepository.existsByEmail(member.getEmail())).isFalse();
+        assertThat(memberRepository.existsByEmail(member.getEmail())).isFalse();
     }
 }
--- src/test/java/roomescape/repository/jpa/JpaReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/jpa/JpaReservationRepositoryTest.java
@@ -3,96 +3,163 @@ package roomescape.repository.jpa;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.entity.Member;
 import roomescape.entity.Reservation;
 import roomescape.entity.ReservationTime;
 import roomescape.entity.Theme;
 
 @DataJpaTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class JpaReservationRepositoryTest {
 
-    @Autowired
+    @PersistenceContext
     private EntityManager em;
     @Autowired
-    private JpaThemeRepository jpaThemeRepository;
+    private JpaThemeRepository themeRepository;
     @Autowired
-    private JpaReservationTimeRepository jpaReservationTimeRepository;
+    private JpaReservationTimeRepository reservationTimeRepository;
     @Autowired
-    private JpaReservationRepository jpaReservationRepository;
+    private JpaReservationRepository reservationRepository;
     @Autowired
-    private JpaMemberRepository jpaMemberRepository;
+    private JpaMemberRepository memberRepository;
 
     @Test
-    @DisplayName("예약이 존재하는 시간 ID를 찾을 수 있다.")
+    @DisplayName("사용자로 예약을 찾을 수 있다.")
+    void findByMemberId() {
+        Member member = saveMember(1L);
+        Theme theme = saveTheme(1L);
+        ReservationTime time = saveTime(LocalTime.of(10, 0));
+        LocalDate date = LocalDate.of(2025, 4, 28);
+
+        em.flush();
+        em.clear();
+
+        Reservation reservation = new Reservation(member, date, time, theme);
+        reservationRepository.save(reservation);
+
+        assertThat(reservationRepository.findByMemberId(member.getId())).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("해당 날짜와 테마에 해당하는 예약을 찾을 수 있다.")
     void findByDateAndThemeId() {
-        Member member = Member.createUser("이름", "이메일", "비밀번호");
-        Theme theme = new Theme("이름", "설명", "썸네일");
-        ReservationTime time = new ReservationTime(LocalTime.of(10, 0));
+        Member member = saveMember(1L);
+        Theme theme = saveTheme(1L);
+        ReservationTime time = saveTime(LocalTime.of(10, 0));
         LocalDate date = LocalDate.of(2025, 4, 28);
-
-        Reservation reservation = new Reservation(member, date, time, theme);
-
-        jpaThemeRepository.save(theme);
-        jpaMemberRepository.save(member);
-        jpaReservationTimeRepository.save(time);
-        jpaReservationRepository.save(reservation);
 
         em.flush();
         em.clear();
 
-        assertThat(jpaReservationRepository.findByDateAndThemeId(date, 1L).getFirst().getTime().getStartAt())
-            .isEqualTo(time.getStartAt());
+        Reservation reservation = new Reservation(member, date, time, theme);
+        reservationRepository.save(reservation);
+
+        assertThat(reservationRepository.findByDateAndThemeId(date, theme.getId())).hasSize(1);
     }
 
     @Test
-    @DisplayName("해당 날짜, 시간, 테마로 해당 예약이 있다면 true를 반환한다.")
+    @DisplayName("테마를 필터링하여 예약을 조회할 수 있다.")
+    void findByFiltersForTheme() {
+        Member member = saveMember(1L);
+        Theme theme1 = saveTheme(1L);
+        Theme theme2 = saveTheme(2L);
+        ReservationTime time = saveTime(LocalTime.of(10, 0));
+        LocalDate date = LocalDate.of(2025, 4, 28);
+
+        em.flush();
+        em.clear();
+
+        Reservation reservation1 = new Reservation(member, date, time, theme1);
+        Reservation reservation2 = new Reservation(member, date, time, theme2);
+        reservationRepository.save(reservation1);
+        reservationRepository.save(reservation2);
+
+        assertThat(reservationRepository.findByFilters(
+            theme1.getId(), null, null, null)).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("테마와 사용자를 필터링하여 예약을 조회할 수 있다.")
+    void findByFiltersForThemeAndMember() {
+        Member member1 = saveMember(1L);
+        Member member2 = saveMember(2L);
+        Theme theme1 = saveTheme(1L);
+        Theme theme2 = saveTheme(2L);
+        ReservationTime time = saveTime(LocalTime.of(10, 0));
+        LocalDate date = LocalDate.of(2025, 4, 28);
+
+        em.flush();
+        em.clear();
+
+        Reservation reservation1 = new Reservation(member1, date, time, theme1);
+        Reservation reservation2 = new Reservation(member2, date, time, theme2);
+        reservationRepository.save(reservation1);
+        reservationRepository.save(reservation2);
+
+        assertThat(reservationRepository.findByFilters(
+            theme1.getId(), member1.getId(), null, null)).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("해당 날짜, 시간, 테마로 예약이 있다면 true를 반환한다.")
     void existReservationByDateTimeAndTheme() {
-        Member member = Member.createUser("이름", "이메일", "비밀번호");
-        Theme theme = new Theme("이름", "설명", "썸네일");
-        ReservationTime time = new ReservationTime(LocalTime.of(10, 0));
+        Member member = saveMember(1L);
+        Theme theme = saveTheme(1L);
+        ReservationTime time = saveTime(LocalTime.of(10, 0));
         LocalDate date = LocalDate.of(2025, 4, 28);
-
-        Reservation reservation = new Reservation(member, date, time, theme);
-
-        jpaThemeRepository.save(theme);
-        jpaMemberRepository.save(member);
-        jpaReservationTimeRepository.save(time);
-        jpaReservationRepository.save(reservation);
 
         em.flush();
         em.clear();
 
-        assertThat(jpaReservationRepository.existsByDateAndTimeIdAndThemeIdAndMemberId(date, 1L, 1L, member.getId()))
-            .isTrue();
+        Reservation reservation = new Reservation(member, date, time, theme);
+        reservationRepository.save(reservation);
+
+        assertThat(reservationRepository.existsByDateAndTimeIdAndThemeIdAndMemberId(
+            date, time.getId(), theme.getId(), member.getId())).isTrue();
     }
 
     @Test
-    @DisplayName("해당 날짜, 시간, 테마로 해당 예약이 없다면 false 반환한다.")
+    @DisplayName("해당 날짜, 시간, 테마로 예약이 없다면 false 반환한다.")
     void notExistReservationByDateTimeAndTheme() {
-        Member member = Member.createUser("이름", "이메일", "비밀번호");
-        Theme theme = new Theme("이름", "설명", "썸네일");
-        ReservationTime time = new ReservationTime(LocalTime.of(10, 0));
+        Member member = saveMember(1L);
+        Theme theme = saveTheme(1L);
+        ReservationTime time = saveTime(LocalTime.of(10, 0));
         LocalDate date = LocalDate.of(2025, 4, 28);
-
-        Reservation reservation = new Reservation(member, date, time, theme);
-
-        jpaThemeRepository.save(theme);
-        jpaMemberRepository.save(member);
-        jpaReservationTimeRepository.save(time);
-        jpaReservationRepository.save(reservation);
 
         em.flush();
         em.clear();
 
-        assertThat(jpaReservationRepository.existsByDateAndTimeIdAndThemeIdAndMemberId(date, 1L, 2L, member.getId()))
-            .isFalse();
+        Reservation reservation = new Reservation(member, date, time, theme);
+        reservationRepository.save(reservation);
+
+        assertThat(reservationRepository.existsByDateAndTimeIdAndThemeIdAndMemberId(
+            date, time.getId(), theme.getId() + 1, member.getId())).isFalse();
+    }
+
+    private Member saveMember(Long tmp) {
+        Member member = Member.createUser("이름" + tmp, "이메일" + tmp, "비밀번호" + tmp);
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    private Theme saveTheme(Long tmp) {
+        Theme theme = new Theme("이름" + tmp, "설명" + tmp, "썸네일" + tmp);
+        themeRepository.save(theme);
+
+        return theme;
+    }
+
+    private ReservationTime saveTime(LocalTime reservationTime) {
+        ReservationTime time = new ReservationTime(reservationTime);
+        reservationTimeRepository.save(time);
+
+        return time;
     }
 }
