aa57dc4ce2e27ce9044f6eacefa18d0d5868879e
ppparkta <pppppark2@gmail.com>
2025-05-24 17:51:00 +0900

    refactor: 반영 완료된 주석 제거

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -39,11 +39,7 @@ function renderTheme(themes) {
     themes.forEach(theme => {
         const name = theme.name;
         const themeId = theme.id;
-        /*
-        TODO: [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
-              response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-              createSlot('theme', theme name, theme id) 형태로 호출
-        */
+
         themeSlots.appendChild(createSlot('theme', name, themeId));
     });
 }
@@ -85,10 +81,7 @@ function checkDateAndTheme() {
 }
 
 function fetchAvailableTimes(date, themeId) {
-    /*
-    TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
-          요청 포맷에 맞게 설정
-    */
+
     let path = `/times/booking-status?date=${date}&themeId=${themeId}`;
     fetch(path, { // 예약 가능 시간 조회 API endpoint
         method: 'GET',
@@ -115,10 +108,7 @@ function renderAvailableTimes(times) {
         return;
     }
     times.forEach(time => {
-        /*
-        TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
-              response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-        */
+
         const startAt = time.startAt;
         const timeId = time.id;
         const alreadyBooked = time.alreadyBooked;
@@ -161,11 +151,6 @@ function onReservationButtonClick() {
 
     if (selectedDate && selectedThemeId && selectedTimeId) {
 
-        /*
-        DONE: [3단계] 사용자 예약 - 예약 요청 API 호출
-              [5단계] 예약 생성 기능 변경 - 사용자
-              request 명세에 맞게 설정
-        */
         const reservationData = {
             date: selectedDate,
             themeId: selectedThemeId,
