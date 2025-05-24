2068b9f3586316e19e7c1ece48e1d54b4c980628
eunseongu <rndmstjs75@gmail.com>
2025-05-25 01:13:06 +0900

    feat: 클라이언트 코드에 alert 추가

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('/admin/reservations/waitings') // 내 예약 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
+            response.json().then(error => alert(error.message));
             throw new Error('Read failed');
         })
         .then(render)
@@ -30,12 +31,8 @@ function render(data) {
 
         const actionCell = row.insertCell(row.cells.length);
 
-        /*
-        TODO: [4단계] 예약 대기 관리 기능
-              예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
-         */
-        // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
-        // actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
+        actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
+        actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
 }
 
@@ -43,32 +40,26 @@ function approve(event) {
     const row = event.target.closest('tr');
     const id = row.cells[0].textContent;
 
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 승인 API 호출
-     */
-    const endpoint = '' + id;
+    const endpoint = '/admin/reservations/waitings/' + id;
     return fetch(endpoint, {
-        method: ''
+        method: 'PATCH'
     }).then(response => {
-        if (response.status === 200) return;
+        if (response.status === 200) return response.json();
+        response.json().then(error => alert(error.message));
         throw new Error('Delete failed');
     }).then(() => location.reload());
 }
+
 
 function deny(event) {
     const row = event.target.closest('tr');
     const id = row.cells[0].textContent;
 
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 거절 API 호출
-     */
-    const endpoint = '' + id;
+    const endpoint = '/admin/reservations/waitings/' + id;
     return fetch(endpoint, {
-        method: ''
+        method: 'DELETE'
     }).then(response => {
-        if (response.status === 200) return;
+        if (response.status === 204) return;
         throw new Error('Delete failed');
     }).then(() => location.reload());
 }
