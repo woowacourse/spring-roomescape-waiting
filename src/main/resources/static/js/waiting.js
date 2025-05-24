6ccbcf31aca3a3097046ac0afe0aab1162881268
kimdev <kim__dev@hufs.ac.kr>
2025-05-24 17:25:32 +0900

    chore: 프론트엔드 코드 수정

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -75,7 +75,7 @@ function deny(event) {
     TODO: [4단계] 예약 대기 목록 관리 기능
           예약 대기 거절 API 호출
      */
-    const endpoint = '/reservations/waiting/' + id;
+    const endpoint = `/reservations/waiting/${id}`;
     return fetch(endpoint, {
         method: 'DELETE'
     }).then(response => {
