e76b86a060fdd295949b462d669ffaca21de2b9a
eoehd1ek <eoehd2ek@gmail.com>
2025-05-25 01:05:51 +0900

    refactor(MemberServiceTest): date.sql 삭제

--- src/test/java/roomescape/business/service/MemberServiceTest.java
+++ src/test/java/roomescape/business/service/MemberServiceTest.java
@@ -7,32 +7,35 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.jdbc.Sql;
+import roomescape.business.domain.Member;
 import roomescape.persistence.repository.MemberRepository;
 import roomescape.presentation.dto.MemberResponse;
 
 @DataJpaTest
-@Sql("classpath:data-memberService.sql")
 class MemberServiceTest {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @Autowired
     public MemberServiceTest(final MemberRepository memberRepository) {
         this.memberService = new MemberService(memberRepository);
+        this.memberRepository = memberRepository;
     }
 
     @Test
     @DisplayName("모든 사용자를 조회한다")
     void findAll() {
         // given
-        // data-memberService.sql
-        // 4명의 사용자가 주어진다.
+        final Member member1 = new Member("후유", "ADMIN", "fuyu@test.com", "1234");
+        memberRepository.save(member1);
+        final Member member2 = new Member("브라운", "USER", "braun@test.com", "1234");
+        memberRepository.save(member2);
 
         // when
-        List<MemberResponse> memberResponses = memberService.findAll();
+        final List<MemberResponse> memberResponses = memberService.findAll();
 
         // then
-        assertThat(memberResponses).hasSize(4);
+        assertThat(memberResponses).hasSize(2);
     }
 }
