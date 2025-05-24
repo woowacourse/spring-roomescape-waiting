74999440343d5a3bc5027bbe83bddc41ea4ba3c7
juno <wnsgh732@gmail.com>
2025-05-24 21:24:04 +0900

    test: `UserWaitingController`에 대한 테스트 작성

--- src/test/java/roomescape/reservation/controller/UserWaitingControllerTest.java
+++ src/test/java/roomescape/reservation/controller/UserWaitingControllerTest.java
@@ -1,17 +1,15 @@
 package roomescape.reservation.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import jakarta.servlet.http.Cookie;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,59 +21,65 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
-import roomescape.reservation.application.AdminWaitingService;
-import roomescape.reservation.application.dto.response.MyWaitingServiceResponse;
+import roomescape.reservation.application.UserWaitingService;
+import roomescape.reservation.application.dto.response.WaitingServiceResponse;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 @Sql(statements = {
-    "INSERT INTO member (name, email, password, role) VALUES ('어드민', 'test_admin@naver.com', '1234', 'ADMIN')",
+    "INSERT INTO member (name, email, password, role) VALUES ('테스트', 'test_user@naver.com', '1234', 'USER')",
 })
-class AdminWaitingControllerTest {
+@Transactional
+class UserWaitingControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockitoBean
-    private AdminWaitingService adminWaitingService;
+    private UserWaitingService userWaitingService;
 
     @Test
-    @DisplayName("GET /admin/waitings 요청에 올바르게 응답한다")
-    void getAllWaitingsTest() throws Exception {
-        given(adminWaitingService.getAllWaitings()).willReturn(List.of(
-            new MyWaitingServiceResponse(1L, "두리", "테마", LocalDate.of(2025, 5, 24),
-                LocalTime.of(10, 0))
-        ));
+    @DisplayName("POST /reservations-wait 요청에 올바르게 응답한다")
+    void createWaitingTest() throws Exception {
+        WaitingServiceResponse response = new WaitingServiceResponse(1L, "테스트",
+            LocalDate.of(2025, 5, 24), LocalTime.of(10, 0), "테마");
+        given(userWaitingService.create(any())).willReturn(response);
 
-        String token = adminLogin();
+        String token = login();
         Cookie cookie = new Cookie("token", token);
-        mockMvc.perform(get("/admin/waitings")
+        mockMvc.perform(post("/reservations-wait")
+                .contentType("application/json")
+                .content("""
+                        {
+                            "themeId": 1,
+                            "date": "2025-05-24",
+                            "timeId": 1
+                        }
+                    """)
                 .cookie(cookie))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.length()").value(1));
+            .andExpect(status().isCreated());
     }
 
     @Test
-    @DisplayName("DELETE /admin/waitings/{id} 삭제시 204를 반환한다")
-    void removeWaitingTest() throws Exception {
+    @DisplayName("DELETE /reservations-wait/{id} 요청에 올바르게 응답한다")
+    void deleteWaitingTest() throws Exception {
         Long id = 1L;
-        doNothing().when(adminWaitingService).deleteById(id);
+        doNothing().when(userWaitingService).deleteMyWaitingById(id, 1L);
 
-        String token = adminLogin();
+        String token = login();
         Cookie cookie = new Cookie("token", token);
-        mockMvc.perform(delete("/admin/waitings/{id}", id)
+        mockMvc.perform(delete("/reservations-wait/{id}", id)
                 .cookie(cookie))
             .andExpect(status().isNoContent());
     }
 
-    private String adminLogin() throws Exception {
+    private String login() throws Exception {
         RequestBuilder request = post("/login")
             .contentType("application/json")
             .content("""
                 {
-                    "email": "test_admin@naver.com",
+                    "email": "test_user@naver.com",
                     "password": "1234"
                 }
                 """);
@@ -85,4 +89,4 @@ class AdminWaitingControllerTest {
             .getCookie("token")
             .getValue();
     }
-}
+} 
