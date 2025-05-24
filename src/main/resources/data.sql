fc11ab3140fd7d14d5160480537a269cb3ddeddf
(minjae8563) <(minjae8563@naver.com)>
2025-05-25 02:46:38 +0900

    feat: 더미 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,50 +20,48 @@ VALUES (1, '레벨2 탈출', '우테코 레벨2를 탈출하는 내용입니다.
 INSERT INTO theme (id, name, description, thumbnail)
 VALUES (2, '지하 감옥', '깊은 감옥에서 탈출하라!', 'https://truthfoundation.or.kr/media/images/68.width-1200.jpg');
 
--- 예약: 오래된 데이터 (2025-04-11 ~ 2025-04-20)
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (1, '2025-04-20', 2, 1, 1);
+VALUES (1, CURRENT_DATE, 2, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (2, '2025-04-19', 3, 2, 1);
+VALUES (2, CURRENT_DATE - 1, 3, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (3, '2025-04-18', 1, 1, 1);
+VALUES (3, CURRENT_DATE - 2, 1, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (4, '2025-04-17', 2, 2, 1);
+VALUES (4, CURRENT_DATE - 3, 2, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (5, '2025-04-16', 3, 1, 1);
+VALUES (5, CURRENT_DATE - 4, 3, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (6, '2025-04-15', 1, 2, 1);
+VALUES (6, CURRENT_DATE - 5, 1, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (7, '2025-04-14', 2, 1, 1);
+VALUES (7, CURRENT_DATE - 6, 2, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (8, '2025-04-13', 3, 2, 1);
+VALUES (8, CURRENT_DATE - 7, 3, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (9, '2025-04-12', 1, 1, 1);
+VALUES (9, CURRENT_DATE - 8, 1, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (10, '2025-04-11', 2, 2, 1);
+VALUES (10, CURRENT_DATE - 9, 2, 2, 1);
 
--- 예약: 최근 데이터 (2025-04-30 ~ 2025-05-06)
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (11, '2025-05-06', 2, 2, 1);
+VALUES (11, CURRENT_DATE - 10, 2, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (12, '2025-05-05', 3, 1, 1);
+VALUES (12, CURRENT_DATE - 11, 3, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (13, '2025-05-04', 1, 2, 1);
+VALUES (13, CURRENT_DATE - 12, 1, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (14, '2025-05-03', 2, 1, 1);
+VALUES (14, CURRENT_DATE - 13, 2, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (15, '2025-05-02', 3, 2, 1);
+VALUES (15, CURRENT_DATE - 14, 3, 2, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (16, '2025-05-01', 1, 1, 1);
+VALUES (16, CURRENT_DATE - 15, 1, 1, 1);
 INSERT INTO reservation (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (17, '2025-04-30', 2, 2, 1);
+VALUES (17, CURRENT_DATE - 16, 2, 2, 1);
 
 INSERT INTO waiting (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (1, '2025-04-20', 2, 1, 2);
+VALUES (1, CURRENT_DATE, 2, 1, 2);
 INSERT INTO waiting (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (2, '2025-04-19', 3, 2, 2);
+VALUES (2, CURRENT_DATE - 1, 3, 2, 2);
 INSERT INTO waiting (id, reservation_date, reservation_time_id, theme_id, member_id)
-VALUES (3, '2025-04-18', 1, 1, 2);
+VALUES (3, CURRENT_DATE - 2, 1, 1, 2);
 
 ALTER TABLE reservation_time
     ALTER COLUMN id RESTART WITH 4;
