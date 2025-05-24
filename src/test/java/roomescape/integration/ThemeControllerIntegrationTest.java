1d694781a4a4366f0cd6d45040f18a14eb7647a3
eoehd1ek <eoehd2ek@gmail.com>
2025-05-25 06:11:34 +0900

    test: 실패하는 인기 테마 조회 테스트 시나리오에 맞게 수정

--- src/test/java/roomescape/integration/ThemeControllerIntegrationTest.java
+++ src/test/java/roomescape/integration/ThemeControllerIntegrationTest.java
@@ -8,6 +8,8 @@ import static org.hamcrest.Matchers.hasSize;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +20,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
+import roomescape.business.domain.Member;
+import roomescape.business.domain.Reservation;
+import roomescape.business.domain.ReservationTime;
+import roomescape.business.domain.Theme;
+import roomescape.business.domain.WaitInfo;
 import roomescape.business.service.ThemeService;
+import roomescape.persistence.repository.MemberRepository;
+import roomescape.persistence.repository.ReservationRepository;
+import roomescape.persistence.repository.ReservationTimeRepository;
+import roomescape.persistence.repository.ThemeRepository;
+import roomescape.persistence.repository.WaitInfoRepository;
 import roomescape.presentation.dto.ThemeRequest;
 import roomescape.presentation.dto.ThemeResponse;
 
@@ -31,6 +43,16 @@ class ThemeControllerIntegrationTest {
     private int port;
     @Autowired
     private ThemeService themeService;
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private ReservationTimeRepository reservationTimeRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ThemeRepository themeRepository;
+    @Autowired
+    private WaitInfoRepository waitInfoRepository;
 
     @BeforeEach
     void setUp() {
@@ -92,19 +114,31 @@ class ThemeControllerIntegrationTest {
     @DisplayName("인기 테마 데이터를 조회할 때, 인기 테마 목록을 반환해야 한다")
     void readPopularThemes() {
         // given
-        final ThemeRequest themeRequest1 = new ThemeRequest("Popular 1", "Popular description 1", "썸네일");
-        themeService.insert(themeRequest1);
-        final ThemeRequest themeRequest2 = new ThemeRequest("Popular 2", "Popular description 2", "썸네일120");
-        themeService.insert(themeRequest2);
+        final ReservationTime time = new ReservationTime(LocalTime.of(14, 0));
+        reservationTimeRepository.save(time);
+
+        final Member member = new Member("후유", "ADMIN", "fuyu@test.com", "pass");
+        memberRepository.save(member);
+
+        final Theme theme1 = new Theme("테마1", "설명1", "썸네일1");
+        themeRepository.save(theme1);
+        final Theme theme2 = new Theme("테마2", "설명2", "썸네일2");
+        themeRepository.save(theme2);
+
+        final Reservation reservation = new Reservation(LocalDate.now().minusDays(1), time, theme2);
+        reservationRepository.save(reservation);
+
+        final WaitInfo waitInfo = new WaitInfo(member, reservation, 1L);
+        waitInfoRepository.save(waitInfo);
 
         // when & then
-        // TODO: Reservation을 넣어 더욱 실제와 유사한 테스트를 작성한다.
         when()
                 .get("/popular")
                 .then()
                 .statusCode(HttpStatus.OK.value())
                 .body("", hasSize(2))
-                .body("name", hasItems("Popular 1", "Popular 2"));
+                .body("name", hasItems("테마1", "테마2"))
+                .body("[0].name", equalTo("테마2"));
     }
 
 
