d966404f46358289fd4bf52d1029e2e888a905bf
BWY <quantumjump10@gmail.com>
2025-05-26 00:59:35 +0900

    test: 예약 대기를 포함한 나의 예약 기록 조회 테스트 구현

--- src/test/java/roomescape/application/MyReservationServiceTest.java
+++ src/test/java/roomescape/application/MyReservationServiceTest.java
@@ -10,12 +10,15 @@ import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.domain.Role;
 import roomescape.domain.Theme;
+import roomescape.domain.Waiting;
 import roomescape.fixture.MemberDbFixture;
 import roomescape.fixture.ReservationDbFixture;
 import roomescape.fixture.ReservationTimeDbFixture;
 import roomescape.fixture.ThemeDbFixture;
+import roomescape.fixture.WaitingDbFixture;
 import roomescape.presentation.dto.request.LoginMember;
 import roomescape.presentation.dto.response.MyReservationResponse;
+import roomescape.presentation.dto.response.MyReservationWithWaitingResponse;
 
 import java.util.List;
 
@@ -40,6 +43,9 @@ class MyReservationServiceTest extends BaseTest {
     @Autowired
     private ReservationDbFixture reservationDbFixture;
 
+    @Autowired
+    private WaitingDbFixture waitingDbFixture;
+
     @Test
     void 나의_예약_기록을_조회한다() {
         ReservationTime reservationTime = reservationTimeDbFixture.예약시간_10시();
@@ -57,4 +63,21 @@ class MyReservationServiceTest extends BaseTest {
         );
     }
 
+    @Test
+    void 예약_대기_포함_나의_예약_기록을_조회한다() {
+        ReservationTime reservationTime = reservationTimeDbFixture.예약시간_10시();
+        Theme theme = themeDbFixture.공포();
+        Member member = memberDbFixture.한스_사용자();
+
+        Waiting waiting = waitingDbFixture.예약_대기_한스_25_4_22_10시_공포(member, reservationTime, theme);
+        LoginMember loginMember = new LoginMember(member.getId(), member.getName(), Role.USER, member.getEmail());
+
+        List<MyReservationWithWaitingResponse> myReservationsWithWaitings = myReservationService.getMyReservationsWithWaitings(loginMember);
+
+        assertAll(
+                () -> assertThat(myReservationsWithWaitings).hasSize(1),
+                () -> assertThat(myReservationsWithWaitings.getFirst().id()).isEqualTo(waiting.getId()),
+                () -> assertThat(myReservationsWithWaitings.getFirst().status()).isEqualTo("1번째 예약대기")
+        );
+    }
 }
