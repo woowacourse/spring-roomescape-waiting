6f8290db7669fef12f10cc3a6c1b1151de07f6b1
Jin409 <sualng123@gmail.com>
2025-05-25 23:20:32 +0900

    test: 앞선 예약의 개수를 세는 기능 테스트 추가

--- src/test/java/roomescape/infrastructure/db/WaitingJpaRepositoryTest.java
+++ src/test/java/roomescape/infrastructure/db/WaitingJpaRepositoryTest.java
@@ -95,4 +95,56 @@ class WaitingJpaRepositoryTest {
                 () -> assertThat(earliestWaiting.get().getId()).isEqualTo(targetWaiting.getId())
         );
     }
+
+    @Test
+    @DisplayName("예약 날짜 테마 시각이 동일하고 특정 시점보다 이른 웨이팅의 개수를 센다")
+    void test2() {
+        // given
+        ReservationTime reservationTime = new ReservationTime(LocalTime.of(12, 30));
+        ReservationTime savedReservationTime = reservationTimeJpaRepository.save(reservationTime);
+
+        Theme theme = new Theme("테마", "설명", "썸네일");
+        Theme savedTheme = themeJpaRepository.save(theme);
+
+        Theme anotherTheme = themeJpaRepository.save(new Theme("다른 테마", "설명", "썸네일"));
+
+        Member member = new Member("도기", "email@gmail.com", "password", Role.ADMIN);
+        Member savedMember = memberJpaRepository.save(member);
+
+        LocalDate reservationDate = LocalDate.now().plusDays(1);
+        Waiting targetWaiting = waitingJpaRepository.save(new Waiting(
+                        LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 30)),
+                        new Reservation(
+                                reservationDate,
+                                savedReservationTime,
+                                savedTheme,
+                                savedMember,
+                                LocalDate.now()
+                        )
+                )
+        );
+
+        Waiting secondOrderWaiting = waitingJpaRepository.save(new Waiting(
+                        LocalDateTime.of(LocalDate.now(), LocalTime.of(13, 30)),
+                        new Reservation(
+                                reservationDate,
+                                savedReservationTime,
+                                savedTheme,
+                                savedMember,
+                                LocalDate.now()
+                        )
+                )
+        );
+
+        // when
+        int actual = waitingJpaRepository.countWaitingBefore(
+                LocalDateTime.of(LocalDate.now(), LocalTime.of(20, 0)),
+                reservationDate,
+                theme,
+                reservationTime
+        );
+
+        // then
+        assertThat(actual).isEqualTo(2);
+    }
 }
