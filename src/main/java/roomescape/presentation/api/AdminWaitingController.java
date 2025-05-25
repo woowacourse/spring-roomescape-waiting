7bf0488c36717ab36aeb1de849b2d531bf88cc0f
BWY <quantumjump10@gmail.com>
2025-05-25 21:00:34 +0900

    feat: admin의 예약 대기 취소 기능 구현

--- src/main/java/roomescape/presentation/api/AdminWaitingController.java
+++ src/main/java/roomescape/presentation/api/AdminWaitingController.java
@@ -1,7 +1,9 @@
 package roomescape.presentation.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.application.WaitingService;
@@ -23,5 +25,11 @@ public class AdminWaitingController {
     public ResponseEntity<List<WaitingResponse>> getAllWaitings() {
         List<WaitingResponse> allWaitings = waitingService.getAllWaitings();
         return ResponseEntity.ok(allWaitings);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> denyWaiting(@PathVariable Long id) {
+        waitingService.deleteWaiting(id);
+        return ResponseEntity.noContent().build();
     }
 }
