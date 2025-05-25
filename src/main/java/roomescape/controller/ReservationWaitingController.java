38bedc3ca3aded82217161bcf8f3b4b665fa9629
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-05-25 23:25:47 +0900

    feat: 예약 대기 생성 api 구현 및 테스트

---
+++ src/main/java/roomescape/controller/ReservationWaitingController.java
@@ -0,0 +1,26 @@
+package roomescape.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.service.ReservationWaitingService;
+import roomescape.service.dto.ReservationWaitingRequest;
+import roomescape.service.dto.ReservationWaitingResponse;
+
+@RestController
+@RequestMapping("/reservations-waiting")
+@RequiredArgsConstructor
+public class ReservationWaitingController {
+
+    private final ReservationWaitingService reservationWaitingService;
+
+    @PostMapping
+    public ResponseEntity<ReservationWaitingResponse> addReservationWaiting(@RequestBody @Valid final ReservationWaitingRequest request, final Long memberId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservationWaitingService.addReservationWaiting(request, memberId));
+    }
+}
