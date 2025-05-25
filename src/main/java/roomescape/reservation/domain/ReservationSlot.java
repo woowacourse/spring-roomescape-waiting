53d0b12893efcbcaf176a8012441a7c2a9fbdf40
Yeongjae Kim <sees77770@gmail.com>
2025-05-25 19:27:22 +0900

    feat: 예약 슬롯 생성 유효성 검사 추가

--- src/main/java/roomescape/reservation/domain/ReservationSlot.java
+++ src/main/java/roomescape/reservation/domain/ReservationSlot.java
@@ -2,15 +2,30 @@ package roomescape.reservation.domain;
 
 import java.util.Comparator;
 import java.util.List;
+import roomescape.reservation.domain.exception.NotSameSlotException;
 
 public class ReservationSlot {
 
     private final List<Reservation> reservations;
 
     public ReservationSlot(List<Reservation> reservations) {
+        validateSameSlot(reservations);
         this.reservations = reservations.stream()
                 .sorted(Comparator.comparing(Reservation::getReservedAt))
                 .toList();
+    }
+
+    private void validateSameSlot(List<Reservation> reservations) {
+        Reservation first = reservations.getFirst();
+        boolean isNotSameSlot = reservations.stream()
+                .anyMatch(reservation ->
+                        !reservation.getDate().equals(first.getDate())
+                                || !reservation.getTimeId().equals(first.getTimeId())
+                                || !reservation.getTheme().equals(first.getTheme())
+                );
+        if (isNotSameSlot) {
+            throw new NotSameSlotException();
+        }
     }
 
     public int getOrder(Reservation reservation) {
--- src/main/java/roomescape/reservation/domain/exception/PastReservationException.java
+++ src/main/java/roomescape/reservation/domain/exception/PastReservationException.java
@@ -1,7 +1,6 @@
 package roomescape.reservation.domain.exception;
 
 public class PastReservationException extends RuntimeException {
-
     public PastReservationException() {
         super("지난 날짜에는 예약할 수 없습니다.");
     }
