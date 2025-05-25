4c50a7bfdc9e830693396a9af46e7fed135b221f
soeun2537 <soeun2537@gmail.com>
2025-05-26 04:09:52 +0900

    refactor: 엔드포인트 수정

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -227,7 +227,7 @@ function onWaitButtonClick() {
         /*
         TODO: [3단계] 예약 대기 생성 요청 API 호출
          */
-        fetch('/waiting', {
+        fetch('/waitings', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
