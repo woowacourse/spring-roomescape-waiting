5a25586f0860ea6091420e28423ca839e9d088d3
soeun2537 <soeun2537@gmail.com>
2025-05-25 13:07:29 +0900

    refactor: 컬럼 네임 직접 명시

--- src/main/java/roomescape/reservation/entity/Reservation.java
+++ src/main/java/roomescape/reservation/entity/Reservation.java
@@ -1,9 +1,12 @@
 package roomescape.reservation.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -24,15 +27,19 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "date")
     private LocalDate date;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "time_id")
     private ReservationTime time;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id")
     private Theme theme;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     public Reservation(LocalDate date, ReservationTime time, Theme theme, Member member) {
