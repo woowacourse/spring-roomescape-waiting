10c92b700b5399392d8c9fca13eb40cb8efb04c7
RaZel713 <razel7130@gmail.com>
2025-05-25 13:01:15 +0900

    refactor: 변수 네이밍 변경

--- src/main/java/roomescape/application/AuthenticationService.java
+++ src/main/java/roomescape/application/AuthenticationService.java
@@ -10,34 +10,34 @@ import roomescape.exception.AuthenticationException;
 @Service
 public class AuthenticationService {
 
-    private final AuthenticationTokenHandler tokenProvider;
+    private final AuthenticationTokenHandler tokenHandler;
     private final UserRepository userRepository;
 
     public AuthenticationService(final AuthenticationTokenHandler tokenHandler, final UserRepository userRepository) {
-        this.tokenProvider = tokenHandler;
+        this.tokenHandler = tokenHandler;
         this.userRepository = userRepository;
     }
 
     public String issueToken(final String email, final String password) {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new AuthenticationException("이메일 또는 비밀번호가 틀렸습니다."));
+                .orElseThrow(() -> new AuthenticationException("이메일이 틀렸습니다."));
 
         if (!user.matchesPassword(password)) {
-            throw new AuthenticationException("이메일 또는 비밀번호가 틀렸습니다.");
+            throw new AuthenticationException("비밀번호가 틀렸습니다.");
         }
 
         AuthenticationInfo authenticationInfo = new AuthenticationInfo(user.id(), user.role());
-        return tokenProvider.createToken(authenticationInfo);
+        return tokenHandler.createToken(authenticationInfo);
     }
 
     public User getUserByToken(final String token) {
-        boolean isValidToken = tokenProvider.isValidToken(token);
+        boolean isValidToken = tokenHandler.isValidToken(token);
 
         if (!isValidToken) {
             throw new AuthenticationException("토큰이 만료되었거나 유효하지 않습니다.");
         }
 
-        long id = tokenProvider.extractId(token);
+        long id = tokenHandler.extractId(token);
 
         return userRepository.findById(id)
                 .orElseThrow(() -> new AuthenticationException("사용자 정보가 없습니다. 다시 로그인 해주세요."));
