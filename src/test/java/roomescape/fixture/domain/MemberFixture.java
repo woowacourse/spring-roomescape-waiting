be2d1221fbe884e0baf127c9d2f62b7e69a73d5f
threepebbles <threepebbles@naver.com>
2025-05-25 15:50:41 +0900

    fix: 회원 테스트 픽스처를 테스트 전역에서 공유하지 않도록 수정

--- src/test/java/roomescape/fixture/domain/MemberFixture.java
+++ src/test/java/roomescape/fixture/domain/MemberFixture.java
@@ -6,21 +6,21 @@ import roomescape.member.domain.Member;
 
 public class MemberFixture {
 
-    public static List<Member> NOT_SAVED_MEMBERS = List.of(
-            Member.of("헤일러", "he@iler.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("머피", "mu@ffy.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("우가", "woo@ga.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("피케이", "p@k.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("매트", "ma@tt.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("파랑", "pa@rang.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("회원1", "member1@member1.com", "비밀번호", AuthRole.MEMBER),
-            Member.of("회원2", "member2@member2.com", "비밀번호", AuthRole.MEMBER)
-    );
-
     private MemberFixture() {
     }
 
     public static List<Member> notSavedMembers(int exclusiveIdx) {
+        final List<Member> NOT_SAVED_MEMBERS = List.of(
+                Member.of("헤일러", "he@iler.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("머피", "mu@ffy.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("우가", "woo@ga.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("피케이", "p@k.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("매트", "ma@tt.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("파랑", "pa@rang.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("회원1", "member1@member1.com", "비밀번호", AuthRole.MEMBER),
+                Member.of("회원2", "member2@member2.com", "비밀번호", AuthRole.MEMBER)
+        );
+
         if (NOT_SAVED_MEMBERS.size() < exclusiveIdx) {
             throw new IllegalStateException("회원 픽스처의 개수는 최대 " + NOT_SAVED_MEMBERS.size() + "개만 가능합니다.");
         }
@@ -29,10 +29,10 @@ public class MemberFixture {
     }
 
     public static Member notSavedMember1() {
-        return NOT_SAVED_MEMBERS.get(0);
+        return Member.of("헤일러", "he@iler.com", "비밀번호", AuthRole.MEMBER);
     }
 
     public static Member notSavedMember2() {
-        return NOT_SAVED_MEMBERS.get(1);
+        return Member.of("머피", "mu@ffy.com", "비밀번호", AuthRole.MEMBER);
     }
 }
