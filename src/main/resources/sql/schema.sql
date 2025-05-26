c40b294ed4de79b16393e4567ccd3a328a08de6f
gabean13 <choegabin171@gmail.com>
2025-05-26 19:58:48 +0900

    refactor: schema query update

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE member
+CREATE TABLE IF NOT EXISTS member
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role     BIGINT       NOT NULL,
+    role     ENUM('ADMIN', 'USER') NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role (id)
 );
@@ -25,28 +25,19 @@ CREATE TABLE IF NOT EXISTS theme
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS
-    reservation_slot
+CREATE TABLE IF NOT EXISTS waiting
 (
-    id       BIGINT NOT NULL AUTO_INCREMENT,
-    theme_id BIGINT NOT NULL,
-    time_id  BIGINT NOT NULL,
-    date     DATE   NOT NULL,
+    id          BIGINT    NOT NULL AUTO_INCREMENT,
+    member_id   BIGINT    NOT NULL,
+    theme_id    BIGINT    NOT NULL,
+    time_id     BIGINT    NOT NULL,
+    date        DATE      NOT NULL,
+    created_at  TIMESTAMP NOT NULL,
+    status      ENUM('PENDING', 'APPROVED', 'DENIED'),
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
-);
-
-CREATE TABLE IF NOT EXISTS waiting
-(
-    id                  BIGINT    NOT NULL AUTO_INCREMENT,
-    member_id           BIGINT    NOT NULL,
-    reservation_slot_id BIGINT    NOT NULL,
-    created_at          TIMESTAMP NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (reservation_slot_id) REFERENCES reservation_slot (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation
@@ -56,5 +47,6 @@ CREATE TABLE IF NOT EXISTS reservation
     reservation_slot_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (reservation_slot_id) REFERENCES reservation_slot (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
