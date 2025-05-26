b157b4723dfb85c9c4e3eab57f3d4a3ae5fe723d
seaniiio <cnc4750@naver.com>
2025-05-26 14:43:12 +0900

    test: MemberController 단위 테스트 작성

--- src/main/java/roomescape/auth/AuthorizationAdminInterceptor.java
+++ src/main/java/roomescape/auth/AuthorizationAdminInterceptor.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.exception.custom.reason.auth.AuthNotExistsCookieException;
 import roomescape.exception.custom.reason.auth.AuthNotValidTokenException;
@@ -13,6 +14,7 @@ import roomescape.member.MemberRole;
 import java.util.Arrays;
 import java.util.Objects;
 
+@Component
 @AllArgsConstructor
 public class AuthorizationAdminInterceptor implements HandlerInterceptor {
 
