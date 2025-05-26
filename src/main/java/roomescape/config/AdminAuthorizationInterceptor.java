9537fb76086944fcad48bdfd655fff2b973de865
gabean13 <choegabin171@gmail.com>
2025-05-26 20:49:10 +0900

    rename: LoginInterceptor를 AdminAuthorizationInterceptor로 이름 변경

--- src/main/java/roomescape/config/AdminAuthorizationInterceptor.java
+++ src/main/java/roomescape/config/AdminAuthorizationInterceptor.java
@@ -2,41 +2,48 @@ package roomescape.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.exception.ExceptionCause;
 import roomescape.exception.UnauthorizedException;
+import roomescape.jwt.CookieManager;
+import roomescape.jwt.TokenProvider;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.Role;
 import roomescape.member.service.MemberService;
-import roomescape.jwt.CookieTokenExtractor;
-import roomescape.jwt.TokenProvider;
 
-public class LoginInterceptor implements HandlerInterceptor {
+public class AdminAuthorizationInterceptor implements HandlerInterceptor {
 
     private final TokenProvider TokenProvider;
     private final MemberService memberService;
-    private final CookieTokenExtractor authorizationExtractor;
 
-    public LoginInterceptor(TokenProvider TokenProvider, MemberService memberService) {
+    public AdminAuthorizationInterceptor(TokenProvider TokenProvider, MemberService memberService) {
         this.TokenProvider = TokenProvider;
         this.memberService = memberService;
-        this.authorizationExtractor = new CookieTokenExtractor();
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String token = authorizationExtractor.extract(request);
-        if (token == null || token.isBlank()) {
-            throw new UnauthorizedException(ExceptionCause.JWT_TOKEN_EMPTY);
-        }
-
+        String token = extractToken(request);
         Long memberId = TokenProvider.getMemberIdFromToken(token);
         Member member = memberService.findById(memberId);
-
         if (!Role.isAdmin(member.getRole())) {
-            response.setStatus(403);
+            redirectToLoginPage(response);
             return false;
         }
         return true;
+    }
+
+    private void redirectToLoginPage(HttpServletResponse response) {
+        try {
+            response.sendRedirect("/login");
+        } catch (IOException e) {
+            throw new RuntimeException("Redirect failed", e);
+        }
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        return CookieManager.extractByName("token", request)
+                .orElseThrow(() -> new UnauthorizedException(ExceptionCause.UNAUTHORIZED_PAGE_ACCESS));
     }
 }
