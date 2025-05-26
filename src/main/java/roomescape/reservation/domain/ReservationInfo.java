a0e92664eccf200882274d61ee716b6ae67d24d9
byesol <bsj5967@naver.com>
2025-05-26 22:52:06 +0900

    refactor(domain) : equals,hashcode 재정의

--- src/main/java/roomescape/reservation/domain/ReservationInfo.java
+++ src/main/java/roomescape/reservation/domain/ReservationInfo.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
+import java.util.Objects;
 import roomescape.reservationtime.domain.ReservationTime;
 import roomescape.theme.domain.Theme;
 
@@ -23,19 +24,18 @@ public class ReservationInfo {
     @JoinColumn(name = "theme_id", nullable = false)
     private Theme theme;
 
+    protected ReservationInfo() {
+    }
+
     public ReservationInfo(final LocalDate date, final ReservationTime time, final Theme theme) {
         this.date = date;
         this.time = time;
         this.theme = theme;
     }
 
-    public ReservationInfo() {
-    }
-
     public LocalDate getDate() {
         return date;
     }
-
 
     public ReservationTime getTime() {
         return time;
@@ -45,4 +45,17 @@ public class ReservationInfo {
         return theme;
     }
 
+    @Override
+    public boolean equals(final Object object) {
+        if (!(object instanceof final ReservationInfo that)) {
+            return false;
+        }
+        return Objects.equals(getDate(), that.getDate()) && Objects.equals(getTheme(), that.getTheme())
+                && Objects.equals(getTime(), that.getTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDate(), getTheme(), getTime());
+    }
 }
