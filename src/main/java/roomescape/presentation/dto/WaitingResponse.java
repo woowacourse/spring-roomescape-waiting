dcabf29b0ce3528895a117b4c32f10f4d7bdb471
mimi <110226866+hongkikii@users.noreply.github.com>
2025-05-27 00:18:28 +0900

    refactor: DTO 클래스 내 도메인 객체 필드 제거

--- src/main/java/roomescape/presentation/dto/WaitingResponse.java
+++ src/main/java/roomescape/presentation/dto/WaitingResponse.java
@@ -2,15 +2,17 @@ package roomescape.presentation.dto;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import roomescape.business.domain.Member;
-import roomescape.business.domain.ReservationTime;
-import roomescape.business.domain.Theme;
 import roomescape.business.domain.Waiting;
 
-public record WaitingResponse(Long id, ReservationTime time, Theme theme, Member member, LocalDate date,
-                              LocalDateTime createdAt) {
+public record WaitingResponse(Long id, ReservationTimeResponse time, ThemeResponse theme, MemberResponse member,
+                              LocalDate date, LocalDateTime createdAt) {
     public static WaitingResponse from(Waiting waiting) {
-        return new WaitingResponse(waiting.getId(), waiting.getTime(), waiting.getTheme(), waiting.getMember(),
-                waiting.getDate(), waiting.getCreatedAt());
+        return new WaitingResponse(
+                waiting.getId(),
+                ReservationTimeResponse.from(waiting.getTime()),
+                ThemeResponse.from(waiting.getTheme()),
+                MemberResponse.from(waiting.getMember()),
+                waiting.getDate(),
+                waiting.getCreatedAt());
     }
 }
