2266b463fee37de4d1773f364540ccc69429b80f
ppparkta <pppppark2@gmail.com>
2025-05-26 22:29:20 +0900

    test: 예약상태 엔티티 테스트 추가

--- src/test/java/roomescape/domain/StatusTest.java
+++ src/test/java/roomescape/domain/StatusTest.java
@@ -1,7 +1,36 @@
 package roomescape.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class StatusTest {
 
+    @DisplayName("예약이 취소된다")
+    @Test
+    void cancelStatus() {
+        // given
+        Status status = Status.statusWithoutId(LocalDateTime.of(2025, 1, 1, 10, 0, 0), ReservationStatus.RESERVED);
+
+        // when
+        status.cancelStatus();
+
+        // then
+        assertThat(status.getStatus()).isEqualTo(ReservationStatus.CANCELED);
+    }
+
+    @DisplayName("예약된다")
+    @Test
+    void reserveStatus() {
+        // given
+        Status status = Status.statusWithoutId(LocalDateTime.of(2025, 1, 1, 10, 0, 0), ReservationStatus.WAITING);
+
+        // when
+        status.reserveStatus();
+
+        // then
+        assertThat(status.getStatus()).isEqualTo(ReservationStatus.RESERVED);
+    }
 }
