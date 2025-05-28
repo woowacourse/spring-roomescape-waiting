575adab26eede39779ac64c406c5d1f10377fd29
Mint <audwl03071@gmail.com>
2025-05-28 22:34:53 +0900

    feat: LocalDate.now()에 대해 clock 설정하여 테스트 용이성을 높인다.

--- src/main/java/roomescape/theme/application/ThemeApplicationService.java
+++ src/main/java/roomescape/theme/application/ThemeApplicationService.java
@@ -1,5 +1,6 @@
 package roomescape.theme.application;
 
+import java.time.Clock;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -14,24 +15,14 @@ public class ThemeApplicationService {
 
     private final ThemeDataService themeDataService;
     private final ReservationSlotDataService reservationSlotDataService;
+    private final Clock clock;
 
     public ThemeApplicationService(final ThemeDataService themeDataService,
-                                   final ReservationSlotDataService reservationSlotDataService) {
+                                   final ReservationSlotDataService reservationSlotDataService,
+                                   final Clock clock) {
         this.themeDataService = themeDataService;
         this.reservationSlotDataService = reservationSlotDataService;
-    }
-
-    public List<ThemeResponse> getThemes() {
-        return themeDataService.getThemes().stream()
-                .map(ThemeResponse::from)
-                .toList();
-    }
-
-    public void delete(Long id) {
-        if (reservationSlotDataService.existsByThemeId(id)) {
-            throw new ReservationTimeInUseException("해당 테마에 대한 예약이 존재하여 삭제할 수 없습니다.");
-        }
-        themeDataService.deleteById(id);
+        this.clock = clock;
     }
 
     public ThemeResponse create(final ThemeCreateRequest request) {
@@ -39,10 +30,27 @@ public class ThemeApplicationService {
         return ThemeResponse.from(theme);
     }
 
-    public List<ThemeResponse> getPopularThemes(int days, int limit) {
-        Page<Theme> popularThemes = themeDataService.findPopularThemes(days, limit);
+    public List<ThemeResponse> findAll() {
+        return themeDataService.findAll().stream()
+                .map(ThemeResponse::from)
+                .toList();
+    }
+
+    public List<ThemeResponse> findPopular(int days, int limit) {
+        Page<Theme> popularThemes = themeDataService.findPopularThemes(clock, days, limit);
         return popularThemes.getContent().stream()
                 .map(ThemeResponse::from)
                 .toList();
+    }
+
+    public void delete(Long id) {
+        validateExist(id);
+        themeDataService.deleteById(id);
+    }
+
+    private void validateExist(final Long id) {
+        if (reservationSlotDataService.existsByThemeId(id)) {
+            throw new ReservationTimeInUseException("해당 테마에 대한 예약이 존재하여 삭제할 수 없습니다.");
+        }
     }
 }
