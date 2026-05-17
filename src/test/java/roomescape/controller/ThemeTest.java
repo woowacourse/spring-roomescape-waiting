9fb45d9cbf487b1a0df2d7cb0e412dd62b221a50
haeseon lee <haeseon4816@gmail.com>
2026-05-17 20:15:15 +0900

    test: 에외 관련 테스트 추가 및 보완

---
+++ src/test/java/roomescape/controller/ThemeTest.java
@@ -0,0 +1,77 @@
+package roomescape.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ThemeTest {
+
+    @Test
+    void 테마_관리_API() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "무서운 이야기");
+        params.put("description", "공포");
+        params.put("url", "http://example.com");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/themes")
+                .then().log().all()
+                .statusCode(201);
+
+        RestAssured.given().log().all()
+                .when().get("/themes")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/admin/themes/1")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    void 예약이_존재하는_테마_삭제시_400을_반환한다() {
+        Map<String, String> theme = new HashMap<>();
+        theme.put("name", "무서운 이야기");
+        theme.put("description", "공포");
+        theme.put("url", "http://example.com");
+
+        RestAssured.given().contentType(ContentType.JSON).body(theme)
+                .when().post("/admin/themes").then().statusCode(201);
+
+        Map<String, String> time = new HashMap<>();
+        time.put("startAt", "10:00");
+
+        RestAssured.given().contentType(ContentType.JSON).body(time)
+                .when().post("/admin/times").then().statusCode(201);
+
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2026-08-05");
+        reservation.put("timeId", 1);
+        reservation.put("themeId", 1);
+
+        RestAssured.given().contentType(ContentType.JSON).body(reservation)
+                .when().post("/reservations").then().statusCode(201);
+
+        RestAssured.given().log().all()
+                .when().delete("/admin/themes/1")
+                .then().log().all()
+                .statusCode(400)
+                .body("errorCode", is("DIFFERENCE_DATA_EXISTS"))
+                .body("message", is("해당 테마에 예약이 존재하여 삭제할 수 없습니다."));
+    }
+
+}
