6aa946757906b4657688d7da1178ae357eb11ca2
JinGoo <zzgg3040@naver.com>
2026-05-26 15:59:58 +0900

    feat: 대기 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,10 +14,24 @@ CREATE TABLE IF NOT EXISTS theme
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS waiting
+(
+    id                  BIGINT      NOT NULL AUTO_INCREMENT,
+    customer_name       VARCHAR(10) NOT NULL,
+    reservation_date    DATE        NOT NULL,
+    created_at          TIMESTAMP    NOT NULL,
+    time_id BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    CONSTRAINT unique_reservation_date_time_theme_name UNIQUE (reservation_date, time_id, theme_id, customer_name)
+);
+
 CREATE TABLE IF NOT EXISTS reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
+    name    VARCHAR(10) NOT NULL,
     date    DATE NOT NULL,
     time_id BIGINT NOT NULL,
     theme_id BIGINT NOT NULL,
