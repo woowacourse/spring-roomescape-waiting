bea5f0195cb52b66dc13954ee842efbc51574cc4
everypine <kenton.lim@gmail.com>
2026-05-26 16:31:55 +0900

    chore(dummy.sql): store 관련 코드 제거

--- src/main/resources/dummy.sql
+++ src/main/resources/dummy.sql
@@ -91,7 +91,3 @@ INSERT INTO member (name, password, role) VALUES ('admin', '8c6976e5b5410415bde9
 INSERT INTO member (name, password, role) VALUES ('member', 'e606e38b0d8c19b24cf0ee3802e858abc6f393291503e30128a158bda25d1109', 'MEMBER');
 INSERT INTO member (name, password, role) VALUES ('다른사람', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'MEMBER');
 INSERT INTO member (name, password, role) VALUES ('송송', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'MEMBER');
-
-INSERT INTO store (name) VALUES ('강남점');
-INSERT INTO store (name) VALUES ('판교점');
-INSERT INTO store (name) VALUES ('홍대점');
