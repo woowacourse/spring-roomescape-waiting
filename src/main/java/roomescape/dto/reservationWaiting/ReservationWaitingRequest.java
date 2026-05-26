827af3a75905bb20ee6ccc7aab14cab7117e92ca
armd482 <armd482@naver.com>
2026-05-26 15:18:44 +0900

    feat: ReservationWaiting request, response dto 구현

---
+++ src/main/java/roomescape/dto/reservationWaiting/ReservationWaitingRequest.java
@@ -0,0 +1,33 @@
+package roomescape.dto.reservationWaiting;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import roomescape.exception.InvalidInputException;
+
+public record ReservationWaitingRequest (String name, LocalDate date, Long timeId, Long themeId) {
+
+    public ReservationWaitingRequest {
+        List<String> emptyFields = new ArrayList<>();
+
+        if (name == null || name.isBlank()) {
+            emptyFields.add("name");
+        }
+
+        if (date == null) {
+            emptyFields.add("date");
+        }
+
+        if (timeId == null) {
+            emptyFields.add("timeId");
+        }
+
+        if (themeId == null) {
+            emptyFields.add("themeId");
+        }
+
+        if (!emptyFields.isEmpty()) {
+            throw new InvalidInputException("%s 필드가 비어있습니다.".formatted(emptyFields));
+        }
+    }
+}
---
+++ src/main/java/roomescape/dto/reservationWaiting/ReservationWaitingResponse.java
@@ -0,0 +1,64 @@
+package roomescape.dto.reservationWaiting;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import roomescape.domain.reservatinWaiting.ReservationWaiting;
+import roomescape.dto.reservationtime.ReservationTimeResponse;
+import roomescape.dto.theme.ThemeResponse;
+
+public class ReservationWaitingResponse {
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final ReservationTimeResponse time;
+    private final ThemeResponse theme;
+    private final Long sequence;
+    private final LocalDateTime createdAt;
+
+    private ReservationWaitingResponse(Long id, String name, LocalDate date, ReservationTimeResponse time, ThemeResponse theme,
+                                       Long sequence, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.theme = theme;
+        this.sequence = sequence;
+        this.createdAt = createdAt;
+    }
+
+    public static ReservationWaitingResponse from(ReservationWaiting reservationWaiting) {
+        ReservationTimeResponse reservationTimeResponse = ReservationTimeResponse.from(reservationWaiting.getTime());
+        ThemeResponse themeResponse = ThemeResponse.from(reservationWaiting.getTheme());
+        return new ReservationWaitingResponse(reservationWaiting.getId(), reservationWaiting.getName(), reservationWaiting.getDate(), reservationTimeResponse, themeResponse,
+                reservationWaiting.getSequence(), reservationWaiting.getCreatedAt());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public ReservationTimeResponse getTime() {
+        return time;
+    }
+
+    public ThemeResponse getTheme() {
+        return theme;
+    }
+
+    public Long getSequence() {
+        return sequence;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
