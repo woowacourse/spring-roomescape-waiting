129da9bbfb91edf69b10cb8a6ee3e57066c90901
armd482 <armd482@naver.com>
2026-05-26 17:05:34 +0900

    feat: 대기열 삭제 전체 조회, 이름 조회 구현

--- src/main/java/roomescape/repository/ReservationWaitingQueryingDao.java
+++ src/main/java/roomescape/repository/ReservationWaitingQueryingDao.java
@@ -3,6 +3,7 @@ package roomescape.repository;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -19,6 +20,33 @@ public class ReservationWaitingQueryingDao {
     public ReservationWaitingQueryingDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    private static final String SELECT_RESERVATION_WAITING_SQL = """
+                select
+                    w.id,
+                    w.name,
+                    w.date,
+                    w.created_at,
+                    t.id       as time_id,
+                    t.start_at as time_start_at,
+                    th.id          as theme_id,
+                    th.name        as theme_name,
+                    th.description as theme_description,
+                    th.url         as theme_url,
+                    ranked.sequence
+                from waiting w
+                join reservation_time t  ON w.time_id  = t.id
+                join theme th            ON w.theme_id = th.id
+                join (
+                    select
+                        id,
+                        ROW_NUMBER() OVER (
+                            PARTITION BY date, time_id, theme_id
+                            ORDER BY created_at, id
+                        ) as sequence
+                    from waiting
+                ) as ranked ON w.id = ranked.id
+                """;
 
     private final RowMapper<ReservationWaiting> reservationWaitingRowMapper = (resultSet, rowNum) -> {
         ReservationTime reservationTime = new ReservationTime(
@@ -59,35 +87,18 @@ public class ReservationWaitingQueryingDao {
     }
 
     public Optional<ReservationWaiting> findReservationWaitingById(long id) {
-        String sql = """
-                select
-                    w.id,
-                    w.name,
-                    w.date,
-                    w.created_at,
-                    t.id       as time_id,
-                    t.start_at as time_start_at,
-                    th.id          as theme_id,
-                    th.name        as theme_name,
-                    th.description as theme_description,
-                    th.url         as theme_url,
-                    ranked.sequence
-                from waiting w
-                join reservation_time t  ON w.time_id  = t.id
-                join theme th            ON w.theme_id = th.id
-                join (
-                    select
-                        id,
-                        ROW_NUMBER() OVER (
-                            PARTITION BY date, time_id, theme_id
-                            ORDER BY created_at, id
-                        ) as sequence
-                    from waiting
-                ) as ranked ON w.id = ranked.id
-                where w.id = ?;
-                """;
+        String sql = SELECT_RESERVATION_WAITING_SQL + "where w.id = ?";
         return jdbcTemplate.query(sql, reservationWaitingRowMapper, id)
                 .stream()
                 .findFirst();
+    }
+
+    public List<ReservationWaiting> findAllReservationWaiting() {
+        return jdbcTemplate.query(SELECT_RESERVATION_WAITING_SQL, reservationWaitingRowMapper);
+    }
+
+    public List<ReservationWaiting> findAllByName(String name) {
+        String sql = SELECT_RESERVATION_WAITING_SQL + "where w.name = ?";
+        return jdbcTemplate.query(sql, reservationWaitingRowMapper, name);
     }
 }
