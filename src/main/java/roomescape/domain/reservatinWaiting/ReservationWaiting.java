934e915c28f6ea3a23e1e8fc71c32fa038b1bc94
armd482 <armd482@naver.com>
2026-05-26 15:18:44 +0900

    feat: ReservationWaiting request, response dto 구현

--- src/main/java/roomescape/domain/reservatinWaiting/ReservationWaiting.java
+++ src/main/java/roomescape/domain/reservatinWaiting/ReservationWaiting.java
@@ -1,6 +1,5 @@
 package roomescape.domain.reservatinWaiting;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import roomescape.domain.reservationtime.ReservationTime;
