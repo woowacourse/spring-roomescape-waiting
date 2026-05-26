984674065f4ff3fb6459ea2e570cea892925a5e4
Giyoul <cla12093@gmail.com>
2026-05-26 15:31:11 +0900

    feat: 스키마 업데이트

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,3 +27,17 @@ CREATE TABLE IF NOT EXISTS reservation
     FOREIGN KEY (theme_id) REFERENCES theme (id),
     UNIQUE (date, time_id, theme_id)
 );
+
+CREATE TABLE IF NOT EXISTS reservation_waiting
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL,
+    date       DATE         NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    time_id    BIGINT       NOT NULL,
+    theme_id   BIGINT       NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    UNIQUE (name, date, time_id, theme_id)
+);
