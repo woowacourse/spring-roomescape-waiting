27b0579193e1b7e8e2d7354c9604a76c4b94710d
gahyeonnni <hyeonnni@naver.com>
2026-05-26 16:25:36 +0900

    feat: 예약 대기 컨트롤러, 서비스 스캐폴딩 추가

---
+++ src/main/java/roomescape/reservationwaiting/controller/ReservationWaitingController.java
@@ -0,0 +1,36 @@
+package roomescape.reservationwaiting.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomescape.reservationwaiting.service.ReservationWaitingService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/waiting")
+public class ReservationWaitingController {
+    private final ReservationWaitingService reservationWaitingService;
+
+    public ReservationWaitingController(ReservationWaitingService reservationWaitingService) {
+        this.reservationWaitingService = reservationWaitingService;
+    }
+
+   @PostMapping
+   public ResponseEntity<ReservationWaitingResponse> createWaiting(@Valid @RequestBody ReservationWaitingRequest request) {
+       ReservationWaitingResponse response = reservationWaitingService.createWaiting(request);
+       return ResponseEntity.created(URI.create("/waiting/" + response.id())).body(response);
+   }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationWaitingResponse>> getWaitingByName(@RequestParam String name) {
+        return ResponseEntity.ok(reservationWaitingService.getWaitingByName(name));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWaiting(@PathVariable Long id) {
+        reservationWaitingService.deleteWaiting(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/roomescape/reservationwaiting/service/ReservationWaitingService.java
@@ -0,0 +1,7 @@
+package roomescape.reservationwaiting.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationWaitingService {
+}
