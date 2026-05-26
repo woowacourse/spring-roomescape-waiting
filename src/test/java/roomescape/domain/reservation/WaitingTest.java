761ec98de128e3455b43cef716ee3aef9648ca57
K-iise <kimyungi1004@naver.com>
2026-05-26 15:03:51 +0900

    feat: waiting domain 추가

---
+++ src/test/java/roomescape/domain/reservation/WaitingTest.java
@@ -0,0 +1,90 @@
+package roomescape.domain.reservation;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static roomescape.config.FixedClockConfig.NOW_TIME;
+import static roomescape.config.FixedClockConfig.TODAY;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.domain.reservation.theme.Description;
+import roomescape.domain.reservation.theme.Theme;
+import roomescape.domain.reservation.theme.ThemeName;
+import roomescape.domain.reservation.theme.ThumbnailUrl;
+import roomescape.domain.reservation.time.ReservationTime;
+
+public class WaitingTest {
+    private final UserName userName = UserName.parse("아나키");
+    private final LocalDate date = LocalDate.parse(TODAY);
+
+    private final ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
+
+    private final ThemeName themeName = ThemeName.parse("공포");
+    private final Description description = Description.parse("너무무서워");
+    private final ThumbnailUrl url = ThumbnailUrl.parse("/images/horror");
+    private final Theme theme = new Theme(1L, themeName, description, url);
+    private final LocalDateTime createdAt =  LocalDateTime.of(
+            LocalDate.parse(TODAY),
+            LocalTime.parse(NOW_TIME)
+    );
+
+
+    @Test
+    @DisplayName("올바른 정보로 예약 대기를 생성하면 성공한다.")
+    void 정상_예약_테스트() {
+        assertDoesNotThrow(() -> new Waiting(userName, date, time, theme, createdAt));
+    }
+
+    @Test
+    @DisplayName("예약자 이름이 null 이면 예외가 발생한다.")
+    void 이름이_null_예외_테스트() {
+        UserName userName = null;
+
+        assertThatThrownBy(() -> new Waiting(userName, date, time, theme, createdAt))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("예약자 이름이 비어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("예약 날짜가 null 이면 예외가 발생한다.")
+    void 날짜가_null_예외_테스트() {
+        LocalDate date = null;
+
+        assertThatThrownBy(() -> new Waiting(userName, date, time, theme, createdAt))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("예약 날짜가 비어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("예약 시간이 null 이면 예외가 발생한다.")
+    void 시간이_null_예외_테스트() {
+        ReservationTime time = null;
+
+        assertThatThrownBy(() -> new Waiting(userName, date, time, theme, createdAt))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("시간이 비어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("테마가 null 이면 예외가 발생한다.")
+    void 테마가_null_예외_테스트() {
+        Theme theme = null;
+
+        assertThatThrownBy(() -> new Waiting(userName, date, time, theme, createdAt))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("테마가 비어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("대기 신청 시간이 null 이면 예외가 발생한다.")
+    void 대기_신청_시간_null_예외_테스트() {
+        LocalDateTime createdAt = null;
+
+        assertThatThrownBy(() -> new Waiting(userName, date, time, theme, createdAt))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("대기 신청 시간이 비어 있습니다.");
+    }
+}
