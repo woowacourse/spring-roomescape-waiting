ff73d486ad55ea1f8a9f9cd2234d7c90e0a34dd1
K-iise <kimyungi1004@naver.com>
2026-05-26 15:02:51 +0900

    chore: Waiting DB Schema 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -26,3 +26,16 @@ CREATE TABLE IF NOT EXISTS reservation
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
+
+CREATE TABLE IF NOT EXISTS waiting
+(
+    id         BIGINT     NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL,
+    date       DATE         NOT NULL,
+    time_id    BIGINT       NOT NULL,
+    theme_id   BIGINT       NOT NULL,
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
+)
