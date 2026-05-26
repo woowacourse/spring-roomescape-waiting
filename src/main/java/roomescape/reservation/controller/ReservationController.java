c55113209f2178d0a7d08f5a6cd07ec5d75b42dd
dahye <dahye9011@naver.com>
2026-05-26 19:11:54 +0900

    feat: 사용자 예약/대기 신청 API 추가

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -4,10 +4,12 @@ import jakarta.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import roomescape.reservation.controller.dto.ReservationResponseDto;
+import roomescape.reservation.controller.dto.ReservationSaveRequestDto;
+import roomescape.reservation.controller.dto.ReservationSaveResponseDto;
 import roomescape.reservation.controller.dto.UserReservationUpdateRequestDto;
 import roomescape.reservation.service.ReservationService;
 
@@ -49,4 +53,21 @@ public class ReservationController {
                 reservationService.update(id, request.timeId()));
         return ResponseEntity.ok(body);
     }
+
+    @PostMapping
+    public ResponseEntity<ReservationSaveResponseDto> create(
+            @RequestBody @Valid ReservationSaveRequestDto reservationRequest) {
+        ReservationSaveResponseDto body = ReservationSaveResponseDto.from(
+                reservationService.create(reservationRequest.toServiceDto()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+    }
+
+
+//    @PostMapping("/waiting")
+//    public ResponseEntity<ReservationWaitingResponseDto> createWaiting(
+//            @Valid @RequestBody ReservationSaveRequestDto request) {
+//        Reservation reservation = reservationService.createWaiting(request.toServiceDto());
+//        ReservationWaitingResponseDto body = ReservationWaitingResponseDto.from(reservation);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+//    }
 }
