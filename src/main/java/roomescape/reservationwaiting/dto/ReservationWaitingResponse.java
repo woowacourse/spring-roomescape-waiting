2da6cbaaeb54d944633da2e1d94c0df43eca435c
mvg01 <dkms625@naver.com>
2026-05-27 16:46:36 +0900

    fix: dto 클래스를 레코드로 변경

--- src/main/java/roomescape/reservationwaiting/dto/ReservationWaitingResponse.java
+++ src/main/java/roomescape/reservationwaiting/dto/ReservationWaitingResponse.java
@@ -2,31 +2,10 @@ package roomescape.reservationwaiting.dto;
 
 import roomescape.reservationwaiting.domain.ReservationWaiting;
 
-public class ReservationWaitingResponse {
-
-    private final Long id;
-    private final String name;
-    private final Long reservationId;
-
-    public ReservationWaitingResponse(Long id, String name,  Long reservationId) {
-        this.id = id;
-        this.name = name;
-        this.reservationId = reservationId;
-    }
+public record ReservationWaitingResponse(Long id, String name, Long reservationId) {
 
     public static ReservationWaitingResponse from(ReservationWaiting reservationWaiting) {
-        return new ReservationWaitingResponse(reservationWaiting.getId(), reservationWaiting.getName(), reservationWaiting.getReservation().getId());
-    }
-
-    public Long id() {
-        return id;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public Long reservationId() {
-        return reservationId;
+        return new ReservationWaitingResponse(reservationWaiting.getId(), reservationWaiting.getName(),
+                reservationWaiting.getReservation().getId());
     }
 }
