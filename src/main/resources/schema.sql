e0d8ad6400fe7d6af410a8f07a967ee512490fb5
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-27 13:44:22 +0900

    refactor : 스키마 UNIQUE 제약 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,13 +8,14 @@ CREATE TABLE reservation_time
     id        BIGINT NOT NULL AUTO_INCREMENT,
     start_at  TIME   NOT NULL,
     finish_at TIME   NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CONSTRAINT unique_time UNIQUE (start_at, finish_at)
 );
 
 CREATE TABLE theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(255) NOT NULL UNIQUE,
     description VARCHAR(500) NOT NULL,
     image_url   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
@@ -25,21 +26,23 @@ CREATE TABLE reservation
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
     date     DATE         NOT NULL,
-    time_id  BIGINT,
-    theme_id BIGINT,
+    time_id  BIGINT       NOT NULL,
+    theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    CONSTRAINT unique_reservation UNIQUE (date, time_id, theme_id)
 );
 
 CREATE TABLE waiting
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    theme_id BIGINT,
-    time_id  BIGINT,
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    theme_id BIGINT NOT NULL,
+    time_id  BIGINT NOT NULL,
     date     DATE         NOT NULL,
     name     VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    CONSTRAINT unique_waiting UNIQUE (date, time_id, theme_id, name)
 );
