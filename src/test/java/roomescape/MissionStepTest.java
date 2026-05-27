198f8a1514ab3ae343253645bcdc0e40a79f9f7b
tjdakf <ksc73450056@gmail.com>
2026-05-27 17:09:19 +0900

    test: 예약 대기 통합테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -25,7 +25,9 @@ public class MissionStepTest {
 
     @BeforeEach
     void setup() {
+        jdbcTemplate.update("DELETE FROM reservation_waiting;");
         jdbcTemplate.update("DELETE FROM reservation;");
+        jdbcTemplate.update("ALTER TABLE reservation_waiting ALTER COLUMN id RESTART WITH 1;");
         jdbcTemplate.update("ALTER TABLE reservation ALTER COLUMN id RESTART WITH 1;");
     }
 
@@ -115,6 +117,157 @@ public class MissionStepTest {
                 .statusCode(409)
                 .body("code", is("DUPLICATE_RESERVATION"))
                 .body("detail", is("이미 예약된 시간입니다."));
+    }
+
+    @Test
+    void 예약_대기_추가_조회_및_삭제() {
+        Map<String, String> reservationParams = new HashMap<>();
+        reservationParams.put("name", "브라운");
+        reservationParams.put("date", LocalDate.now().plusDays(1).toString());
+        reservationParams.put("timeId", "1");
+        reservationParams.put("themeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationParams)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        Map<String, String> waitingParams = new HashMap<>(reservationParams);
+        waitingParams.put("name", "구구");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(waitingParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(1))
+                .body("name", is("구구"))
+                .body("turn", is(1));
+
+        RestAssured.given().log().all()
+                .queryParam("name", "구구")
+                .when().get("/waitings")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1))
+                .body("[0].id", is(1))
+                .body("[0].name", is("구구"))
+                .body("[0].turn", is(1));
+
+        RestAssured.given().log().all()
+                .queryParam("name", "구구")
+                .when().delete("/waitings/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given().log().all()
+                .queryParam("name", "구구")
+                .when().get("/waitings")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    void 같은_예약_슬롯의_대기는_신청_순서대로_순번이_부여된다() {
+        Map<String, String> reservationParams = new HashMap<>();
+        reservationParams.put("name", "브라운");
+        reservationParams.put("date", LocalDate.now().plusDays(1).toString());
+        reservationParams.put("timeId", "1");
+        reservationParams.put("themeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationParams)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        Map<String, String> firstWaitingParams = new HashMap<>(reservationParams);
+        firstWaitingParams.put("name", "구구");
+        Map<String, String> secondWaitingParams = new HashMap<>(reservationParams);
+        secondWaitingParams.put("name", "포비");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(firstWaitingParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(201)
+                .body("turn", is(1));
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(secondWaitingParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(201)
+                .body("turn", is(2));
+    }
+
+    @Test
+    void 예약_대기_신청_예외_응답() {
+        Map<String, String> reservationParams = new HashMap<>();
+        reservationParams.put("name", "브라운");
+        reservationParams.put("date", LocalDate.now().plusDays(1).toString());
+        reservationParams.put("timeId", "1");
+        reservationParams.put("themeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationParams)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(409)
+                .body("code", is("WAITING_NOT_ALLOWED_FOR_OWN_RESERVATION"))
+                .body("detail", is("본인이 예약한 시간에는 대기를 신청할 수 없습니다."));
+
+        Map<String, String> waitingParams = new HashMap<>(reservationParams);
+        waitingParams.put("name", "구구");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(waitingParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(201);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(waitingParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(409)
+                .body("code", is("DUPLICATE_RESERVATION"))
+                .body("detail", is("이미 예약 대기를 신청한 시간입니다."));
+    }
+
+    @Test
+    void 예약_가능한_시간에는_대기를_신청할_수_없다() {
+        Map<String, String> waitingParams = new HashMap<>();
+        waitingParams.put("name", "브라운");
+        waitingParams.put("date", LocalDate.now().plusDays(1).toString());
+        waitingParams.put("timeId", "1");
+        waitingParams.put("themeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(waitingParams)
+                .when().post("/waitings")
+                .then().log().all()
+                .statusCode(400)
+                .body("code", is("INVALID_INPUT"))
+                .body("detail", is("예약 가능한 시간에는 대기를 신청할 수 없습니다."));
     }
 
     @Test
