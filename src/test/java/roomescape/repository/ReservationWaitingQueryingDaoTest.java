cf6259cc8c68810e3e40b6f4befe3c95f8058b84
armd482 <armd482@naver.com>
2026-05-27 17:57:48 +0900

    test: 예약 대기 조회 dao 테스트 코드 추가

---
+++ src/test/java/roomescape/repository/ReservationWaitingQueryingDaoTest.java
@@ -0,0 +1,59 @@
+package roomescape.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+public class ReservationWaitingQueryingDaoTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private ReservationWaitingQueryingDao reservationWaitingQueryingDao;
+
+    @BeforeEach
+    void setUp() {
+        this.reservationWaitingQueryingDao = new ReservationWaitingQueryingDao(jdbcTemplate);
+
+        jdbcTemplate.update("delete from waiting");
+        jdbcTemplate.update("delete from reservation_time");
+        jdbcTemplate.update("delete from theme");
+        jdbcTemplate.update("alter table waiting alter column id restart with 1");
+        jdbcTemplate.update("alter table reservation_time alter column id restart with 1");
+        jdbcTemplate.update("alter table theme alter column id restart with 1");
+
+        jdbcTemplate.update("insert into reservation_time (start_at) values ('10:00')");
+        jdbcTemplate.update("insert into theme (name, description, url) values ('테스트', '설명', 'url')");
+        jdbcTemplate.update("insert into waiting (name, date, time_id, theme_id, created_at) values ('테스트', '2027-05-27', 1, 1, '2026-05-15 10:30:00')");
+    }
+
+    @Test
+    void 예약_대기가_제대로_존재하는_지_조회한다() {
+        assertThat(reservationWaitingQueryingDao.isExistByNameAndDateAndTimeIdAndThemeId("테스트", LocalDate.parse("2027-05-27"), 1L, 1L)).isTrue();
+        assertThat(reservationWaitingQueryingDao.isExistByNameAndDateAndTimeIdAndThemeId("테스트", LocalDate.parse("2027-05-26"), 1L, 1L)).isFalse();
+    }
+
+    @Test
+    void 예약_대기가_id로_정상_조회한다() {
+        assertThat(reservationWaitingQueryingDao.findReservationWaitingById(1).isPresent()).isTrue();
+        assertThat(reservationWaitingQueryingDao.findReservationWaitingById(1).get().getName()).isEqualTo("테스트");
+    }
+
+    @Test
+    void 예약_대기_전체_조회가_정상_조회한다() {
+        assertThat(reservationWaitingQueryingDao.findAllReservationWaiting().size()).isEqualTo(1);
+    }
+
+    @Test
+    void 예약_대기가_이름으로_정상_조회한다() {
+        assertThat(reservationWaitingQueryingDao.findAllByName("테스트").size()).isEqualTo(1);
+        assertThat(reservationWaitingQueryingDao.findAllByName("테스트").getFirst().getCreatedAt())
+                .isEqualTo(LocalDateTime.parse("2026-05-15T10:30:00"));
+    }
+}
