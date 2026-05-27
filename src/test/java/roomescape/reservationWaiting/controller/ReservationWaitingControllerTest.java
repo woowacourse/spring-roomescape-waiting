44be0e889c379984a0959917603d3d3da0ee086d
simhokyung <ghrudtla@naver.com>
2026-05-27 15:18:05 +0900

    test: 예약 대기 테스트 추가

---
+++ src/test/java/roomescape/reservationWaiting/controller/ReservationWaitingControllerTest.java
@@ -0,0 +1,80 @@
+package roomescape.reservationWaiting.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.DatabaseInitializer;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationWaitingControllerTest {
+
+    @Autowired
+    private DatabaseInitializer databaseInitializer;
+
+    @BeforeEach
+    void setUp() {
+        databaseInitializer.clear();
+    }
+
+    @Test
+    void 예약을_추가한다() {
+        int timeId = createTime("10:00");
+        int themeId = createTheme("방탈출1", "다함께 탈출해요 방탈출", "https://asdfsdf.sdfs");
+
+        createReservationWaiting("브라운", LocalDate.of(2026, 6, 10), timeId, themeId)
+                .statusCode(201)
+                .body("id", notNullValue())
+                .header("Location", "/waitings/1");
+    }
+
+    @Test
+    void 예약을_삭제한다() {
+        int timeId = createTime("10:00");
+        int themeId = createTheme("방탈출11", "다함께 탈출해요 방탈출", "https://asdfsdf.sdfs");
+        int reservationWaitingId = createReservationWaiting("브라운", LocalDate.of(2026, 6, 10), timeId, themeId)
+                .statusCode(201)
+                .extract().path("id");
+
+        RestAssured.given().log().all()
+                .when().delete("/waitings/" + reservationWaitingId)
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    private int createTime(String startAt) {
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(Map.of("startAt", startAt))
+                .when().post("/admin/times")
+                .then().statusCode(201)
+                .extract().jsonPath().getInt("id");
+    }
+
+    private int createTheme(String name, String description, String thumbnail) {
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(Map.of("name", name, "description", description, "thumbnail", thumbnail))
+                .when().post("/admin/themes")
+                .then().statusCode(201)
+                .extract().jsonPath().getInt("id");
+    }
+
+    private ValidatableResponse createReservationWaiting(String name, LocalDate reservationDate, int timeId, int themeId) {
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(Map.of("name", name, "reservationDate", reservationDate, "timeId", timeId, "themeId", themeId))
+                .when().post("/waitings")
+                .then().log().all();
+    }
+}
