ae888e4aa0353c2ef98ceff2011a79690ed3ce8e
jhk01007 <jhk001007@naver.com>
2026-05-27 13:27:19 +0900

    refactor(schema): reservation 테이블에 changed_at 컬럼 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE theme
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     thumbnail   VARCHAR(255) NOT NULL,
-    deleted_at TIMESTAMP,
+    deleted_at  TIMESTAMP,
     PRIMARY KEY (id)
 );
 
@@ -20,16 +20,17 @@ CREATE TABLE reservation_time
 
 CREATE TABLE reservation
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    guest_name VARCHAR(255) NOT NULL,
-    date     DATE         NOT NULL,
-    time_id  BIGINT       NOT NULL,
-    theme_id BIGINT       NOT NULL,
-    cancel_token BIGINT NOT NULL DEFAULT 0,
-    status VARCHAR(50) NOT NULL,
+    id           BIGINT       NOT NULL AUTO_INCREMENT,
+    guest_name   VARCHAR(255) NOT NULL,
+    date         DATE         NOT NULL,
+    time_id      BIGINT       NOT NULL,
+    theme_id     BIGINT       NOT NULL,
+    cancel_token BIGINT       NOT NULL DEFAULT 0,
+    status       VARCHAR(50)  NOT NULL,
+    changed_at   TIMESTAMP    NOT NULL,
 
     PRIMARY KEY (id),
-    UNIQUE (guest_name ,date, time_id, theme_id, cancel_token),
+    UNIQUE (guest_name, date, time_id, theme_id, cancel_token),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
