28cf644c302db35f54a01ab01e1afb554152259b
koreaioi <koreaioi@gachon.ac.kr>
2026-05-27 09:41:57 +0900

    feat: 더미 회원 데이터 추가

--- src/main/resources/dummy.sql
+++ src/main/resources/dummy.sql
@@ -92,3 +92,4 @@ INSERT INTO member (name, password, role) VALUES ('admin', '8c6976e5b5410415bde9
 INSERT INTO member (name, password, role) VALUES ('member', 'e606e38b0d8c19b24cf0ee3802e858abc6f393291503e30128a158bda25d1109', 'MEMBER');
 INSERT INTO member (name, password, role) VALUES ('다른사람', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'MEMBER');
 INSERT INTO member (name, password, role) VALUES ('송송', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'MEMBER');
+INSERT INTO member (name, password, role) VALUES ('pine', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'MEMBER');
