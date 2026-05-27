83a5de9ee6b3e89f40f43c01bd730dd20d363b9d
simhokyung <ghrudtla@naver.com>
2026-05-27 13:56:17 +0900

    feat: 예약 대기 신청/취소 기능 구현

---
+++ src/main/java/roomescape/common/exception/code/ReservationWaitingErrorCode.java
@@ -0,0 +1,28 @@
+package roomescape.common.exception.code;
+
+import org.springframework.http.HttpStatus;
+
+public enum ReservationWaitingErrorCode implements ErrorCode {
+    NOT_FOUND("존재하지 않는 예약 대기입니다.", HttpStatus.NOT_FOUND),
+    DUPLICATE("동일한 사용자 이름, 예약 날짜, 시간, 테마에 이미 예약 대기가 존재합니다.", HttpStatus.CONFLICT),
+    PAST_DATETIME("지나간 날짜·시간에 대한 예약 생성은 불가능합니다.", HttpStatus.UNPROCESSABLE_ENTITY),
+    ;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    ReservationWaitingErrorCode(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
