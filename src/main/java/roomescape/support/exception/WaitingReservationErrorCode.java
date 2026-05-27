f3f729ce670ce447d920e79700d4da348f0890ee
rin <chaerin0916@gmail.com>
2026-05-27 14:53:25 +0900

    feat: 예약 대기 도메인 테스트 및 구현

---
+++ src/main/java/roomescape/support/exception/WaitingReservationErrorCode.java
@@ -0,0 +1,29 @@
+package roomescape.support.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum WaitingReservationErrorCode implements ErrorCode {
+    INVALID_RESERVATION_NAME(HttpStatus.BAD_REQUEST,
+        "예약자 성명 데이터가 유효하지 않습니다.", "요청 바디의 name 필드 유효성 제약 조건을 확인하십시오."),
+    INVALID_CREATED_AT(HttpStatus.BAD_REQUEST,
+        "생성 시간이 유효하지 않습니다.", "요청 바디의 createdAt 필드 유효성 제약 조건을 확인하십시오."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final String action;
+
+    WaitingReservationErrorCode(HttpStatus httpStatus, String message, String action) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.action = action;
+    }
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+}
