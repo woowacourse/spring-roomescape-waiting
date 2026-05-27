fb48a779ccc01c22bf8854922f59c8ebba1c0524
SeungGyu <phillipskl123@gmail.com>
2026-05-27 18:24:08 +0900

    fix: SpringBootTest 컨텍스트 간 테이블 중복 생성 충돌 해결

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE theme (
+CREATE TABLE IF NOT EXISTS theme (
    id BIGINT NOT NULL AUTO_INCREMENT,
    name VARCHAR(255) NOT NULL,
    description TEXT NOT NULL,
@@ -6,13 +6,13 @@ CREATE TABLE theme (
    PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation_time (
+CREATE TABLE IF NOT EXISTS reservation_time (
     id BIGINT NOT NULL AUTO_INCREMENT,
     start_at TIME NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation (
+CREATE TABLE IF NOT EXISTS reservation (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE reservation (
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
 
-CREATE TABLE waiting (
+CREATE TABLE IF NOT EXISTS waiting (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
