d8f037d29826666ae6d9b6ba7bfcadd42e4bfa9a
simhokyung <ghrudtla@naver.com>
2026-05-27 15:18:05 +0900

    test: 예약 대기 테스트 추가

---
+++ src/test/java/roomescape/reservationWaiting/dao/ReservationWaitingDaoTest.java
@@ -0,0 +1,137 @@
+package roomescape.reservationWaiting.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import roomescape.reservationWaiting.domain.ReservationWaiting;
+import roomescape.reservationtime.dao.ReservationTimeDao;
+import roomescape.reservationtime.domain.ReservationTime;
+import roomescape.theme.dao.ThemeDao;
+import roomescape.theme.domain.Theme;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@JdbcTest
+@Import({ReservationWaitingDao.class, ReservationTimeDao.class, ThemeDao.class})
+class ReservationWaitingDaoTest {
+
+    @Autowired
+    private ReservationWaitingDao reservationWaitingDao;
+
+    @Autowired
+    private ReservationTimeDao timeDao;
+
+    @Autowired
+    private ThemeDao themeDao;
+
+    @Test
+    void 예약_대기를_생성한다() {
+        // given
+        ReservationTime savedTime = saveTime(10, 0);
+        Theme savedTheme = saveTheme("방탈출1", "설명", "https://asdfsdf.sdfs");
+        ReservationWaiting reservationWaiting = ReservationWaiting.createWithoutId("맥스", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+
+        // when
+        ReservationWaiting saved = reservationWaitingDao.insert(reservationWaiting);
+
+        // then
+        assertThat(saved)
+                .extracting(ReservationWaiting::getName, ReservationWaiting::getCreatedAt, ReservationWaiting::getReservationDate, ReservationWaiting::getTime, ReservationWaiting::getTheme)
+                .containsExactly(reservationWaiting.getName(), reservationWaiting.getCreatedAt(), reservationWaiting.getReservationDate(), reservationWaiting.getTime(), reservationWaiting.getTheme());
+    }
+
+    @Test
+    void 예약_대기를_삭제한다() {
+        // given
+        ReservationTime savedTime = saveTime(10, 0);
+        Theme savedTheme = saveTheme("방탈출1", "설명", "https://asdfsdf.sdfs");
+
+        ReservationWaiting reservationWaiting = ReservationWaiting.createWithoutId("맥스", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+        ReservationWaiting saved = reservationWaitingDao.insert(reservationWaiting);
+
+        // when
+        reservationWaitingDao.delete(saved.getId());
+
+        // then
+        assertThat(reservationWaitingDao.select()).isEmpty();
+    }
+
+    @Test
+    void 특정_날짜_테마_시간_사용자_이름에_예약_대기가_존재하면_true를_반환한다() {
+        // given
+        ReservationTime savedTime = saveTime(10, 0);
+        Theme savedTheme = saveTheme("방탈출1", "설명", "https://asdfsdf.sdfs");
+
+        ReservationWaiting reservationWaiting = ReservationWaiting.createWithoutId("맥스", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+        reservationWaitingDao.insert(reservationWaiting);
+
+        // when
+        boolean result = reservationWaitingDao.existsByNameAndDateAndTimeIdAndThemeId(reservationWaiting.getName(), reservationWaiting.getReservationDate(), savedTime.getId(), savedTheme.getId());
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 특정_날짜_테마_시간_사용자_이름에_예약_대기가_존재하지_않으면_false를_반환한다() {
+        // when
+        boolean result = reservationWaitingDao.existsByNameAndDateAndTimeIdAndThemeId("맥스", LocalDate.of(2026, 6, 10), 999L, 999L);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 대기_순번을_계산한다() {
+        // given
+        ReservationTime savedTime = saveTime(10, 0);
+        Theme savedTheme = saveTheme("방탈출1", "설명", "https://asdfsdf.sdfs");
+
+        ReservationWaiting reservationWaiting1 = ReservationWaiting.createWithoutId("맥스", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+        ReservationWaiting reservationWaiting2 = ReservationWaiting.createWithoutId("로지", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+        ReservationWaiting savedReservationWaiting1 = reservationWaitingDao.insert(reservationWaiting1);
+        ReservationWaiting savedReservationWaiting2 = reservationWaitingDao.insert(reservationWaiting2);
+
+        // when
+        int countOrder = reservationWaitingDao.countOrder(LocalDate.of(2026, 6, 10), savedTime.getId(), savedTheme.getId(), savedReservationWaiting2.getId());
+
+        // then
+        assertThat(countOrder).isEqualTo(2);
+    }
+
+    @Test
+    void 전체_대기_목록을_조회한다() {
+        // given
+        ReservationTime savedTime = saveTime(10, 0);
+        Theme savedTheme = saveTheme("방탈출1", "설명", "https://asdfsdf.sdfs");
+
+        ReservationWaiting reservationWaiting1 = ReservationWaiting.createWithoutId("맥스", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+        ReservationWaiting reservationWaiting2 = ReservationWaiting.createWithoutId("로지", LocalDateTime.now(), LocalDate.of(2026, 6, 10), savedTime, savedTheme);
+        ReservationWaiting savedReservationWaiting1 = reservationWaitingDao.insert(reservationWaiting1);
+        ReservationWaiting savedReservationWaiting2 = reservationWaitingDao.insert(reservationWaiting2);
+
+        // when
+        List<ReservationWaiting> reservationWaitings = reservationWaitingDao.select();
+
+        // then
+        assertAll(
+                () -> assertThat(reservationWaitings.size()).isEqualTo(2),
+                () -> assertThat(reservationWaitings.getFirst().getName()).isEqualTo("맥스")
+        );
+    }
+
+    private ReservationTime saveTime(int hour, int minute) {
+        return timeDao.insert(ReservationTime.createWithoutId(LocalTime.of(hour, minute)));
+    }
+
+    private Theme saveTheme(String name, String description, String thumbnail) {
+        return themeDao.insert(Theme.createWithoutId(name, description, thumbnail));
+    }
+}
