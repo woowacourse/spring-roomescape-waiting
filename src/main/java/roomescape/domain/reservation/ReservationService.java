7799d05719e5d14c3c87a629a66be94b67067d96
박병훈 <leafloveu@naver.com>
2026-05-27 15:45:28 +0900

    fix: update 시 빈 슬롯 제거

--- src/main/java/roomescape/domain/reservation/ReservationService.java
+++ src/main/java/roomescape/domain/reservation/ReservationService.java
@@ -143,6 +143,8 @@ public class ReservationService {
         if (!sameReservationSlot) {
             reorderWaitingNumbers(reservationSlot);
         }
+
+        deleteEmptyReservationSlot(reservation);
     }
 
     private void deleteEmptyReservationSlot(Reservation reservation) {
