7c1673512297b6f0b9886318d699e5eae5e9d2ac
simhokyung <ghrudtla@naver.com>
2026-05-27 17:34:00 +0900

    refactor: 관리자용 예약 목록 조회 분리

---
+++ src/main/java/roomescape/reservation/controller/AdminReservationController.java
@@ -0,0 +1,27 @@
+package roomescape.reservation.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.reservation.dto.response.ReservationResponse;
+import roomescape.reservation.service.ReservationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/reservations")
+public class AdminReservationController {
+
+    private final ReservationService reservationService;
+
+    public AdminReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationResponse>> getReservations() {
+        List<ReservationResponse> responses = reservationService.getAllReservations();
+        return ResponseEntity.ok(responses);
+    }
+}
