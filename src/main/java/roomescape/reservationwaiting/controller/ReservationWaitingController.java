a92bee12c1851903cc7c4fde4dd586cf66d130b8
mvg01 <dkms625@naver.com>
2026-05-28 17:10:07 +0900

    refactor: URI 오타 수정

--- src/main/java/roomescape/reservationwaiting/controller/ReservationWaitingController.java
+++ src/main/java/roomescape/reservationwaiting/controller/ReservationWaitingController.java
@@ -1,15 +1,21 @@
 package roomescape.reservationwaiting.controller;
 
 import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservationwaiting.dto.ReservationWaitingRequest;
 import roomescape.reservationwaiting.dto.ReservationWaitingResponse;
 import roomescape.reservationwaiting.dto.ReservationWaitingTurnResponse;
 import roomescape.reservationwaiting.service.ReservationWaitingService;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/waitings")
@@ -24,7 +30,7 @@ public class ReservationWaitingController {
     public ResponseEntity<ReservationWaitingResponse> createWaiting(
             @Valid @RequestBody ReservationWaitingRequest request) {
         ReservationWaitingResponse response = reservationWaitingService.createWaiting(request);
-        return ResponseEntity.created(URI.create("/waiting/" + response.id())).body(response);
+        return ResponseEntity.created(URI.create("/waitings/" + response.id())).body(response);
     }
 
     @GetMapping
