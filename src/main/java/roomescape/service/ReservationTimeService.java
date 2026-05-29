1ab56e5b75bb9cd9307723629604ba7439089945
yItWorks <poiu7825@pusan.ac.kr>
2026-05-29 13:53:53 +0900

    feat: 예약 시간 삭제 시, 존재 여부 검사하지 않도록 변경

--- src/main/java/roomescape/service/ReservationTimeService.java
+++ src/main/java/roomescape/service/ReservationTimeService.java
@@ -3,7 +3,6 @@ package roomescape.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import roomescape.common.exception.ConflictException;
-import roomescape.common.exception.NotFoundException;
 import roomescape.dao.ReservationDao;
 import roomescape.dao.ReservationTimeDao;
 import roomescape.domain.reservation.time.ReservationTime;
@@ -39,10 +38,6 @@ public class ReservationTimeService {
     }
 
     public void deleteReservationTime(Long id) {
-        if (!reservationTimeDao.existsById(id)) {
-            throw new NotFoundException("존재하지 않는 시간입니다.");
-        }
-
         if (reservationDao.existsByTimeId(id)) {
             throw new ConflictException("예약이 존재하는 시간은 삭제할 수 없습니다.");
         }
