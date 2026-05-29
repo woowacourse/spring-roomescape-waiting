8d0b3510d8ed0a81125200c58bcb5c36b6a026cd
choubung <choubung@sookmyung.ac.kr>
2026-05-29 23:34:10 +0900

    test(domain): 도메인 단위 테스트 작성

---
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -0,0 +1,40 @@
+package roomescape.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReservationTimeTest {
+
+    @Test
+    @DisplayName("새로운 예약을 위한 시간을 성공적으로 생성한다.")
+    void createReservationTimeTest() {
+        // given
+        LocalTime startAt = LocalTime.of(10, 0);
+
+        // when
+        ReservationTime reservationTime = ReservationTime.create(startAt);
+
+        // then
+        assertThat(reservationTime.getId()).isNull();
+        assertThat(reservationTime.getStartAt()).isEqualTo(LocalTime.of(10, 0));
+    }
+
+    @Test
+    @DisplayName("동일한 ID를 가진 예약 시간은 같은 객체로 판단한다.")
+    void equalsAndHashCodeTest() {
+        // given
+        LocalTime time1 = LocalTime.of(10, 0);
+        LocalTime time2 = LocalTime.of(13, 0);
+
+        ReservationTime reservationTime1 = ReservationTime.from(1L, time1);
+        ReservationTime reservationTime2 = ReservationTime.from(1L, time2);
+
+        // when & then
+        assertThat(reservationTime1).isEqualTo(reservationTime2);
+        assertThat(reservationTime1.hashCode()).isEqualTo(reservationTime2.hashCode());
+    }
+}
---
+++ src/test/java/roomescape/domain/WaitingTest.java
@@ -0,0 +1,87 @@
+package roomescape.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.exception.ForbiddenException;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WaitingTest {
+
+    @Test
+    @DisplayName("새로운 예약 대기를 성공적으로 생성한다.")
+    void createWaitingTest() {
+        // given
+        String name = "파도";
+        LocalDateTime now = LocalDateTime.of(2026, 5, 29, 10, 0);
+        Slot slot = createValidSlot(now.plusDays(1));
+
+        // when
+        Waiting waiting = Waiting.create(name, slot, now);
+
+        // then
+        assertThat(waiting.getId()).isNull();
+        assertThat(waiting.getName()).isEqualTo("파도");
+        assertThat(waiting.getWaitingDate()).isEqualTo(now.plusDays(1).toLocalDate());
+        assertThat(waiting.createAt()).isEqualTo(now);
+    }
+
+    @Test
+    @DisplayName("예약 대기 취소 시 슬롯의 시간이 현재 시간보다 미래라면 검증을 통과한다.")
+    void validateCancelableSuccessTest() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2026, 5, 29, 10, 0);
+        Slot slot = createValidSlot(now.plusDays(1));
+        Waiting waiting = Waiting.from(1L, "파도", slot, now);
+
+        // when & then
+        waiting.validateCancelable(now);
+    }
+
+    @Test
+    @DisplayName("본인의 예약 대기가 맞는지 검증하고, 타인의 대기라면 예외를 발생시킨다.")
+    void validateOwnedByTest() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2026, 5, 29, 10, 0);
+        Waiting waiting = Waiting.from(1L, "파도", createAnySlot(), now);
+
+        // when & then
+        assertThatThrownBy(() -> waiting.validateOwnedBy("다른사람"))
+                .isInstanceOf(ForbiddenException.class)
+                .hasMessage("타인의 예약대기는 취소할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("동일한 ID를 가진 예약 대기는 같은 객체로 판단한다.")
+    void equalsAndHashCodeTest() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2026, 5, 29, 10, 0);
+        Slot slot = createAnySlot();
+
+        Waiting waiting1 = Waiting.from(1L, "파도", slot, now);
+        Waiting waiting2 = Waiting.from(1L, "다른사람", slot, now.plusMinutes(1));
+
+        // when & then
+        assertThat(waiting1).isEqualTo(waiting2);
+        assertThat(waiting1.hashCode()).isEqualTo(waiting2.hashCode());
+    }
+
+
+    private Slot createValidSlot(LocalDateTime dateTime) {
+        ReservationTime time = ReservationTime.from(1L, LocalTime.from(dateTime));
+        Theme theme = Theme.from(1L, "테마", "썸네일", "설명");
+
+        return Slot.from(
+                Schedule.from(dateTime.toLocalDate(), time),
+                theme
+        );
+    }
+
+    private Slot createAnySlot() {
+        return createValidSlot(LocalDateTime.of(2026, 6, 5, 10, 0));
+    }
+}
