b78291735b6937e4b72c726078570a42724fbe36
gahyeonnni <hyeonnni@naver.com>
2026-05-29 21:36:49 +0900

    feat: 로그인/회원가입 페이지 라우팅 추가

--- src/main/java/roomescape/page/PageController.java
+++ src/main/java/roomescape/page/PageController.java
@@ -35,4 +35,14 @@ public class PageController {
     public String myReservations() {
         return "my-reservations";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 }
