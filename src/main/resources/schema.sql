9e00fd4f6ad76157432590570e939e36226a3876
gahyeonnni <hyeonnni@naver.com>
2026-05-29 20:44:46 +0900

    feat: member 테이블 추가 및 unique 제약조건 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS reservation_waiting;
 DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS member;
 DROP TABLE IF EXISTS theme;
 DROP TABLE IF EXISTS reservation_time;
 
@@ -20,23 +21,41 @@ CREATE TABLE theme
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation
+CREATE TABLE member
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    date     DATE         NOT NULL,
-    time_id  BIGINT,
-    theme_id BIGINT,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
+    CONSTRAINT unique_member_email UNIQUE (email)
+);
+
+CREATE TABLE reservation
+(
+    id        BIGINT NOT NULL AUTO_INCREMENT,
+    member_id BIGINT NOT NULL,
+    date      DATE   NOT NULL,
+    time_id   BIGINT NOT NULL,
+    theme_id  BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    CONSTRAINT unique_reservation_date_time_theme UNIQUE (date, time_id, theme_id)
 );
 
 CREATE TABLE reservation_waiting
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    reservation_id  BIGINT,
+    id         BIGINT    NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT    NOT NULL,
+    date       DATE      NOT NULL,
+    time_id    BIGINT    NOT NULL,
+    theme_id   BIGINT    NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (reservation_id) REFERENCES reservation (id)
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    CONSTRAINT unique_waiting_date_time_theme_member UNIQUE (date, time_id, theme_id, member_id)
 );
