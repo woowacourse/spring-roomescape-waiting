25986b2c87ce8899d2ad8e629de6344e6c5f5af4
yItWorks <poiu7825@pusan.ac.kr>
2026-05-30 18:39:29 +0900

    chore: 파일 저장 경로 설정

--- src/main/java/roomescape/infrastructure/LocalFileUploader.java
+++ src/main/java/roomescape/infrastructure/LocalFileUploader.java
@@ -3,16 +3,20 @@ package roomescape.infrastructure;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import roomescape.service.FileUploader;
 
 @Component
 public class LocalFileUploader implements FileUploader {
-    private static final String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/images/";
+
+    @Value("${file.image-dir}")
+    private String rawUploadDir;
 
     @Override
     public String upload(MultipartFile file) {
+        String uploadDir = rawUploadDir.replace("file:", "");
         String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
         String filePath = uploadDir + fileName;
 
@@ -22,7 +26,7 @@ public class LocalFileUploader implements FileUploader {
                 directory.mkdirs();
             }
 
-            file.transferTo(new File(filePath));
+            file.transferTo(new File(filePath).getAbsoluteFile());
 
         } catch (IOException e) {
             throw new RuntimeException("이미지 저장에 실패했습니다.", e);
