1f4172905835cefc5d1a33427fa6883626404aeb
kdongsu5509 <kod66170@gmail.com>
2026-05-30 19:41:51 +0900

    test: update test configurations and performance metrics

--- src/test/java/roomescape/testSupport/TestConfig.java
+++ src/test/java/roomescape/testSupport/TestConfig.java
@@ -1,11 +1,11 @@
 package roomescape.testSupport;
 
-import java.time.Clock;
-import java.time.Instant;
-import java.time.ZoneOffset;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.ApplicationListener;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import io.restassured.RestAssured;
 
 @TestConfiguration
 public class TestConfig {
@@ -16,10 +16,7 @@ public class TestConfig {
     }
 
     @Bean
-    public Clock clock() {
-        return Clock.fixed(
-                Instant.parse("2026-05-01T09:00:00Z"),
-                ZoneOffset.UTC
-        );
+    public ApplicationListener<WebServerInitializedEvent> webServerInitializedEventApplicationListener() {
+        return event -> RestAssured.port = event.getWebServer().getPort();
     }
 }
