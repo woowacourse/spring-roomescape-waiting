84b468122210ebc740e7257baf1f6524aced49ed
gahyeonnni <hyeonnni@naver.com>
2026-05-30 20:49:49 +0900

    style: repository 인터페이스 메서드 개행 통일

--- src/main/java/roomescape/reservationtime/repository/ReservationTimeRepository.java
+++ src/main/java/roomescape/reservationtime/repository/ReservationTimeRepository.java
@@ -6,10 +6,16 @@ import java.util.Optional;
 import roomescape.reservationtime.domain.ReservationTime;
 
 public interface ReservationTimeRepository {
+
     ReservationTime save(ReservationTime time);
+
     List<ReservationTime> findAll();
+
     Optional<ReservationTime> findById(Long id);
+
     List<ReservationTime> findAvailableByDateAndThemeId(LocalDate date, Long themeId);
+
     boolean existsReservationByTimeId(Long timeId);
+
     void deleteById(Long id);
 }
--- src/main/java/roomescape/theme/repository/ThemeRepository.java
+++ src/main/java/roomescape/theme/repository/ThemeRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ThemeRepository {
+
     Theme save(Theme theme);
 
     Optional<Theme> findById(Long id);
