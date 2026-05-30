54f17bf2f027b6763187723f83eb035b55867b2d
yj9107v <yj9107v@gmail.com>
2026-05-30 16:46:40 +0900

    test: 예약대기 삭제 실패 케이스를 검증한다
    존재하지 않는 예약대기 삭제 시 404를 응답하는지 검증한다.
    예약대기 삭제 요청자의 이름이 일치하지 않으면 403을 응답하는지 검증한다.

--- src/test/java/roomescape/WaitlistApiTest.java
+++ src/test/java/roomescape/WaitlistApiTest.java
@@ -51,4 +51,40 @@ public class WaitlistApiTest {
                 .statusCode(200)
                 .body("size()", is(0));
     }
+
+    @Test
+    void 없는_예약대기를_삭제할_수_없다_404() {
+        RestAssured.given().log().all()
+                .queryParam("name", "브라운")
+                .when().delete("/waitlists/" + 1)
+                .then().log().all()
+                .statusCode(404);
+    }
+
+    @Test
+    @Sql("/data_relative_dates.sql")
+    void 예약대기를_삭제할_때_사용자_이름이_일치하지_않으면_403() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", FUTURE_EIGHT_DATE);
+        params.put("timeId", 9);
+        params.put("themeId", 8);
+
+        Integer waitlistId = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .body("status", is(ReservationStatus.WAITING.name()))
+                .body("waitingOrder", is(1))
+                .extract().jsonPath().get("id");
+
+        String other = "브리";
+        RestAssured.given().log().all()
+                .queryParam("name", other)
+                .when().delete("/waitlists/" + waitlistId)
+                .then().log().all()
+                .statusCode(403);
+    }
 }
