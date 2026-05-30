556a9de8339128df9b4c613f1d5e4a84f092da2a
everypine <kenton.lim@gmail.com>
2026-05-30 20:24:51 +0900

    chore(schema): reservation_slot 테이블 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,47 +1,57 @@
 CREATE TABLE IF NOT EXISTS reservation_time
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    start_at TIME NOT NULL UNIQUE,
+    id        BIGINT  NOT NULL AUTO_INCREMENT,
+    start_at  TIME    NOT NULL UNIQUE,
     is_active BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation_date
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    date DATE NOT NULL UNIQUE,
+    id        BIGINT  NOT NULL AUTO_INCREMENT,
+    date      DATE    NOT NULL UNIQUE,
     is_active BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS theme
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(255) NOT NULL,
+    description   VARCHAR(255) NOT NULL,
     thumbnail_url VARCHAR(255) NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT FALSE,
+    is_active     BOOLEAN      NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS member
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM ('MEMBER', 'MANAGER') NOT NULL,
+    id       BIGINT                     NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255)               NOT NULL,
+    password VARCHAR(255)               NOT NULL,
+    role     ENUM ('MEMBER', 'MANAGER') NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS reservation_slot
+(
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    date_id  BIGINT NOT NULL,
+    time_id  BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (date_id, time_id, theme_id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date_id BIGINT NOT NULL,
-    time_id BIGINT NOT NULL,
-    theme_id BIGINT NOT NULL,
-    reserved_at TIMESTAMP NOT NULL,
-    status ENUM ('RESERVED', 'WAITING', 'CANCELED') NOT NULL,
+    id          BIGINT                                   NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255)                             NOT NULL,
+    date_id     BIGINT                                   NOT NULL,
+    time_id     BIGINT                                   NOT NULL,
+    theme_id    BIGINT                                   NOT NULL,
+    reserved_at TIMESTAMP                                NOT NULL,
+    status      ENUM ('RESERVED', 'WAITING', 'CANCELED') NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (date_id) REFERENCES reservation_date (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
