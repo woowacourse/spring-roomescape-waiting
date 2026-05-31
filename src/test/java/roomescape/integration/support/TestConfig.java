c1d6a1dd92cbfa9e65b96a17b4768f4b873e6101
woohyeon <woohyeon6885@gmail.com>
2026-05-31 15:20:14 +0900

    test(testConfig): 기준 시간을 KST로 변경

--- src/test/java/roomescape/integration/support/TestConfig.java
+++ src/test/java/roomescape/integration/support/TestConfig.java
@@ -2,7 +2,7 @@ package roomescape.integration.support;
 
 import java.time.Clock;
 import java.time.Instant;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,8 +18,8 @@ public class TestConfig {
     @Bean
     public Clock clock() {
         return Clock.fixed(
-                Instant.parse("2026-05-01T09:00:00Z"),
-                ZoneOffset.UTC
+                Instant.parse("2026-05-01T09:00:00+09:00"),
+                ZoneId.of("Asia/Seoul")
         );
     }
 }
