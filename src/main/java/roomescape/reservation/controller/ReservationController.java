f448befa46370084b0b18b98c090e0fd2b000def
woohyeon <woohyeon6885@gmail.com>
2026-05-31 15:48:29 +0900

    refactor(reservationController): 내 예약 컨트롤러를 예약 컨트롤러로 통합

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -3,14 +3,20 @@ package roomescape.reservation.controller;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.auth.annotation.Authenticated;
+import roomescape.auth.annotation.LoginName;
 import roomescape.reservation.controller.dto.ReservationRequest;
 import roomescape.reservation.controller.dto.ReservationResponse;
+import roomescape.reservation.controller.dto.ReservationUpdateRequest;
 import roomescape.reservation.controller.dto.ReservationWithStatusResponse;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.exception.InvalidReservationRequestFormatException;
@@ -49,5 +55,30 @@ public class ReservationController {
                 .toList();
 
         return ResponseEntity.ok(responses);
+    }
+
+    @Authenticated
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updateMyReservation(
+            @LoginName String name,
+            @PathVariable Long id,
+            @RequestBody ReservationUpdateRequest request
+    ) {
+        reservationService.validateReservationOwnership(id, name);
+        reservationService.updateReservation(request.toCommand(), id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Authenticated
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMyReservation(
+            @LoginName String name,
+            @PathVariable Long id
+    ) {
+        reservationService.validateReservationOwnership(id, name);
+        reservationService.validateReservationNotExpired(id);
+        reservationService.deleteReservationById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
