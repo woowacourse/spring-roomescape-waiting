93cb9288a572f8a95b310c23a19ad92ffa2fa9ed
smiinii <sm020320@naver.com>
2026-06-01 19:58:17 +0900

    refactor: AcceptanceTest에서 @LocalServerPort와 RestAssured 설정 추가 및 테스트 환경 개선

--- src/test/java/roomescape/acceptance/AcceptanceTest.java
+++ src/test/java/roomescape/acceptance/AcceptanceTest.java
@@ -1,23 +1,34 @@
 package roomescape.acceptance;
 
+import io.restassured.RestAssured;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.config.TestClockConfig;
 
 @Import(TestClockConfig.class)
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
 
     public static final String NOW_DATE = "2026-05-02";
     public static final String FUTURE_TIME = "10:00";
 
+    @LocalServerPort
+    private int port;
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssured.port = port;
+    }
 
     @AfterEach
     void afterEach() {
