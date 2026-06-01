24e0db6f64360ace62337bfaf72e4f20f392fecf
sinequanonn <dleck288@gmail.com>
2026-06-01 22:31:22 +0900

    refactor: 사용자 예약 조회 dto 수정

--- src/main/java/roomescape/reservation/controller/dto/ReservationWithWaitingOrderResponse.java
+++ src/main/java/roomescape/reservation/controller/dto/ReservationWithWaitingOrderResponse.java
@@ -1,7 +1,8 @@
 package roomescape.reservation.controller.dto;
 
+import roomescape.reservation.domain.MyReservation;
+import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.Status;
-import roomescape.reservation.repository.dto.ReservationWithWaitingOrder;
 import roomescape.theme.controller.dto.ThemeResponse;
 
 public record ReservationWithWaitingOrderResponse(
@@ -12,14 +13,15 @@ public record ReservationWithWaitingOrderResponse(
         Status status,
         Integer waitingOrder
 ) {
-    public static ReservationWithWaitingOrderResponse from(ReservationWithWaitingOrder reservation) {
+    public static ReservationWithWaitingOrderResponse from(MyReservation myReservation) {
+        Reservation reservation = myReservation.reservation();
         return new ReservationWithWaitingOrderResponse(
-                reservation.id(),
-                reservation.name(),
-                reservation.time(),
-                reservation.theme(),
-                reservation.status(),
-                reservation.waitingOrder()
+                reservation.getId(),
+                reservation.getName(),
+                ReservationTimeResponse.from(reservation.getTime()),
+                ThemeResponse.from(reservation.getTheme()),
+                reservation.getStatus(),
+                myReservation.waitingOrder()
         );
     }
 }
