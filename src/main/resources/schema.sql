0c7868ed14e7e5918652efca156f85fe757c4bb7
koreaioi <koreaioi@gachon.ac.kr>
2026-06-01 20:01:40 +0900

    feat: ReservationSlot 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -33,13 +33,25 @@ CREATE TABLE IF NOT EXISTS member
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS reservation_slot(
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    date_id  BIGINT NOT NULL,
+    time_id  BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (date_id)   REFERENCES reservation_date (id),
+    FOREIGN KEY (time_id)   REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id)  REFERENCES theme (id),
+    CONSTRAINT slot_save_uq UNIQUE (date_id, time_id, theme_id)
+);
+
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    date_id  BIGINT       NOT NULL,
-    time_id  BIGINT       NOT NULL,
-    theme_id BIGINT       NOT NULL,
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    date_id     BIGINT       NOT NULL,
+    time_id     BIGINT       NOT NULL,
+    theme_id    BIGINT       NOT NULL,
     reserved_at TIMESTAMP NOT NULL,
     status   ENUM('RESERVED', 'WAITING', 'CANCELED') NOT NULL,
     PRIMARY KEY (id),
