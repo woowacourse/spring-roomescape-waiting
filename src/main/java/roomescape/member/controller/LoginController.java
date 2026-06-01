32e69e8e00fb35b5c83efb30d1f09c57bcc4090c
gahyeonnni <hyeonnni@naver.com>
2026-06-01 20:34:38 +0900

    refactor: 로그인/회원가입 뷰 라우팅을 WebConfig RouterFunction으로 이동

--- src/main/java/roomescape/member/controller/LoginController.java
+++ src/main/java/roomescape/member/controller/LoginController.java
@@ -3,18 +3,16 @@ package roomescape.member.controller;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.member.domain.Member;
 import roomescape.member.dto.LoginRequest;
 import roomescape.member.dto.MemberResponse;
 import roomescape.member.dto.SignupRequest;
 import roomescape.member.service.MemberService;
 
-@Controller
+@RestController
 public class LoginController {
 
     private static final String SESSION_KEY = "memberId";
@@ -25,25 +23,13 @@ public class LoginController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
-
-    @GetMapping("/signup")
-    public String signupPage() {
-        return "signup";
-    }
-
     @PostMapping("/signup")
-    @ResponseBody
     public ResponseEntity<MemberResponse> signup(@Valid @RequestBody SignupRequest request) {
         MemberResponse response = memberService.signup(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
-    @ResponseBody
     public ResponseEntity<MemberResponse> login(@Valid @RequestBody LoginRequest request, HttpSession session) {
         Member member = memberService.login(request);
         session.setAttribute(SESSION_KEY, member.getId());
@@ -51,7 +37,6 @@ public class LoginController {
     }
 
     @PostMapping("/logout")
-    @ResponseBody
     public ResponseEntity<Void> logout(HttpSession session) {
         session.invalidate();
         return ResponseEntity.ok().build();
