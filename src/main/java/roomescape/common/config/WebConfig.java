fe21f31017bd2bd765ae411c6e727c256ffb7e9d
zzaekkii <zzaekkii@gmail.com>
2026-06-01 21:21:23 +0900

    refactor: addViewControllers로 라우팅 방식 변경 및 예약 페이지 경로 API와 분리

--- src/main/java/roomescape/common/config/WebConfig.java
+++ src/main/java/roomescape/common/config/WebConfig.java
@@ -2,6 +2,7 @@ package roomescape.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/");
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/reservation").setViewName("forward:/reservations.html");
+        registry.addViewController("/my-reservations").setViewName("forward:/my-reservations.html");
+        registry.addViewController("/admin").setViewName("forward:/admin.html");
     }
 }
