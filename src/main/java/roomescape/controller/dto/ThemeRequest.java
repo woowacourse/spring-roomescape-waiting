f1d166d34cb2278356309c07e48ec9d33bf55e58
minzzun99 <skg06123@naver.com>
2026-06-02 11:05:06 +0900

    refactor: ThemeRequest 내부 검증 메시지 수정

--- src/main/java/roomescape/controller/dto/ThemeRequest.java
+++ src/main/java/roomescape/controller/dto/ThemeRequest.java
@@ -3,11 +3,11 @@ package roomescape.controller.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record ThemeRequest(
-        @NotBlank(message = "이름은 필수입니다.")
+        @NotBlank(message = "테마 이름은 필수입니다.")
         String name,
-        @NotBlank(message = "이름은 필수입니다.")
+        @NotBlank(message = "테마 설명은 필수입니다.")
         String description,
-        @NotBlank(message = "이름은 필수입니다.")
+        @NotBlank(message = "테마의 썸네일 주소는 필수입니다.")
         String thumbnailUrl
 ) {
 }
