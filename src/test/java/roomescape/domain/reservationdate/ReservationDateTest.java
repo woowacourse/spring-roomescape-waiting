5b3159cbedc526e33d01cfc00fb3e31cf2081fcb
rin <chaerin0916@gmail.com>
2026-06-03 19:39:59 +0900

    test: ReservationDate 과거, 오늘 테스트 추가

--- src/test/java/roomescape/domain/reservationdate/ReservationDateTest.java
+++ src/test/java/roomescape/domain/reservationdate/ReservationDateTest.java
@@ -4,8 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import roomescape.domain.reservationtime.ReservationTime;
 import roomescape.support.exception.RoomescapeException;
 
 class ReservationDateTest {
@@ -37,5 +39,29 @@ class ReservationDateTest {
         assertThat(pastDate.isAvailable(today)).isFalse();
         assertThat(todayDate.isAvailable(today)).isTrue();
         assertThat(futureDate.isAvailable(today)).isTrue();
+    }
+
+    @Test
+    @DisplayName("과거인지 확인한다.")
+    void isPast() {
+        LocalDate today = LocalDate.now();
+        ReservationDate pastDate = ReservationDate.createWithoutId(today.minusDays(1));
+        ReservationDate futureDate = ReservationDate.createWithoutId(today.plusDays(1));
+        LocalTime time = LocalTime.now().minusSeconds(1);
+        ReservationTime reservationTime = ReservationTime.createWithoutId(time);
+
+        assertThat(pastDate.isPast(reservationTime)).isTrue();
+        assertThat(futureDate.isPast(reservationTime)).isFalse();
+    }
+
+    @Test
+    @DisplayName("오늘인지 확인한다.")
+    void isToday() {
+        LocalDate today = LocalDate.now();
+        ReservationDate todayDate = ReservationDate.createWithoutId(today);
+        ReservationDate notTodayDate = ReservationDate.createWithoutId(today.minusDays(1));
+
+        assertThat(todayDate.isToday()).isTrue();
+        assertThat(notTodayDate.isToday()).isFalse();
     }
 }
