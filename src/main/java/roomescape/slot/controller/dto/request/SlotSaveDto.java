2522ed56328831df046d9133398b89b2f572d51b
koreaioi <koreaioi@gachon.ac.kr>
2026-06-03 22:48:32 +0900

    feat: TODO dto 검증 추가

--- src/main/java/roomescape/slot/controller/dto/request/SlotSaveDto.java
+++ src/main/java/roomescape/slot/controller/dto/request/SlotSaveDto.java
@@ -1,12 +1,19 @@
 package roomescape.slot.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import roomescape.slot.service.dto.SlotSaveCommand;
 
-// TODO validation 추가
 public record SlotSaveDto(
+
+        @NotNull(message = "dateId는 필수 입력값입니다.")
         Long dateId,
+
+        @NotNull(message = "timeId는 필수 입력값입니다.")
         Long timeId,
+
+        @NotNull(message = "themeId는 필수 입력값입니다.")
         Long themeId
+
 ) {
 
     public SlotSaveCommand toCommand() {
