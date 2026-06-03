09dbf1b389cfcba96c48c007a1be0eb7e0be5b75
조상준 <juncho12011201@gmail.com>
2026-06-03 15:49:20 +0900

    fix: 불필요한 팩토리 메서드 제거

--- src/main/java/roomescape/domain/ReservationStatus.java
+++ src/main/java/roomescape/domain/ReservationStatus.java
@@ -1,16 +1,6 @@
 package roomescape.domain;
 
-import roomescape.exception.InvalidDomainException;
-
 public enum ReservationStatus {
     RESERVED,
     WAITING;
-
-    public static ReservationStatus from(String status) {
-        return switch (status) {
-            case "RESERVED" -> RESERVED;
-            case "WAITING" -> WAITING;
-            default -> throw new InvalidDomainException("올바르지 않은 예약 상태 이름입니다. 변환 희망 상태 값: " + status);
-        };
-    }
 }
