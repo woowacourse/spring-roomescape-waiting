ba5b68ea8f5eea5de544857340977e570ae65735
jyt6640 <jyt6640@gmail.com>
2026-06-04 11:29:23 +0900

    test(reservation): 자동 승격 인수 테스트 추가
    예약 취소 후 1순위 대기가 예약으로 자동 전환되고 남은 대기 순번이 재정렬되는 사용자 시나리오를 검증한다.
    
    - 사용자 예약 취소 API를 통해 자동 전환을 확인한다.
    - 승격된 대기는 예약 목록에 나타나고 남은 대기는 rank 1로 조회되는지 확인한다.

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -268,6 +268,57 @@ public class MissionStepTest {
                 .body("waitings[0].rank", is(2));
     }
 
+    @Test
+    void 예약이_취소되면_1순위_대기가_예약으로_자동_전환되고_남은_대기_순번이_재정렬된다() {
+        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", LocalTime.now().plusHours(1).toString());
+        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail_url) VALUES (?, ?, ?)", "김인직", "레전드 방송", "gamst.jpg");
+        jdbcTemplate.update(
+                "INSERT INTO reservation (name, date, time_id, theme_id, created_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)",
+                "리오",
+                TODAY.plusDays(1).toString(),
+                1,
+                1
+        );
+        jdbcTemplate.update(
+                "INSERT INTO waiting (name, date, time_id, theme_id, created_at) VALUES (?, ?, ?, ?, DATEADD('SECOND', -1, CURRENT_TIMESTAMP))",
+                "브라운",
+                TODAY.plusDays(1).toString(),
+                1,
+                1
+        );
+        jdbcTemplate.update(
+                "INSERT INTO waiting (name, date, time_id, theme_id, created_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)",
+                "흑곰",
+                TODAY.plusDays(1).toString(),
+                1,
+                1
+        );
+
+        RestAssured.given().log().all()
+                .queryParam("name", "리오")
+                .when().delete("/reservations/me/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given().log().all()
+                .queryParam("name", "브라운")
+                .when().get("/reservations/me")
+                .then().log().all()
+                .statusCode(200)
+                .body("reservations.size()", is(1))
+                .body("reservations[0].name", is("브라운"))
+                .body("waitings.size()", is(0));
+
+        RestAssured.given().log().all()
+                .queryParam("name", "흑곰")
+                .when().get("/reservations/me")
+                .then().log().all()
+                .statusCode(200)
+                .body("waitings.size()", is(1))
+                .body("waitings[0].name", is("흑곰"))
+                .body("waitings[0].rank", is(1));
+    }
+
     @Autowired
     private ReservationController reservationController;
 
