6f8c88d231dfe03f7639a476970565ecd5e8243f
dahye <dahye9011@naver.com>
2026-06-04 16:03:44 +0900

    refactor: 사용자 API 응답 수정

--- src/main/java/roomescape/reservation/controller/dto/ReservationResponse.java
+++ src/main/java/roomescape/reservation/controller/dto/ReservationResponse.java
@@ -1,13 +1,15 @@
 package roomescape.reservation.controller.dto;
 
 import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.domain.Status;
 import roomescape.theme.controller.dto.ThemeResponse;
 
 public record ReservationResponse(
         Long id,
         String name,
         ReservationTimeResponse time,
-        ThemeResponse theme
+        ThemeResponse theme,
+        Status status
 ) {
 
     public static ReservationResponse from(Reservation reservation) {
@@ -15,7 +17,8 @@ public record ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 ReservationTimeResponse.from(reservation.getTime()),
-                ThemeResponse.from(reservation.getTheme())
+                ThemeResponse.from(reservation.getTheme()),
+                reservation.getStatus()
         );
     }
 }
