c2be0ae22ce464276b2005da99a4c210a28dccc8
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-06-04 16:36:35 +0900

    feat: 슬롯 1순위 대기 조회/삭제 리포지토리 추가

--- src/main/java/roomescape/waiting/domain/WaitingRepository.java
+++ src/main/java/roomescape/waiting/domain/WaitingRepository.java
@@ -8,6 +8,8 @@ public interface WaitingRepository {
     Waiting save(Waiting waiting);
     Optional<Waiting> findById(Long id);
     Optional<Waiting> findByNameAndDateAndTimeIdAndThemeId(String name, LocalDate date, Long timeId, Long themeId);
+    Optional<Waiting> findFirstBySlot(LocalDate date, Long timeId, Long themeId);
     void deleteByIdAndName(Long id, String name);
+    void deleteById(Long id);
     List<Waiting> findByName(String name);
 }
