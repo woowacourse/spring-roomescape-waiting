a146c378a6a9ee4391dd22a1a5cf5d6c407117e9
smiinii <sm020320@naver.com>
2026-06-05 14:40:25 +0900

    refactor: 예외 처리 핸들러에 새로운 예외 타입 추가 및 메시지 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,21 +12,21 @@ INSERT INTO reservation_time (start_at) VALUES
 ('20:00');
 
 INSERT INTO theme (name, description, thumbnail_url) VALUES
-('잃어버린 왕국', '사라진 고대 왕국의 비밀을 추적하는 모험 테마', 'https://example.com/images/lost-kingdom.jpg'),
-('심야의 연구소', '한밤중 폐쇄된 연구소에서 탈출 단서를 찾는 스릴러 테마', 'https://example.com/images/midnight-lab.jpg'),
-('해적선의 저주', '저주받은 해적선에서 보물을 찾아 탈출하는 테마', 'https://example.com/images/pirate-curse.jpg'),
-('시간 여행자', '흩어진 시간의 조각을 맞춰 현재로 돌아오는 SF 테마', 'https://example.com/images/time-traveler.jpg'),
-('마법사의 서재', '마법 주문과 숨겨진 장치를 풀어내는 판타지 테마', 'https://example.com/images/wizard-library.jpg'),
-('사라진 탐정', '실종된 탐정이 남긴 단서를 따라 사건을 해결하는 추리 테마', 'https://example.com/images/missing-detective.jpg'),
-('지하 벙커', '폐쇄된 지하 벙커의 보안 시스템을 해제하는 생존 테마', 'https://example.com/images/underground-bunker.jpg'),
-('고대 신전', '신전 깊은 곳의 퍼즐을 풀고 봉인을 해제하는 모험 테마', 'https://example.com/images/ancient-temple.jpg'),
-('유령 호텔', '기묘한 호텔 방마다 숨은 이야기를 밝혀내는 공포 테마', 'https://example.com/images/ghost-hotel.jpg'),
-('우주 정거장', '고장 난 우주 정거장을 복구하고 귀환하는 SF 테마', 'https://example.com/images/space-station.jpg'),
-('비밀 카지노', '비밀 카지노의 금고 암호를 찾아내는 잠입 테마', 'https://example.com/images/secret-casino.jpg'),
-('눈보라 산장', '눈보라에 갇힌 산장에서 범인을 찾아내는 추리 테마', 'https://example.com/images/snow-cabin.jpg'),
-('인형의 집', '낡은 인형의 집에 숨겨진 진실을 발견하는 미스터리 테마', 'https://example.com/images/doll-house.jpg'),
-('기억의 방', '잃어버린 기억을 되찾기 위해 단서를 연결하는 감성 테마', 'https://example.com/images/memory-room.jpg'),
-('네온 시티', '미래 도시의 보안망을 돌파하는 사이버펑크 테마', 'https://example.com/images/neon-city.jpg');
+('잃어버린 왕국', '사라진 고대 왕국의 비밀을 추적하는 모험 테마', 'https://images.unsplash.com/photo-1533154683836-84ea7a0bc310?w=400'),
+('심야의 연구소', '한밤중 폐쇄된 연구소에서 탈출 단서를 찾는 스릴러 테마', 'https://images.unsplash.com/photo-1582719471384-894fbb16e074?w=400'),
+('해적선의 저주', '저주받은 해적선에서 보물을 찾아 탈출하는 테마', 'https://images.unsplash.com/photo-1534447677768-be436bb09401?w=400'),
+('시간 여행자', '흩어진 시간의 조각을 맞춰 현재로 돌아오는 SF 테마', 'https://images.unsplash.com/photo-1501139083538-0139583c060f?w=400'),
+('마법사의 서재', '마법 주문과 숨겨진 장치를 풀어내는 판타지 테마', 'https://images.unsplash.com/photo-1481627834876-b7833e8f5570?w=400'),
+('사라진 탐정', '실종된 탐정이 남긴 단서를 따라 사건을 해결하는 추리 테마', 'https://images.unsplash.com/photo-1509515837298-2c67a3933321?w=400'),
+('지하 벙커', '폐쇄된 지하 벙커의 보안 시스템을 해제하는 생존 테마', 'https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=400'),
+('고대 신전', '신전 깊은 곳의 퍼즐을 풀고 봉인을 해제하는 모험 테마', 'https://images.unsplash.com/photo-1564760055775-d63b17a55c44?w=400'),
+('유령 호텔', '기묘한 호텔 방마다 숨은 이야기를 밝혀내는 공포 테마', 'https://images.unsplash.com/photo-1455587734955-081b22074882?w=400'),
+('우주 정거장', '고장 난 우주 정거장을 복구하고 귀환하는 SF 테마', 'https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?w=400'),
+('비밀 카지노', '비밀 카지노의 금고 암호를 찾아내는 잠입 테마', 'https://images.unsplash.com/photo-1596838132731-3301c3fd4317?w=400'),
+('눈보라 산장', '눈보라에 갇힌 산장에서 범인을 찾아내는 추리 테마', 'https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?w=400'),
+('인형의 집', '낡은 인형의 집에 숨겨진 진실을 발견하는 미스터리 테마', 'https://images.unsplash.com/photo-1553969914-1397d0f0b865?w=400'),
+('기억의 방', '잃어버린 기억을 되찾기 위해 단서를 연결하는 감성 테마', 'https://images.unsplash.com/photo-1531306728370-e2ebd9d7bb99?w=400'),
+('네온 시티', '미래 도시의 보안망을 돌파하는 사이버펑크 테마', 'https://images.unsplash.com/photo-1518770660439-4636190af475?w=400');
 
 INSERT INTO reservation (name, date, time_id, theme_id) VALUES
 ('예약자01', '2026-05-01', 1, 1),
