e835295260bed2b078bdd2082f123ce699f18396
ghkdtjfxkd <syt1128@gmail.com>
2026-06-07 23:09:00 +0900

    fix: WaitingSaved 이벤트를 AFTER_COMMIT으로 변경하여 고아 대기 레이스 컨디션 수정

--- src/main/java/roomescape/reservation/event/WaitingEventListener.java
+++ src/main/java/roomescape/reservation/event/WaitingEventListener.java
@@ -3,6 +3,8 @@ package roomescape.reservation.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import roomescape.reservation.application.service.PromotionService;
 import roomescape.reservation.application.service.WaitingService;
 import roomescape.reservation.event.schema.WaitingPromotedToReservation;
@@ -15,7 +17,7 @@ public class WaitingEventListener {
     private final PromotionService promotionService;
     private final WaitingService waitingService;
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleWaitingSaved(WaitingSaved event) {
         promotionService.promoteFromWaiting(event.date(), event.themeId(), event.timeId());
     }
