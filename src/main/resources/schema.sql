28f5a872e7ad9d31600543ce09ae38a8e0cf8243
sun <sun007021@gmail.com>
2026-06-08 14:36:28 +0900

    feat: 가상컬럼 이용 unique제약으로 예약 생성시 동시성 문제 방어

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -66,16 +66,18 @@ CREATE TABLE reservation_slot
 
 CREATE TABLE reservation
 (
-    id         BIGINT      NOT NULL AUTO_INCREMENT,
-    user_id    BIGINT      NOT NULL,
-    slot_id    BIGINT      NOT NULL,
-    status     VARCHAR(30) NOT NULL,
-    created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id               BIGINT      NOT NULL AUTO_INCREMENT,
+    user_id          BIGINT      NOT NULL,
+    slot_id          BIGINT      NOT NULL,
+    status           VARCHAR(30) NOT NULL,
+    reserved_slot_id BIGINT GENERATED ALWAYS AS (CASE WHEN status = 'RESERVED' THEN slot_id END),
+    created_at       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (slot_id) REFERENCES reservation_slot (id),
-    CONSTRAINT unique_reservation UNIQUE (slot_id, user_id)
+    CONSTRAINT unique_reservation UNIQUE (slot_id, user_id),
+    CONSTRAINT unique_reserved_per_slot UNIQUE (reserved_slot_id)
 );
 
 CREATE INDEX idx_reservation_user_id ON reservation (user_id);
