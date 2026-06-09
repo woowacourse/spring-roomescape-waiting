4949cabf668339f17e434dedbdc63ca88649804f
Jihyun3478 <leejihyun3478@gmail.com>
2026-06-09 09:12:42 +0900

    refactor: 중복되는 테스트 더미데이터 및 DB 데이터 초기화 ControllerTestSupport로 분리

--- src/test/java/roomescape/controller/AdminReservationControllerTest.java
+++ src/test/java/roomescape/controller/AdminReservationControllerTest.java
@@ -1,28 +1,12 @@
 package roomescape.controller;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
-import java.time.LocalDate;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import roomescape.DatabaseInitializer;
-
 import static org.hamcrest.Matchers.hasItem;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class AdminReservationControllerTest {
+import io.restassured.RestAssured;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
 
-    @Autowired
-    private DatabaseInitializer databaseInitializer;
-
-    @BeforeEach
-    void setUp() {
-        databaseInitializer.clear();
-    }
+public class AdminReservationControllerTest extends ControllerTestSupport {
 
     @Test
     void 전체_예약을_조회한다() {
@@ -37,31 +21,5 @@ public class AdminReservationControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("name", hasItem("브라운"));
-    }
-
-    private int createTime(String startAt) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("startAt", startAt))
-                .when().post("/admin/times")
-                .then().statusCode(201)
-                .extract().jsonPath().getInt("id");
-    }
-
-    private int createTheme(String name, String description, String thumbnail) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("name", name, "description", description, "thumbnail", thumbnail))
-                .when().post("/admin/themes")
-                .then().statusCode(201)
-                .extract().jsonPath().getInt("id");
-    }
-
-    private ValidatableResponse createReservation(String name, String date, int timeId, int themeId) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("name", name, "date", date, "timeId", timeId, "themeId", themeId))
-                .when().post("/reservations")
-                .then().log().all();
     }
 }
--- src/test/java/roomescape/controller/ReservationWaitingControllerTest.java
+++ src/test/java/roomescape/controller/ReservationWaitingControllerTest.java
@@ -3,26 +3,10 @@ package roomescape.controller;
 import static org.hamcrest.Matchers.notNullValue;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
 import java.time.LocalDate;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import roomescape.DatabaseInitializer;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class ReservationWaitingControllerTest {
-
-    @Autowired
-    private DatabaseInitializer databaseInitializer;
-
-    @BeforeEach
-    void setUp() {
-        databaseInitializer.clear();
-    }
+class ReservationWaitingControllerTest extends ControllerTestSupport {
 
     @Test
     void 예약_대기를_추가한다() {
@@ -77,39 +61,5 @@ class ReservationWaitingControllerTest {
                 .when().delete("/waitings/999")
                 .then().log().all()
                 .statusCode(404);
-    }
-
-    private int createTime(String startAt) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("startAt", startAt))
-                .when().post("/admin/times")
-                .then().statusCode(201)
-                .extract().jsonPath().getInt("id");
-    }
-
-    private int createTheme(String name, String description, String thumbnail) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("name", name, "description", description, "thumbnail", thumbnail))
-                .when().post("/admin/themes")
-                .then().statusCode(201)
-                .extract().jsonPath().getInt("id");
-    }
-
-    private ValidatableResponse createReservationWaiting(String name, LocalDate reservationDate, int timeId, int themeId) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("name", name, "reservationDate", reservationDate.toString(), "timeId", timeId, "themeId", themeId))
-                .when().post("/waitings")
-                .then().log().all();
-    }
-
-    private ValidatableResponse createReservation(String name, String date, int timeId, int themeId) {
-        return RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(Map.of("name", name, "date", date, "timeId", timeId, "themeId", themeId))
-                .when().post("/reservations")
-                .then().log().all();
     }
 }
