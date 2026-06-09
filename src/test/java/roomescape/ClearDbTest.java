92ed93d07cc5b415058aa9867e0086700f107af4
Yeji-Kim-Erica <yejikim.erica@gmail.com>
2026-06-09 18:28:27 +0900

    test: 테스트 서버가 RANDOM_PORT를 사용하도록 수정

--- src/test/java/roomescape/ClearDbTest.java
+++ src/test/java/roomescape/ClearDbTest.java
@@ -1,6 +1,7 @@
 package roomescape;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.lang.annotation.ElementType;
@@ -10,7 +11,11 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestExecutionListeners(
+        value = SetupListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
 @Sql(value = "/clear.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public @interface ClearDbTest {
 }
