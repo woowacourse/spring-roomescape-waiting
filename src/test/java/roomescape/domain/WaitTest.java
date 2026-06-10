9f9ca4564a1e2fc409fd46d9a2f0a2d36d5b05ab
wontop02 <wjy2002kr@gmail.com>
2026-06-11 02:15:36 +0900

    test: 도메인 테스트 추가, 수정

--- src/test/java/roomescape/domain/WaitTest.java
+++ src/test/java/roomescape/domain/WaitTest.java
@@ -3,26 +3,36 @@ package roomescape.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.exception.custom.InvalidDomainValueException;
 
-public class ReservationTest {
+public class WaitTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
     void nameBlankExceptionTest(String name) {
         ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
         Theme theme = new Theme("피즈의 모험", "모험 이야기", "url.jpg");
-        assertThatThrownBy(() -> new Reservation(1L, name, new Slot(LocalDate.of(2026, 5, 2), reservationTime, theme)))
+        assertThatThrownBy(() -> new Wait(1L, LocalDateTime.of(2026, 5, 2, 10, 0), name,
+                new Slot(LocalDate.of(2026, 5, 2), reservationTime, theme)))
+                .isInstanceOf(InvalidDomainValueException.class);
+    }
+
+    @Test
+    void createdAtNullExceptionTest() {
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
+        Theme theme = new Theme("피즈의 모험", "모험 이야기", "url.jpg");
+        assertThatThrownBy(() -> new Wait(1L, null, "fizz", new Slot(LocalDate.of(2026, 5, 2), reservationTime, theme)))
                 .isInstanceOf(InvalidDomainValueException.class);
     }
 
     @Test
     void slotNullExceptionTest() {
-        assertThatThrownBy(() -> new Reservation(1L, "fizz", null))
+        assertThatThrownBy(() -> new Wait(1L, LocalDateTime.of(2026, 5, 2, 10, 0), "fizz", null))
                 .isInstanceOf(InvalidDomainValueException.class);
     }
 }
