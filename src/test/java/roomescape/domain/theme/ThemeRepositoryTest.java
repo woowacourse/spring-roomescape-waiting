b837478594b18d9f5afa51324a827609a5d12c39
Jeshin Kim <jjason0904@cau.ac.kr>
2026-06-16 18:18:31 +0900

    fix: Spring Boot 4 -> 3 변경으로 인한 JdbcTest import 수정

--- src/test/java/roomescape/domain/theme/ThemeRepositoryTest.java
+++ src/test/java/roomescape/domain/theme/ThemeRepositoryTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.test.autoconfigure.JdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
