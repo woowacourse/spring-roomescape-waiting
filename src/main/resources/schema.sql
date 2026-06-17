f0dbe3e3ee25e9f383f024ada0b9e6a6f4782bb0
조상준 <juncho12011201@gmail.com>
2026-06-17 19:17:12 +0900

    feat: 승인 요청 이전, 결제 금액 일치 여부 판단

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -63,6 +63,7 @@ CREATE TABLE payment_order
     reservation_id BIGINT      NOT NULL,
     order_id       VARCHAR(64) NOT NULL,
     amount         BIGINT      NOT NULL,
+    payment_key    VARCHAR(200),
     created_at     DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     UNIQUE (order_id),
