630d7f67fe322df92d825ac50d2fb008eb437d2e
kangrae <kangrae0501@gmail.com>
2026-06-18 09:59:38 +0900

    [단계 1] findAll이 정렬된 결과를 반환하도록 수정

--- src/main/java/roomescape/repository/ReservationTimeRepository.java
+++ src/main/java/roomescape/repository/ReservationTimeRepository.java
@@ -1,7 +1,10 @@
 package roomescape.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.domain.ReservationTime;
 
 public interface ReservationTimeRepository extends JpaRepository<ReservationTime, Long> {
+
+    List<ReservationTime> findAllByOrderByStartAtAsc();
 }
