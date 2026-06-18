f9159b07e2c5f77eebb1784b90520e7c22062211
koreaioi <koreaioi@gachon.ac.kr>
2026-06-19 00:38:44 +0900

    feat: TokenBucketRateLimiter 추가

---
+++ src/main/java/roomescape/payment/client/bucket/TokenBucketRateLimiter.java
@@ -0,0 +1,48 @@
+package roomescape.payment.client.bucket;
+
+import java.util.function.LongSupplier;
+
+public class TokenBucketRateLimiter {
+
+    private final long capacity;
+    private final double refillPerSec;
+    private final LongSupplier nanoClock;
+
+    private double availableTokens;
+    private long lastRefillNanos;
+
+    public TokenBucketRateLimiter(long capacity, double refillPerSec, LongSupplier nanoClock) {
+        this.capacity = capacity;
+        this.refillPerSec = refillPerSec;
+        this.nanoClock = nanoClock;
+        this.availableTokens = capacity;
+        this.lastRefillNanos = nanoClock.getAsLong();
+    }
+
+    public synchronized boolean tryConsume() {
+        refill();
+        if (availableTokens >= 1) {
+            availableTokens -= 1;
+            return true;
+        }
+
+        return false;
+    }
+
+    public synchronized long retryAfterSeconds() {
+        refill();
+        if (availableTokens >= 1) {
+            return 0;
+        }
+
+        return (long) Math.ceil((1 - availableTokens) / refillPerSec);
+    }
+
+    private void refill() {
+        var now = nanoClock.getAsLong();
+        var elapsedSec = (now - lastRefillNanos) / 1_000_000_000.0;
+        availableTokens = Math.min(capacity, availableTokens + elapsedSec * refillPerSec);
+        lastRefillNanos = now;
+    }
+
+}
