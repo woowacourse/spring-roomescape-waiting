207506e30aef7068281595bea0ce423660201dbb
koreaioi <koreaioi@gachon.ac.kr>
2026-06-19 00:40:49 +0900

    feat: 결제 승인 엔드포인트에 @RateLimit 적용

--- src/main/java/roomescape/payment/controller/PaymentController.java
+++ src/main/java/roomescape/payment/controller/PaymentController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import roomescape.payment.client.inbound.annotation.RateLimit;
 import roomescape.payment.exception.PaymentAmountMismatchException;
 import roomescape.payment.exception.TossPaymentException;
 import roomescape.payment.service.PaymentService;
@@ -17,6 +18,7 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @RateLimit
     @GetMapping("/success")
     public String success(
             @RequestParam String paymentKey,
