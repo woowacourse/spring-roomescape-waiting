3a705656247555aa04f3a08b8093acb58008c6db
choiyoung69 <choiyoung609@gmail.com>
2026-06-22 11:56:15 +0900

    feat: 토스 RestClient에 양방향 제한과 재시도 연결

--- src/main/java/roomescape/payment/config/PaymentConfig.java
+++ src/main/java/roomescape/payment/config/PaymentConfig.java
@@ -1,24 +1,41 @@
 package roomescape.payment.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import roomescape.payment.infra.toss.OutboundRateLimitInterceptor;
+import roomescape.payment.infra.toss.RetryAfterInterceptor;
+import roomescape.ratelimit.TokenBucketRateLimiter;
 
-@EnableConfigurationProperties(PaymentProperties.class)
+@EnableConfigurationProperties({PaymentProperties.class, TossRetryProperties.class})
 @Configuration
 public class PaymentConfig {
 
     @Bean
-    RestClient tossRestClient(RestClient.Builder builder, PaymentProperties properties) {
+    RestClient tossRestClient(
+            RestClient.Builder builder,
+            PaymentProperties properties,
+            TossRetryProperties retryProperties,
+            @Qualifier("outboundRateLimiter") TokenBucketRateLimiter outboundRateLimiter
+    ) {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(properties.toss().connectTimeout());
         requestFactory.setReadTimeout(properties.toss().readTimeout());
+        OutboundRateLimitInterceptor outboundInterceptor =
+                new OutboundRateLimitInterceptor(outboundRateLimiter);
 
         return builder
                 .baseUrl(properties.toss().baseUrl())
                 .requestFactory(requestFactory)
+                .requestInterceptor(outboundInterceptor)
+                .requestInterceptor(new RetryAfterInterceptor(
+                        retryProperties.maxAttempts(),
+                        retryProperties.fallbackDelay(),
+                        outboundInterceptor::acquire
+                ))
                 .build();
     }
 }
