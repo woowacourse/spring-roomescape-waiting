498739ce57c1e6328df8c9c6adb5d4c8f6123e2e
wonsang.choi <wonsang.choi@e4net.net>
2026-06-22 11:47:57 +0900

    merge

---
+++ src/main/java/roomescape/payment/web/dto/PaymentOrdersResponse.java
@@ -0,0 +1,16 @@
+package roomescape.payment.web.dto;
+
+import roomescape.payment.domain.PaymentOrderDetails;
+
+import java.util.List;
+
+public record PaymentOrdersResponse(List<PaymentOrderResponse> orders) {
+
+    public static PaymentOrdersResponse from(List<PaymentOrderDetails> paymentOrders) {
+        return new PaymentOrdersResponse(
+                paymentOrders.stream()
+                        .map(PaymentOrderResponse::from)
+                        .toList()
+        );
+    }
+}
