05e8a36c34d54dda9cc4e084f3666473a06cb6e8
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-06-25 08:06:44 +0900

    feat: 토스 호출 RestClient에 타임아웃 설정
    - 타임 아웃 설정
    - application.properties를 yml로 변경

--- src/main/java/roomescape/infrastructure/toss/TossPaymentConfig.java
+++ src/main/java/roomescape/infrastructure/toss/TossPaymentConfig.java
@@ -1,16 +1,36 @@
 package roomescape.infrastructure.toss;
 
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class TossPaymentConfig {
 
     @Bean
-    public RestClient tossRestClient(RestClient.Builder restClientBuilder) {
+    public ClientHttpRequestFactory tossClientHttpRequestFactory(
+            @Value("${payment.toss.connect-timeout}") Duration connectTimeout,
+            @Value("${payment.toss.read-timeout}") Duration readTimeout
+    ) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(connectTimeout);
+        requestFactory.setReadTimeout(readTimeout);
+        return requestFactory;
+    }
+
+    @Bean
+    public RestClient tossRestClient(
+            RestClient.Builder restClientBuilder,
+            ClientHttpRequestFactory tossClientHttpRequestFactory
+    ) {
         return restClientBuilder
                 .baseUrl("https://api.tosspayments.com")
+                .requestFactory(tossClientHttpRequestFactory)
                 .build();
     }
 }
