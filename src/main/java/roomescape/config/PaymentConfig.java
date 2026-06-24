fb30b4f3e3f24ed57c86e1d1770f6137153b59fe
mvg01 <dkms625@naver.com>
2026-06-24 19:45:11 +0900

    chore: 결제 타임아웃 설정을 application.properties로 외부화

--- src/main/java/roomescape/config/PaymentConfig.java
+++ src/main/java/roomescape/config/PaymentConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.payment.adapter.TossPaymentGateway;
 import roomescape.payment.port.PaymentGateway;
@@ -21,10 +22,15 @@ public class PaymentConfig {
         String encoded = Base64.getEncoder()
                 .encodeToString((properties.secretKey() + ":").getBytes(StandardCharsets.UTF_8));
 
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(properties.connectTimeoutMs());  // SYN -> ACK 대기 최대 시간
+        factory.setReadTimeout(properties.readTimeoutMs());  // 연결 후 응답 도착 최대 시간
+
         return RestClient.builder()
                 .baseUrl(properties.baseUrl())
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + encoded)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .requestFactory(factory)
                 .build();
     }
 
