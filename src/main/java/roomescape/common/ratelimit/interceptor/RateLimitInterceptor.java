f96117c35418f9da28bc618fbcace4607082ec5f
Jihyun3478 <leejihyun3478@gmail.com>
2026-06-26 02:37:53 +0900

    [🚀 방탈출 예약 외부 API 연동 3단계] 로지(이지현) 미션 제출합니다. (#687)
    * feat: 토큰 버킷 구현
    
    * fix: maxAttempts를 넘어도 429면 도메인 예외 던지도록 수정

---
+++ src/main/java/roomescape/common/ratelimit/interceptor/RateLimitInterceptor.java
@@ -0,0 +1,28 @@
+package roomescape.common.ratelimit.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.common.ratelimit.TokenBucketRateLimiter;
+
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    private final TokenBucketRateLimiter rateLimiter;
+
+    public RateLimitInterceptor(TokenBucketRateLimiter rateLimiter) {
+        this.rateLimiter = rateLimiter;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        if (rateLimiter.tryConsume()) {
+            return true;
+        }
+        response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+        response.setHeader(HttpHeaders.RETRY_AFTER, String.valueOf(rateLimiter.retryAfterSeconds()));
+        return false;
+    }
+}
